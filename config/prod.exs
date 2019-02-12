1898a8c635109a9fda73ca1b278c8cdcd89e964f
James Bowers <james.bowers@bbc.co.uk>
2019-02-12 09:24:34 +0000

    capitalize INSTANCE_ROLE_NAME env variable

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :ingress, instance_role_name:        System.get_env("instance_role_name")
+config :ingress, instance_role_name:        System.get_env("INSTANCE_ROLE_NAME")
 config :ingress, lambda_presentation_role:  System.get_env("LAMBDA_PRESENTATION_ROLE")
 config :ingress, lambda_business_role:      System.get_env("LAMBDA_BUSINESS_ROLE")
 config :ingress, lambda_presentation_layer: System.get_env("LAMBDA_PRESENTATION_LAYER")
