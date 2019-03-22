b42ab6f4d8953bae541f7587f043de038d77c01e
James Bowers <james.bowers@bbc.co.uk>
2019-03-22 15:13:37 +0000

    service-worker.js route

--- lib/ingress_web/router.ex
+++ lib/ingress_web/router.ex
@@ -7,7 +7,8 @@ defmodule IngressWeb.Router do
 
   alias IngressWeb.{View, WebCoreRoutes, LegacyRoutes}
 
-  @product_allowlist ~w{_web_core news sport weather cbeebies bitesize dynasties web graphql}
+  # TODO: convince me we need an allowlist here
+  @product_allowlist ~w{_web_core news sport weather cbeebies bitesize dynasties web graphql service-worker.js}
   @allowed_http_methods [:head, :get, :post]
 
   get "/status" do
@@ -21,38 +22,6 @@ defmodule IngressWeb.Router do
   )
 
   match(_, via: @allowed_http_methods, to: LegacyRoutes)
-
-  # get "/service-worker.js" do
-  #   instance_role_name = Application.fetch_env!(:ingress, :instance_role_name)
-  #   lambda_role_arn = Application.fetch_env!(:ingress, :lambda_service_worker_role)
-  #   lambda = Application.fetch_env!(:ingress, :lambda_service_worker)
-
-  #   function_payload = %{}
-
-  #   {200, resp} = Ingress.handle(instance_role_name, lambda_role_arn, lambda, function_payload)
-
-  #   conn
-  #   |> put_resp_content_type("application/javascript")
-  #   |> send_resp(200, resp["body"])
-  # end
-
-  # post "/graphql" do
-  #   {:ok, body, conn} = Plug.Conn.read_body(conn)
-
-  #   function_payload = %{
-  #     body: body,
-  #     httpMethod: "POST"
-  #   }
-
-  #   instance_role_name = Application.fetch_env!(:ingress, :instance_role_name)
-  #   lambda_role_arn = Application.fetch_env!(:ingress, :lambda_business_role)
-  #   lambda = Application.fetch_env!(:ingress, :lambda_business_layer)
-  #   {200, resp} = Ingress.handle(instance_role_name, lambda_role_arn, lambda, function_payload)
-
-  #   conn
-  #   |> put_resp_content_type("application/json")
-  #   |> send_resp(200, resp["body"])
-  # end
 
   def child_spec(_arg) do
     scheme = Application.fetch_env!(:ingress, :http_scheme)
