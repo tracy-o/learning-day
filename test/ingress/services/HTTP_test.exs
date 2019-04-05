c749e03439b40d2baf2d8f25393d62995bddf255
BradleyTesterSpratt <bradley.tester-spratt@bbc.co.uk>
2019-04-05 14:46:27 +0100

    create barebone files for HTTP service and tests

---
+++ test/ingress/services/HTTP_test.exs
@@ -0,0 +1,12 @@
+defmodule Ingress.Services.HTTPTest do
+  alias Ingress.Services.HTTPTest
+  
+  use Test.Support.Helper, :ingress_mox
+  use ExUnit.Case
+
+  describe "HTTP service" do
+    test "" do
+      #stuff
+    end
+  end
+end
