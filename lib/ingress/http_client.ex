356fe475fdc5ef1dfb8e483f70e27eb9a6b9b41e
BradleyTesterSpratt <bradley.tester-spratt@bbc.co.uk>
2019-04-08 14:32:26 +0100

    remove terrible and misleading alignment

--- lib/ingress/http_client.ex
+++ lib/ingress/http_client.ex
@@ -7,13 +7,11 @@ defmodule Ingress.HTTPClient do
   @callback post(String.t(), String.t(), Map.t(), List.t()) :: HTTPoison.Response
   @callback post(String.t(), String.t(), Map.t(), List.t(), Keyword.t()) :: HTTPoison.Response
 
-  def get(host, path, headers \\ [], options \\ [])
-      when is_binary(host) and is_binary(path) and is_list(headers) do
+  def get(host, path, headers \\ [], options \\ []) do
     HTTPoison.get(host <> path, headers, options)
   end
 
-  def post(host, path, body, headers \\ [], options \\ [])
-      when is_binary(host) and is_binary(path) and is_binary(body) and is_list(headers) do
+  def post(host, path, body, headers \\ [], options \\ []) when is_binary(body) do
     HTTPoison.post(host <> path, body, headers, options)
   end
 end
