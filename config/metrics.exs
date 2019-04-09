72dd4eb2c1b0c936c89c2a4b4eb9697c21c4e582
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2019-04-09 10:48:10 +0100

    Correct the metric definitions and add metrics for the HTTP service

--- config/metrics.exs
+++ config/metrics.exs
@@ -1,13 +1,20 @@
 use Mix.Config
 
-lambda_response_code_metrics = Enum.map(200..599, fn x -> "service.lambda.response.#{x}}" end)
+lambda_response_code_metrics =
+  Enum.map(200..599, fn status -> "service.lambda.response.#{status}" end)
+
+http_response_code_metrics =
+  Enum.map(200..599, fn status -> "service.HTTP.response.#{status}" end)
 
 config :ex_metrics,
-  metrics: [
-    "function.timing.service.lambda.invoke",
-    "error.loop.threshold.exceeded",
-    "error.loop.state",
-    "error.pipeline.process",
-    "error.pipeline.process.redirect",
-    "error.pipeline.process.unhandled"
-  ] ++ lambda_response_code_metrics
+  metrics:
+    [
+      "function.timing.service.lambda.invoke",
+      "function.timing.service.HTTP.request",
+      "error.service.HTTP.request",
+      "error.loop.threshold.exceeded",
+      "error.loop.state",
+      "error.pipeline.process",
+      "error.pipeline.process.redirect",
+      "error.pipeline.process.unhandled"
+    ] ++ lambda_response_code_metrics ++ http_response_code_metrics
--- lib/ingress/services/HTTP.ex
+++ lib/ingress/services/HTTP.ex
@@ -1,7 +1,8 @@
 defmodule Ingress.Services.HTTP do
+  use ExMetrics
+
   alias Ingress.Behaviours.Service
-  alias Ingress.HTTPClient
-  alias Ingress.Struct
+  alias Ingress.{HTTPClient, Struct}
 
   @http_client Application.get_env(:ingress, :http_client, HTTPClient)
 
@@ -9,28 +10,31 @@ defmodule Ingress.Services.HTTP do
 
   @impl Service
   def dispatch(struct = %Struct{}) do
-    execute_request(struct)
-    |> handle_response()
+    ExMetrics.timeframe "function.timing.service.HTTP.request" do
+      execute_request(struct)
+      |> handle_response()
+    end
   end
 
   defp handle_response({{:ok, %HTTPoison.Response{status_code: status, body: body}}, struct}) do
+    ExMetrics.increment("service.HTTP.response.#{status}")
     Map.put(struct, :response, %Struct.Response{http_status: status, body: body})
   end
 
   defp handle_response({{:error, _reason}, struct}) do
-    # Log error and add debug info to struct
+    ExMetrics.increment("error.service.HTTP.request")
     struct
   end
 
   defp execute_request(
-        struct = %Struct{request: request = %Struct.Request{method: "POST"}, private: private}
-      ) do
+         struct = %Struct{request: request = %Struct.Request{method: "POST"}, private: private}
+       ) do
     {@http_client.post(private.origin, request.path, request.payload), struct}
   end
 
   defp execute_request(
-        struct = %Struct{request: request = %Struct.Request{method: "GET"}, private: private}
-      ) do
+         struct = %Struct{request: request = %Struct.Request{method: "GET"}, private: private}
+       ) do
     {@http_client.get(private.origin, request.path), struct}
   end
 end
