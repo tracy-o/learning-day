fe6f8b698a37b0b043e9fc5405520a3f0fcf0403
James Bowers <james.bowers@bbc.co.uk>
2019-04-09 08:34:00 +0100

    correct default values for struct

--- lib/ingress/struct.ex
+++ lib/ingress/struct.ex
@@ -2,24 +2,21 @@ defmodule Ingress.Struct.Debug do
   defstruct pipeline_trail: []
 end
 
-defmodule Ingress.Struct do
-  defstruct request: Ingress.Struct.Request,
-            private: Ingress.Struct.Private,
-            response: Ingress.Struct.Response,
-            debug: %Ingress.Struct.Debug{}
-end
-
 defmodule Ingress.Struct.Request do
-  @enforce_keys [:path, :method]
   defstruct [:path, :payload, :method]
 end
 
 defmodule Ingress.Struct.Response do
-  @enforce_keys [:http_status, :body]
-  defstruct http_status: nil, headers: %{}, body: nil
+  defstruct [:http_status, :headers, :body]
 end
 
 defmodule Ingress.Struct.Private do
-  @enforce_keys [:loop_id]
   defstruct loop_id: nil, origin: nil, counter: %{}, pipeline: []
+end
+
+defmodule Ingress.Struct do
+  defstruct request: %Ingress.Struct.Request{},
+            private: %Ingress.Struct.Private{},
+            response: %Ingress.Struct.Response{},
+            debug: %Ingress.Struct.Debug{}
 end
