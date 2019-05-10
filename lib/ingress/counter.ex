82e07128cf40b683fa241c62390dc4906a0dfbb4
BradleyTesterSpratt <bradley.tester-spratt@bbc.co.uk>
2019-05-10 15:15:16 +0100

    slightly change inc/origin logic

--- lib/ingress/counter.ex
+++ lib/ingress/counter.ex
@@ -11,7 +11,6 @@ defmodule Ingress.Counter do
 
   def inc(counter, key, origin) when is_error(key) do
     counter
-    |> ensure_origin(origin)
     |> increment_key(origin, key)
     |> increment_key(origin, :errors)
     |> Map.update(:errors, 1, &(&1 + 1))
@@ -23,7 +22,6 @@ defmodule Ingress.Counter do
 
   def inc(counter, key, origin) do
     counter
-    |> ensure_origin(origin)
     |> increment_key(origin, key)
   end
 
@@ -35,16 +33,17 @@ defmodule Ingress.Counter do
     state[key] || 0
   end
 
+  defp increment_key(counter, origin, key) do
+    counter
+    |> ensure_origin(origin)
+    |> get_and_update_in([origin, key], &{&1, (&1||0) + 1})  
+    |> elem(1)
+  end
+
   defp ensure_origin(counter, origin) do
     case Map.has_key?(counter, origin) do
       true  -> counter
       false -> Map.put(counter, origin, %{errors: 0})
     end
-  end
-
-  defp increment_key(counter, origin, key) do
-    counter
-    |> get_and_update_in([origin, key], &{&1, (&1||0) + 1})  
-    |> elem(1)
   end
 end
