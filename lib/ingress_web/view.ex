7e783a904af9859868e2b40ced72788b2a31f6e4
Joseph Pack <josephapack@hotmail.co.uk>
2019-05-16 15:26:36 +0100

    Added TODOs for 404/500 edge cases
    as per discussions here:
    https://github.com/bbc/ingress/pull/41#discussion_r281946726
    and here:
    https://github.com/bbc/ingress/pull/41#discussion_r281946187

--- lib/ingress_web/view.ex
+++ lib/ingress_web/view.ex
@@ -35,6 +35,7 @@ defmodule IngressWeb.View do
     internal_server_error(conn)
   end
 
+  # TODO: Should we set the content type to be relating to the original request, as per this discussion: https://github.com/bbc/ingress/pull/41#discussion_r281946726
   defp error(conn, status, content) do
     conn
     |> add_default_headers(%Struct{response: %Struct.Response{http_status: status}})
