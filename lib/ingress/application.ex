073115da5d6475bf233fffb67c6925d012783293
Alex Muller <alex.muller@bbc.co.uk>
2019-05-22 16:19:52 +0100

    Add runtime config mechanism
    This runtime config isn't used when running `mix run` but is used
    as part of `mix release`. This means that development config is
    separate to Cosmos config which is unfortunate but probably the least
    bad outcome.
    
    I've introduced a safety check so that whenever a Cosmos config
    is not set in the environment, the application will fail to start.
    This prevents the case where a config value is mysteriously empty and
    fails more obviously.

--- lib/ingress/application.ex
+++ lib/ingress/application.ex
@@ -3,12 +3,6 @@ defmodule Ingress.Application do
 
   use Application
 
-  def start(_type, [env: :prod] = args) do
-    Application.put_env(:ingress, :instance_role_name, System.get_env("INSTANCE_ROLE_NAME"))
-
-    Ingress.Supervisor.start_link(args)
-  end
-
   def start(_type, args) do
     Ingress.Supervisor.start_link(args)
   end
