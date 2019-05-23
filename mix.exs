6208921924355a1835858ad0931d6d2a4d29c7da
Joseph Pack <josephapack@hotmail.co.uk>
2019-05-23 18:32:49 +0100

    Add Crimpex dependency

--- mix.exs
+++ mix.exs
@@ -26,20 +26,21 @@ defmodule Ingress.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"},
-      {:mojito, "~> 0.3.0"},
-      {:poison, "~> 3.1"},
-      {:ex_aws, "~> 2.0"},
-      {:ex_aws_sts, "~> 2.0"},
-      {:ex_aws_lambda, "~> 2.0"},
-      {:sweet_xml, "~> 0.6"},
-      {:mock, "~> 0.3", only: :test},
+      {:benchee, "~> 1.0.1", only: :dev},
+      {:crimpex, git: "https://github.com/bbc-news/Crimpex.git"},
       {:distillery, "~> 2.0"},
-      {:mox, "~> 0.5", only: :test},
+      {:ex_aws, "~> 2.0"},
+      {:ex_aws_lambda, "~> 2.0"},
+      {:ex_aws_sts, "~> 2.0"},
       {:ex_metrics, git: "https://github.com/bbc/ExMetrics.git"},
-      {:stump, "~> 1.0"},
       {:logger_file_backend, "~> 0.0.10"},
-      {:benchee, "~> 1.0.1", only: :dev}
+      {:mock, "~> 0.3", only: :test},
+      {:mojito, "~> 0.3.0"},
+      {:mox, "~> 0.5", only: :test},
+      {:plug_cowboy, "~> 2.0"},
+      {:poison, "~> 3.1"},
+      {:stump, "~> 1.0"},
+      {:sweet_xml, "~> 0.6"}
     ]
   end
 end
