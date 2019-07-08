c1c54dc878a68bb97c60a5169941a51fe726d3a8
James Bowers <james@bowers.dev>
2019-07-08 09:04:27 +0100

    refactor sending monitor loop logic, and update dependencies

--- lib/belfrage/monitor.ex
+++ lib/belfrage/monitor.ex
@@ -1,14 +1,10 @@
 defmodule Belfrage.Monitor do
   def record_loop(loop_state) do
-    with monitor_node when not is_nil(monitor_node) <- monitor_address() do
-      GenServer.cast({:data_store, monitor_node}, {:store, loop_state})
-    end
+    Node.list()
+    |> Enum.each(&send_to_monitor_node(&1, loop_state))
   end
 
-  defp monitor_address do
-    case Node.list() do
-      [monitor] -> monitor
-      _ -> nil
-    end
+  defp send_to_monitor_node(node, loop_state) do
+    GenServer.cast({:data_store, node}, {:store, loop_state})
   end
 end
