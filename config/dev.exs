b1288b3a7a9e6636a5b148f003029fd12bf6e281
Joseph Pack <josephapack@hotmail.co.uk>
2019-07-16 09:13:30 +0100

    Local dev config vars for functions

--- config/dev.exs
+++ config/dev.exs
@@ -3,6 +3,8 @@ use Mix.Config
 config :belfrage,
   origin: "http://test-compo-1chgcj5evn9oo-f0a64863a5d33db4.elb.eu-west-1.amazonaws.com",
   pwa_lambda_function: "pwa-lambda-function",
+  graphql_lambda_function: "graphql-lambda-function",
+  service_worker_lambda_function: "service-worker-lambda-function",
   webcore_lambda_role_arn: "webcore-lambda-role-arn"
 
 config :ex_metrics,
