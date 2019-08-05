f107d15b071d7e907acf40f3c1ffdfc47d9b89a3
Ettore Berardi <ettore.berardi@bbc.co.uk>
2019-08-05 17:21:06 +0100

    Adds the /graphql matcher and RouteSpec to the new Router.

--- lib/routes/routefile.ex
+++ lib/routes/routefile.ex
@@ -4,12 +4,15 @@ defmodule Routes.Routefile do
   redirect "/news/0", to: "/news", status: 302
   redirect "/weather/0", to: "/weather", status: 301
 
+  handle "/", using: "HomePage", examples: ["/"]
   handle "/news", using: "NewsFrontPage", examples: ["/news"]
   handle "/sport", using: "SportFrontPage", examples: ["/sport"]
   handle "/weather", using: "WeatherFrontPage", examples: ["/weather"]
   handle "/bitesize", using: "BitesizeFrontPage", examples: ["/bitesize"]
   handle "/cbeebies", using: "CBeebiesFrontPage", examples: ["/cbeebies"]
   handle "/dynasties", using: "DynastiesFrontPage", examples: ["/dynasties"]
+
+  handle "/graphql", using: "Graphql", examples: ["/graphql"]
 
   handle "/sport/videos/:id", using: "SportVideos", examples: ["/sport/videos/p077pnkr"] do
     return_404 if: String.length(id) != 8
