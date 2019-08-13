cb361fd1614fe7b6385e927b83ba2dedfa12419d
BradleyTesterSpratt <bradley.tester-spratt@bbc.co.uk>
2019-08-13 10:26:24 +0100

    allow varying on X-BBC-Edge-Scheme
    update tests to reflect changes

--- test/belfrage_web/response_headers/vary_test.exs
+++ test/belfrage_web/response_headers/vary_test.exs
@@ -10,7 +10,7 @@ defmodule BelfrageWeb.ResponseHeaders.VaryTest do
     input_conn = conn(:get, "/_web_core")
     output_conn = Vary.add_header(input_conn, nil)
 
-    assert ["Accept-Encoding, X-BBC-Edge-Cache, X-BBC-Edge-Country, Replayed-Traffic"] ==
+    assert ["Accept-Encoding, X-BBC-Edge-Cache, X-BBC-Edge-Country, Replayed-Traffic, X-BBC-Edge-Scheme"] ==
              get_resp_header(output_conn, "vary")
   end
 end
