7ae18c09cfc45a5e46412eb529d0a17311eb6391
Colin Barnwell <colin.barnwell@bbc.co.uk>
2019-08-16 10:34:02 +0100

    Add Topics specific details to /topics spec

--- lib/routes/specs/topic_page.ex
+++ lib/routes/specs/topic_page.ex
@@ -1,8 +1,8 @@
 defmodule Routes.Specs.TopicPage do
   def specs do
     %{
-      owner: "DENewsFrameworksTeam@bbc.co.uk",
-      runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
+      owner: "D&EKLDevelopmentOnCallTeam@bbc.co.uk",
+      runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Runbooks",
       platform: :webcore,
       pipeline: ["HTTPredirect", "LambdaOriginAliasTransformer", "ReplayedTrafficTransformer"],
       resp_pipeline: [],
