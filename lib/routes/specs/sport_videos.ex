e0bf68984c813e9ae7e9837040229628e57d61ec
Bill <billthornton@users.noreply.github.com>
2019-11-15 14:16:29 +0000

    Add short-form video as the contact/owner for the Sport video routes

--- lib/routes/specs/sport_videos.ex
+++ lib/routes/specs/sport_videos.ex
@@ -2,8 +2,8 @@ defmodule Routes.Specs.SportVideos do
   def specs do
     # TODO: this can be a struct with all the validations!
     %{
-      owner: "DENewsFrameworksTeam@bbc.co.uk",
-      runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
+      owner: "sfv-team@bbc.co.uk",
+      runbook: "https://confluence.dev.bbc.co.uk/display/SFV/Short+Form+Video+Run+Book",
       platform: :webcore,
       pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias"],
       resp_pipeline: [],
