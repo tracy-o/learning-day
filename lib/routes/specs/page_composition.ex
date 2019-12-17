be3abfe529197ad287dcc44f32f886ebbe09f254
Joseph Pack <josephapack@hotmail.co.uk>
2019-12-17 15:34:11 +0000

    Add the CircuitBreaker to the remaining route_specs

--- lib/routes/specs/page_composition.ex
+++ lib/routes/specs/page_composition.ex
@@ -4,9 +4,10 @@ defmodule Routes.Specs.PageComposition do
       owner: "DENewsFrameworksTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
       platform: :webcore,
-      pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias"],
+      pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias", "CircuitBreaker"],
       resp_pipeline: [],
-      query_params_allowlist: ["path"]
+      query_params_allowlist: ["path"],
+      circuit_breaker_error_threshold: 100
     }
   end
 end
--- lib/routes/specs/topic_page.ex
+++ lib/routes/specs/topic_page.ex
@@ -4,8 +4,9 @@ defmodule Routes.Specs.TopicPage do
       owner: "D&EKLDevelopmentOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Runbooks",
       platform: :webcore,
-      pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias"],
+      pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias", "CircuitBreaker"],
       resp_pipeline: [],
+      circuit_breaker_error_threshold: 100
     }
   end
 end
