b98480a07194df74eb4981f782d2ff7c949c88dd
James Bowers <james@bowers.dev>
2020-02-05 13:19:04 +0000

    add end to end test for content encoding for a response from the cache

--- test/end_to_end/encoding_test.exs
+++ test/end_to_end/encoding_test.exs
@@ -7,6 +7,11 @@ defmodule ContentEncodingTest do
 
   @moduletag :end_to_end
 
+  setup do
+    :ets.delete_all_objects(:cache)
+    :ok
+  end
+
   test "when the client accepts gzip the response should be gzipped" do
     Belfrage.Clients.HTTPMock
     |> expect(:execute, fn %Belfrage.Clients.HTTP.Request{} ->
@@ -44,21 +49,41 @@ defmodule ContentEncodingTest do
     assert [] == Plug.Conn.get_req_header(conn, "content-encoding")
   end
 
-  test "when no accept-encoding header is sent the response is not encoded" do
-    Belfrage.Clients.HTTPMock
-    |> expect(:execute, fn %Belfrage.Clients.HTTP.Request{} ->
-      {:ok,
-       %Belfrage.Clients.HTTP.Response{
-         status_code: 200,
-         body: :zlib.gzip("<p>content</p>"),
-         headers: %{"content-encoding" => "gzip"}
-       }}
-    end)
+  describe "when no accept-encoding header is sent" do
+    setup do
+      Belfrage.Clients.HTTPMock
+      |> expect(:execute, fn %Belfrage.Clients.HTTP.Request{} ->
+        {:ok,
+         %Belfrage.Clients.HTTP.Response{
+           status_code: 200,
+           body: :zlib.gzip("<p>content</p>"),
+           headers: %{"content-encoding" => "gzip", "cache-control" => "public, max-age=30"}
+         }}
+      end)
 
-    conn = conn(:get, "/proxy-pass")
-    conn = Router.call(conn, [])
+      :ok
+    end
 
-    assert {200, headers, "<p>content</p>"} = sent_resp(conn)
-    assert [] == Plug.Conn.get_req_header(conn, "content-encoding")
+    test "the response is not encoded (when response is from origin)" do
+      conn = conn(:get, "/proxy-pass")
+      conn = Router.call(conn, [])
+
+      assert {200, headers, "<p>content</p>"} = sent_resp(conn)
+      assert [] == Plug.Conn.get_req_header(conn, "content-encoding")
+    end
+
+    test "the response is not encoded (when response is from cache)" do
+      # seed the cache
+      conn(:get, "/proxy-pass")
+      |> put_req_header("accept-encoding", "deflate, br")
+      |> Router.call([])
+
+      conn =
+        conn(:get, "/proxy-pass")
+        |> Router.call([])
+
+      assert {200, headers, "<p>content</p>"} = sent_resp(conn)
+      assert [] == Plug.Conn.get_req_header(conn, "content-encoding")
+    end
   end
 end
