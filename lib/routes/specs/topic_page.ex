d5ad11e0e07f6615120d4b655e97f73461d882c8
loki-bbc <lokman.lau@bbc.co.uk>
2020-02-14 14:52:23 +0000

    Update Runbook URL for Topics

--- lib/routes/specs/topic_page.ex
+++ lib/routes/specs/topic_page.ex
@@ -2,7 +2,7 @@ defmodule Routes.Specs.TopicPage do
   def specs do
     %{
       owner: "D&EKLDevelopmentOnCallTeam@bbc.co.uk",
-      runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Runbooks",
+      runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Topics+Runbook",
       platform: :webcore,
       pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias", "CircuitBreaker"],
       resp_pipeline: [],
