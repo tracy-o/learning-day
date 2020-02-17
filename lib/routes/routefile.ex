133956ecc7314833444e14ef04c746c3c6acc978
loki-bbc <lokman.lau@bbc.co.uk>
2020-02-17 08:15:56 +0000

    add quotes around sports topics ids in list

--- lib/routes/routefile.ex
+++ lib/routes/routefile.ex
@@ -58,21 +58,21 @@ defmodule Routes.Routefile do
   end
 
   existing_topic_ids = [
-    c7gj2g87ez8t,  # Alpine Skiing
-    c9em2e59y83t,  # Biathlon
-    c85z25g35kdt,  # Bobsleigh
-    c7gj2g8l8qdt,  # Cross Country Skiiing
-    c2yx2y7q8x0t,  # Curling
-    cv7dr79gjjet,  # Figure Skating
-    cmj5ljxk69yt,  # Freestyle Skiing
-    c2yx2y9qgr0t,  # Luge
-    c53gk34rmlkt,  # Nordic Combined
-    c0mz5mvjj09t,  # Short Track Skating
-    cezpvz7y3g6t,  # Skeleton
-    ck0r604dlrzt,  # Ski Jumping
-    cezpvzp93m5t,  # Snowboarding
-    c3dr5drg040t,  # Speed Skating
-    clmq6mqqdpqt,  # Rugby Sevens
+    "c7gj2g87ez8t",  # Alpine Skiing
+    "c9em2e59y83t",  # Biathlon
+    "c85z25g35kdt",  # Bobsleigh
+    "c7gj2g8l8qdt",  # Cross Country Skiiing
+    "c2yx2y7q8x0t",  # Curling
+    "cv7dr79gjjet",  # Figure Skating
+    "cmj5ljxk69yt",  # Freestyle Skiing
+    "c2yx2y9qgr0t",  # Luge
+    "c53gk34rmlkt",  # Nordic Combined
+    "c0mz5mvjj09t",  # Short Track Skating
+    "cezpvz7y3g6t",  # Skeleton
+    "ck0r604dlrzt",  # Ski Jumping
+    "cezpvzp93m5t",  # Snowboarding
+    "c3dr5drg040t",  # Speed Skating
+    "clmq6mqqdpqt",  # Rugby Sevens
   ]
 
   handle "/sport/:discipline", using: "TopicPage", examples: ["/sport/cpzrw9qgwelt"] do
