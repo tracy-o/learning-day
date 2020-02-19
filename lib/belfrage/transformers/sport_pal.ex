a9d97b9689439b288bb480187c22c87edbf8353b
Joseph Pack <josephapack@hotmail.co.uk>
2020-02-19 16:05:45 +0000

    Ensure .app is not removed from url

--- lib/belfrage/transformers/sport_pal.ex
+++ lib/belfrage/transformers/sport_pal.ex
@@ -1,10 +1,12 @@
 defmodule Belfrage.Transformers.SportPal do
   use Belfrage.Transformers.Transformer
 
+  @end_of_path_pal_matcher ~r/\/pal(?=$|\.)/
+
   @impl true
   def call(rest, struct) do
     cond do
-      String.match?(struct.request.path, ~r/\/pal($|[.?]+)/) ->
+      String.match?(struct.request.path, @end_of_path_pal_matcher) ->
         struct = clean_url(struct, struct.request.path)
         then(rest, struct)
 
@@ -14,7 +16,7 @@ defmodule Belfrage.Transformers.SportPal do
   end
 
   defp clean_url(struct, path) do
-    new_path = String.replace(path, ~r/\/pal($|[.?]+)/, "")
+    new_path = String.replace(path, @end_of_path_pal_matcher, "")
     Struct.add(struct, :request, %{path: new_path})
   end
 end
--- test/belfrage/transformers/sport_pal_test.exs
+++ test/belfrage/transformers/sport_pal_test.exs
@@ -16,37 +16,6 @@ defmodule Belfrage.Transformers.SportPalTest do
     }
   }
 
-  @pal_request_without_qs %Struct{
-    request: %Struct.Request{path: "/_web_core/pal"},
-    private: %Struct.Private{
-      origin: "www.bbc.co.uk"
-    }
-  }
-
-  @no_pal_request_with_qs %Struct{
-    request: %Struct.Request{
-      path: "/_web_core?show-service-calls=true",
-      query_params: %{
-        "show-service-calls" => "true"
-      }
-    },
-    private: %Struct.Private{
-      origin: "www.bbc.co.uk"
-    }
-  }
-
-  @no_pal_request %Struct{
-    request: %Struct.Request{
-      path: "/_web_core",
-      query_params: %{
-        "show-service-calls" => "true"
-      }
-    },
-    private: %Struct.Private{
-      origin: "www.bbc.co.uk"
-    }
-  }
-
   describe "with a query string and /pal in the url" do
     test "/pal is removed from the url and the query string remains" do
       assert {
@@ -63,6 +32,41 @@ defmodule Belfrage.Transformers.SportPalTest do
     end
   end
 
+  @pal_app_request_with_qs %Struct{
+    request: %Struct.Request{
+      path: "/_web_core/pal.app",
+      query_params: %{
+        "show-service-calls" => "true"
+      }
+    },
+    private: %Struct.Private{
+      origin: "www.bbc.co.uk"
+    }
+  }
+
+  describe "with a query string and /pal.app in the url" do
+    test "/pal.app is removed from the url and the query string remains" do
+      assert {
+               :ok,
+               %Struct{
+                 private: %Struct.Private{
+                   origin: "www.bbc.co.uk"
+                 },
+                 request: %Struct.Request{
+                   path: "/_web_core.app"
+                 }
+               }
+             } = SportPal.call([], @pal_app_request_with_qs)
+    end
+  end
+
+  @pal_request_without_qs %Struct{
+    request: %Struct.Request{path: "/_web_core/pal"},
+    private: %Struct.Private{
+      origin: "www.bbc.co.uk"
+    }
+  }
+
   describe "a request is made without a query string but with /pal in the url" do
     test "/pal is removed from the url" do
       assert {
@@ -77,6 +81,39 @@ defmodule Belfrage.Transformers.SportPalTest do
     end
   end
 
+  @pal_app_request_without_qs %Struct{
+    request: %Struct.Request{path: "/_web_core/pal.app"},
+    private: %Struct.Private{
+      origin: "www.bbc.co.uk"
+    }
+  }
+
+  describe "a request is made without a query string but with /pal.app in the url" do
+    test "/pal is removed from the url" do
+      assert {
+               :ok,
+               %Struct{
+                 private: %Struct.Private{
+                   origin: "www.bbc.co.uk"
+                 },
+                 request: %Struct.Request{path: "/_web_core.app"}
+               }
+             } = SportPal.call([], @pal_app_request_without_qs)
+    end
+  end
+
+  @no_pal_request_with_qs %Struct{
+    request: %Struct.Request{
+      path: "/_web_core?show-service-calls=true",
+      query_params: %{
+        "show-service-calls" => "true"
+      }
+    },
+    private: %Struct.Private{
+      origin: "www.bbc.co.uk"
+    }
+  }
+
   describe "a request is made with a query string" do
     test "the path remains the same" do
       assert {
@@ -90,6 +127,18 @@ defmodule Belfrage.Transformers.SportPalTest do
              } = SportPal.call([], @no_pal_request_with_qs)
     end
   end
+
+  @no_pal_request %Struct{
+    request: %Struct.Request{
+      path: "/_web_core",
+      query_params: %{
+        "show-service-calls" => "true"
+      }
+    },
+    private: %Struct.Private{
+      origin: "www.bbc.co.uk"
+    }
+  }
 
   describe "a request is made for a url without /pal" do
     test "the path remains the same" do
