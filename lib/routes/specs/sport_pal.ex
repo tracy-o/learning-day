0bd6746c7ebe4517e0af6e0f31451abc6f676492
Joseph Pack <josephapack@hotmail.co.uk>
2020-02-20 11:36:16 +0000

    Remove SportPal transformer...
    We may not need this because we are unsure if /pal approach will be used
    in Belfrage. It is likely it wont be as it probably isnt the best
    approach but this commit allows us to find this work again if it is
    required.

--- lib/routes/specs/sport_pal.ex
+++ lib/routes/specs/sport_pal.ex
@@ -4,7 +4,7 @@ defmodule Routes.Specs.SportFrontPage do
       owner: "DENewsFrameworksTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
       platform: :pal,
-      pipeline: ["HTTPredirect", "SportPal", "CircuitBreaker"],
+      pipeline: ["HTTPredirect", "CircuitBreaker"],
       resp_pipeline: [],
       query_params_allowlist: ["show-service-calls"],
       circuit_breaker_error_threshold: 100
