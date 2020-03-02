9a093f0f259db54797eeaadc5ead108737c437f0
Joseph Pack <josephapack@hotmail.co.uk>
2020-03-02 16:07:08 +0000

    Handle Nested Query Params

--- test/belfrage/helpers/query_params_test.exs
+++ test/belfrage/helpers/query_params_test.exs
@@ -15,4 +15,16 @@ defmodule Belfrage.Helpers.QueryParamsTest do
 
     refute "?enabled?true?disabled=false" == QueryParams.parse(query_params)
   end
+
+  test "it can handle a nested query params" do
+    query_params = %{"options" => %{"enabled" => "true", "disabled" => "false", "elixir" => "cool"}}
+
+    assert "?options[disabled]=false&options[elixir]=cool&options[enabled]=true" == QueryParams.parse(query_params)
+  end
+
+  test "it can handle a deeply nested map" do
+    query_params = %{"options" => %{"enabled" => "true", "disabled" => "false", "elixir" => %{"is" => "cool"}}}
+
+    assert "?options[disabled]=false&options[elixir][is]=cool&options[enabled]=true" == QueryParams.parse(query_params)
+  end
 end
