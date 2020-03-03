5ef8501ddad51d39b6cbbff3462ed2f6c6523000
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2020-03-03 14:12:27 +0000

    Add new WS batch 2 routes and their routespecs

--- lib/routes/routefile.ex
+++ lib/routes/routefile.ex
@@ -54,8 +54,17 @@ defmodule Routes.Routefile do
 
   handle "/pres-test/*any", using: "PresTest", only_on: "test", examples: ["/pres-test/greeting-loader", "/pres-test/hcraes"]
 
+  handle "/igbo", using: "WorldServiceIgbo", examples: ["/igbo"]
+  handle "/igbo/*_any", using: "WorldServiceIgbo", examples: ["/igbo/afirika-51708561"]
+
+  handle "/pidgin", using: "WorldServicePidgin", examples: ["/pidgin"]
+  handle "/pidgin/*_any", using: "WorldServicePidgin", examples: ["/pidgin/tori-51717056"]
+
   handle "/tajik", using: "WorldServiceTajik", examples: ["/tajik"]
   handle "/tajik/*_any", using: "WorldServiceTajik", examples: ["/tajik/news/2015/03/150331_l16_bbc-tajik_closure"]
+
+  handle "/yoruba", using: "WorldServiceYoruba", examples: ["/yoruba"]
+  handle "/yoruba/*_any", using: "WorldServiceYoruba", examples: ["/yoruba/awon-iroyin-miran-51716954"]
 
   handle "/topics/:id/:pageNumber", using: "TopicPage", examples: ["/topics/cmj34zmwm1zt/1"] do
     return_404 if: !String.match?(id, ~r/^c[\w]{10}t$/) or !String.match?(pageNumber , ~r/^[1-9][0-9]*$/)
---
+++ lib/routes/specs/world_service_pidgin.ex
@@ -0,0 +1,12 @@
+defmodule Routes.Specs.WorldServicePidgin do
+  def specs do
+    %{
+      owner: "DENewsFrameworksTeam@bbc.co.uk",
+      runbook: "https://confluence.dev.bbc.co.uk/display/MOZART/Mozart+Run+Book",
+      platform: :pal,
+      pipeline: ["WorldServiceRedirect", "CircuitBreaker"],
+      resp_pipeline: [],
+      circuit_breaker_error_threshold: 100
+    }
+  end
+end
