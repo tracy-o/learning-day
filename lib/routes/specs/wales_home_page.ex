7617f5e957252b2b38b87359d4f9a8900ed2e461
Natalia <n.zmyslowska@gmail.com>
2020-03-03 16:11:14 +0000

    Add Wales and Northern Ireland home pages

---
+++ lib/routes/specs/wales_home_page.ex
@@ -0,0 +1,12 @@
+defmodule Routes.Specs.WalesHomePage do
+  def specs do
+    %{
+      owner: "DENewsFrameworksTeam@bbc.co.uk",
+      runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
+      platform: :webcore,
+      pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias", "CircuitBreaker"],
+      resp_pipeline: [],
+      circuit_breaker_error_threshold: 100
+    }
+  end
+end
