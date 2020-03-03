b88de402b5fb392f0a66e5ee8dfbddfac7525fcc
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2020-03-03 14:19:33 +0000

    Point the route specs correctly at Mozart

--- lib/routes/specs/world_service_igbo.ex
+++ lib/routes/specs/world_service_igbo.ex
@@ -3,7 +3,7 @@ defmodule Routes.Specs.WorldServiceIgbo do
     %{
       owner: "DENewsFrameworksTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/MOZART/Mozart+Run+Book",
-      platform: :pal,
+      platform: :mozart,
       pipeline: ["WorldServiceRedirect", "CircuitBreaker"],
       resp_pipeline: [],
       circuit_breaker_error_threshold: 100
