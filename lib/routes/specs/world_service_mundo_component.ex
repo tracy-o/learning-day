a1d5fdfa1f0b52b0c27d8eb34832f1ded6e2e875
Joseph Pack <josephapack@hotmail.co.uk>
2020-03-05 09:52:27 +0000

    Uplift mundo components using WorldServiceRedirect Transformer

--- lib/routes/specs/world_service_mundo_component.ex
+++ lib/routes/specs/world_service_mundo_component.ex
@@ -4,7 +4,7 @@ defmodule Routes.Specs.WorldServiceMundoComponent do
       owner: "DENewsFrameworksTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/MOZART/Mozart+Run+Book",
       platform: :pal,
-      pipeline: ["HTTPredirect", "CircuitBreaker"],
+      pipeline: ["WorldServiceRedirect", "CircuitBreaker"],
       resp_pipeline: [],
       query_params_allowlist: ["alternativeJsLoading", "batch"],
       circuit_breaker_error_threshold: 100
