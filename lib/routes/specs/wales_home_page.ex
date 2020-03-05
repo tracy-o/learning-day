3a30016e0d2f429eac323b14270b2e6b8a87c8a8
Natalia <n.zmyslowska@gmail.com>
2020-03-05 10:39:42 +0000

    changed runbook links

--- lib/routes/specs/wales_home_page.ex
+++ lib/routes/specs/wales_home_page.ex
@@ -2,7 +2,7 @@ defmodule Routes.Specs.WalesHomePage do
   def specs do
     %{
       owner: "DENewsFrameworksTeam@bbc.co.uk",
-      runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
+      runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Runbook",
       platform: :webcore,
       pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias", "CircuitBreaker"],
       resp_pipeline: [],
