213976ca66840ebe65780c6bc8f78103b4945fea
Jon Ashdown <jon.ashdown@bbc.co.uk>
2020-03-11 15:48:59 +0000

    use D-list

--- lib/routes/specs/search.ex
+++ lib/routes/specs/search.ex
@@ -1,7 +1,7 @@
 defmodule Routes.Specs.Search do
   def specs do
     %{
-      owner: "natalia.miller@bbc.co.uk",
+      owner: "D+ESearchAndNavigationDev@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/x/xo2KD",
       platform: :webcore,
       pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias", "CircuitBreaker"],
