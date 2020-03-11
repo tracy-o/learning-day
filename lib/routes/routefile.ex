634d517c47b35433dc44cc2ca1ea7a6c90c95a1f
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2020-03-11 11:28:37 +0000

    Remove needless excess route lines
    the indexes match on the /*_any and use the right routespec as shown by the routes tests

--- lib/routes/routefile.ex
+++ lib/routes/routefile.ex
@@ -53,128 +53,47 @@ defmodule Routes.Routefile do
 
   handle "/pres-test/*any", using: "PresTest", only_on: "test", examples: ["/pres-test/greeting-loader", "/pres-test/hcraes"]
 
-  handle "/afrique", using: "WorldServiceAfrique", examples: ["/afrique"]
-  handle "/afrique/*_any", using: "WorldServiceAfrique", examples: ["/afrique/example-123"]
-
-  handle "/afaanoromoo", using: "WorldServiceAfaanoromoo", examples: ["/afaanoromoo"]
-  handle "/afaanoromoo/*_any", using: "WorldServiceAfaanoromoo", examples: ["/afaanoromoo/oduu-51797939"]
-
-  handle "/amharic", using: "WorldServiceAmharic", examples: ["/amharic"]
-  handle "/amharic/*_any", using: "WorldServiceAmharic", examples: ["/amharic/news-51797888"]
-
-  handle "/arabic", using: "WorldServiceArabic", examples: ["/arabic"]
-  handle "/arabic/*_any", using: "WorldServiceArabic", examples: ["/arabic/example-123"]
-
-  handle "/azeri", using: "WorldServiceAzeri", examples: ["/azeri"]
-  handle "/azeri/*_any", using: "WorldServiceAzeri", examples: ["/azeri/magazine-51736698"]
-
-  handle "/bengali", using: "WorldServiceBengali", examples: ["/bengali"]
-  handle "/bengali/*_any", using: "WorldServiceBengali", examples: ["/bengali/example-123"]
-
-  handle "/burmese", using: "WorldServiceBurmese", examples: ["/burmese"]
-  handle "/burmese/*_any", using: "WorldServiceBurmese", examples: ["/burmese/example-123"]
-
-  handle "/cymrufyw", using: "WorldServiceCymrufyw", examples: ["/cymrufyw"]
-  handle "/cymrufyw/*_any", using: "WorldServiceCymrufyw", examples: ["/cymrufyw/example-123"]
-
-  handle "/gahuza", using: "WorldServiceGahuza", examples: ["/gahuza"]
-  handle "/gahuza/*_any", using: "WorldServiceGahuza", examples: ["/gahuza/example-123"]
-
-  handle "/gujarati", using: "WorldServiceGujarati", examples: ["/gujarati"]
-  handle "/gujarati/*_any", using: "WorldServiceGujarati", examples: ["/gujarati/example-123"]
-
-  handle "/hausa", using: "WorldServiceHausa", examples: ["/hausa"]
-  handle "/hausa/*_any", using: "WorldServiceHausa", examples: ["/hausa/example-123"]
-
-  handle "/hindi", using: "WorldServiceHindi", examples: ["/hindi"]
-  handle "/hindi/*_any", using: "WorldServiceHindi", examples: ["/hindi/example-123"]
-
-  handle "/igbo", using: "WorldServiceIgbo", examples: ["/igbo"]
-  handle "/igbo/*_any", using: "WorldServiceIgbo", examples: ["/igbo/afirika-51708561"]
-
-  handle "/indonesia", using: "WorldServiceIndonesia", examples: ["/indonesia"]
-  handle "/indonesia/*_any", using: "WorldServiceIndonesia", examples: ["/indonesia/example-123"]
-
-  handle "/japanese", using: "WorldServiceJapanese", examples: ["/japanese"]
-  handle "/japanese/*_any", using: "WorldServiceJapanese", examples: ["/japanese/51748582"]
-
-  handle "/korean", using: "WorldServiceKorean", examples: ["/korean"]
-  handle "/korean/*_any", using: "WorldServiceKorean", examples: ["/korean/example-123"]
-
-  handle "/kyrgyz", using: "WorldServiceKyrgyz", examples: ["/kyrgyz"]
-  handle "/kyrgyz/*_any", using: "WorldServiceKyrgyz", examples: ["/kyrgyz/51748492"]
-
-  handle "/marathi", using: "WorldServiceMarathi", examples: ["/marathi"]
-  handle "/marathi/*_any", using: "WorldServiceMarathi", examples: ["/marathi/example-123"]
-
-  handle "/mundo", using: "WorldServiceMundo", examples: ["/mundo"]
-  handle "/mundo/*_any", using: "WorldServiceMundo", examples: ["/mundo/noticias-51503412", "/mundo/components"]
-
-  handle "/naidheachdan", using: "WorldServiceNaidheachdan", examples: ["/naidheachdan"]
-  handle "/naidheachdan/*_any", using: "WorldServiceNaidheachdan", examples: ["/naidheachdan/example-123"]
-
-  handle "/persian", using: "WorldServicePersian", examples: ["/persian"]
-  handle "/persian/*_any", using: "WorldServicePersian", examples: ["/persian/example-123"]
-
-  handle "/portuguese", using: "WorldServicePortuguese", examples: ["/portuguese"]
-  handle "/portuguese/*_any", using: "WorldServicePortuguese", examples: ["/portuguese/example-123"]
-
-  handle "/nepali", using: "WorldServiceNepali", examples: ["/nepali"]
-  handle "/nepali/*_any", using: "WorldServiceNepali", examples: ["/nepali/example-123"]
-
-  handle "/pashto", using: "WorldServicePashto", examples: ["/pashto"]
-  handle "/pashto/*_any", using: "WorldServicePashto", examples: ["/pashto/example-123"]
-
-  handle "/pidgin", using: "WorldServicePidgin", examples: ["/pidgin"]
-  handle "/pidgin/*_any", using: "WorldServicePidgin", examples: ["/pidgin/tori-51717056"]
-
-  handle "/punjabi", using: "WorldServicePunjabi", examples: ["/punjabi"]
-  handle "/punjabi/*_any", using: "WorldServicePunjabi", examples: ["/punjabi/india-51743542"]
-
-  handle "/russian", using: "WorldServiceRussian", examples: ["/russian"]
-  handle "/russian/*_any", using: "WorldServiceRussian", examples: ["/russian/example-123"]
-
-  handle "/sinhala", using: "WorldServiceSinhala", examples: ["/sinhala"]
-  handle "/sinhala/*_any", using: "WorldServiceSinhala", examples: ["/sinhala/example-123"]
-
-  handle "/somali", using: "WorldServiceSomali", examples: ["/somali"]
-  handle "/somali/*_any", using: "WorldServiceSomali", examples: ["/somali/example-123"]
-
-  handle "/swahili", using: "WorldServiceSwahili", examples: ["/swahili"]
-  handle "/swahili/*_any", using: "WorldServiceSwahili", examples: ["/swahili/example-123"]
-
-  handle "/tajik", using: "WorldServiceTajik", examples: ["/tajik"]
-  handle "/tajik/*_any", using: "WorldServiceTajik", examples: ["/tajik/components", "/tajik/news/2015/03/150331_l16_bbc-tajik_closure"]
-
-  handle "/tamil", using: "WorldServiceTamil", examples: ["/tamil"]
-  handle "/tamil/*_any", using: "WorldServiceTamil", examples: ["/tamil/example-123"]
-
-  handle "/telugu", using: "WorldServiceTelugu", examples: ["/telugu"]
-  handle "/telugu/*_any", using: "WorldServiceTelugu", examples: ["/telugu/example-123"]
-
-  handle "/thai", using: "WorldServiceThai", examples: ["/thai"]
-  handle "/thai/*_any", using: "WorldServiceThai", examples: ["/thai/thailand-51706739"]
-
-  handle "/tigrinya", using: "WorldServiceTigrinya", examples: ["/tigrinya"]
-  handle "/tigrinya/*_any", using: "WorldServiceTigrinya", examples: ["/tigrinya/news-51799994"]
-
-  handle "/turkce", using: "WorldServiceTurkce", examples: ["/turkce"]
-  handle "/turkce/*_any", using: "WorldServiceTurkce", examples: ["/turkce/example-123"]
-
-  handle "/ukrainian", using: "WorldServiceUkrainian", examples: ["/ukrainian"]
-  handle "/ukrainian/*_any", using: "WorldServiceUkrainian", examples: ["/ukrainian/example-123"]
-
-  handle "/urdu", using: "WorldServiceUrdu", examples: ["/urdu"]
-  handle "/urdu/*_any", using: "WorldServiceUrdu", examples: ["/urdu/example-123"]
-
-  handle "/uzbek", using: "WorldServiceUzbek", examples: ["/uzbek"]
-  handle "/uzbek/*_any", using: "WorldServiceUzbek", examples: ["/uzbek/example-123"]
-
-  handle "/vietnamese", using: "WorldServiceVietnamese", examples: ["/vietnamese"]
-  handle "/vietnamese/*_any", using: "WorldServiceVietnamese", examples: ["/vietnamese/example-123"]
-
-  handle "/yoruba", using: "WorldServiceYoruba", examples: ["/yoruba"]
-  handle "/yoruba/*_any", using: "WorldServiceYoruba", examples: ["/yoruba/awon-iroyin-miran-51716954"]
+  handle "/afaanoromoo/*_any", using: "WorldServiceAfaanoromoo", examples: ["/afaanoromoo", "/afaanoromoo/oduu-51797939"]
+  handle "/afrique/*_any", using: "WorldServiceAfrique", examples: ["/afrique", "/afrique/example-123"]
+  handle "/amharic/*_any", using: "WorldServiceAmharic", examples: ["/amharic", "/amharic/news-51797888"]
+  handle "/arabic/*_any", using: "WorldServiceArabic", examples: ["/arabic", "/arabic/example-123"]
+  handle "/azeri/*_any", using: "WorldServiceAzeri", examples: ["/azeri", "/azeri/magazine-51736698"]
+  handle "/bengali/*_any", using: "WorldServiceBengali", examples: ["/bengali", "/bengali/example-123"]
+  handle "/burmese/*_any", using: "WorldServiceBurmese", examples: ["/burmese", "/burmese/example-123"]
+  handle "/cymrufyw/*_any", using: "WorldServiceCymrufyw", examples: ["/cymrufyw", "/cymrufyw/example-123"]
+  handle "/gahuza/*_any", using: "WorldServiceGahuza", examples: ["/gahuza", "/gahuza/example-123"]
+  handle "/gujarati/*_any", using: "WorldServiceGujarati", examples: ["/gujarati", "/gujarati/example-123"]
+  handle "/hausa/*_any", using: "WorldServiceHausa", examples: ["/hausa", "/hausa/example-123"]
+  handle "/hindi/*_any", using: "WorldServiceHindi", examples: ["/hindi", "/hindi/example-123"]
+  handle "/igbo/*_any", using: "WorldServiceIgbo", examples: ["/igbo", "/igbo/afirika-51708561"]
+  handle "/indonesia/*_any", using: "WorldServiceIndonesia", examples: ["/indonesia", "/indonesia/example-123"]
+  handle "/japanese/*_any", using: "WorldServiceJapanese", examples: ["/japanese", "/japanese/51748582"]
+  handle "/korean/*_any", using: "WorldServiceKorean", examples: ["/korean", "/korean/example-123"]
+  handle "/kyrgyz/*_any", using: "WorldServiceKyrgyz", examples: ["/kyrgyz", "/kyrgyz/51748492"]
+  handle "/marathi/*_any", using: "WorldServiceMarathi", examples: ["/marathi", "/marathi/example-123"]
+  handle "/mundo/*_any", using: "WorldServiceMundo", examples: ["/mundo", "/mundo/noticias-51503412", "/mundo/components"]
+  handle "/naidheachdan/*_any", using: "WorldServiceNaidheachdan", examples: ["/naidheachdan", "/naidheachdan/example-123"]
+  handle "/nepali/*_any", using: "WorldServiceNepali", examples: ["/nepali", "/nepali/example-123"]
+  handle "/pashto/*_any", using: "WorldServicePashto", examples: ["/pashto", "/pashto/example-123"]
+  handle "/persian/*_any", using: "WorldServicePersian", examples: ["/persian", "/persian/example-123"]
+  handle "/pidgin/*_any", using: "WorldServicePidgin", examples: ["/pidgin", "/pidgin/tori-51717056"]
+  handle "/portuguese/*_any", using: "WorldServicePortuguese", examples: ["/portuguese", "/portuguese/example-123"]
+  handle "/punjabi/*_any", using: "WorldServicePunjabi", examples: ["/punjabi", "/punjabi/india-51743542"]
+  handle "/russian/*_any", using: "WorldServiceRussian", examples: ["/russian", "/russian/example-123"]
+  handle "/sinhala/*_any", using: "WorldServiceSinhala", examples: ["/sinhala", "/sinhala/example-123"]
+  handle "/somali/*_any", using: "WorldServiceSomali", examples: ["/somali", "/somali/example-123"]
+  handle "/swahili/*_any", using: "WorldServiceSwahili", examples: ["/swahili", "/swahili/example-123"]
+  handle "/tajik/*_any", using: "WorldServiceTajik", examples: ["/tajik", "/tajik/components", "/tajik/news/2015/03/150331_l16_bbc-tajik_closure"]
+  handle "/tamil/*_any", using: "WorldServiceTamil", examples: ["/tamil", "/tamil/example-123"]
+  handle "/telugu/*_any", using: "WorldServiceTelugu", examples: ["/telugu", "/telugu/example-123"]
+  handle "/thai/*_any", using: "WorldServiceThai", examples: ["/thai", "/thai/thailand-51706739"]
+  handle "/tigrinya/*_any", using: "WorldServiceTigrinya", examples: ["/tigrinya", "/tigrinya/news-51799994"]
+  handle "/turkce/*_any", using: "WorldServiceTurkce", examples: ["/turkce", "/turkce/example-123"]
+  handle "/ukrainian/*_any", using: "WorldServiceUkrainian", examples: ["/ukrainian", "/ukrainian/example-123"]
+  handle "/urdu/*_any", using: "WorldServiceUrdu", examples: ["/urdu", "/urdu/example-123"]
+  handle "/uzbek/*_any", using: "WorldServiceUzbek", examples: ["/uzbek", "/uzbek/example-123"]
+  handle "/vietnamese/*_any", using: "WorldServiceVietnamese", examples: ["/vietnamese", "/vietnamese/example-123"]
+  handle "/yoruba/*_any", using: "WorldServiceYoruba", examples: ["/yoruba", "/yoruba/awon-iroyin-miran-51716954"]
 
   handle "/topics/:id/:pageNumber", using: "TopicPage", examples: ["/topics/cmj34zmwm1zt/1"] do
     return_404 if: !String.match?(id, ~r/^c[\w]{10}t$/) or !String.match?(pageNumber , ~r/^[1-9][0-9]*$/)
