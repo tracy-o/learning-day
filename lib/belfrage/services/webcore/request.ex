edb9844f358ba2ecdffaba2fa68cfb011a299279
Ryan Wood <Ryan.Wood01@bbc.co.uk>
2020-03-11 17:08:14 +0000

    Manually convert bad characters to � if String is invalid

--- lib/belfrage/services/webcore/request.ex
+++ lib/belfrage/services/webcore/request.ex
@@ -21,5 +21,23 @@ defmodule Belfrage.Services.Webcore.Request do
   defp key_value_pair_to_utf8({key, value}) when is_map(value), do: {string_to_utf8(key), utf8_sanitise(value)}
   defp key_value_pair_to_utf8({key, value}), do: {string_to_utf8(key), string_to_utf8(value)}
 
-  defp string_to_utf8(s), do: Codepagex.from_string!(s, :iso_8859_1, Codepagex.use_utf_replacement())
+  defp string_to_utf8(s) do
+    case String.valid?(s) do
+      true -> s
+      false -> deal_with_invalid_chars(s)
+    end
+  end
+
+  defp deal_with_invalid_chars(s) do
+    Enum.map(
+      :binary.bin_to_list(s),
+      fn char ->
+        case String.valid?(<<char>>) do
+          true -> <<char>>
+          false -> "�"
+        end
+      end
+    )
+    |> Enum.join()
+  end
 end
