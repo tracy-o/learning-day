512f1a153b70516e3e4e5180c30d041b5536bf22
Jimish Shah - Content Discovery <jimish.shah02.ext@bbc.co.uk>
2020-03-20 15:04:58 +0000

    Add query param to allow list

--- lib/routes/specs/page_composition.ex
+++ lib/routes/specs/page_composition.ex
@@ -2,7 +2,7 @@ defmodule Routes.Specs.PageComposition do
   def specs do
     %{
       platform: Webcore,
-      query_params_allowlist: ["path", "params"]
+      query_params_allowlist: ["path", "params", "query"]
     }
   end
 end
