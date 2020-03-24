3de69a5910e9e6a74903e66f7cb7612487df5bf2
James Bowers <james@bowers.dev>
2020-03-24 16:46:54 +0000

    start a fake belfrage ccp to be used to test the distributed fallback cache strategy

---
+++ test/belfrage/cache/distributed_fallback_test.exs
@@ -0,0 +1,22 @@
+defmodule Belfrage.Cache.DistributedFallbackTest do
+  use ExUnit.Case
+  alias Belfrage.Struct
+  alias Belfrage.Cache.DistributedFallback
+
+  setup do
+    Test.Support.FakeBelfrageCcp.start()
+
+    :ok
+  end
+
+  test "store/1" do
+    struct = %Struct{
+      request: %Struct.Request{request_hash: "distributed-cache-test"},
+      response: %Struct.Response{body: "<p>Hello</p>"}
+    }
+
+    DistributedFallback.store(struct)
+
+    assert Test.Support.FakeBelfrageCcp.received_message?("distributed-cache-test", struct.response)
+  end
+end
