b9382646aa70dbe1a0094e843c8f385f7968c641
Mohammed Dwina <dwinam01@mc-n354237.local>
2020-04-24 11:41:31 +0100

    Remove pipeline, resp_pipeline and circuit_breaker_error_threshold

--- lib/routes/specs/automation_home_page.ex
+++ lib/routes/specs/automation_home_page.ex
@@ -3,10 +3,7 @@ defmodule Routes.Specs.AutomationHomePage do
     %{
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Runbook",
-      platform: Webcore,
-      pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias", "CircuitBreaker"],
-      resp_pipeline: [],
-      circuit_breaker_error_threshold: 100
+      platform: Webcore
     }
   end
 end
