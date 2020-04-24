bffc0083c2c773a933a53af24bbf32c60fc98802
Mohammed Dwina <dwinam01@mc-n354237.local>
2020-04-24 10:49:20 +0100

    Add pipeline, resp_pipeline and circuit_breaker_error_threshold

--- lib/routes/specs/automation_home_page.ex
+++ lib/routes/specs/automation_home_page.ex
@@ -3,7 +3,10 @@ defmodule Routes.Specs.AutomationHomePage do
     %{
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Runbook",
-      platform: Webcore
+      platform: Webcore,
+      pipeline: ["HTTPredirect", "DevelopmentRequests", "LambdaOriginAlias", "CircuitBreaker"],
+      resp_pipeline: [],
+      circuit_breaker_error_threshold: 100
     }
   end
 end
