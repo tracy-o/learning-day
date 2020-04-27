9c71f7a629bc0aa3cee8f58cdc9a64634b4b3a77
Boon Low <boon.low@bbc.co.uk>
2020-04-27 14:54:07 +0100

    refactor, setup and parallelise cache limit tests

--- test/belfrage/supervisor_test.exs
+++ test/belfrage/supervisor_test.exs
@@ -5,8 +5,21 @@ defmodule Belfrage.SupervisorTest do
   import Fixtures.Struct
   import Supervisor.Spec, only: [{:worker, 2}]
 
-  @local_cache :cache
+  @belfrage_local_cache :cache
   @test_cache :test_cache
+
+  setup_all do
+    conf = Application.get_env(:cachex, :limit)
+
+    overflow = 5
+    overflow_size = conf[:size] + overflow
+    limit = limit(size: conf[:size], policy: conf[:policy], reclaim: conf[:reclaim], options: conf[:options])
+
+    Supervisor.start_link([worker(Cachex, [@test_cache, [limit: limit]])], strategy: :one_for_one)
+    seed_test_cache(1..overflow_size, @test_cache)
+
+    %{conf: conf, overflow_size: overflow_size}
+  end
 
   describe "local cache" do
     test "is alive" do
@@ -18,54 +31,52 @@ defmodule Belfrage.SupervisorTest do
       assert Process.alive?(pid)
     end
 
-    test "size limit and reclaim policy configured" do
-      conf = Application.get_env(:cachex, :limit)
+    test "size limit and reclaim policy configured", %{conf: conf} do
       limit = limit(size: conf[:size], policy: conf[:policy], reclaim: conf[:reclaim], options: conf[:options])
 
       assert [limit] ==
-               Cachex.inspect(@local_cache, :cache)
+               Cachex.inspect(@belfrage_local_cache, :cache)
                |> elem(1)
                |> Tuple.to_list()
                |> Enum.filter(fn x -> is_tuple(x) and elem(x, 0) == :limit end)
     end
 
     # least-recently used (LRU) reclaim policy configured
-    test "reclaim policy" do
-      conf = Application.get_env(:cachex, :limit)
-      limit = limit(size: conf[:size], policy: conf[:policy], reclaim: conf[:reclaim], options: conf[:options])
+    test "reclaim policy: post-eviction size", %{conf: conf} do
+      {:ok, post_eviction_size} = Cachex.count(@test_cache)
 
-      overflow = 5
-      cache_overflow_size = conf[:size] + overflow
+      assert post_eviction_size < conf[:size]
+      assert post_eviction_size / conf[:size] == conf[:reclaim]
+    end
 
-      Supervisor.start_link([worker(Cachex, [@test_cache, [limit: limit]])], strategy: :one_for_one)
+    test "reclaim policy: post-eviction cache content", %{overflow_size: overflow_size} do
+      {:ok, post_eviction_size} = Cachex.count(@test_cache)
 
-      Enum.each(1..cache_overflow_size, fn key ->
-        struct = struct_with_gzip_resp()
-
-        Cachex.put(
-          @test_cache,
-          key,
-          {struct.response, Belfrage.Timer.now_ms()},
-          ttl: struct.private.fallback_ttl
-        )
-
-        :timer.sleep(1)
-      end)
-
-      # allow time for cache eviction
-      Process.sleep(30)
-      {:ok, post_reclaim_size} = Cachex.count(@test_cache)
-
-      assert post_reclaim_size < conf[:size]
-      assert post_reclaim_size / conf[:size] == conf[:reclaim]
-
-      for evicted_cache_key <- Enum.take(1..cache_overflow_size, cache_overflow_size - post_reclaim_size) do
+      for evicted_cache_key <- Enum.take(1..overflow_size, overflow_size - post_eviction_size) do
         assert {:ok, false} = Cachex.exists?(@test_cache, evicted_cache_key)
       end
 
-      for remaining_cache_key <- Enum.take(1..cache_overflow_size, -post_reclaim_size) do
+      for remaining_cache_key <- Enum.take(1..overflow_size, -post_eviction_size) do
         assert {:ok, true} = Cachex.exists?(@test_cache, remaining_cache_key)
       end
     end
+  end
+
+  defp seed_test_cache(test_key_range, cache) do
+    Enum.each(test_key_range, fn key ->
+      struct = struct_with_gzip_resp()
+
+      Cachex.put(
+        cache,
+        key,
+        {struct.response, Belfrage.Timer.now_ms()},
+        ttl: struct.private.fallback_ttl
+      )
+
+      :timer.sleep(1)
+    end)
+
+    # allow time for cache eviction
+    Process.sleep(20)
   end
 end
