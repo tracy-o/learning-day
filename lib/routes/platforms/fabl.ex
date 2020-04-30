c726524d897f8c92381256bf154eadaf299c6047
James Bowers <james@bowers.dev>
2020-04-30 16:23:16 +0100

    DevelopmentRequest transformer not on live

--- lib/routes/platforms/fabl.ex
+++ lib/routes/platforms/fabl.ex
@@ -1,13 +1,21 @@
 defmodule Routes.Platforms.Fabl do
-  def specs(_production_env) do
+  def specs(production_env) do
     %{
       origin: Application.get_env(:belfrage, :fabl_endpoint),
       owner: "D&EMorphCoreEngineering@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/WebCore/FABL+Run+Book",
-      pipeline: ["HTTPredirect", "DevelopmentRequests", "CircuitBreaker"],
+      pipeline: pipeline(production_env),
       resp_pipeline: [],
       query_params_allowlist: "*",
       circuit_breaker_error_threshold: 100
     }
+  end
+
+  defp pipeline("live") do
+    ["HTTPredirect", "CircuitBreaker"]
+  end
+
+  defp pipeline(_production_env) do
+    ["DevelopmentRequests"] ++ pipeline("live")
   end
 end
--- lib/routes/platforms/pal.ex
+++ lib/routes/platforms/pal.ex
@@ -1,12 +1,20 @@
 defmodule Routes.Platforms.Pal do
-  def specs(_production_env) do
+  def specs(production_env) do
     %{
       origin: Application.get_env(:belfrage, :pal_endpoint),
       owner: "DENewsFrameworksTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
-      pipeline: ["HTTPredirect", "DevelopmentRequests", "CircuitBreaker"],
+      pipeline: pipeline(production_env),
       resp_pipeline: [],
       circuit_breaker_error_threshold: 100
     }
+  end
+
+  defp pipeline("live") do
+    ["HTTPredirect", "CircuitBreaker"]
+  end
+
+  defp pipeline(_production_env) do
+    ["DevelopmentRequests"] ++ pipeline("live")
   end
 end
