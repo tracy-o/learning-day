38704ae8d37d3ea86e19cb2dccdd16ffb79ddbf5
James Bowers <james@bowers.dev>
2020-05-04 15:54:42 +0100

    html_body is not only for errors

--- lib/belfrage_web/view/internal_response.ex
+++ lib/belfrage_web/view/internal_response.ex
@@ -58,24 +58,24 @@ defmodule BelfrageWeb.View.InternalResponse do
   end
 
   defp put_body(response = %Response{headers: %{"content-type" => @html_content_type}}) do
-    Map.put(response, :body, html_error_body(response.http_status))
+    Map.put(response, :body, html_body(response.http_status))
   end
 
-  defp html_error_body(301), do: ""
-  defp html_error_body(302), do: ""
-  defp html_error_body(404), do: html_error_body(Application.get_env(:belfrage, :not_found_page), 404)
-  defp html_error_body(405), do: html_error_body(Application.get_env(:belfrage, :not_supported_page), 405)
-  defp html_error_body(500), do: html_error_body(Application.get_env(:belfrage, :internal_error_page), 500)
-  defp html_error_body(status_code), do: default_html_error_body(status_code)
+  defp html_body(301), do: ""
+  defp html_body(302), do: ""
+  defp html_body(404), do: html_body(Application.get_env(:belfrage, :not_found_page), 404)
+  defp html_body(405), do: html_body(Application.get_env(:belfrage, :not_supported_page), 405)
+  defp html_body(500), do: html_body(Application.get_env(:belfrage, :internal_error_page), 500)
+  defp html_body(status_code), do: default_html_body(status_code)
 
-  defp html_error_body(path, status) do
+  defp html_body(path, status) do
     case @file_io.read(path) do
       {:ok, body} -> body <> "<!-- Belfrage -->"
-      {:error, _} -> default_html_error_body(status)
+      {:error, _} -> default_html_body(status)
     end
   end
 
-  defp default_html_error_body(500), do: "<h1>500 Internal Server Error</h1>\n<!-- Belfrage -->"
-  defp default_html_error_body(404), do: "<h1>404 Page Not Found</h1>\n<!-- Belfrage -->"
-  defp default_html_error_body(http_status), do: "<h1>#{http_status}</h1>\n<!-- Belfrage -->"
+  defp default_html_body(500), do: "<h1>500 Internal Server Error</h1>\n<!-- Belfrage -->"
+  defp default_html_body(404), do: "<h1>404 Page Not Found</h1>\n<!-- Belfrage -->"
+  defp default_html_body(http_status), do: "<h1>#{http_status}</h1>\n<!-- Belfrage -->"
 end
