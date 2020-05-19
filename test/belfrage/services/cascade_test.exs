72b6b3e1cbe91029581cd43a0d4a252612caece3
chrishop <christopher.hopkins@bbc.co.uk>
2020-05-19 09:35:45 +0100

    added cascade service tests

---
+++ test/belfrage/services/cascade_test.exs
@@ -0,0 +1,168 @@
+defmodule Belfrage.Service.CascadeTest do
+  alias Belfrage.Clients
+  alias Belfrage.Services.Cascade
+  alias Belfrage.Struct
+
+  use ExUnit.Case
+  use Test.Support.Helper, :mox
+
+  @arn Application.fetch_env!(:belfrage, :webcore_lambda_role_arn)
+
+  @http_struct %Struct{
+    private: %Struct.Private{
+      origin: "https://www.bbc.co.uk"
+    },
+    request: %Struct.Request{
+      method: "GET",
+      path: "/_web_core"
+    }
+  }
+
+  @fabl_struct %Struct{
+    private: %Struct.Private{
+      origin: "https://fabl.test.api.bbci.co.uk"
+    },
+    request: %Struct.Request{
+      method: "GET",
+      path: "/fd/example-module",
+      path_params: %{
+        "name" => "example-module"
+      }
+    }
+  }
+
+  @webcore_struct %Struct{
+    private: %Struct.Private{origin: "arn:aws:lambda:eu-west-1:123456:function:a-lambda-function"},
+    request: %Struct.Request{
+      method: "GET",
+      path: "/_web_core"
+    }
+  }
+
+  def mock_http_with(status_code) do
+    Clients.HTTPMock
+    |> expect(
+      :execute,
+      fn %Belfrage.Clients.HTTP.Request{
+           method: :get,
+           url: "https://www.bbc.co.uk/_web_core"
+         } ->
+        {:ok,
+         %Belfrage.Clients.HTTP.Response{
+           status_code: status_code,
+           body: "from http"
+         }}
+      end
+    )
+  end
+
+  def mock_fabl_with(status_code) do
+    Clients.HTTPMock
+    |> expect(
+      :execute,
+      fn %Belfrage.Clients.HTTP.Request{
+           method: :get,
+           url: "https://fabl.test.api.bbci.co.uk/module/example-module"
+         },
+         :fabl ->
+        {
+          :ok,
+          %Belfrage.Clients.HTTP.Response{
+            status_code: status_code,
+            body: "from fabl"
+          }
+        }
+      end
+    )
+  end
+
+  def mock_webcore_with(status_code) do
+    expect(Clients.LambdaMock, :call, fn _role_arn = @arn,
+                                         _lambda_func = "arn:aws:lambda:eu-west-1:123456:function:a-lambda-function",
+                                         _payload = %{
+                                           httpMethod: "GET",
+                                           path: "/_web_core"
+                                         },
+                                         _opts ->
+      {:ok,
+       %{
+         "headers" => %{},
+         "statusCode" => status_code,
+         "body" => "<h1>Hello from the Lambda!</h1>"
+       }}
+    end)
+  end
+
+  describe "Cascade service with a single request" do
+    test "gets a response from http service" do
+      mock_http_with(200)
+
+      assert %Struct{
+               response: %Struct.Response{
+                 http_status: 200,
+                 body: "from http"
+               }
+             } = Cascade.dispatch([@http_struct])
+    end
+
+    test "gets a response from fabl service" do
+      mock_fabl_with(200)
+
+      assert %Struct{
+               response: %Struct.Response{
+                 http_status: 200,
+                 body: "from fabl"
+               }
+             } = Cascade.dispatch([@fabl_struct])
+    end
+
+    test "gets a response from webcore service" do
+      mock_webcore_with(200)
+
+      assert %Struct{
+               response: %Struct.Response{
+                 http_status: 200,
+                 body: "<h1>Hello from the Lambda!</h1>"
+               }
+             } = Cascade.dispatch([@webcore_struct])
+    end
+  end
+
+  describe "Cascade Service with multiple requests" do
+    test "returns the first response, as it was successful" do
+      mock_http_with(200)
+
+      assert %Struct{
+               response: %Struct.Response{
+                 http_status: 200,
+                 body: "from http"
+               }
+             } = Cascade.dispatch([@http_struct, @fabl_struct])
+    end
+
+    test "returns the second response, as the first one returned a 404" do
+      mock_webcore_with(404)
+      mock_fabl_with(200)
+
+      assert %Struct{
+               response: %Struct.Response{
+                 http_status: 200,
+                 body: "from fabl"
+               }
+             } = Cascade.dispatch([@webcore_struct, @fabl_struct])
+    end
+
+    test "returns a 404, as all of the responses returned a 404" do
+      mock_http_with(404)
+      mock_fabl_with(404)
+      mock_webcore_with(404)
+
+      assert %Struct{
+               response: %Struct.Response{
+                 http_status: 404,
+                 body: ""
+               }
+             } = Cascade.dispatch([@http_struct, @fabl_struct, @webcore_struct])
+    end
+  end
+end
