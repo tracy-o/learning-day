94143815e90be417587c556344374ed8f18e864c
Alasdair Stalker <astalker@users.noreply.github.com>
2020-05-26 12:01:50 +0100

    feat(smoke-tests): Add live urls and update Jenkinsfile to pass args through (#404)
    * Refactor with @boonious

--- config/smoke_test.exs
+++ config/smoke_test.exs
@@ -6,13 +6,22 @@ config :belfrage,
   routefile: Routes.RoutefileMock
 
 config :smoke,
-  opts: %{
+  header: %{
+    header_belfrage: %{:id => "bid", :value => "www"},
+    header_bruce: %{:id => "bid", :value => "bruce"},
+    header_cedric: %{:id => "bid", :value => "cedric"}
+  },
+  test: %{
     endpoint_belfrage: "https://www.belfrage.test.api.bbc.co.uk",
     endpoint_gtm: "https://www.test.bbc.co.uk",
     endpoint_gtm_com: "https://www.test.bbc.com",
     endpoint_bruce: "https://bruce.belfrage.test.api.bbc.co.uk",
-    endpoint_cedric: "https://cedric.belfrage.test.api.bbc.co.uk",
-    header_belfrage: %{:id => "bid", :value => "www"},
-    header_bruce: %{:id => "bid", :value => "bruce"},
-    header_cedric: %{:id => "bid", :value => "cedric"}
+    endpoint_cedric: "https://cedric.belfrage.test.api.bbc.co.uk"
+  },
+  live: %{
+    endpoint_belfrage: "https://www.belfrage.api.bbc.co.uk",
+    endpoint_gtm: "https://www.bbc.co.uk",
+    endpoint_gtm_com: "https://www.bbc.com",
+    endpoint_bruce: "https://bruce.belfrage.api.bbc.co.uk",
+    endpoint_cedric: "https://cedric.belfrage.api.bbc.co.uk"
   }
--- test/smoke/smoke_test.exs
+++ test/smoke/smoke_test.exs
@@ -2,15 +2,16 @@ defmodule BelfrageSmokeTest do
   use ExUnit.Case
   alias Test.Support.Helper
 
-  @opts Application.get_env(:smoke, :opts)
   @moduletag :smoke_test
 
   setup do
-    @opts
+    smoke_env = System.get_env("SMOKE_ENV") || "test"
+    Map.merge(Application.get_env(:smoke, String.to_atom(smoke_env)), Application.get_env(:smoke, :header))
   end
 
-  describe "topics" do
-    test "Belfrage /sport/alpine-skiing", %{endpoint_belfrage: endpoint, header_belfrage: header_id} do
+  describe "belfrage" do
+    @describetag stack: "belfrage"
+    test "/sport/alpine-skiing", %{endpoint_belfrage: endpoint, header_belfrage: header_id} do
       resp = Helper.get_route(endpoint, "/sport/alpine-skiing")
 
       assert resp.status_code == 200
@@ -18,48 +19,42 @@ defmodule BelfrageSmokeTest do
       assert resp.body =~ "The home of Alpine Skiing on BBC Sport online"
     end
 
-    test "Belfrage /sport/snowboarding", %{endpoint_belfrage: endpoint, header_belfrage: header_id} do
-      resp = Helper.get_route(endpoint, "/sport/snowboarding")
-
-      assert resp.status_code == 200
-      assert Helper.header_item_exists(resp.headers, header_id)
-      assert resp.body =~ "The home of Snowboarding on BBC Sport online"
-    end
-
-    test "Bruce /sport/alpine-skiing", %{endpoint_bruce: endpoint, header_bruce: header_id} do
-      resp = Helper.get_route(endpoint, "/sport/alpine-skiing")
-
-      assert resp.status_code == 200
-      assert Helper.header_item_exists(resp.headers, header_id)
-      assert resp.body =~ "The home of Alpine Skiing on BBC Sport online"
-    end
-
-    test "Cedric /sport/alpine-skiing", %{endpoint_cedric: endpoint, header_cedric: header_id} do
-      resp = Helper.get_route(endpoint, "/sport/alpine-skiing")
-
-      assert resp.status_code == 200
-      assert Helper.header_item_exists(resp.headers, header_id)
-      assert resp.body =~ "The home of Alpine Skiing on BBC Sport online"
-    end
-  end
-
-  describe "sfv" do
-    test "/sport/videos/48521428", %{endpoint_gtm: endpoint, header_belfrage: header_id} do
+    test "GTM /sport/videos/48521428", %{endpoint_gtm: endpoint, header_belfrage: header_id} do
       resp = Helper.get_route(endpoint, "/sport/videos/48521428")
 
       assert resp.status_code == 200
       assert Helper.header_item_exists(resp.headers, header_id)
       assert resp.body =~ "Bowlers star as Sri Lanka edge Afghanistan"
     end
-  end
 
-  describe "ws" do
-    test "/tajik", %{endpoint_gtm_com: endpoint, header_belfrage: header_id} do
+    test "GTM .com /tajik", %{endpoint_gtm_com: endpoint, header_belfrage: header_id} do
       resp = Helper.get_route(endpoint, "/tajik")
 
       assert resp.status_code == 200
       assert Helper.header_item_exists(resp.headers, header_id)
       assert resp.body =~ "Сафҳаи аслӣ - BBC Tajik/Persian"
+    end
+  end
+
+  describe "bruce-belfrage" do
+    @describetag stack: "bruce-belfrage"
+    test "/sport/alpine-skiing", %{endpoint_bruce: endpoint, header_bruce: header_id} do
+      resp = Helper.get_route(endpoint, "/sport/alpine-skiing")
+
+      assert resp.status_code == 200
+      assert Helper.header_item_exists(resp.headers, header_id)
+      assert resp.body =~ "The home of Alpine Skiing on BBC Sport online"
+    end
+  end
+
+  describe "cedric-belfrage" do
+    @describetag stack: "cedric-belfrage"
+    test "/sport/alpine-skiing", %{endpoint_cedric: endpoint, header_cedric: header_id} do
+      resp = Helper.get_route(endpoint, "/sport/alpine-skiing")
+
+      assert resp.status_code == 200
+      assert Helper.header_item_exists(resp.headers, header_id)
+      assert resp.body =~ "The home of Alpine Skiing on BBC Sport online"
     end
   end
 end
