be4a247366d3ed45acd9690c7d0dc2638403c6ee
Boon Low <boon.low@bbc.co.uk>
2020-05-27 12:05:51 +0100

    update Webcore tests to test different on test/live as per `only_on` setting

--- test/smoke/webcore/cedric_test.exs
+++ test/smoke/webcore/cedric_test.exs
@@ -1,9 +1,11 @@
 defmodule Belfrage.SmokeTest.Webcore.Cedric do
   use ExUnit.Case, async: true
   alias Test.Support.Helper
+  alias Belfrage.RouteSpec
 
   @platform Webcore
   @stack "cedric-belfrage"
+  @smoke_env System.get_env("SMOKE_ENV") || "test"
 
   @moduletag :smoke_test
   @moduletag platform: @platform
@@ -12,27 +14,37 @@ defmodule Belfrage.SmokeTest.Webcore.Cedric do
   @ignore_specs ["Hcraes", "ContainerData", "PageComposition", "PresTest"]
 
   setup do
-    smoke_env = System.get_env("SMOKE_ENV") || "test"
-    Map.merge(Application.get_env(:smoke, String.to_atom(smoke_env)), Application.get_env(:smoke, :header))
+    Map.merge(
+      Application.get_env(:smoke, String.to_atom(@smoke_env)),
+      Application.get_env(:smoke, :header)
+    )
   end
 
-  Routes.Routefile.routes()
-  |> Enum.filter(&(Belfrage.RouteSpec.specs_for(elem(&1, 1))[:platform] == @platform))
-  |> Enum.each(fn {route_matcher, loop_id, examples} ->
+  Routes.Routefile.routes_with_env()
+  |> Enum.filter(fn {_, %{using: loop_id}} -> RouteSpec.specs_for(loop_id)[:platform] == @platform end)
+  |> Enum.each(fn {route_matcher, %{using: loop_id, examples: examples, only_on: env}} ->
     describe "#{@stack} route: #{route_matcher}," do
       @describetag spec: loop_id
 
       for example <- examples, loop_id not in @ignore_specs do
         @example example
+        @only_on env
 
         @tag route: route_matcher
         test "spec: #{loop_id}, path: #{example}", %{endpoint_cedric: endpoint, header_cedric: header_id} do
           resp = Helper.get_route(endpoint, @example)
 
-          assert resp.status_code == 200
-          assert not is_nil(resp.body) and String.length(resp.body) > 32
-          assert Helper.header_item_exists(resp.headers, header_id)
-          refute Helper.header_item_exists(resp.headers, %{id: "bfa", value: "1"})
+          cond do
+            @smoke_env == "live" and @only_on == "test" ->
+              assert resp.status_code == 404
+              assert Helper.header_item_exists(resp.headers, header_id)
+
+            true ->
+              assert resp.status_code == 200
+              assert not is_nil(resp.body) and String.length(resp.body) > 32
+              assert Helper.header_item_exists(resp.headers, header_id)
+              refute Helper.header_item_exists(resp.headers, %{id: "bfa", value: "1"})
+          end
         end
       end
     end
