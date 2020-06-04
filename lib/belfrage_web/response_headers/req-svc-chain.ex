2ab5019c17b2d5971f1c5a43b7084153d75d746a
Alasdair Stalker <astalker@users.noreply.github.com>
2020-06-04 16:07:20 +0100

    feat(req-svc-chain): Pass request value when origin response value is nil (#435)

--- lib/belfrage_web/response_headers/req-svc-chain.ex
+++ lib/belfrage_web/response_headers/req-svc-chain.ex
@@ -7,10 +7,12 @@ defmodule BelfrageWeb.ResponseHeaders.ReqSvcChain do
 
   @impl ResponseHeaders
   def add_header(conn, struct) do
-    put_resp_header(conn, "req-svc-chain", header(Map.get(struct.response.headers, "req-svc-chain")))
+    put_resp_header(conn, "req-svc-chain", header(Map.get(struct.response.headers, "req-svc-chain"), struct.request.req_svc_chain))
   end
 
-  defp header(_existing = nil), do: "BELFRAGE"
+  defp header(_existing = nil, _req_svc_chain = nil), do: "BELFRAGE"
 
-  defp header(existing), do: existing
+  defp header(_existing = nil, req_svc_chain), do: req_svc_chain
+
+  defp header(existing, _), do: existing
 end
