2e84beea17c5c59b64cb733d5a3c6548c5aa5e5b
Ettore Berardi <ettore.berardi@bbc.co.uk>
2020-06-05 15:54:54 +0100

    first iteration - frogs and squids

--- lib/joe_formatter.ex
+++ lib/joe_formatter.ex
@@ -1,6 +1,9 @@
 defmodule JoeFormatter do
   use GenServer
 
+  import ExUnit.Formatter,
+  only: [format_time: 2, format_filters: 2, format_test_failure: 5, format_test_all_failure: 5]
+
   def init(opts) do
     ExUnit.CLIFormatter.init(opts)
   end
@@ -9,7 +12,36 @@ defmodule JoeFormatter do
     {:noreply, state}
   end
 
+  def handle_cast(arg = {:test_finished, test = %ExUnit.Test{state: {:failed, failures}}}, state) do
+    #IO.inspect(test)
+
+    counter = state.failure_counter + 1
+    #format_test_failure(test, failures, counter, width, formatter) 
+    IO.puts  IO.ANSI.red() <> "[🦑] #{test.name}" <>  IO.ANSI.reset()
+
+    {:noreply, %{state|failure_counter: counter}}
+  end
+
+  def handle_cast(arg = {:test_finished, test}, state) do
+    #IO.inspect(test)
+
+    IO.puts  IO.ANSI.green() <> "[🐸] #{test.name}" <>  IO.ANSI.reset()
+
+    #%{test_counter: {test: 10}} 
+    {:noreply, %{state|test_counter: update_test_counter(state.test_counter, test)}}
+  end
+
+  def handle_cast({:suite_finished, _run_us , _load_us}, state) do
+    #IO.inspect state
+    IO.puts "Joes has finished"
+    {:noreply, state}
+  end
+
   def handle_cast(anything, state) do
     {:noreply, state}
+  end
+
+  defp update_test_counter(test_counter, %{tags: %{test_type: test_type}}) do
+    Map.update(test_counter, test_type, 1, &(&1 + 1))
   end
 end
