689f13a880e018b5a18566ea641d45e5b421eee1
Boon Low <boon.low@bbc.co.uk>
2020-06-09 15:06:54 +0100

    rename test formatter GenServer state as config (same as ExUnit.CLIFormatter)

--- lib/joe_formatter.ex
+++ lib/joe_formatter.ex
@@ -5,49 +5,48 @@ defmodule JoeFormatter do
   only: [format_time: 2, format_filters: 2, format_test_failure: 5, format_test_all_failure: 5]
 
   def init(opts) do
-    {:ok, state} = ExUnit.CLIFormatter.init(opts)
-    {:ok, Map.put(state, :failure_output, [])}
+    {:ok, config} = ExUnit.CLIFormatter.init(opts)
+    {:ok, Map.put(config, :failure_output, [])}
   end
 
-  def handle_cast(arg = {:test_finished, test = %ExUnit.Test{state: {:excluded, _reason}}}, state) do
-    {:noreply, state}
+  def handle_cast(arg = {:test_finished, test = %ExUnit.Test{state: {:excluded, _reason}}}, config) do
+    {:noreply, config}
   end
 
-  def handle_cast(arg = {:test_finished, test = %ExUnit.Test{state: {:failed, failures}}}, state) do
+  def handle_cast(arg = {:test_finished, test = %ExUnit.Test{state: {:failed, failures}}}, config) do
     #IO.inspect(test)
 
-    counter = state.failure_counter + 1
+    counter = config.failure_counter + 1
     #format_test_failure(test, failures, counter, width, formatter) 
     line = IO.ANSI.red() <> "[🦑] #{test.name}" <>  IO.ANSI.reset()
     IO.puts line
 
-    {:noreply, %{state|failure_counter: counter, failure_output: [line | state.failure_output]}}
+    {:noreply, %{config|failure_counter: counter, failure_output: [line | config.failure_output]}}
   end
 
-  def handle_cast(arg = {:test_finished, test}, state) do
+  def handle_cast(arg = {:test_finished, test}, config) do
     #IO.inspect(test)
 
     IO.puts  IO.ANSI.green() <> "[🐸] #{test.name}" <>  IO.ANSI.reset()
 
     #%{test_counter: {test: 10}} 
-    {:noreply, %{state|test_counter: update_test_counter(state.test_counter, test)}}
+    {:noreply, %{config|test_counter: update_test_counter(config.test_counter, test)}}
   end
 
-  def handle_cast({:suite_finished, _run_us , _load_us}, state) do
-    #IO.inspect state
+  def handle_cast({:suite_finished, _run_us , _load_us}, config) do
     IO.puts "Joes has finished"
-    if (state.failure_output == []) do
+    if (config.failure_output == []) do
       IO.puts "YOU PASSED"
     else
       IO.puts "YOU FAILED"
-      IO.puts Enum.join(state.failure_output, "\n")
+      IO.puts Enum.join(config.failure_output, "\n")
     end
 
 
-    {:noreply, state}
+    {:noreply, config}
   end
 
-  defdelegate handle_cast(event, state), to: ExUnit.CLIFormatter
+  defdelegate handle_cast(event, config), to: ExUnit.CLIFormatter
 
   defp update_test_counter(test_counter, %{tags: %{test_type: test_type}}) do
     Map.update(test_counter, test_type, 1, &(&1 + 1))
