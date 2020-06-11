3ac0fd33f25a55eee68b6b80396b0fe4e84d7ea7
Lee Driscoll <lee.driscoll@bbc.co.uk>
2020-06-11 08:02:07 +0100

    add missing commar

--- lib/routes/specs/cymrufyw_article_page.ex
+++ lib/routes/specs/cymrufyw_article_page.ex
@@ -3,7 +3,7 @@ defmodule Routes.Specs.CymrufywArticlePage do
     %{
       owner: "DENewsCardiff@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/NEWSCPSSTOR/News+CPS+Stories+Run+Book",
-      platform: Webcore
+      platform: Webcore,
       default_language: "cy"
     }
   end
