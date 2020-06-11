5bfec31ac15c5eede2a06cf7a9f2ea35d23f92e2
Boon Low <boon.low@bbc.co.uk>
2020-06-11 09:28:12 +0100

    test formatter: format and display failure/stacktrace at tests completion

--- lib/joe_formatter.ex
+++ lib/joe_formatter.ex
@@ -2,10 +2,11 @@ defmodule JoeFormatter do
   @moduledoc false
   use GenServer
   alias ExUnit.Test
+  import ExUnit.Formatter, only: [format_test_failure: 5]
 
   def init(opts) do
     {:ok, config} = ExUnit.CLIFormatter.init(opts)
-    {:ok, Map.put(config, :failure_output, [])}
+    {:ok, Map.put(config, :failed_tests, [])}
   end
 
   def handle_cast({:test_started, _} = event, %{trace: true} = config) do
@@ -19,8 +20,7 @@ defmodule JoeFormatter do
   def handle_cast({:test_finished, %Test{state: {:invalid, _}}}, %{trace: false} = config), do: {:noreply, config}
 
   def handle_cast({:test_finished, %Test{state: {:failed, _}} = test}, %{trace: false} = config) do
-    line = "[🦑] #{test.name}"
-    print(line, :failed, config.colors[:enabled])
+    print("[🦑] #{test.name}", :failed, config.colors[:enabled])
 
     {
       :noreply,
@@ -28,7 +28,7 @@ defmodule JoeFormatter do
         config
         | test_counter: update_test_counter(config.test_counter, test),
           failure_counter: config.failure_counter + 1,
-          failure_output: [line | config.failure_output]
+          failed_tests: [test | config.failed_tests]
       }
     }
   end
@@ -39,12 +39,9 @@ defmodule JoeFormatter do
   end
 
   def handle_cast({:suite_finished, _run_us, _load_us} = event, %{trace: false} = config) do
-    if config.failure_output == [] do
-      IO.puts("YOU PASSED")
-    else
-      IO.puts("YOU FAILED")
-      IO.puts(Enum.join(config.failure_output, "\n"))
-    end
+    config.failed_tests
+    |> Enum.with_index()
+    |> Enum.each(&print_failure(&1, config))
 
     ExUnit.CLIFormatter.handle_cast(event, config)
 
@@ -57,8 +54,17 @@ defmodule JoeFormatter do
     Map.update(test_counter, test_type, 1, &(&1 + 1))
   end
 
-  defp print(line, outcome \\ :success, color_enabled? \\ false)
   defp print(line, :success, true), do: IO.puts(IO.ANSI.green() <> line <> IO.ANSI.reset())
   defp print(line, :failed, true), do: IO.puts(IO.ANSI.red() <> line <> IO.ANSI.reset())
   defp print(line, _outcome, false), do: IO.puts(line)
+
+  defp print_failure({%Test{state: {:failed, reason}} = test, index}, config) do
+    IO.puts("")
+    IO.puts(format_test_failure(test, reason, index + 1, config.width, &formatter(&1, &2, config)))
+  end
+
+  # TODO: format failure / stacktrace with color schemes
+  # based on message type :test_info, :location_info,
+  # :diff_enabled?, :error_info, stacktrace_info
+  defp formatter(type, msg, _config), do: msg
 end
