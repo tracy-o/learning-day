018e3409baf9752a3cc2f5dc9fdbd9efd96d2e65
James Bowers <james@bowers.dev>
2020-06-17 09:03:42 +0100

    add platform spec to allow filtering by platform

--- test/smoke/belfrage_smoke_test_case.ex
+++ test/smoke/belfrage_smoke_test_case.ex
@@ -32,6 +32,7 @@ defmodule Belfrage.SmokeTestCase do
 
         describe "#{@matcher_spec.using} #{@route_matcher} against #{@smoke_env} #{@target}" do
           @describetag spec: @matcher_spec.using
+          @describetag platform: Belfrage.RouteSpec.specs_for(@matcher_spec.using, smoke_env).platform
 
           for example <- @matcher_spec.examples do
             @example example
