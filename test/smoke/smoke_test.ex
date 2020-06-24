0f3ba224f4eb52a458b795357fd8f7f6dfb764ae
James Bowers <james@bowers.dev>
2020-06-24 12:58:56 +0100

    more performant test looping

--- test/smoke/smoke_test.ex
+++ test/smoke/smoke_test.ex
@@ -10,22 +10,18 @@ defmodule Belfrage.SmokeTest do
 
   Routes.Routefile.routes()
   |> Enum.filter(fn {_route_matcher, %{using: loop_id}} -> not (loop_id in @ignore_specs) end)
-  |> Enum.sort_by(fn {_route_matcher, %{using: loop_id}} -> loop_id end)
-  |> Enum.chunk_by(fn {_route_matcher, %{using: loop_id}} -> loop_id end)
-  |> Enum.each(fn route_matcher_specs ->
-    for {{route_matcher, matcher_spec}, index} <- Enum.with_index(route_matcher_specs) do
-      @matcher_spec Macro.escape(matcher_spec)
+  |> Enum.each(fn {route_matcher, matcher_spec} ->
+    @matcher_spec Macro.escape(matcher_spec)
 
-      contents =
-        quote do
-          use Belfrage.SmokeTestCase,
-            route_matcher: unquote(route_matcher),
-            matcher_spec: unquote(@matcher_spec),
-            environments: unquote(@environments)
-        end
+    contents =
+      quote do
+        use Belfrage.SmokeTestCase,
+          route_matcher: unquote(route_matcher),
+          matcher_spec: unquote(@matcher_spec),
+          environments: unquote(@environments)
+      end
 
-      module = Module.concat(["Belfrage.SmokeTest", matcher_spec.using, Integer.to_string(index)])
-      Module.create(module, contents, Macro.Env.location(__ENV__))
-    end
+    module = Module.concat(["Belfrage.SmokeTest", matcher_spec.using, UUID.uuid4(:hex)])
+    Module.create(module, contents, Macro.Env.location(__ENV__))
   end)
 end
