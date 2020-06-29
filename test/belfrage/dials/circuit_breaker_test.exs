a50cfdf3d69f549dee19a2bd04109ee463307fe8
Boon Low <boon.low@bbc.co.uk>
2020-06-29 12:54:00 +0100

    Update test/belfrage/dials/circuit_breaker_test.exs
    Co-authored-by: Alasdair Stalker <astalker@users.noreply.github.com>

--- test/belfrage/dials/circuit_breaker_test.exs
+++ test/belfrage/dials/circuit_breaker_test.exs
@@ -28,7 +28,7 @@ defmodule Belfrage.Dials.CircuitBreakerTest do
       assert Dials.CircuitBreaker.state() |> is_boolean()
     end
 
-    test "returns a state corresponds to dials.json" do
+    test "returns a state which corresponds to the dials.json" do
       Belfrage.Helpers.FileIOMock
       |> expect(:read, fn _ -> {:ok, ~s({"circuit_breaker": "false"})} end)
 
