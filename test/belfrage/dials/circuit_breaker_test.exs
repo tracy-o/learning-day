e0de079167004bd102a19d8870f5d1d64f415952
Boon Low <boon.low@bbc.co.uk>
2020-06-30 09:21:51 +0100

    tests for dial defaults, update circuit breaker dial tests

--- test/belfrage/dials/circuit_breaker_test.exs
+++ test/belfrage/dials/circuit_breaker_test.exs
@@ -2,12 +2,7 @@ defmodule Belfrage.Dials.CircuitBreakerTest do
   use ExUnit.Case, async: true
   use Test.Support.Helper, :mox
 
-  import Belfrage.Dials.Defaults
   alias Belfrage.Dials
-
-  @dial "circuit_breaker"
-  @codec Application.get_env(:belfrage, :json_codec)
-  @cosmos_dials File.read!("cosmos/dials.json") |> @codec.decode!()
 
   describe "state/0" do
     setup do
@@ -38,6 +33,10 @@ defmodule Belfrage.Dials.CircuitBreakerTest do
     end
   end
 
+  test "default/0  returns a boolean state" do
+    assert Dials.CircuitBreaker.default() |> is_boolean()
+  end
+
   test "dial correctly handles changed event in which the dial boolean state is flipped" do
     dial_state = Dials.CircuitBreaker.state()
 
@@ -45,18 +44,5 @@ defmodule Belfrage.Dials.CircuitBreakerTest do
     |> GenServer.cast({:dials_changed, %{"circuit_breaker" => "#{!dial_state}"}})
 
     assert Dials.CircuitBreaker.state() == !dial_state
-  end
-
-  test "default/0 transforms and returns the string default state in Cosmos dials.json" do
-    dial = @cosmos_dials |> Enum.find(&(&1["name"] == @dial))
-    assert Dials.CircuitBreaker.default() == transform(dial["default-value"], @dial)
-  end
-
-  test "all dial values in Cosmos dials.json are transformed into booleans" do
-    dial = @cosmos_dials |> Enum.find(&(&1["name"] == @dial))
-
-    for dial <- dial["values"] do
-      assert dial["value"] |> transform(@dial) |> is_boolean()
-    end
   end
 end
