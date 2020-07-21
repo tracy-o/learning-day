9b4b03781870f56e5eb0fee8a667c5d97a20fdc5
chrishop <christopher.hopkins@bbc.co.uk>
2020-07-21 10:59:59 +0100

    use :status to get workers
    rather than :get_avail_workers and :get_all_workers which returns a list of pids

--- lib/belfrage/metrics/pool.ex
+++ lib/belfrage/metrics/pool.ex
@@ -5,6 +5,32 @@ defmodule Belfrage.Metrics.Pool do
     GenServer.start_link(__MODULE__, opts, name: :pool_observer)
   end
 
+  def send() do
+    pools = Supervisor.which_children(MachineGun.Supervisor)
+
+    pools
+    |> active_workers()
+    |> send_metrics("http.pools.active_workers")
+
+    pools
+    |> all_workers()
+    |> send_metrics("http.pools.all_workers")
+  end
+
+  def send_metrics(metrics, metric_name) do
+    Enum.each(metrics, fn metric -> ExMetrics.increment(metric_name, metric) end)
+  end
+
+  def all_workers(pools) do
+    call_pools(pools, :status)
+    |> Enum.map(fn {_, free_workers, _, active_workers} -> free_workers + active_workers end)
+  end
+
+  def active_workers(pools) do
+    call_pools(pools, :status)
+    |> Enum.map(fn {_, _, _, active_workers} -> active_workers end)
+  end
+
   # callbacks
   @impl true
   def init(init) do
@@ -18,37 +44,6 @@ defmodule Belfrage.Metrics.Pool do
 
     loop()
     {:noreply, state}
-  end
-
-  def send() do
-    pools = Supervisor.which_children(MachineGun.Supervisor)
-
-    pools
-    |> active_workers()
-    |> send_metrics("http.pools.active_workers")
-
-    supervisor
-    |> all_workers()
-    |> send_metrics("http.pools.all_workers")
-  end
-
-  def send_metrics(metrics, metric_name) do
-    Enum.each(metrics, fn metric -> ExMetrics.increment(metric_name, metric) end)
-  end
-
-  def all_workers(pools) do
-    call_pools(pools, :get_all_workers)
-    |> Enum.map(&Enum.count/1)
-  end
-
-  def active_workers(pools) do
-    Enum.zip(all_workers(pools), available_workers(pools))
-    |> Enum.map(fn {all, avail} -> all - avail end)
-  end
-
-  defp available_workers(pools) do
-    call_pools(pools, :get_avail_workers)
-    |> Enum.map(&Enum.count/1)
   end
 
   defp loop() do
