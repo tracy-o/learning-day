e329bf7152ca067961a8bec025d24476d22e2fa5
Boon Low <boon.low@bbc.co.uk>
2020-07-30 17:09:53 +0100

    test dial GenServer crash in isolation for all current (+future) dials; remove hardcodes

--- test/belfrage/dials_supervisor_test.exs
+++ test/belfrage/dials_supervisor_test.exs
@@ -6,16 +6,6 @@ defmodule Belfrage.DialsSupervisorTest do
   @dials_supervisor Belfrage.DialsSupervisor
   @dials_poller Belfrage.Dials.Poller
 
-  defp expected_dial_default(dial_name) do
-    Enum.find_value(dial_config(), fn
-      {dial_mod, ^dial_name, default_value} ->
-        apply(dial_mod, :transform, [default_value])
-
-      _other ->
-        nil
-    end)
-  end
-
   test "dials supervisor is alive" do
     assert Process.whereis(@dials_supervisor) |> Process.alive?()
   end
@@ -51,23 +41,45 @@ defmodule Belfrage.DialsSupervisorTest do
     end
   end
 
-  test "when dial crashes, error is logged and dial is restarted with default state" do
-    supervised_dial = Belfrage.Dials.CircuitBreaker
-    pid = Process.whereis(supervised_dial)
-    expected_default_value = expected_dial_default(:circuit_breaker)
+  Enum.each(dial_config(), fn {module, name, default} ->
+    @name name
+    @module module
+    @default default
+    @test_dial :"test_#{to_string(name)}"
 
-    assert capture_log(fn ->
-             GenServer.cast(:circuit_breaker, {:dials_changed, %{"circuit_breaker" => "bar"}})
-             :timer.sleep(100)
-           end) =~ "no function clause matching"
+    describe "when #{@name} dial crashes" do
+      setup do
+        start_supervised({Belfrage.Dial, {@module, @test_dial, @default}})
+        :ok
+      end
 
-    new_pid = GenServer.whereis(:circuit_breaker)
-    refute is_nil(new_pid)
-    refute new_pid == pid, "Dial did not crash, so this test is invalid."
+      test "it should restart" do
+        pid = Process.whereis(@test_dial)
+        Process.exit(pid, :kill)
+        Process.sleep(10)
 
-    assert {Belfrage.Dials.CircuitBreaker, "circuit_breaker", expected_default_value} ==
-             :sys.get_state(:circuit_breaker)
-  end
+        new_pid = Process.whereis(@test_dial)
+
+        refute is_nil(new_pid)
+        refute new_pid == pid, "Dial did not crash, so this test is invalid."
+      end
+
+      test "it should log error" do
+        assert capture_log(fn ->
+                 GenServer.cast(@test_dial, {:dials_changed, %{to_string(@test_dial) => "this crashes the dial"}})
+                 :timer.sleep(50)
+               end) =~ "terminating\n** (FunctionClauseError) no function clause matching"
+      end
+
+      test "it should restart with default state" do
+        Process.whereis(@test_dial) |> Process.exit(:kill)
+        Process.sleep(10)
+
+        expected_default = apply(@module, :transform, [@default])
+        assert expected_default == :sys.get_state(@test_dial) |> elem(2)
+      end
+    end
+  end)
 
   test "notifies dials of new dial data" do
     dials_data = %{
