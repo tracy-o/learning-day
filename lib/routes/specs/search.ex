010d36ece78891ce62b7d676aeddbf7a0acc3f8c
Boon Low <boon.low@bbc.co.uk>
2020-08-10 11:25:29 +0100

    fix .com to .co.uk redirect transformer naming in Search route spec pipeline

--- lib/routes/specs/search.ex
+++ lib/routes/specs/search.ex
@@ -3,7 +3,7 @@ defmodule Routes.Specs.Search do
     %{
       owner: "D+ESearchAndNavigationDev@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/x/xo2KD",
-      pipeline: [ComToUKRedirect],
+      pipeline: ["ComToUKRedirect"],
       platform: Webcore,
       query_params_allowlist: ["q", "page", "scope", "filter"]
     }
