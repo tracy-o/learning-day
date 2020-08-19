87c74ebafae94272a9c7203dbccf9e88285ce21d
VictoriaSheppard <Victoria.Sheppard@bbc.co.uk>
2020-08-19 10:17:09 +0100

    DPTOPICS-555: remove dupes

--- lib/routes/routefile.ex
+++ lib/routes/routefile.ex
@@ -203,13 +203,9 @@ defmodule Routes.Routefile do
 
   handle "/sport/:discipline/:competition", using: "TopicPage", examples: ["/sport/football/champions-league"]
 
-  handle "/sport/topics/:id", using: "TopicPage", examples: ["/sport/topics/cnmey0x98r9t"]
-  
   handle "/sport/topics/:id", using: "TopicPage", examples: ["/sport/topics/cnmey0x98r9t"] do
     return_404 if: !String.match?(id, ~r/^c[\w]{10}t$/)
   end
-
-  handle "/news/topics/:id", using: "TopicPage", examples: ["/news/topics/cyz0z8w0ydwt"]
 
   handle "/news/topics/:id", using: "TopicPage", examples: ["/news/topics/cyz0z8w0ydwt"] do
     return_404 if: !String.match?(id, ~r/^c[\w]{10}t$/)
