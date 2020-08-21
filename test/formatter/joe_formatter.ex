276ad6c9263ea7c64e096f2b3b37084d7c54e7ab
Boon Low <boon.low@bbc.co.uk>
2020-08-21 09:01:03 +0100

    smoke test JoeFormatter: show fallbacks num in results summary

--- test/formatter/joe_formatter.ex
+++ test/formatter/joe_formatter.ex
@@ -3,11 +3,11 @@ defmodule JoeFormatter do
   @moduledoc false
   use GenServer
   alias ExUnit.Test
-  import ExUnit.Formatter, only: [format_test_failure: 5]
+  import ExUnit.Formatter, only: [format_test_failure: 5, format_time: 2]
 
   def init(opts) do
     {:ok, config} = ExUnit.CLIFormatter.init(opts)
-    {:ok, Map.put(config, :failed_tests, [])}
+    {:ok, Map.merge(config, %{failed_tests: [], fallback_count: 0})}
   end
 
   # Do not count excluded/skipped/invalid tests to provide precise total tests executed (compared to ExUnit.CLIFormatter)
@@ -24,7 +24,8 @@ defmodule JoeFormatter do
         config
         | test_counter: update_test_counter(config.test_counter, test),
           failure_counter: config.failure_counter + 1,
-          failed_tests: [test | config.failed_tests]
+          failed_tests: [test | config.failed_tests],
+          fallback_count: if(fallback?(test), do: config.fallback_count + 1, else: config.fallback_count)
       }
     }
   end
@@ -34,15 +35,64 @@ defmodule JoeFormatter do
     {:noreply, %{config | test_counter: update_test_counter(config.test_counter, test)}}
   end
 
-  def handle_cast(event = {:suite_finished, _run_us, _load_us}, config = %{trace: false}) do
+  def handle_cast(_event = {:suite_finished, run_us, load_us}, config = %{trace: false}) do
     config.failed_tests
     |> Enum.with_index()
     |> Enum.each(&print_failure(&1, config))
 
-    ExUnit.CLIFormatter.handle_cast(event, config)
+    IO.write("\n\n")
+    IO.puts(format_time(run_us, load_us))
+
+    report = [
+      "#{config.test_counter[:test]}",
+      " ",
+      "#{pluralize(config.test_counter[:test], "test", "tests")}",
+      ?,,
+      " ",
+      "#{config.failure_counter}",
+      " ",
+      "#{pluralize(config.failure_counter, "failure", "failures")}"
+    ]
+
+    error_digest =
+      cond do
+        config.fallback_count > 0 and config.fallback_count == config.failure_counter ->
+          [
+            " ",
+            ?(,
+            "#{config.fallback_count}",
+            " ",
+            "#{pluralize(config.failure_counter, "fallback", "fallbacks")}",
+            ?)
+          ]
+
+        config.fallback_count > 0 ->
+          [
+            " ",
+            ?(,
+            "#{config.fallback_count}",
+            " ",
+            "#{pluralize(config.failure_counter, "fallback", "fallbacks")}",
+            ?,,
+            " ",
+            "#{config.failure_counter - config.fallback_count}",
+            " ",
+            "#{pluralize(config.failure_counter - config.fallback_count, "other", "others")}",
+            ?)
+          ]
+
+        true ->
+          []
+      end
+
+    IO.puts(IO.iodata_to_binary(report ++ error_digest))
+    IO.puts("\nRandomized with seed #{config.seed}")
 
     {:noreply, config}
   end
+
+  defp pluralize(num, singular, _plural) when num in [0, 1], do: singular
+  defp pluralize(_, _singular, plural), do: plural
 
   defdelegate handle_cast(event, config), to: ExUnit.CLIFormatter
 
@@ -63,4 +113,19 @@ defmodule JoeFormatter do
   # based on message type :test_info, :location_info,
   # :diff_enabled?, :error_info, stacktrace_info
   defp formatter(_type, msg, _config), do: msg
+
+  defp fallback?(%Test{state: {:failed, failures}}) do
+    Enum.reduce_while(failures, false, fn {_kind, reason, _stack}, _state ->
+      case fallback?(reason) do
+        true -> {:halt, true}
+        false -> {:cont, false}
+      end
+    end)
+  end
+
+  defp fallback?(%ExUnit.AssertionError{args: [_, "bfa"]}) do
+    true
+  end
+
+  defp fallback?(_error), do: false
 end
