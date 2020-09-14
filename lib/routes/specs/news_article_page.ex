5be7622fd7bcf36a331640274e7e0e73b2396823
James Bowers <james.bowers@bbc.co.uk>
2020-09-14 15:28:50 +0100

    bump circuit breaker threshold value for news articles

--- lib/routes/specs/news_article_page.ex
+++ lib/routes/specs/news_article_page.ex
@@ -3,7 +3,8 @@ defmodule Routes.Specs.NewsArticlePage do
     %{
       owner: "DENewsCardiff@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/NEWSCPSSTOR/News+CPS+Stories+Run+Book",
-      platform: Webcore
+      platform: Webcore,
+      circuit_breaker_error_threshold: 250
     }
   end
 end
