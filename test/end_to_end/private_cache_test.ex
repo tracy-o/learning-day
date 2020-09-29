42ff4b8da158976cd4ef9740831542c449263fb9
Raymonoir <rw468@kent.ac.uk>
2020-09-29 09:38:59 +0100

    e2e test for private cache-control

---
+++ test/end_to_end/private_cache_test.ex
@@ -0,0 +1,34 @@
+defmodule EndToEndTest.PrivateCacheTest do
+  use ExUnit.Case
+  use Plug.Test
+  alias BelfrageWeb.Router
+  use Test.Support.Helper, :mox
+
+  @moduletag :end_to_end
+
+  @lambda_response %{
+    "headers" => %{
+      "cache-control" => "private, max-age=60"
+    },
+    "statusCode" => 200,
+    "body" => "<h1>Hello from the Lambda!</h1>"
+  }
+
+  setup do
+    :ets.delete_all_objects(:cache)
+    Belfrage.LoopsSupervisor.kill_all()
+  end
+
+  test "cache header is private" do
+    Belfrage.Clients.LambdaMock
+    |> expect(:call, fn _role_arn, _function, _payload, _opts ->
+      {:ok, @lambda_response}
+    end)
+
+    conn = conn(:get, "/200-ok-response")
+    conn = Router.call(conn, [])
+
+    assert {200, headers, _body} = sent_resp(conn)
+    assert {"cache-control", "private, stale-while-revalidate=30, max-age=60"} in headers
+  end
+end
