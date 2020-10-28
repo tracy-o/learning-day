c62c640e5a3182196bf1b2e3ed668d7ac2b00ad4
Sam French <Sam.French@bbc.co.uk>
2020-10-28 11:34:14 +0000

    Check token type is JWT

--- lib/belfrage/authentication/verify_hook.ex
+++ lib/belfrage/authentication/verify_hook.ex
@@ -1,25 +1,24 @@
 defmodule Belfrage.Authentication.VerifyHook do
   use Joken.Hooks
 
-  alias Belfrage.Authentication.Jwk
-
   @impl true
   def before_verify(_options, {jwt, %Joken.Signer{} = _signer}) do
-    with {:ok, %{"kid" => kid, "alg" => alg}} <- Joken.peek_header(jwt),
-         {:ok, algorithm, key} <- fetch_key(kid, alg) do
+    with {:ok, header} <- Joken.peek_header(jwt),
+         {:ok, algorithm, key} <- fetch_key(header) do
       {:cont, {jwt, Joken.Signer.create(algorithm, key)}}
     else
-      error -> {:halt, {:error, :no_signer}}
+      :error -> {:halt, {:error, :no_signer}}
     end
   end
 
-  defp fetch_key(kid, alg) do
+  defp fetch_key(%{"kid" => kid, "alg" => alg, "typ" => "JWT"}) do
     keys = Belfrage.Authentication.Jwk.get_keys()
-    # todo: check typ is JWT
     key = get_matching_key(keys, kid, alg)
 
     {:ok, alg, key}
   end
+
+  defp fetch_key(_header), do: {:error, nil}
 
   defp get_matching_key(%{"keys" => keys}, kid, alg) do
     Enum.find(keys, fn key -> key["kid"] == kid && key["alg"] == alg end)
