f15b803d11b513345f90dd84d3d41d8a8b209b3d
Joseph Pack <josephapack@hotmail.co.uk>
2020-11-13 10:08:09 +0000

    Correct Transformer name in the NewsTopic routespec pipeline

--- lib/routes/specs/news_topics.ex
+++ lib/routes/specs/news_topics.ex
@@ -4,7 +4,7 @@ defmodule Routes.Specs.NewsTopics do
       owner: "D&EKLDevelopmentOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Topics+Runbook",
       platform: Mozart,
-      pipeline: ["HTTPredirect", "TrailingSlashRedirector", "NewsTopicsOriginDiscriminator", "LambdaOriginAlias", "CircuitBreaker", "Language"],
+      pipeline: ["HTTPredirect", "TrailingSlashRedirector", "NewsTopicsPlatformDiscriminator", "LambdaOriginAlias", "CircuitBreaker", "Language"],
       query_params_allowlist: ["page"]
     }
   end
