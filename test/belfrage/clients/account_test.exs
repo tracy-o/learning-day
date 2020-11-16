e05d041285b72c1b8a95e0e6ea9f66e0390d0bb2
Boon Low <boon.low@bbc.co.uk>
2020-11-16 12:53:30 +0000

    fix error logging bug in Jwk Account client

--- test/belfrage/clients/account_test.exs
+++ test/belfrage/clients/account_test.exs
@@ -2,9 +2,13 @@ defmodule Belfrage.Client.AccountTest do
   use ExUnit.Case, async: true
   use Test.Support.Helper, :mox
 
+  import ExUnit.CaptureLog
+
   alias Belfrage.Clients
   alias Belfrage.Clients.Account
 
+  @authentication Application.get_env(:belfrage, :authentication)
+  @error_response {:error, %Clients.HTTP.Error{reason: :timeout}}
   @ok_response {
     :ok,
     %Clients.HTTP.Response{
@@ -12,8 +16,6 @@ defmodule Belfrage.Client.AccountTest do
       body: Jason.encode!(%{keys: Fixtures.AuthToken.keys()})
     }
   }
-
-  @authentication Application.get_env(:belfrage, :authentication)
 
   test "get_jwk_keys/0 returns the keys from the account api" do
     expected_request = %Belfrage.Clients.HTTP.Request{
@@ -28,5 +30,15 @@ defmodule Belfrage.Client.AccountTest do
     |> expect(:execute, fn ^expected_request -> @ok_response end)
 
     assert Belfrage.Clients.AccountStub.get_jwk_keys() == Account.get_jwk_keys()
+  end
+
+  test "logs error response" do
+    Clients.HTTPMock |> expect(:execute, fn _ -> @error_response end)
+    assert capture_log(fn -> Account.get_jwk_keys() end) =~ "Error received from the JWK API: timeout"
+  end
+
+  test "logs unknown error response" do
+    Clients.HTTPMock |> expect(:execute, fn _ -> {:error, nil} end)
+    assert capture_log(fn -> Account.get_jwk_keys() end) =~ "Unknown error received from the JWK API"
   end
 end
