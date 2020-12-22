752523a6423097e0ef681cb9337c741becc94241
James Bowers <james.bowers@bbc.co.uk>
2020-12-22 11:23:52 +0000

    add comment to explain background processes

--- lib/belfrage/supervisor.ex
+++ lib/belfrage/supervisor.ex
@@ -31,6 +31,41 @@ defmodule Belfrage.Supervisor do
     ]
   end
 
+  @doc """
+  background_children/1
+
+  Processes which demand work from themselves, are regarded
+  as "background" processes. For example these processes might
+  use `Process.send_after(self(), :refresh, 5_000)`.
+
+  These processes will not be started with the application
+  when running the tests. Instead, they will need to be started
+  independently in the test setup. For example:
+  `start_supervised!(Poller)`
+  or
+  `start_supervised!({Belfrage.DialsSupervisor, name: :test_dials_supervisor})`
+  https://hexdocs.pm/ex_unit/ExUnit.Callbacks.html#start_supervised!/2
+
+  For more help, see the testing best practices docs
+  https://github.com/bbc/belfrage/wiki/Testing-best-practices
+
+  Belfrage.DialsSupervisor
+  - Starts `Belfrage.Dials.Poller` process
+    which periodically checks the dials file for dial changes.
+
+  Belfrage.Credentials.Refresh
+  - Periodically refreshes AWS tokens.
+
+  Belfrage.Authentication.Jwk
+  - Periodically refreshes public certificates to verify
+    session tokens.
+
+  Belfrage.Authentication.Flagpole
+  - Periodically checks personalisation flagpole.
+
+  Belfrage.Cache.Cleaner
+  - Periodically checks memory limit, and removes cached items.
+  """
   defp background_children(:test), do: []
 
   defp background_children(_env) do
