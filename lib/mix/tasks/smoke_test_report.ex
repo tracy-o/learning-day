771eee3e53e686250973dbccf59c0e821c123142
James Bowers <james.bowers@bbc.co.uk>
2021-01-13 13:11:39 +0000

    build http requests that will send test failure reports to slack

--- lib/mix/tasks/smoke_test_report.ex
+++ lib/mix/tasks/smoke_test_report.ex
@@ -3,20 +3,27 @@ defmodule Mix.Tasks.ReportSmokeTestResults do
 
   @shortdoc "Reports smoke test failures to route owner teams."
 
+  @http_client Application.get_env(:belfrage, :http_client, Clients.HTTP)
+
+  @slack_auth_token_env_var_name "SLACK_AUTH_TOKEN"
+
   @impl Mix.Task
   def run([path_to_raw_input]) do
     test_results = :erlang.binary_to_term(File.read!(path_to_raw_input))
 
+    slack_auth_token = System.fetch_env!(@slack_auth_token_env_var_name)
+
     test_results
     |> failures_per_routespec()
     |> format_failure_messages()
+    |> broadcast_results_to_teams(slack_auth_token)
+    |> do_send()
   end
 
-  def run(_), do: IO.puts "Please provide one argument, which is the path to the raw output file."
+  def run(_), do: IO.puts("Please provide one argument, which is the path to the raw output file.")
 
   def failures_per_routespec(_test_results = %{failed_tests: failed_tests}) do
     Enum.reduce(failed_tests, %{}, fn failure, acc ->
-
       route_spec = get_in(failure, [Access.key(:tags), Access.key(:spec)])
 
       update_in(acc, [route_spec], fn
@@ -42,8 +49,51 @@ defmodule Mix.Tasks.ReportSmokeTestResults do
 #{assertion}
 Left: #{assertion_error.left}
 Right: #{assertion_error.right}
-#{assertion_error.message}\n\n)
-
+#{assertion_error.message})
     end)
+  end
+
+  def broadcast_results_to_teams(formatted_routespec_failures, slack_auth_token) do
+    Enum.map(formatted_routespec_failures, &send_slack_message(&1, slack_auth_token))
+  end
+
+  defp send_slack_message({routespec, failure_messages}, slack_auth_token) do
+    # specs = Belfrage.RouteSpec.specs_for(routespec)
+    specs = %{
+      smoke_test_failure_channel: "team-belfrage"
+    }
+
+    msg = Enum.join(failure_messages, "\n\n")
+
+    %Belfrage.Clients.HTTP.Request{
+      method: :post,
+      url: "https://slack.com/api/chat.postMessage",
+      headers: %{
+        "Content-Type" => "application/json",
+        "Authorization" => "Bearer #{slack_auth_token}"
+      },
+      payload: %{
+        text: "Smoke test failures",
+        channel: specs.smoke_test_failure_channel,
+        attachments: [
+          %{
+            blocks: [
+              %{
+                type: "section",
+                text: %{
+                  type: "mrkdwn",
+                  text: msg
+                },
+                block_id: "smoke_test_failure_output"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  end
+
+  defp do_send(http_requests) do
+    Enum.each(http_requests, &@http_client.execute/1)
   end
 end
--- test/mix/tasks/report_smoke_test_results_test.exs
+++ test/mix/tasks/report_smoke_test_results_test.exs
@@ -25,23 +25,45 @@ defmodule Mix.Tasks.ReportSmokeTestResultsTest do
 
     expected = %{
       "SportHomePage" => [
-        "test SportHomePage /sport against test belfrage /sport\nassert(response.status_code() == expected_status_code)\nLeft: 404\nRight: 200\nAssertion with == failed\n\n",
-        "test SportHomePage /sport against test cedric-belfrage /sport\nassert(response.status_code() == expected_status_code)\nLeft: 404\nRight: 200\nAssertion with == failed\n\n",
-        "test SportHomePage /sport against test bruce-belfrage /sport\nassert(response.status_code() == expected_status_code)\nLeft: 404\nRight: 200\nAssertion with == failed\n\n"
+        "test SportHomePage /sport against test belfrage /sport\nassert(response.status_code() == expected_status_code)\nLeft: 404\nRight: 200\nAssertion with == failed",
+        "test SportHomePage /sport against test cedric-belfrage /sport\nassert(response.status_code() == expected_status_code)\nLeft: 404\nRight: 200\nAssertion with == failed",
+        "test SportHomePage /sport against test bruce-belfrage /sport\nassert(response.status_code() == expected_status_code)\nLeft: 404\nRight: 200\nAssertion with == failed"
       ]
     }
 
     assert expected == ReportSmokeTestResults.format_failure_messages(failures_per_routespec)
   end
-end
 
-Macro.to_string(
-  {:assert, [],
-   [
-     {:==, [],
-      [
-        {{:., [], [{:response, [], nil}, :status_code]}, [], []},
-        {:expected_status_code, [], nil}
-      ]}
-   ]}
-)
+  test "broadcast_results_to_teams/2", %{output_with_failures: output} do
+    slack_auth_token = "foobar"
+
+    formatted_messages =
+      output
+      |> ReportSmokeTestResults.failures_per_routespec()
+      |> ReportSmokeTestResults.format_failure_messages()
+
+    assert [
+             %Belfrage.Clients.HTTP.Request{
+               headers: %{"Authorization" => "Bearer foobar", "Content-Type" => "application/json"},
+               method: :post,
+               payload: %{
+                 channel: "team-belfrage",
+                 text: "Smoke test failures",
+                 attachments: [
+                   %{
+                     blocks: [
+                       %{
+                         block_id: "smoke_test_failure_output",
+                         text: %{type: "mrkdwn", text: Enum.join(formatted_messages["SportHomePage"], "\n\n")},
+                         type: "section"
+                       }
+                     ]
+                   }
+                 ]
+               },
+               timeout: 6000,
+               url: "https://slack.com/api/chat.postMessage"
+             }
+           ] == ReportSmokeTestResults.broadcast_results_to_teams(formatted_messages, slack_auth_token)
+  end
+end
