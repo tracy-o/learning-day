3eca3e335298675fccad66748974072b2bc5940e
Alasdair Stalker <alasdair.stalker@bbc.co.uk>
2021-01-18 14:58:08 +0000

    Some refactoring

--- jwk-fetcher.py
+++ jwk-fetcher.py
@@ -4,14 +4,23 @@ import json
 import sys
 import traceback
 
-def jwk_fetch(env):
+def get_jwk_data(env):
     if (env != "test." and env != ""):
         print("invalid env value")
         sys.exit()
 
     endpoint = f"https://access.{env}api.bbc.com/v1/oauth/connect/jwk_uri"
     request = Request(endpoint, None, {})
-    return json.load(urlopen(request))
+    jwk_data = json.load(urlopen(request))
+
+    if jwk_data['keys']:
+        print("keys found")
+        return jwk_data
+
+def write_jwk_data(jwk_data, env):
+    with open(f"priv/static/jwk_{env}.json", 'w+') as f:
+        json.dump(jwk_data, f)
+    print("success")
 
 try:
     if sys.argv[1] == "test":
@@ -26,18 +35,13 @@ except IndexError:
     sys.exit()
 
 try:
-    jwk_data = jwk_fetch(env)
-    if jwk_data['keys']:
-        print("keys found")
+    jwk_data = get_jwk_data(env)
 except KeyError:
     print("No keys found in jwk_data file")
     sys.exit()
 
 try:
-    env_name = sys.argv[1]
-    with open(f"priv/static/jwk_{env_name}.json", 'w+') as f:
-        json.dump(jwk_data, f)
-    print("success")
+    write_jwk_data(jwk_data, env)
 except Exception as e:
     print(f"Failed to write jwk file")
     sys.exit(traceback.format_exc())
