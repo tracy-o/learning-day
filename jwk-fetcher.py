4b2047425fa64d88d20075511a0836acb4de0abb
Alasdair Stalker <alasdair.stalker@bbc.co.uk>
2021-01-18 14:18:48 +0000

    feat(session-token): Get the JWK data during build time

---
+++ jwk-fetcher.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+from datetime import datetime
+from urllib.request import urlopen, Request
+import json
+import sys
+import traceback
+
+try:
+    if sys.argv[1] == "test":
+        env = "test."
+    elif sys.argv[1] == "live":
+        env = ""
+    else:
+        print("Invalid env specified, accepted values: 'test' or 'live'")
+        sys.exit()
+except IndexError:
+    print("No env specified")
+    sys.exit()
+
+def jwk_fetch(env):
+    if (env != "test." and env != ""):
+        print("invalid env value")
+        sys.exit()
+
+    endpoint = f"https://access.{env}api.bbc.com/v1/oauth/connect/jwk_uri"
+    request = Request(endpoint, None, {})
+    return json.load(urlopen(request))
+
+jwk_data = jwk_fetch(env)
+
+try:
+    if jwk_data['keys']:
+        print("keys found")
+except KeyError:
+    print("No keys found in jwk_data file")
+    sys.exit()
+
+try:
+    env_name = sys.argv[1]
+    with open(f"priv/static/jwk_{env_name}.json", 'w+') as f:
+        json.dump(jwk_data, f)
+    print("success")
+except Exception as e:
+    print(f"Failed to write jwk file")
+    sys.exit(traceback.format_exc())
