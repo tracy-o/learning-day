395745e3792c0d759230cd6ab6176f750383b1f9
James Bowers <james.bowers@bbc.co.uk>
2021-01-19 15:47:38 +0000

    remove PAL references

--- lib/routes/specs/world_service_mundo.ex
+++
@@ -1,9 +0,0 @@
-defmodule Routes.Specs.WorldServiceMundo do
-  def specs do
-    %{
-      platform: Pal,
-      pipeline: ["WorldServiceRedirect", "CircuitBreaker"],
-      query_params_allowlist: ["alternativeJsLoading", "batch"]
-    }
-  end
-end
