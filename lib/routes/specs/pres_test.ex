61eaf7429974ddbbc795f78b2d9418f1c6b92de0
Simon Scarfe <simon.scarfe@bbc.co.uk>
2021-01-28 13:47:05 +0000

    s/simon.scarfe/D&EWebCorePresentationTeam/g

--- lib/routes/specs/pres_test.ex
+++ lib/routes/specs/pres_test.ex
@@ -1,7 +1,7 @@
 defmodule Routes.Specs.PresTest do
   def specs do
     %{
-      owner: "simon.scarfe@bbc.co.uk",
+      owner: "D&EWebCorePresentationTeam@bbc.co.uk",
       platform: Webcore,
       query_params_allowlist: ["q", "page", "scope", "filter"]
     }
