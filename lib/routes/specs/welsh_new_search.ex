6e98fc21920c23692c3a583e4ed46db72a9353fc
Mohamed Rashid <mohamed.rashid@bbc.co.uk>
2021-02-03 15:04:16 +0000

    Try remove code that calls itself

--- lib/routes/specs/welsh_new_search.ex
+++ lib/routes/specs/welsh_new_search.ex
@@ -1,11 +1,8 @@
 defmodule Routes.Specs.WelshNewSearch do
   def specs do
-    Map.merge(
-        Routes.Specs.WelshNewSearch.specs(),
-        %{
-           default_language: "cy",
-           query_params_allowlist: ["q", "page", "scope", "filter"]
-         }
-    )
+    %{
+       default_language: "cy",
+       query_params_allowlist: ["q", "page", "scope", "filter"]
+     }
   end
 end
