3dfb61f97e0c7fd744a392cf6cbdff3941e4655c
Mohamed Rashid <mohamed.rashid@bbc.co.uk>
2021-02-03 15:11:06 +0000

    Add platform

--- lib/routes/specs/welsh_new_search.ex
+++ lib/routes/specs/welsh_new_search.ex
@@ -1,6 +1,7 @@
 defmodule Routes.Specs.WelshNewSearch do
   def specs do
     %{
+       platform: Webcore,
        default_language: "cy",
        query_params_allowlist: ["q", "page", "scope", "filter"]
      }
