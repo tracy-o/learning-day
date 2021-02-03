627c344078634ddfa243eb8d51162c18c2d4a893
Mohamed Rashid <mohamed.rashid@bbc.co.uk>
2021-02-03 11:44:15 +0000

    Add query param support

--- lib/routes/specs/welsh_new_search.ex
+++ lib/routes/specs/welsh_new_search.ex
@@ -1,5 +1,11 @@
 defmodule Routes.Specs.WelshNewSearch do
   def specs do
-    Map.merge(Routes.Specs.WelshNewSearch.specs(), %{default_language: "cy"})
+    Map.merge(
+        Routes.Specs.WelshNewSearch.specs(),
+        %{
+           default_language: "cy",
+           query_params_allowlist: ["q", "page", "scope", "filter"]
+         }
+    )
   end
 end
