d8b6a79402656d66b4c78cf0e91a554b8356087d
Emma Carruthers <emma.carruthers@bbc.co.uk>
2021-02-14 16:05:05 +0000

    Update owner

--- lib/routes/specs/sport_morph_index.ex
+++ lib/routes/specs/sport_morph_index.ex
@@ -1,7 +1,7 @@
 defmodule Routes.Specs.SportMorphIndex do
   def specs do
     %{
-      owner: "D&ESportOneWebTeam@bbc.co.uk",
+      owner: "#help-sport",
       runbook: "https://confluence.dev.bbc.co.uk/display/ONEWEB/BBC+Sport+Mozart+Content+Pages+Run+Book",
       platform: MozartSport
     }
