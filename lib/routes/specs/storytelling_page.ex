0c2a718b28a27f716eb195c1ddc6c86b14a6c7b7
jblaut <jagoda.blaut@bbc.co.uk>
2021-02-16 16:06:37 +0000

    udpate runbook link

--- lib/routes/specs/storytelling_page.ex
+++ lib/routes/specs/storytelling_page.ex
@@ -2,7 +2,7 @@ defmodule Routes.Specs.StorytellingPage do
   def specs do
     %{
       owner: "DEArticleReadingExperience@bbc.co.uk",
-      runbook: "https://confluence.dev.bbc.co.uk/display/NEWSART",
+      runbook: "https://confluence.dev.bbc.co.uk/display/NEWSART/Web+Core+Storytelling+Experience+Run+Book",
       platform: Webcore,
       circuit_breaker_error_threshold: 500
     }
