00039fa9b8160d2122ed7cfcbb60ff7735b15216
Emma Carruthers <emma.carruthers@bbc.co.uk>
2021-02-24 22:50:09 +0000

    Remove Sport catch all

--- lib/routes/routefile.ex
+++ lib/routes/routefile.ex
@@ -758,9 +758,7 @@ defmodule Routes.Routefile do
   handle "/sport/:discipline/:id", using: "SportStoryPage", examples: ["/sport/football/56064289?morph_env=live&renderer_env=live"] do
     return_404 if: !String.match?(id, ~r/^[0-9]{4,9}$/)
   end
-  
-  handle "/sport/*_any", using: "Sport", examples: []
-  
+    
   # Weather
 
   handle "/weather", using: "WeatherHomePage", examples: ["/weather"]
