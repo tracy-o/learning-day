2109f26776bfe2f28ba52fc9d0f3fe5dc0671392
dwinatech <diwina1993@hotmail.com>
2021-02-26 09:13:33 +0000

    Create newstipo test route

---
+++ lib/routes/specs/news_tipo_home_page.ex
@@ -0,0 +1,10 @@
+defmodule Routes.Specs.NewsTipoHomePage do
+    def specs do
+      %{
+        owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
+        runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Runbook",
+        platform: Webcore
+      }
+    end
+  end
+  
