d8ee63d4055ca8715e6644e753c817e21fa24b41
Michael Kryworuk <michael.kryworuk@bbc.co.uk>
2021-03-03 08:38:41 +0000

    Assert against a subset of headers

--- test/belfrage_web/plugs/infinite_loop_guardian_test.exs
+++ test/belfrage_web/plugs/infinite_loop_guardian_test.exs
@@ -15,20 +15,11 @@ defmodule BelfrageWeb.Plugs.InfiniteLoopGuardianTest do
       conn(:get, "/foo/bar/123.json")
       |> Plug.Conn.put_req_header("req-svc-chain", "GTM,BELFRAGE,MOZART")
 
-    assert %Plug.Conn{
-             status: 404,
-             halted: true,
-             resp_headers: [
-               {"cache-control", "public, stale-if-error=90, stale-while-revalidate=60, max-age=30"},
-               {"content-type", "text/html; charset=utf-8"},
-               {"vary", "Accept-Encoding,X-BBC-Edge-Cache,X-Country,X-IP_Is_UK_Combined,X-BBC-Edge-Scheme"},
-               {"server", "Belfrage"},
-               {"bid", "local"},
-               {"via", "1.1 Belfrage"},
-               {"req-svc-chain", "BELFRAGE"},
-               {"belfrage-cache-status", "MISS"}
-             ]
-           } = InfiniteLoopGuardian.call(conn, _opts = [])
+    assert %Plug.Conn{status: 404, halted: true, resp_headers: resp_headers} =
+             InfiniteLoopGuardian.call(conn, _opts = [])
+
+    assert {"req-svc-chain", "BELFRAGE"} in resp_headers
+    assert {"bid", "local"} in resp_headers
   end
 
   test "continues if there is no req-svc-chain header" do
