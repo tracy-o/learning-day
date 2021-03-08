fbbc1842df1e48e32aca0826e0fbb599d9d34e86
Lucy Copp <lucy.copp@bbc.co.uk>
2021-03-08 14:23:57 +0000

    added run book link

--- lib/routes/specs/sport_alpha.ex
+++ lib/routes/specs/sport_alpha.ex
@@ -2,7 +2,7 @@ defmodule Routes.Specs.SportAlpha do
   def specs do
     %{
       owner: "#help-sport",
-      runbook: "",
+      runbook: "https://confluence.dev.bbc.co.uk/display/sport/BBC+Sport+Technical+Design",
       platform: Webcore
     }
   end
