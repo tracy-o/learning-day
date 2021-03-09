129d168ab6dbf1c9a63a6970d4b210330a5ccf71
Lucy Copp <lucy.copp@bbc.co.uk>
2021-03-09 09:55:00 +0000

    changed example for basketball fixtures

--- lib/routes/routefile.ex
+++ lib/routes/routefile.ex
@@ -438,7 +438,7 @@ defmodule Routes.Routefile do
   handle "/sport/top-4.app", using: "Sport", examples: ["/sport/top-4.app"]
   handle "/sport/top-4", using: "Sport", examples: ["/sport/top-4"]
 
-  handle "/sport/alpha/*_any", using: "SportAlpha", examples: ["/sport/alpha/basketball/nba"]
+  handle "/sport/alpha/*_any", using: "SportAlpha", examples: ["/sport/alpha/basketball/nba/fixtures"]
 
   ## Sport BBC Live
   handle "/sport/live/*_any", using: "SportLivePage", examples: ["/sport/live/football/52581366.app", "/sport/live/football/52581366"]
