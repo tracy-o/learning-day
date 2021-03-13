c1b289db4f88566d3b8acc918107db08dcbf8afb
Emma Carruthers <emma.carruthers@bbc.co.uk>
2021-03-13 00:29:03 +0000

    Use redirect in routefile where possible

--- lib/belfrage/transformers/sport_temporary_redirect.ex
+++ lib/belfrage/transformers/sport_temporary_redirect.ex
@@ -12,46 +12,37 @@ defmodule Belfrage.Transformers.SportTemporaryRedirect do
 
   def redirect_to(path) do
     cond do
+      String.match?(path, ~r/\/sport\/commonwealth-games\/(home-nations|medals|results|schedule|sports).app$/) ->
+        "/sport/commonwealth-games.app"
+
       String.match?(path, ~r/\/sport\/commonwealth-games\/(medals|results|schedule)\/.*.app$/) ->
         "/sport/commonwealth-games.app"
 
       String.match?(path, ~r/\/sport\/commonwealth-games\/(medals|results|schedule)\/.*/) ->
         "/sport/commonwealth-games"
 
-      String.match?(path, ~r/\/sport\/commonwealth-games\/(home-nations|medals|results|schedule|sports).app$/) ->
-        "/sport/commonwealth-games.app"
-
-      String.match?(path, ~r/\/sport\/commonwealth-games\/(home-nations|medals|results|schedule|sports)$/) ->
-        "/sport/commonwealth-games"
-
       String.match?(path, ~r/\/sport\/formula1\/standings.app$/) ->
         "/sport/formula1/drivers-world-championship/standings.app"
-
-      String.match?(path, ~r/\/sport\/formula1\/standings$/) ->
-        "/sport/formula1/drivers-world-championship/standings"
 
       String.match?(path, ~r/\/sport\/football\/european-championship\/schedule(\/.*)?.app$/) ->
         "/sport/football/european-championship.app"
 
-      String.match?(path, ~r/\/sport\/football\/european-championship\/schedule(\/.*)?$/) ->
+      String.match?(path, ~r/\/sport\/football\/european-championship\/schedule\/.*?$/) ->
         "/sport/football/european-championship"
 
       String.match?(path, ~r/\/sport\/football\/world-cup\/schedule(\/.*)?.app$/) ->
         "/sport/football/world-cup.app"
 
-      String.match?(path, ~r/\/sport\/football\/world-cup\/schedule(\/.*)?$/) ->
+      String.match?(path, ~r/\/sport\/football\/world-cup\/schedule\/.*?$/) ->
         "/sport/football/world-cup"
 
       String.match?(path, ~r/\/sport\/winter-olympics\/(medals|results|schedule)\/.*.app$/) ->
         "/sport/winter-olympics.app"
 
-      String.match?(path, ~r/\/sport\/winter-olympics\/(medals|results|schedule)\/.*/) ->
-        "/sport/winter-olympics"
-
       String.match?(path, ~r/\/sport\/winter-olympics\/(home-nations|medals|results|schedule|sports).app$/) ->
         "/sport/winter-olympics.app"
 
-      String.match?(path, ~r/\/sport\/winter-olympics\/(home-nations|medals|results|schedule|sports)$/) ->
+      String.match?(path, ~r/\/sport\/winter-olympics\/(medals|results|schedule)\/.*/) ->
         "/sport/winter-olympics"
 
       true ->
