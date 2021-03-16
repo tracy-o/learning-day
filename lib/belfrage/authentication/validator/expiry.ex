89c73a2d738bb347966a902bea96c6cef92baa4b
Michael Kryworuk <michael.kryworuk@bbc.co.uk>
2021-03-16 15:52:48 +0000

    Guard against nil expiry and threshold values

--- lib/belfrage/authentication/validator/expiry.ex
+++ lib/belfrage/authentication/validator/expiry.ex
@@ -1,7 +1,21 @@
 defmodule Belfrage.Authentication.Validator.Expiry do
   @callback valid?(non_neg_integer(), non_neg_integer()) :: boolean()
 
+  def valid?(_, nil), do: false
+
+  def valid?(nil, expiry) do
+    expired?(expiry, now_time())
+  end
+
   def valid?(threshold, expiry) do
-    expiry > System.os_time(:second) + threshold
+    expired?(expiry, now_time() + threshold)
+  end
+
+  defp expired?(expiry, now_time) do
+    expiry > now_time
+  end
+
+  defp now_time() do
+    System.os_time(:second)
   end
 end
