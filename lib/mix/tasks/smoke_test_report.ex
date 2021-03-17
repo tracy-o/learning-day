434b1df511aa92e9a087e81d0bf25b4560b69c37
Raymonoir <raymond.ward01@bbc.co.uk>
2021-03-17 15:22:03 +0000

    scratch that

--- lib/mix/tasks/smoke_test_report.ex
+++ lib/mix/tasks/smoke_test_report.ex
@@ -54,8 +54,6 @@ defmodule Mix.Tasks.ReportSmokeTestResults do
   end
 
   defp send_slack_message({routespec, failure_messages}, slack_auth_token) do
-    IO.inspect(routespec <> "<<< ROUTESPEC HERE")
-
     specs = Belfrage.RouteSpec.specs_for(routespec)
 
     msg = Enum.join(failure_messages, "\n\n")
