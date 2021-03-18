1e4548a689712d6c7a5155f61867660a62b4ac7e
SeanButlerBBC <sean.butler@bbc.co.uk>
2021-03-18 10:18:09 +0000

    mix format

--- lib/belfrage/transformers/sport_videos_platform_discriminator.ex
+++ lib/belfrage/transformers/sport_videos_platform_discriminator.ex
@@ -8,7 +8,7 @@ defmodule Belfrage.Transformers.SportVideosPlatformDiscriminator do
     # Block 6
     "get-inspired"
   ]
-  
+
   @webcore_sections_live [
     # Block 1
     "sports-personality",
