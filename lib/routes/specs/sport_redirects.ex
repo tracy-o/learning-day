5e52acb7ca38d21e0d561d657855c842fafb1170
Emma Carruthers <emma.carruthers@bbc.co.uk>
2021-03-24 10:48:50 +0000

    Remove regex transformer and send to Mozart to handle for now

--- lib/routes/specs/sport_redirects.ex
+++ lib/routes/specs/sport_redirects.ex
@@ -4,7 +4,7 @@ defmodule Routes.Specs.SportRedirects do
       owner: "#help-sport",
       runbook: "https://confluence.dev.bbc.co.uk/display/ONEWEB/BBC+Sport+Mozart+Content+Pages+Run+Book",
       platform: MozartSport,
-      pipeline: ["HTTPredirect", "SportRedirect"]
+      pipeline: ["HTTPredirect"]
     }
   end
 end
