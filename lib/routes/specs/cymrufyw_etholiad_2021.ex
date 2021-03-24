b37f97108626d88392f0165e0944eefbaf9dea02
Michael Taylor-New Media Tech <michael.taylor1@bbc.co.uk>
2021-03-24 09:45:34 +0000

    CONNPOL-6344 - Removing mode allowlist - already set in webcore platform

--- lib/routes/specs/cymrufyw_etholiad_2021.ex
+++ lib/routes/specs/cymrufyw_etholiad_2021.ex
@@ -3,8 +3,7 @@ defmodule Routes.Specs.CymrufywEtholiad2021 do
     %{
       owner: "DENewsElections@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/connpol/Run+book+-+UK+2021",
-       platform: Webcore,
-       query_params_allowlist: ["mode"]
+       platform: Webcore
      }
   end
 end
