8e41cb5b9877e70bb213e8ee79fa0da51de2b364
Michael Taylor-New Media Tech <michael.taylor1@bbc.co.uk>
2021-03-24 10:07:46 +0000

    CONNPOL-6344 - Add examples

--- lib/routes/routefile.ex
+++ lib/routes/routefile.ex
@@ -189,7 +189,7 @@ defmodule Routes.Routefile do
 
   handle "/news", using: "NewsHomePage", examples: ["/news"]
 
-  handle "/news/election/2021/:polity/:divisionName/:divisionId", using: "NewsElection2021", examples: ["/news/election/2021/scotland/constituencies/S16000084"] do
+  handle "/news/election/2021/:polity/:divisionName/:divisionId", using: "NewsElection2021", examples: ["/news/election/2021/scotland/constituencies/S16000084?mode=testData", "/news/election/2021/wales/constituencies/W09000001"] do
     return_404 if: !String.match?(polity, ~r/^(scotland|wales)$/)
     return_404 if: !String.match?(divisionName, ~r/^(regions|constituencies)$/)
     return_404 if: !String.match?(divisionId, ~r/^[SW][0-9]{8}$/)
@@ -249,7 +249,7 @@ defmodule Routes.Routefile do
   redirect "/newyddion/*any", to: "/cymrufyw/*any", status: 302
   redirect "/democratiaethfyw", to: "/cymrufyw/gwleidyddiaeth", status: 302
 
-  handle "/cymrufyw/etholiad/2021/cymru/:divisionName/:divisionId", using: "CymrufywEtholiad2021", examples: ["/cymrufyw/etholiad/2021/cymru/etholaethau/W09000001"] do
+  handle "/cymrufyw/etholiad/2021/cymru/:divisionName/:divisionId", using: "CymrufywEtholiad2021", examples: ["/cymrufyw/etholiad/2021/cymru/etholaethau/W09000001?mode=testData"] do
     return_404 if: !String.match?(divisionName, ~r/^(rhanbarthau|etholaethau)$/)
     return_404 if: !String.match?(divisionId, ~r/^W[0-9]{8}$/)
   end
