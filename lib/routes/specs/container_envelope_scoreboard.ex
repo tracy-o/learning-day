1a48dc265dbf3c9aa45d7e7ed2afc5aa54023cd6
Matt Williams <matt.williams02@bbc.co.uk>
2021-04-08 16:01:42 +0100

    Add static to container api query allow list

--- lib/routes/specs/container_envelope_scoreboard.ex
+++ lib/routes/specs/container_envelope_scoreboard.ex
@@ -3,7 +3,8 @@ defmodule Routes.Specs.ContainerEnvelopeScoreboard do
     %{
       owner: "DENewsElections@bbc.co.uk",
       platform: Webcore,
-      runbook: "https://confluence.dev.bbc.co.uk/display/connpol/Run+book+-+UK+2021"
+      runbook: "https://confluence.dev.bbc.co.uk/display/connpol/Run+book+-+UK+2021",
+      query_params_allowlist: ["static"]
     } # TODO: Add something (pipeline?) that enforces the user agent to match an allow-list
   end
 end
