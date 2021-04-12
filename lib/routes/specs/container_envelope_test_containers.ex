f7815960ff5f0ddb3dc76529bd863cddfa29d076
Joseph Pack <josephapack@hotmail.co.uk>
2021-04-12 16:02:15 +0100

    Add UserAgentValidator to ContainerEnvelopeTest routespec pipeline

--- lib/routes/specs/container_envelope_test_containers.ex
+++ lib/routes/specs/container_envelope_test_containers.ex
@@ -4,7 +4,8 @@ defmodule Routes.Specs.ContainerEnvelopeTestContainers do
       owner: "d&ewebcorepresentationteam@bbc.co.uk",
       platform: Webcore,
       runbook: "https://confluence.dev.bbc.co.uk/display/WebCore/Presentation+Layer+Run+Book#PresentationLayerRunBook-ContainerAPI",
-      query_params_allowlist: ["q", "page", "scope", "filter", "static"]
+      query_params_allowlist: ["q", "page", "scope", "filter", "static"],
+      pipeline: ["UserAgentValidator"]
     }
   end
 end
