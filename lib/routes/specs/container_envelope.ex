7b113562c6f465fd00895df2b7c4809cc1494832
Joseph Pack <josephapack@hotmail.co.uk>
2021-04-13 09:11:26 +0100

    Add UserAgentValidator to all Container RouteSpecs

--- lib/routes/specs/container_envelope.ex
+++ lib/routes/specs/container_envelope.ex
@@ -1,10 +1,19 @@
 defmodule Routes.Specs.ContainerEnvelope do
-  def specs do
+  def specs(production_env) do
     %{
       owner: "d&ewebcorepresentationteam@bbc.co.uk",
       platform: Webcore,
       runbook: "https://confluence.dev.bbc.co.uk/display/WebCore/Presentation+Layer+Run+Book#PresentationLayerRunBook-ContainerAPI",
-      query_params_allowlist: ["static"]
+      query_params_allowlist: ["static"],
+      pipeline: pipeline(production_env)
     } # TODO: Add something (query string requirement?) that indicates this catch-all shouldn't be used in production
+  end
+
+  defp pipeline("live") do
+    ["UserAgentValidator", "HTTPredirect", "TrailingSlashRedirector", "LambdaOriginAlias", "CircuitBreaker", "Language"]
+  end
+
+  defp pipeline(_production_env) do
+    pipeline("live") ++ ["DevelopmentRequests"]
   end
 end
