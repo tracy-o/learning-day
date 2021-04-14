9c3c51745e7f1808521ac791c0ff669ac2357bb3
Paul Crane <paul.crane@bbc.co.uk>
2021-04-14 11:55:29 +0100

    Created new sport story specs

---
+++ lib/routes/specs/sport_football_story_page.ex
@@ -0,0 +1,9 @@
+defmodule Routes.Specs.SportFootballStoryPage do
+  def specs do
+    %{
+      owner: "#help-sport",
+      runbook: "https://confluence.dev.bbc.co.uk/display/ONEWEB/BBC+Sport+Mozart+Content+Pages+Run+Book",
+      platform: MozartSport
+    }
+  end
+end
