df9f20b3c229cadf0a0467dc1a8bfbd5fd0f02d6
Ettore Berardi <ettore.berardi@bbc.co.uk>
2021-04-20 19:16:15 +0100

    Refactor Belfrage Supervisor.

--- lib/belfrage/supervisor.ex
+++ lib/belfrage/supervisor.ex
@@ -6,76 +6,21 @@ defmodule Belfrage.Supervisor do
   end
 
   def children(env: env) do
-    router_options =
-      case env do
-        :test -> [scheme: :http, port: 7081]
-        :end_to_end -> [scheme: :http, port: 7082]
-        :routes_test -> [scheme: :http, port: 7083]
-        :smoke_test -> [scheme: :http, port: 7084]
-        :dev -> [scheme: :http, port: 7080]
-        :prod -> [scheme: :https, port: 7443]
-      end
-
     [
-      BelfrageWeb.Router.child_spec(router_options)
-    ] ++ http_router(env) ++ default_children(env) ++ background_children(env)
-  end
-
-  def default_children(env) do
-    [
+      {BelfrageWeb.Router, router_options(env)},
       Belfrage.LoopsRegistry,
       Belfrage.LoopsSupervisor,
-      {Belfrage.NonEssentialSupervisor, env: env},
+      {Belfrage.Authentication.Supervisor, [env: env]},
+      {Belfrage.Credentials.Supervisor, [env: env]},
+      {Belfrage.Metrics.Supervisor, [env: env]},
       worker(Cachex, [:cache, [limit: cachex_limit()]])
-    ]
-  end
-
-  @doc """
-  background_children/1
-
-  Processes which demand work from themselves, are regarded
-  as "background" processes. For example these processes might
-  use `Process.send_after(self(), :refresh, 5_000)`.
-
-  These processes will not be started with the application
-  when running the tests. Instead, they will need to be started
-  independently in the test setup. For example:
-  `start_supervised!(Poller)`
-  or
-  `start_supervised!({Belfrage.Dials.Supervisor, name: :test_dials_supervisor})`
-  https://hexdocs.pm/ex_unit/ExUnit.Callbacks.html#start_supervised!/2
-
-  For more help, see the testing best practices docs
-  https://github.com/bbc/belfrage/wiki/Testing-best-practices
-
-  Belfrage.Dials.Supervisor
-  - Starts `Belfrage.Dials.Poller` process
-    which periodically checks the dials file for dial changes.
-
-  Belfrage.Credentials.Refresh
-  - Periodically refreshes AWS tokens.
-
-  Belfrage.Authentication.Jwk
-  - Periodically refreshes public certificates to verify
-    session tokens.
-
-  Belfrage.Authentication.Flagpole
-  - Periodically checks personalisation flagpole.
-
-  """
-  defp background_children(:test), do: []
-
-  defp background_children(_env) do
-    [
-      Belfrage.Dials.Supervisor,
-      Belfrage.Authentication.Jwk,
-      Belfrage.Authentication.Flagpole
-    ]
+      # {Cachex, [name: :cache, limit: cachex_limit()]}
+    ] ++ http_router(env)
   end
 
   @impl true
   def init(args) do
-    Supervisor.init(children(args), strategy: :one_for_one)
+    Supervisor.init(children(args), strategy: :one_for_one, max_restarts: 10)
   end
 
   defp cachex_limit(conf \\ Application.get_env(:cachex, :limit))
@@ -84,6 +29,19 @@ defmodule Belfrage.Supervisor do
     {:limit, size, policy, reclaim, options}
   end
 
-  defp http_router(:prod), do: [BelfrageWeb.Router.child_spec(scheme: :http, port: 7080)]
+  # this is due to the www stack still handlying the TLS termination,
+  # it should go soon.
+  defp http_router(:prod), do: [BelfrageWeb.Router.child_spec(scheme: :https, port: 7443)]
   defp http_router(_env), do: []
+
+  defp router_options(env) do
+    case env do
+      :test -> [scheme: :http, port: 7081]
+      :end_to_end -> [scheme: :http, port: 7082]
+      :routes_test -> [scheme: :http, port: 7083]
+      :smoke_test -> [scheme: :http, port: 7084]
+      :dev -> [scheme: :http, port: 7080]
+      :prod -> [scheme: :https, port: 7080]
+    end
+  end
 end
