a3c1c0d1b06086794c5d18026e3589660c29d90a
John Sanderson <john.sanderson@bbc.co.uk>
2021-04-21 14:09:11 +0100

    RESFRAME-4171: Add dial for bypassing CCP

---
+++ test/belfrage/dials/ccp_bypass_test.exs
@@ -0,0 +1,12 @@
+defmodule Belfrage.Dials.CcpBypassTest do
+  use ExUnit.Case, async: true
+  alias Belfrage.Dials.CcpBypass
+
+  test "transform/1 converts string representation of 'on' to boolean" do
+    assert CcpBypass.transform("on") === true
+  end
+
+  test "transform/1 converts string representation of 'off' to boolean" do
+    assert CcpBypass.transform("off") === false
+  end
+end
