fca621fbf398ef546e908f0ff61dfa84a2f2515c
John Sanderson <john.sanderson@bbc.co.uk>
2021-04-28 17:53:22 +0100

    [SPIKE] RESFRAME-4201: Initial genserver

--- lib/belfrage/metrics/telemetry_supervisor.ex
+++ lib/belfrage/metrics/telemetry_supervisor.ex
@@ -9,7 +9,9 @@ defmodule Belfrage.Metrics.TelemetrySupervisor do
   @impl true
   def init(_args) do
     children = [
-      {TelemetryMetricsStatsd, metrics: telemetry_metrics()}
+      {TelemetryMetricsStatsd, metrics: telemetry_metrics()},
+      # I'm not sure this is the right place tbh
+      Belfrage.LatencyMonitor
     ]
 
     Supervisor.init(children, strategy: :one_for_one, max_restarts: 40)
