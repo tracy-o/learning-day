e59c40afe9f26e8be57eeb602a0569fdec4b2ceb
John Sanderson <john.sanderson@bbc.co.uk>
2021-04-29 10:00:07 +0100

    [SPIKE] RESFRAME-4201: Remove Times struct

--- lib/belfrage/metrics/latency_monitor.ex
+++ lib/belfrage/metrics/latency_monitor.ex
@@ -1,19 +1,9 @@
-defmodule Belfrage.Metrics.LatencyMonitor.Times do
-  defstruct request_start: nil,
-            request_end: nil,
-            response_start: nil,
-            response_end: nil
-
-  @type t :: %__MODULE__{}
-end
-
 defmodule Belfrage.Metrics.LatencyMonitor do
   use GenServer
 
-  alias Belfrage.LatencyMonitor.{Times}
-
   @cleanup_rate 10_000
   @cleanup_ttl 30_000
+  @valid_checkpoints [:request_start, :request_end, :response_start, :response_end]
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -40,50 +30,46 @@ defmodule Belfrage.Metrics.LatencyMonitor do
   def handle_info(_, state), do: {:noreply, state}
 
   @impl GenServer
-  def handle_cast({:checkpoint, :response_send, request_id, time}, _from, state) do
-    send_metrics(%Times{Map.get(state, request_id) | response_send: time}, request_id)
+  def handle_cast({:checkpoint, :response_end, request_id, time}, state) do
+    send_metrics(%{Map.get(state, request_id) | response_end: time}, request_id)
     {:noreply, remove_request_id(state, request_id)}
   end
 
   @impl GenServer
-  def handle_cast({:checkpoint, name, request_id, time}, _from, state) do
+  def handle_cast({:checkpoint, name, request_id, time}, state) when name in @valid_checkpoints do
     {:noreply, append_time(state, request_id, name, time)}
   end
 
   defp append_time(state, request_id, name, time) do
     new_times =
-      Map.get(state, request_id, %Times{})
+      Map.get(state, request_id, %{})
       |> append_time(name, time)
 
     %{state | request_id => new_times}
   end
 
-  defp append_time(%Times{} = times, name, time), do: %Times{times | name => time}
+  defp append_time(times, name, time) when name in @valid_checkpoints, do: %{times | name => time}
 
   defp remove_request_id(state, request_id), do: Map.delete(state, request_id)
 
   defp send_metrics(state, request_id), do: Map.get(state, request_id) |> send_metrics()
+  defp send_metrics(%{response_end: t}) when is_nil(t), do: {:error, :incomplete_times}
 
-  defp send_metrics(%Times{} = times) when is_integer(times.response_end) do
-    request_latency = times.request_end - times.request_start
-    response_latency = times.response_end - times.response_start
+  defp send_metrics(times) do
+    request_latency = compute_latency(times.request_start, times.request_end)
+    response_latency = compute_latency(times.response_start, times.response_end)
+    combined_latency = request_latency + response_latency
 
-    durations = %{
-      request_latency: request_latency / 1_000,
-      response_latency: response_latency / 1_000,
-      combined_latency: (request_latency + response_latency) / 1_000
-    }
-
-    ExMetrics.timing("web.latency.internal.request", durations.request_latency)
-    ExMetrics.timing("web.latency.internal.response", durations.response_latency)
-    ExMetrics.timing("web.latency.internal.combined", durations.combined_latency)
+    ExMetrics.timing("web.latency.internal.request", request_latency)
+    ExMetrics.timing("web.latency.internal.response", response_latency)
+    ExMetrics.timing("web.latency.internal.combined", combined_latency)
 
     {:ok, true}
   end
 
-  defp send_metrics(%Times{} = times), do: {:error, :incomplete_times}
+  defp compute_latency(start_time, end_time), do: (end_time - start_time) / 1_000
 
-  defp schedule_work, do: Process.send_after(__MODULE__, :cleanup, @cleanup_rate)
+  defp schedule_work(), do: Process.send_after(__MODULE__, :cleanup, @cleanup_rate)
 
   defp perform_cleanup(state), do: perform_cleanup(state, System.monotonic_time() - @cleanup_ttl)
   defp perform_cleanup(state, ttl_threshold), do: Enum.filter(state, is_request_alive(ttl_threshold))
