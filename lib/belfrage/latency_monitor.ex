68c61bf9e66fcb4d71c3fbec3775e40060ab15eb
John Sanderson <john.sanderson@bbc.co.uk>
2021-04-29 10:10:29 +0100

    Revert "[SPIKE] RESFRAME-4201: refactor genserver"
    This reverts commit 09d2b8ea72f795127c2507197f0509a1f8a97d89.

--- lib/belfrage/latency_monitor.ex
+++ lib/belfrage/latency_monitor.ex
@@ -1,4 +1,4 @@
-defmodule Belfrage.LatencyMonitor.Times do
+defmodule Belfrage.LatencyMonitor.Timings do
   defstruct request_start: nil,
             request_end: nil,
             response_start: nil,
@@ -10,7 +10,7 @@ end
 defmodule Belfrage.LatencyMonitor do
   use GenServer
 
-  alias Belfrage.LatencyMonitor.{Times}
+  alias Belfrage.LatencyMonitor.{Timings}
 
   @cleanup_rate 10_000
   @cleanup_ttl 30_000
@@ -31,17 +31,19 @@ defmodule Belfrage.LatencyMonitor do
   end
 
   @impl GenServer
-  def handle_info(:cleanup, state) do
+  def handle_info(:cleanup, _old) do
     schedule_work()
-    {:noreply, perform_cleanup(state)}
+    perform_cleanup()
   end
 
+  # Catch all to handle unexpected messages
+  # https://elixir-lang.org/getting-started/mix-otp/genserver.html#call-cast-or-info
   @impl GenServer
-  def handle_info(_, state), do: {:noreply, state}
+  def handle_info(_any_message, state), do: {:noreply, state}
 
   @impl GenServer
   def handle_cast({:checkpoint, :response_send, request_id, time}, _from, state) do
-    send_metrics(%Times{Map.get(state, request_id) | response_send: time}, request_id)
+    send_metrics(%Timings{Map.get(state, request_id) | response_send: time}, request_id)
     {:noreply, remove_request_id(state, request_id)}
   end
 
@@ -50,23 +52,27 @@ defmodule Belfrage.LatencyMonitor do
     {:noreply, append_time(state, request_id, name, time)}
   end
 
-  defp append_time(state, request_id, name, time) do
-    new_times =
-      Map.get(state, request_id, %Times{})
-      |> append_time(name, time)
-
-    %{state | request_id => new_times}
+  defp schedule_work do
+    Process.send_after(__MODULE__, :cleanup, @cleanup_rate)
   end
 
-  defp append_time(%Times{} = times, name, time), do: %Times{times | name => time}
+  defp append_time(state, request_id, name, time) do
+    %{state | request_id => %Timings{Map.get(state, request_id, %Timings{}) | name => time}}
+  end
 
   defp remove_request_id(state, request_id), do: Map.delete(state, request_id)
 
+  defp perform_cleanup(state), do: perform_cleanup(state, System.monotonic_time() - @cleanup_ttl)
+
+  defp perform_cleanup(state, ttl_threshold) do
+    {:noreply, Enum.filter(state, fn {_k, v} -> Map.get(v, :request_start, 0) > ttl_threshold end)}
+  end
+
   defp send_metrics(state, request_id), do: Map.get(state, request_id) |> send_metrics()
 
-  defp send_metrics(%Times{} = times) when is_integer(times.response_end) do
-    request_latency = times.request_end - times.request_start
-    response_latency = times.response_end - times.response_start
+  defp send_metrics(%Timings{} = timings) when is_integer(timings.response_end) do
+    request_latency = timings.request_end - timings.request_start
+    response_latency = timings.response_end - timings.response_start
 
     durations = %{
       request_latency: request_latency / 1_000,
@@ -77,19 +83,5 @@ defmodule Belfrage.LatencyMonitor do
     ExMetrics.timing("web.latency.internal.request", durations.request_latency)
     ExMetrics.timing("web.latency.internal.response", durations.response_latency)
     ExMetrics.timing("web.latency.internal.combined", durations.combined_latency)
-
-    {:ok, true}
   end
-
-  defp send_metrics(%Times{} = times), do: {:error, :incomplete_times}
-
-  defp schedule_work do
-    Process.send_after(__MODULE__, :cleanup, @cleanup_rate)
-  end
-
-  defp perform_cleanup(state), do: perform_cleanup(state, System.monotonic_time() - @cleanup_ttl)
-  defp perform_cleanup(state, ttl_threshold), do: Enum.filter(state, is_request_alive(ttl_threshold))
-
-  defp is_request_alive(ttl_threshold), do: &is_request_alive(&1, ttl_threshold)
-  defp is_request_alive({_, times}, ttl_threshold), do: times.request_start > ttl_threshold
 end
