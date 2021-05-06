950df237fee24d8bbe72222f6b75b52b1880e11c
rashmirajaraman <rashmi.rajaraman@bbc.co.uk>
2021-05-06 14:23:16 +0100

    add support for features query param for non-live environments

--- lib/routes/specs/sport_videos.ex
+++ lib/routes/specs/sport_videos.ex
@@ -1,11 +1,15 @@
 defmodule Routes.Specs.SportVideos do
-  def specs do
+  def specs(production_env) do
     %{
       owner: "sfv-team@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/SFV/Short+Form+Video+Run+Book",
-      platform: Webcore
+      platform: Webcore,
+      query_params_allowlist: query_params_allowlist(production_env)
     }
   end
+
+  defp query_params_allowlist("live"), do: []
+  defp query_params_allowlist(_production_env), do: ["features"]
 
   def sports_disciplines_routes do
     [
