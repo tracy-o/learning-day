3edb04b7711de3841d2cc405cd1656157029e2fb
Ettore Berardi <ettore.berardi@bbc.co.uk>
2021-05-11 18:42:33 +0100

    BelfrageWeb.routefiles not needed anymore.

--- lib/belfrage_web.ex
+++ lib/belfrage_web.ex
@@ -1,8 +1,3 @@
 defmodule BelfrageWeb do
   # Namespace for anything web related.
-
-  def routefile(_section) do
-    env = Application.get_env(:belfrage, :production_environment) |> String.capitalize
-    Module.concat(["Routes", "Routefiles", env])
-  end
 end
