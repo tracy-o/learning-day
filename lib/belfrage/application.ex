007606859db34f5e8203c4c8083a4b0389240e43
Ettore Berardi <ettore.berardi@bbc.co.uk>
2021-05-11 17:57:21 +0100

    Removes the Application-level Routefile recompilation.

--- lib/belfrage/application.ex
+++ lib/belfrage/application.ex
@@ -7,15 +7,4 @@ defmodule Belfrage.Application do
     :ok = Belfrage.Metrics.Statix.connect()
     Belfrage.Supervisor.start_link(args)
   end
-
-  # for "only_on" routes
-  # route file already built for live production environment
-  # only need to recompile on test
-  defp recompile_routefile("test") do
-    Application.put_env(:belfrage, :production_environment, "live")
-    Code.compile_file(@routefile_location)
-    Application.put_env(:belfrage, :production_environment, "test")
-  end
-
-  defp recompile_routefile(_), do: :noop
 end
