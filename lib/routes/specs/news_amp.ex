0d1ead7784a624397077142b2dc766c7d0b17ed0
Marty-Scott Sainty <marty-scott.sainty@bbc.co.uk>
2021-05-12 14:16:56 +0100

    Update order of routes to route live Optimo assets on Belfrage Test to Webcore and update naming conventions

---
+++ lib/routes/specs/news_amp.ex
@@ -0,0 +1,13 @@
+defmodule Routes.Specs.NewsAmp do
+  def specs(production_env) do
+    %{
+      owner: "DENewsFrameworksTeam@bbc.co.uk",
+      runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
+      platform: Simorgh,
+      pipeline: pipeline(production_env)
+    }
+  end
+
+  defp pipeline("live"), do: ["HTTPredirect", "TrailingSlashRedirector", "CircuitBreaker"]
+  defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
+end
