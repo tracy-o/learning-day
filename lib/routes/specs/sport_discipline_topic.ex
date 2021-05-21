65c92dc0b2060ff89a7de34e8377aa53c2a424a3
Asa Dawson <asa.dawson@bbc.co.uk>
2021-05-21 14:15:13 +0100

    take production_env as specs arg on all follow-applicable routes

--- lib/routes/specs/sport_discipline_topic.ex
+++ lib/routes/specs/sport_discipline_topic.ex
@@ -1,5 +1,5 @@
 defmodule Routes.Specs.SportDisciplineTopic do
-  def specs do
+  def specs(production_env) do
     %{
       owner: "D&EKLDevelopmentOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Topics+Runbook",
--- lib/routes/specs/topic_page.ex
+++ lib/routes/specs/topic_page.ex
@@ -1,5 +1,5 @@
 defmodule Routes.Specs.TopicPage do
-  def specs do
+  def specs(production_env) do
     %{
       owner: "D&EKLDevelopmentOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Topics+Runbook",
