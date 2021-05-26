fedda2a9c96c0f3a6617b8cf7d2d86d535e852be
Martin Reilly <martin.reilly@bbc.co.uk>
2021-05-26 14:13:04 +0100

    BBCHOME-860 add pipeline

--- lib/routes/specs/test_home_page.ex
+++ lib/routes/specs/test_home_page.ex
@@ -1,12 +1,19 @@
 defmodule Routes.Specs.TestHomePage do
-  def specs do
+  def specs(production_env) do
     %{
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Homepage%20&%20Nations%20-%20WebCore%20-%20Runbook",
       platform: Webcore,
       query_params_allowlist: ["personalisationMode"],
+      pipeline: pipeline(production_env),
       cookie_allowlist: ["ckns_atkn", "ckns_id"],
       headers_allowlist: ["x-id-oidc-signedin"]
     }
   end
+
+  defp pipeline("live") do
+    ["HTTPredirect", "TrailingSlashRedirector", "UserSession", "LambdaOriginAlias", "CircuitBreaker", "Language"]
+  end
+
+  defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
 end
