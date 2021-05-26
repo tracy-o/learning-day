028c8d7b01fdc5ed9f5902c9fee94cb54e7e86e6
Martin Reilly <martin.reilly@bbc.co.uk>
2021-05-26 14:03:34 +0100

    BBCHOME-860 allow cookie and headers in TestHomePage spec

--- lib/routes/specs/test_home_page.ex
+++ lib/routes/specs/test_home_page.ex
@@ -3,7 +3,10 @@ defmodule Routes.Specs.TestHomePage do
     %{
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Homepage%20&%20Nations%20-%20WebCore%20-%20Runbook",
-      platform: Webcore
+      platform: Webcore,
+      query_params_allowlist: ["personalisationMode"],
+      cookie_allowlist: ["ckns_atkn", "ckns_id"],
+      headers_allowlist: ["x-id-oidc-signedin"]
     }
   end
 end
