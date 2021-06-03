867535bebb5f54a3811ccc6f485ede638219e4f2
chrishop <christopher.hopkins@bbc.co.uk>
2021-06-03 16:14:41 +0100

    removed inspects to pass credo

--- lib/belfrage/metrics/cowboy_telemetry_handler.ex
+++ lib/belfrage/metrics/cowboy_telemetry_handler.ex
@@ -7,8 +7,6 @@ defmodule Belfrage.Metrics.CowboyTelemetryHandler do
       [:cowboy, :request, :early_error]
     ]
 
-    IO.inspect("setup run")
-
     :telemetry.attach_many(
       "cowboy-telemetry-handler",
       events,
@@ -17,22 +15,18 @@ defmodule Belfrage.Metrics.CowboyTelemetryHandler do
     )
   end
 
-  defp handle_event([:cowboy, :request, :start], measurements, metadata, _config) do
-    IO.inspect("just got a start")
-  end
+  # defp handle_event([:cowboy, :request, :start], measurements, metadata, _config) do
+  # end
 
   defp handle_event([:cowboy, :request, :stop], measurements, metadata, _config) do
-    IO.inspect("just got a stop")
     Belfrage.Metrics.Statix.timing("cowboy.request.duration", Map.get(measurements, :duration))
   end
 
   defp handle_event([:cowboy, :request, :exception], measurements, metadata, _config) do
-    IO.inspect("just got an exception")
     Belfrage.Metrics.Statix.increment("cowboy.request.exception")
   end
 
   defp handle_event([:cowboy, :request, :early_error], measurements, metadata, _config) do
-    IO.inspect("just got an early error")
     Belfrage.Metrics.Statix.increment("cowboy.request.early_error")
   end
 end
