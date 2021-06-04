5a9c246c11f24e44f63b75cbe4c016693ab61a73
Marty-Scott Sainty <marty-scott.sainty@bbc.co.uk>
2021-06-04 09:18:13 +0100

    Update Belfrage Routing to route traffic for live Optimo news articles to Webcore

--- lib/routes/routefile.ex
+++ lib/routes/routefile.ex
@@ -269,22 +269,9 @@ defmodule Routes.Routefile do
   handle "/news/articles/:optimo_id.amp", using: "NewsAmp", examples: []
   handle "/news/articles/:optimo_id.json", using: "NewsAmp", examples: []
 
-  handle "/news/articles/:optimo_id", using: "StorytellingPage", only_on: "test", examples: ["/news/articles/crkxdvxzwxko?mode=testData", "/news/articles/c3wmq4d1y3wo?mode=testData"] do
+  handle "/news/articles/:optimo_id", using: "StorytellingPage", examples: ["/news/articles/c5ll353v7y9o", "/news/articles/c8xxl4l3dzeo"] do
     return_404 if: !String.match?(optimo_id, ~r/^c[abcdefghjklmnpqrstuvwxyz0-9]{10,}o$/)
   end
-
-  # Temporary routes until Webcore supports Optimo articles
-  handle "/news/articles/c744mjrym37o", using: "NewsAmp", examples: []
-  handle "/news/articles/c5ll353v7y9o", using: "NewsAmp", examples: []
-  handle "/news/articles/ce9992y0reyo", using: "NewsAmp", examples: []
-  handle "/news/articles/cd4vr7lvrvmo", using: "NewsAmp", examples: []
-  handle "/news/articles/clldg965yzjo", using: "NewsAmp", examples: []
-  handle "/news/articles/cw4jd7vxxqdo", using: "NewsAmp", examples: []
-  handle "/news/articles/ce9v85jlrzzo", using: "NewsAmp", examples: []
-  handle "/news/articles/cd4117egk3go", using: "NewsAmp", examples: []
-  handle "/news/articles/cj7xrxz0e8zo", using: "NewsAmp", examples: []
-  handle "/news/articles/cp03zwze47zo", using: "NewsAmp", examples: []
-  handle "/news/articles/c8xxl4l3dzeo", using: "NewsAmp", examples: []
 
   handle "/news/:id", using: "NewsArticlePage", examples: ["/news/uk-politics-49336144", "/news/world-asia-china-51787936", "/news/technology-51960865", "/news/uk-england-derbyshire-18291916", "/news/entertainment+arts-10636043"] do
     return_404 if: !String.match?(id, ~r/^([a-zA-Z0-9\+]+-)*[0-9]{4,9}$/)
@@ -387,7 +374,7 @@ defmodule Routes.Routefile do
   redirect "/zhongwen/trad/institutional-38228429", to: "/zhongwen/trad/podcasts/p02pc9xp", status: 301
 
   ## World Service - Simorgh and ARES
-  ##    Kaleidoscope Redirects: /<service>/mobile/image/*any 
+  ##    Kaleidoscope Redirects: /<service>/mobile/image/*any
   ##    Mobile Redirects: /<service>/mobile/*any
   handle "/afaanoromoo.amp", using: "WorldServiceAfaanoromoo", examples: ["/afaanoromoo.amp"]
   handle "/afaanoromoo.json", using: "WorldServiceAfaanoromoo", examples: ["/afaanoromoo.json"]
@@ -518,7 +505,7 @@ defmodule Routes.Routefile do
   handle "/punjabi.amp", using: "WorldServicePunjabi", examples: ["/punjabi.amp"]
   handle "/punjabi.json", using: "WorldServicePunjabi", examples: ["/punjabi.json"]
   handle "/punjabi/*_any", using: "WorldServicePunjabi", examples: ["/punjabi", "/punjabi/example-123", "/punjabi/example-123.amp", "/punjabi/example-123.json"]
-  
+
   redirect "/russian/mobile/*any", to: "/russian", status: 301
 
   handle "/russian.amp", using: "WorldServiceRussian", examples: ["/russian.amp"]
