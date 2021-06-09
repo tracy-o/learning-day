b1b175689adb203232148050eece2c7e619c296d
Martin Reilly <martin.reilly@bbc.co.uk>
2021-06-09 11:46:40 +0100

    BBCHOME-NT add personalisation to automation page

--- lib/routes/specs/automation_home_page.ex
+++ lib/routes/specs/automation_home_page.ex
@@ -3,7 +3,17 @@ defmodule Routes.Specs.AutomationHomePage do
     %{
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Homepage%20&%20Nations%20-%20WebCore%20-%20Runbook",
-      platform: Webcore
+      platform: Webcore,
+      query_params_allowlist: ["personalisationMode"],
+      pipeline: pipeline(production_env),
+      cookie_allowlist: ["ckns_atkn", "ckns_id"],
+      headers_allowlist: ["x-id-oidc-signedin"]
     }
   end
+
+  defp pipeline("live") do
+    ["HTTPredirect", "TrailingSlashRedirector", "UserSession", "LambdaOriginAlias", "CircuitBreaker", "Language"]
+  end
+
+  defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
 end
