72a26cb95214882988bfe4a36e9e4f7ea3ebfbaa
Joseph Pack <josephapack@hotmail.co.uk>
2021-06-18 14:15:44 +0100

    Clearer test blocks for lambda origin alias tests

--- test/belfrage/transformers/lambda_origin_alias_test.exs
+++ test/belfrage/transformers/lambda_origin_alias_test.exs
@@ -4,43 +4,49 @@ defmodule Belfrage.Transformers.LambdaOriginAliasTest do
   alias Belfrage.Transformers.LambdaOriginAlias
   alias Belfrage.Struct
 
-  test "the production_environment is used as the alias" do
-    production_env = Application.get_env(:belfrage, :production_environment)
+  describe "when preview mode boolean is non existant" do
+    test "the production_environment is used as the alias" do
+      production_env = Application.get_env(:belfrage, :production_environment)
 
-    struct = %Struct{
-      private: %Struct.Private{origin: "lambda-function", production_environment: "test"}
-    }
-
-    {:ok, %Struct{private: %Struct.Private{origin: origin}}} = LambdaOriginAlias.call([], struct)
-
-    assert origin == "lambda-function:#{production_env}"
-  end
-
-  test "the production_environment is used as the alias when preview_mode is off" do
-    production_env = Application.get_env(:belfrage, :production_environment)
-
-    struct = %Struct{
-      private: %Struct.Private{origin: "lambda-function", production_environment: "test", preview_mode: "off"}
-    }
-
-    {:ok, %Struct{private: %Struct.Private{origin: origin}}} = LambdaOriginAlias.call([], struct)
-
-    assert origin == "lambda-function:#{production_env}"
-  end
-
-  test "the subdomain is used as the alias when preview_mode is on" do
-    struct = %Struct{
-      request: %Struct.Request{subdomain: "example-branch"},
-      private: %Struct.Private{
-        loop_id: "SportVideos",
-        origin: "lambda-function",
-        production_environment: "test",
-        preview_mode: "on"
+      struct = %Struct{
+        private: %Struct.Private{origin: "lambda-function", production_environment: "test"}
       }
-    }
 
-    {:ok, %Struct{private: %Struct.Private{origin: origin}}} = LambdaOriginAlias.call([], struct)
+      {:ok, %Struct{private: %Struct.Private{origin: origin}}} = LambdaOriginAlias.call([], struct)
 
-    assert origin == "lambda-function:example-branch"
+      assert origin == "lambda-function:#{production_env}"
+    end
+  end
+
+  describe "when the preview mode is off" do
+    test "the production_environment is used as the alias" do
+      production_env = Application.get_env(:belfrage, :production_environment)
+
+      struct = %Struct{
+        private: %Struct.Private{origin: "lambda-function", production_environment: "test", preview_mode: "off"}
+      }
+
+      {:ok, %Struct{private: %Struct.Private{origin: origin}}} = LambdaOriginAlias.call([], struct)
+
+      assert origin == "lambda-function:#{production_env}"
+    end
+  end
+
+  describe "when preview mode is on" do
+    test "the subdomain is used as the alias" do
+      struct = %Struct{
+        request: %Struct.Request{subdomain: "example-branch"},
+        private: %Struct.Private{
+          loop_id: "SportVideos",
+          origin: "lambda-function",
+          production_environment: "test",
+          preview_mode: "on"
+        }
+      }
+
+      {:ok, %Struct{private: %Struct.Private{origin: origin}}} = LambdaOriginAlias.call([], struct)
+
+      assert origin == "lambda-function:example-branch"
+    end
   end
 end
