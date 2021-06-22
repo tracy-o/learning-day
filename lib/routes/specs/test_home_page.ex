eaee42ad98fef5d29450ac254898e199a189cca6
Inti Herteleer <inti.herteleer@bbc.co.uk>
2021-06-22 08:55:57 +0100

    /homepage/test is no longer personalised To improve Editorial's experience

--- lib/routes/specs/test_home_page.ex
+++ lib/routes/specs/test_home_page.ex
@@ -4,15 +4,12 @@ defmodule Routes.Specs.TestHomePage do
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Homepage%20&%20Nations%20-%20WebCore%20-%20Runbook",
       platform: Webcore,
-      query_params_allowlist: ["personalisationMode"],
       pipeline: pipeline(production_env),
-      cookie_allowlist: ["ckns_atkn", "ckns_id"],
-      headers_allowlist: ["x-id-oidc-signedin"]
     }
   end
 
   defp pipeline("live") do
-    ["HTTPredirect", "TrailingSlashRedirector", "UserSession", "LambdaOriginAlias", "CircuitBreaker", "Language"]
+    ["HTTPredirect", "TrailingSlashRedirector", "LambdaOriginAlias", "CircuitBreaker", "Language"]
   end
 
   defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
