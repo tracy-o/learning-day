861f33806dd2067f9fc64fa6160c254475817d42
chrishop <christopher.hopkins@bbc.co.uk>
2021-06-23 10:54:22 +0100

    decided upon TelemetryMetricsStatsd option

--- lib/belfrage/metrics/supervisor.ex
+++ lib/belfrage/metrics/supervisor.ex
@@ -1,16 +1,12 @@
 defmodule Belfrage.Metrics.Supervisor do
   use Supervisor, restart: :temporary
 
-  alias Belfrage.Metrics.CowboyTelemetryHandler
-
   def start_link(init_arg) do
     Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 
   @impl true
   def init(args) do
-    Belfrage.Metrics.CowboyTelemetryHandler.setup()
-
     Supervisor.init(children(args), strategy: :one_for_one, max_restarts: 10)
   end
 
