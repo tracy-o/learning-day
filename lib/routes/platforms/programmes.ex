165222a1c1445623afa58f252e1f7040c44f20cd
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2021-06-30 16:52:04 +0100

    Fix syntax

--- lib/routes/platforms/programmes.ex
+++ lib/routes/platforms/programmes.ex
@@ -11,7 +11,7 @@ defmodule Routes.Platforms.Programmes do
     }
   end
 
-  defp pipeline("live"), do
+  defp pipeline("live") do
     ["HTTPredirect", "TrailingSlashRedirector", "CircuitBreaker"]
   end
 
