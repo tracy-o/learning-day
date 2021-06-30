e7a33e95a77111f818552d0a70da4f577bf95576
Eugene Bolshakov <eugene.bolshakov@bbc.co.uk>
2021-06-30 12:06:08 +0100

    Remove redundant `Mox.set_mox_global()` from test_helper.exs
    Global mode is enabled in test_helper.exs for all environments above, so
    no need to do it again for end to end tests.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,14 +2,8 @@ Code.require_file("formatter/joe_formatter.ex", __DIR__)
 Code.require_file("smoke/belfrage_smoke_test_case.ex", __DIR__)
 
 :ok = Application.start(:mox)
-
 Test.Support.Helper.setup_stubs()
-
 Mox.set_mox_global()
-
-if Mix.env() == :end_to_end do
-  Mox.set_mox_global()
-end
 
 {:ok, _apps} = Application.ensure_all_started(:belfrage)
 
