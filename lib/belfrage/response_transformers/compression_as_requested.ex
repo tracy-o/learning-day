b8fce9ee146d3e251fcc1ba6347a839fdb320278
Raymonoir <raymond.ward01@bbc.co.uk>
2021-07-05 12:17:15 +0100

    change compression as requested
    Compression as requested response transformer now only decompresses struct body if content-encoding header does contain 'gzip' but accept_encoding does not contain 'gzip'

--- lib/belfrage/response_transformers/compression_as_requested.ex
+++ lib/belfrage/response_transformers/compression_as_requested.ex
@@ -4,18 +4,26 @@ defmodule Belfrage.ResponseTransformers.CompressionAsRequested do
   @behaviour ResponseTransformer
 
   @impl true
-  def call(struct = %Struct{request: %Struct.Request{accept_encoding: accept_encoding}}) do
-    case should_return_gzip?(accept_encoding) do
-      true ->
-        struct
-
-      false ->
-        response_headers = Map.delete(struct.response.headers, "content-encoding")
-        Belfrage.Event.record(:metric, :increment, "web.response.uncompressed")
-        Struct.add(struct, :response, %{body: :zlib.gunzip(struct.response.body), headers: response_headers})
+  def call(
+        struct = %Struct{
+          request: %Struct.Request{accept_encoding: accept_encoding},
+          response: %Struct.Response{headers: headers}
+        }
+      ) do
+    with true <- contains_gzip?(headers["content-encoding"]),
+         false <- contains_gzip?(accept_encoding) do
+      decompress_body(struct)
+    else
+      _ -> struct
     end
   end
 
-  defp should_return_gzip?(nil), do: false
-  defp should_return_gzip?(accept_encoding), do: String.contains?(accept_encoding, "gzip")
+  defp contains_gzip?(nil), do: false
+  defp contains_gzip?(string), do: String.contains?(string, "gzip")
+
+  defp decompress_body(struct) do
+    response_headers = Map.delete(struct.response.headers, "content-encoding")
+    Belfrage.Event.record(:metric, :increment, "web.response.uncompressed")
+    Struct.add(struct, :response, %{body: :zlib.gunzip(struct.response.body), headers: response_headers})
+  end
 end
--- test/belfrage/response_transformers/compression_as_requested_test.exs
+++ test/belfrage/response_transformers/compression_as_requested_test.exs
@@ -2,57 +2,77 @@ defmodule Belfrage.ResponseTransformers.CompressionAsRequestedTest do
   alias Belfrage.ResponseTransformers.CompressionAsRequested
   alias Belfrage.Struct
   use ExUnit.Case
-  import Test.Support.Helper, only: [assert_gzipped: 2]
 
-  setup do
-    %{
-      struct_fixture: %Struct{
+  describe "when content-encoding header contains gzip" do
+    test "when accept_encoding contains gzip, passes the struct through untouched" do
+      struct = %Struct{
+        request: %Struct.Request{accept_encoding: "gzip"},
         response: %Struct.Response{
           body: :zlib.gzip("<p>Hi. I am some content</p>"),
-          headers: %{
-            "content-encoding" => "gzip"
-          },
-          http_status: 200
+          http_status: 200,
+          headers: %{"content-encoding" => "gzip"}
         }
       }
-    }
-  end
 
-  describe "when no accept_encoding value is set" do
-    test "unzips the body in the struct & removes content-encoding response header", %{
-      struct_fixture: struct_fixture
-    } do
       assert %Struct.Response{
-               body: "<p>Hi. I am some content</p>",
-               headers: %{},
-               http_status: 200
+               body: :zlib.gzip("<p>Hi. I am some content</p>"),
+               http_status: 200,
+               headers: %{"content-encoding" => "gzip"}
              } ==
-               CompressionAsRequested.call(struct_fixture).response
-    end
-  end
-
-  describe "when accept-encoding request header is set" do
-    test "when gzip is accepted", %{struct_fixture: struct_fixture} do
-      struct = Struct.add(struct_fixture, :request, %{accept_encoding: "gzip"})
-
-      %Struct.Response{
-        body: compressed_body,
-        headers: %{
-          "content-encoding" => "gzip"
-        },
-        http_status: 200
-      } = CompressionAsRequested.call(struct).response
-
-      assert_gzipped(compressed_body, "<p>Hi. I am some content</p>")
+               CompressionAsRequested.call(struct).response
     end
 
-    test "when gzip is not accepted", %{struct_fixture: struct_fixture} do
-      struct = Struct.add(struct_fixture, :request, %{accept_encoding: "br, deflate, compression"})
+    test "when accept_encoding does not contain gzip, unzips the body in the struct & removes content-encoding response header" do
+      struct = %Struct{
+        request: %Struct.Request{accept_encoding: "br,deflate"},
+        response: %Struct.Response{
+          body: :zlib.gzip("<p>Hi. I am some content</p>"),
+          http_status: 200,
+          headers: %{"content-encoding" => "gzip"}
+        }
+      }
 
       assert %Struct.Response{
                body: "<p>Hi. I am some content</p>",
-               headers: %{},
-               http_status: 200
+               http_status: 200,
+               headers: %{}
+             } == CompressionAsRequested.call(struct).response
+    end
+  end
+
+  describe "when content-encoding header does not contain gzip" do
+    test "when accept_encoding contains gzip, passes the struct through untouched" do
+      struct = %Struct{
+        request: %Struct.Request{accept_encoding: "gzip"},
+        response: %Struct.Response{
+          body: :zlib.gzip("<p>Hi. I am some content</p>"),
+          http_status: 200,
+          headers: %{}
+        }
+      }
+
+      assert %Struct.Response{
+               body: :zlib.gzip("<p>Hi. I am some content</p>"),
+               http_status: 200,
+               headers: %{}
+             } ==
+               CompressionAsRequested.call(struct).response
+    end
+
+    test "when accept_encoding does not contain gzip, passes the struct through untouched" do
+      struct = %Struct{
+        request: %Struct.Request{accept_encoding: "deflate"},
+        response: %Struct.Response{
+          body: "<p>Hi. I am some content</p>",
+          http_status: 200,
+          headers: %{}
+        }
+      }
+
+      assert %Struct.Response{
+               body: "<p>Hi. I am some content</p>",
+               http_status: 200,
+               headers: %{}
              } ==
                CompressionAsRequested.call(struct).response
     end
