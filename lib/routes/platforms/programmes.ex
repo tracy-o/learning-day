a8bf6409464fb1476966e70636a40ba53e60d4a2
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2021-07-05 16:12:27 +0100

    Add query parameters allowlist

--- lib/routes/platforms/programmes.ex
+++ lib/routes/platforms/programmes.ex
@@ -6,7 +6,7 @@ defmodule Routes.Platforms.Programmes do
       runbook: "https://confluence.dev.bbc.co.uk/pages/viewpage.action?pageId=152098352",
       pipeline: pipeline(production_env),
       resp_pipeline: [],
-      query_params_allowlist: "*",
+      query_params_allowlist: query_params_allowlist(production_env)
       circuit_breaker_error_threshold: 200
     }
   end
@@ -16,4 +16,8 @@ defmodule Routes.Platforms.Programmes do
   end
 
   defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
+
+  defp query_params_allowlist(_production_env) do
+    ["page", "limit", "preview", "noorb", "__2016minimap", "nestedlevel", "utcoffset", "no_chrome", "partial", "callback", "branding_context", "service", "__flush_cache", "branding-theme-version", "xtor", "at_campaign", "at_medium", "at_custom1", "at_custom2", "at_custom3", "at_custom4"]
+  end
 end
