2445c2577ed643c2e7ca4599d7e27fcf3be481c5
Iain Kydd <iain.kydd@bbc.co.uk>
2021-07-06 09:54:57 +0100

    Add RouteSpec for test page

---
+++ lib/routes/specs/full_stack_homepage_test.ex
@@ -0,0 +1,17 @@
+defmodule Routes.Specs.FullStackHomePageTest do
+  def specs(production_env) do
+    %{
+      owner: "fabl@onebbc.onmicrosoft.com",
+      platform: Webcore,
+      pipeline: pipeline(production_env),
+      cookie_allowlist: ["ckns_atkn", "ckns_id"],
+      headers_allowlist: ["x-id-oidc-signedin"]
+    }
+  end
+
+  defp pipeline("live") do
+    ["HTTPredirect", "TrailingSlashRedirector", "Personalisation", "LambdaOriginAlias", "CircuitBreaker", "Language"]
+  end
+
+  defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
+end
