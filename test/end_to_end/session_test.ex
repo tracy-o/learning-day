cf12fe52e813dc88b67c3769b661e809a4fea29b
Eugene Bolshakov <eugene.bolshakov@bbc.co.uk>
2021-07-08 10:37:38 +0100

    Clean up end-to-end session test
    Removed duplication and unnecessary detail to make it easier to see what
    is being tested.
    
    We essentially want to test three things:
    
    * Personalisation headers are not sent to origin if request is not
    personalised
    * Personalisation headers are sent to origin if request is
    personalised
    * Client is redirected if auth token in the request is invalid

--- test/end_to_end/session_test.ex
+++ test/end_to_end/session_test.ex
@@ -2,109 +2,97 @@ defmodule EndToEnd.SessionTest do
   use ExUnit.Case
   use Plug.Test
   use Test.Support.Helper, :mox
+
   alias BelfrageWeb.Router
+  alias Belfrage.Clients.LambdaMock
+  alias Fixtures.AuthToken
 
   @moduletag :end_to_end
 
-  setup do
-    %{
-      valid_access_token: Fixtures.AuthToken.valid_access_token(),
-      invalid_access_token: Fixtures.AuthToken.invalid_access_token(),
-      lambda_response: %{
-        "headers" => %{
-          "cache-control" => "private"
-        },
-        "statusCode" => 200,
-        "body" => "<h1>Hello from the Lambda!</h1>"
-      }
-    }
-  end
+  @response %{
+    "headers" => %{
+      "cache-control" => "private"
+    },
+    "statusCode" => 200,
+    "body" => "<h1>Hello from the Lambda!</h1>"
+  }
 
-  test "when no authorization token is provided", %{lambda_response: lambda_response} do
-    Belfrage.Clients.LambdaMock
-    |> expect(:call, fn _role_arn, _function_arn, payload, _request_id, _opts ->
-      assert %{
-               body: "",
-               headers: %{
-                 "accept-encoding": "gzip",
-                 country: "gb",
-                 host: "www.bbc.co.uk",
-                 is_uk: false,
-                 language: "en-GB"
-               },
-               httpMethod: "GET",
-               path: "/my/session/webcore-platform",
-               pathParameters: %{},
-               queryStringParameters: %{}
-             } == payload
-
-      {:ok, lambda_response}
+  test "non-personalised request" do
+    expect_origin_request(fn %{headers: headers} ->
+      refute headers[:authorization]
+      refute headers[:"x-authentication-provider"]
+      refute headers[:"pers-env"]
+      refute headers[:"ctx-age-bracket"]
+      refute headers[:"ctx-allow-personalisation"]
     end)
 
-    response_conn = conn(:get, "/my/session/webcore-platform") |> Map.put(:host, "www.bbc.co.uk") |> Router.call([])
-
-    assert {200, headers, body} = sent_resp(response_conn)
-    assert {"cache-control", "private, stale-if-error=90, stale-while-revalidate=30"} in headers
-    assert body == lambda_response["body"]
+    build_request()
+    |> make_request()
+    |> assert_successful_response()
   end
 
-  test "when a valid authorization token is provided", %{
-    valid_access_token: access_token,
-    lambda_response: lambda_response
-  } do
-    Belfrage.Clients.LambdaMock
-    |> expect(:call, fn _role_arn, _function_arn, payload, _request_id, _opts ->
-      assert %{
-               body: "",
-               headers: %{
-                 "accept-encoding": "gzip",
-                 authorization: "Bearer #{access_token}",
-                 "x-authentication-provider": "idv5",
-                 "pers-env": "int",
-                 country: "gb",
-                 host: "www.bbc.co.uk",
-                 "ctx-age-bracket": "o18",
-                 "ctx-allow-personalisation": "true",
-                 is_uk: false,
-                 language: "en-GB"
-               },
-               httpMethod: "GET",
-               path: "/my/session/webcore-platform",
-               pathParameters: %{},
-               queryStringParameters: %{}
-             } == payload
+  test "personalised request" do
+    token = AuthToken.valid_access_token()
 
-      {:ok, lambda_response}
+    expect_origin_request(fn %{headers: headers} ->
+      assert headers[:authorization] == "Bearer #{token}"
+      assert headers[:"ctx-allow-personalisation"] == "true"
+      assert headers[:"x-authentication-provider"]
+      assert headers[:"pers-env"]
+      assert headers[:"ctx-age-bracket"]
     end)
 
-    response_conn =
-      conn(:get, "/my/session/webcore-platform")
-      |> Map.put(:host, "www.bbc.co.uk")
-      |> put_req_header("cookie", "ckns_atkn=#{access_token}")
-      |> put_req_header("x-id-oidc-signedin", "1")
-      |> Router.call([])
-
-    assert {200, headers, body} = sent_resp(response_conn)
-    assert {"cache-control", "private, stale-if-error=90, stale-while-revalidate=30"} in headers
-    assert body == lambda_response["body"]
+    build_request()
+    |> personalise_request(token)
+    |> make_request()
+    |> assert_successful_response()
   end
 
-  test "when an invalid authorization token is provided", %{invalid_access_token: access_token} do
-    Belfrage.Clients.LambdaMock
-    |> expect(:call, 0, fn _role_arn, _function_arn, _payload, _request_id, _opts ->
-      flunk("Lambda should not be called")
-    end)
+  test "invalid auth token" do
+    token = AuthToken.invalid_access_token()
 
-    response_conn =
-      conn(:get, "/my/session/webcore-platform")
-      |> Map.put(:host, "www.bbc.co.uk")
-      |> put_req_header("cookie", "ckns_atkn=#{access_token}")
-      |> put_req_header("x-id-oidc-signedin", "1")
-      |> Router.call([])
+    expect_no_origin_request()
 
-    assert {302, headers, _body} = sent_resp(response_conn)
+    conn =
+      build_request()
+      |> personalise_request(token)
+      |> make_request()
+
+    assert conn.status == 302
 
     assert {"location",
-            "https://session.test.bbc.co.uk/session?ptrt=https%3A%2F%2Fwww.bbc.co.uk%2Fmy%2Fsession%2Fwebcore-platform"} in headers
+            "https://session.test.bbc.co.uk/session?ptrt=https%3A%2F%2Fwww.bbc.co.uk%2Fmy%2Fsession%2Fwebcore-platform"} in conn.resp_headers
+  end
+
+  defp expect_origin_request(fun) do
+    expect(LambdaMock, :call, fn _role_arn, _function_arn, request, _request_id, _opts ->
+      fun.(request)
+      {:ok, @response}
+    end)
+  end
+
+  defp expect_no_origin_request() do
+    expect(LambdaMock, :call, 0, fn _role_arn, _function_arn, _request, _request_id, _opts -> nil end)
+  end
+
+  defp build_request() do
+    :get
+    |> conn("/my/session/webcore-platform")
+    |> Map.put(:host, "www.bbc.co.uk")
+  end
+
+  defp personalise_request(conn, token) do
+    conn
+    |> put_req_header("cookie", "ckns_atkn=#{token}")
+    |> put_req_header("x-id-oidc-signedin", "1")
+  end
+
+  defp make_request(conn) do
+    Router.call(conn, [])
+  end
+
+  defp assert_successful_response(conn) do
+    assert conn.status == 200
+    assert conn.resp_body == @response["body"]
   end
 end
