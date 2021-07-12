d7288bc14c7de124a9609d49516aa9c124ab0868
Joseph Pack <josephapack@hotmail.co.uk>
2021-07-12 09:52:21 +0100

    Use if statement instead of case statement and remove unneeded {}

--- lib/belfrage/transformers/platform_kill_switch.ex
+++ lib/belfrage/transformers/platform_kill_switch.ex
@@ -1,6 +1,6 @@
 defmodule Belfrage.Transformers.PlatformKillSwitch do
   use Belfrage.Transformers.Transformer
-  alias Belfrage.Struct.{Private}
+  alias Belfrage.Struct.Private
 
   @dial Application.get_env(:belfrage, :dial)
 
@@ -10,19 +10,16 @@ defmodule Belfrage.Transformers.PlatformKillSwitch do
 
   @impl true
   def call(rest, struct = %Struct{private: %Private{platform: platform}}) do
-    if killswitch_active(platform) do
+    if killswitch_active?(platform) do
       {:stop_pipeline, Struct.add(struct, :response, %{http_status: 500})}
     else
       then(rest, struct)
     end
   end
 
-  defp killswitch_active(platform) do
-    platform_dial_name = @platform_map[platform]
-
-    case platform_dial_name do
-      nil -> false
-      _ -> @dial.state(platform_dial_name)
+  defp killswitch_active?(platform) do
+    if dial = @platform_map[platform] do
+      @dial.state(dial)
     end
   end
 end
