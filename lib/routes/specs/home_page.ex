4bec569b5f10897b86d047cf9279e812f41580b6
Martin Reilly <martin.reilly@bbc.co.uk>
2021-07-19 17:29:48 +0100

    BBCHOME-928 Make UK page personalised

--- lib/routes/specs/home_page.ex
+++ lib/routes/specs/home_page.ex
@@ -1,9 +1,17 @@
 defmodule Routes.Specs.HomePage do
-  def specs do
+  def specs(production_env) do
     %{
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Homepage%20&%20Nations%20-%20WebCore%20-%20Runbook",
-      platform: Webcore
+      platform: Webcore,
+      pipeline: pipeline(production_env),
+      personalisation: "on"
     }
   end
+
+  defp pipeline("live") do
+    ["HTTPredirect", "TrailingSlashRedirector", "Personalisation", "LambdaOriginAlias", "PlatformKillSwitch", "CircuitBreaker", "Language"]
+  end
+
+  defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
 end
