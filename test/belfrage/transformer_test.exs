2c0aefa58fd9d47439aa99c4d4f0ea61384c756e
Joseph Pack <josephapack@hotmail.co.uk>
2021-09-02 10:56:52 +0100

    No need to skip for placeholder as it will not exist

--- test/belfrage/transformer_test.exs
+++ test/belfrage/transformer_test.exs
@@ -14,13 +14,4 @@ defmodule Belfrage.Transformers.TransformerTest do
     struct = %Struct{private: %Struct.Private{pipeline: []}}
     assert {:ok, _struct} = Subject.then([], struct)
   end
-
-  test "when the next transformer is :_routespec_pipeline_placeholder it skips it and calls the next one" do
-    struct = %Struct{
-      private: %Struct.Private{pipeline: ["HTTPredirect", :_routespec_pipeline_placeholder, "CircuitBreaker"]}
-    }
-
-    output_struct = %Struct{private: %Struct.Debug{pipeline_trail: ["CircuitBreaker", "HTTPredirect"]}}
-    assert {:ok, output_struct} = Subject.then(struct.private.pipeline, struct)
-  end
 end
