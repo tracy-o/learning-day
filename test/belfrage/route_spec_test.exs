3aeb9283e7ebe0c940b38a4d0ba3685957252012
Joseph Pack <josephapack@hotmail.co.uk>
2021-09-02 10:57:28 +0100

    remove unnecessary args from tests to avoid confusion

--- test/belfrage/route_spec_test.exs
+++ test/belfrage/route_spec_test.exs
@@ -4,48 +4,48 @@ defmodule Belfrage.RouteSpecTest do
 
   describe "merge_specs/2" do
     test "when values are lists, lists are concatenated" do
-      platform_specs = %{a: ["one"], pipeline: ["foo"], resp_pipeline: ["foo"]}
+      platform_specs = %{a: ["one"]}
       route_specs = %{a: ["two"]}
 
       result = RouteSpec.merge_specs(platform_specs, route_specs)
 
-      assert result == %{a: ["one", "two"], pipeline: ["foo"], resp_pipeline: ["foo"]}
+      assert result == %{a: ["one", "two"]}
     end
 
     test "platform's allow all '*' does not always override routespec value" do
-      platform_specs = %{some_key: "*", pipeline: ["foo"], resp_pipeline: ["foo"]}
+      platform_specs = %{some_key: "*"}
       route_specs = %{some_key: "a-value"}
 
       result = RouteSpec.merge_specs(platform_specs, route_specs)
 
-      assert result == %{some_key: "a-value", pipeline: ["foo"], resp_pipeline: ["foo"]}
+      assert result == %{some_key: "a-value"}
     end
 
     test "when platform allows all headers" do
-      platform_specs = %{headers_allowlist: "*", pipeline: ["foo"], resp_pipeline: ["foo"]}
+      platform_specs = %{headers_allowlist: "*"}
       route_specs = %{headers_allowlist: ["a-header"]}
 
       result = RouteSpec.merge_specs(platform_specs, route_specs)
 
-      assert result == %{headers_allowlist: "*", pipeline: ["foo"], resp_pipeline: ["foo"]}
+      assert result == %{headers_allowlist: "*"}
     end
 
     test "when platform allows all query params" do
-      platform_specs = %{query_params_allowlist: "*", pipeline: ["foo"], resp_pipeline: ["foo"]}
+      platform_specs = %{query_params_allowlist: "*"}
       route_specs = %{query_params_allowlist: ["a-param"]}
 
       result = RouteSpec.merge_specs(platform_specs, route_specs)
 
-      assert result == %{query_params_allowlist: "*", pipeline: ["foo"], resp_pipeline: ["foo"]}
+      assert result == %{query_params_allowlist: "*"}
     end
 
     test "basic behaviour for values, is that route value takes precedence" do
-      platform_specs = %{foo: "I want the platform to win", pipeline: ["foo"], resp_pipeline: ["foo"]}
+      platform_specs = %{foo: "I want the platform to win"}
       route_specs = %{foo: "I want the route spec to win"}
 
       result = RouteSpec.merge_specs(platform_specs, route_specs)
 
-      assert result == %{foo: "I want the route spec to win", pipeline: ["foo"], resp_pipeline: ["foo"]}
+      assert result == %{foo: "I want the route spec to win"}
     end
 
     test "platforms keys are returned if the same response key is not set" do
