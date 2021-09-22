2539b00e6f7d93004de0c62a385ad936473876d3
Eugene Bolshakov <eugene.bolshakov@bbc.co.uk>
2021-09-22 19:33:55 +0100

    Add tests for Belfrage.Metrics
    Testing that metrics are recorded somewhere in the code is not always
    necessary, but we need to have tests for the module that records them.

---
+++ test/belfrage/metrics_test.exs
@@ -0,0 +1,49 @@
+defmodule Belfrage.MetricsTest do
+  use ExUnit.Case, async: true
+  import Belfrage.Test.MetricsHelper
+
+  alias Belfrage.Metrics
+
+  describe "measurement/3" do
+    test "emits an event with the passed measurements and metadata" do
+      assert_metric({:foo, %{bar: 1}, %{baz: 2}}, fn ->
+        Metrics.measurement(:foo, %{bar: 1}, %{baz: 2})
+      end)
+    end
+  end
+
+  describe "duration/2" do
+    test "emits a stop event with the duration of the passed function" do
+      {_, %{duration: duration}, %{}} =
+        intercept_metric([:foo, :stop], fn ->
+          Metrics.duration(:foo, fn -> Process.sleep(1) end)
+        end)
+
+      assert duration > 0
+    end
+  end
+
+  describe "duration/3" do
+    test "emits a stop event with the duration of the passed function and metadata" do
+      {_, %{duration: duration}, %{bar: :baz}} =
+        intercept_metric([:foo, :stop], fn ->
+          Metrics.duration(:foo, %{bar: :baz}, fn -> Process.sleep(1) end)
+        end)
+
+      assert duration > 0
+    end
+  end
+
+  describe "stop/3" do
+    test "emits a stop event with duration measurement and metadata" do
+      start_time = System.monotonic_time() - 1
+
+      {_, %{duration: duration}, %{bar: :baz}} =
+        intercept_metric([:foo, :stop], fn ->
+          Metrics.stop(:foo, start_time, %{bar: :baz})
+        end)
+
+      assert duration > 0
+    end
+  end
+end
--- test/support/metrics_helper.ex
+++ test/support/metrics_helper.ex
@@ -2,18 +2,48 @@ defmodule Belfrage.Test.MetricsHelper do
   import ExUnit.Assertions
 
   @doc """
-  Assert that the passed metric is recorded during the execution of the passed
+  Asserts that the passed metric is recorded during the execution of the passed
   function.
   """
   def assert_metric(metric, fun), do: assert_metrics(List.wrap(metric), fun)
 
   @doc """
-  Assert that the passed metrics are recorded during the execution of the
+  Asserts that the passed metrics are recorded during the execution of the
   passed function.
   """
   def assert_metrics(metrics, fun) do
-    metrics = Enum.map(metrics, fn {event, measurement, metadata} -> {[:belfrage | event], measurement, metadata} end)
-    events = Enum.map(metrics, fn {event, _, _} -> event end)
+    metrics = Enum.map(metrics, fn {event, measurement, metadata} -> {prefix_event(event), measurement, metadata} end)
+
+    metrics
+    |> Enum.map(fn {event, _, _} -> event end)
+    |> listen_to_events()
+
+    fun.()
+
+    Enum.each(metrics, fn metric ->
+      message = Tuple.insert_at(metric, 0, :telemetry_event)
+      assert_receive ^message
+    end)
+  end
+
+  @doc """
+  Asserts that a metric with the passed name is recorded during execution of
+  the passed function and returns the metric as a {event, measurements,
+  metadata} tuple.
+  """
+  def intercept_metric(name, fun) do
+    event = prefix_event(name)
+    listen_to_events([event])
+    fun.()
+    assert_receive {:telemetry_event, ^event, _measurement, _metatdata} = metric
+    Tuple.delete_at(metric, 0)
+  end
+
+  defp prefix_event(name) do
+    [:belfrage | List.wrap(name)]
+  end
+
+  defp listen_to_events(events) do
     self = self()
 
     :telemetry.attach_many(
@@ -24,12 +54,5 @@ defmodule Belfrage.Test.MetricsHelper do
       end,
       nil
     )
-
-    fun.()
-
-    Enum.each(metrics, fn metric ->
-      message = Tuple.insert_at(metric, 0, :telemetry_event)
-      assert_receive ^message
-    end)
   end
 end
