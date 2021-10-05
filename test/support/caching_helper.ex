c40b359eb2bb87f9de009c4b71db4c7f8ee0ecd7
chrishop <christopher.hopkins@bbc.co.uk>
2021-10-05 11:40:16 +0100

    adds docs to all caching_helper functions
    also rearanges functions to be in logical order private functions at the bottom

--- test/support/caching_helper.ex
+++ test/support/caching_helper.ex
@@ -14,6 +14,10 @@ defmodule Belfrage.Test.CachingHelper do
     end
   end
 
+  @doc """
+  Using this will ensure that the caching key is unique to the test and prevent
+  clashes with other tests. But will also ensure the key is in the uuid format.
+  """
   defmacro unique_cache_key() do
     quote do
       UUID.uuid4(:hex) |> cache_key()
@@ -51,12 +55,13 @@ defmodule Belfrage.Test.CachingHelper do
     response
   end
 
-  defp default_cache_directive(cache_directive) do
-    if cache_directive == %CacheControl{cacheability: "private"} do
-      %CacheControl{cacheability: "public", max_age: 60}
-    else
-      cache_directive
-    end
+  @doc """
+  Puts the struct's response into the cache and marks the cached response as stale.
+  """
+  def put_into_cache_as_stale(struct = %Struct{}) do
+    request_hash = struct.request.request_hash || Belfrage.RequestHash.generate(struct)
+    put_into_cache(request_hash, struct.response)
+    make_cached_reponse_stale(request_hash)
   end
 
   @doc """
@@ -67,12 +72,11 @@ defmodule Belfrage.Test.CachingHelper do
     put_into_cache(key, %Response{response | cache_last_updated: Timer.now_ms() - 61_000})
   end
 
-  @doc """
-  Puts the struct's response into the cache and marks the cached response as stale.
-  """
-  def put_into_cache_as_stale(struct = %Struct{}) do
-    request_hash = struct.request.request_hash || Belfrage.RequestHash.generate(struct)
-    put_into_cache(request_hash, struct.response)
-    make_cached_reponse_stale(request_hash)
+  defp default_cache_directive(cache_directive) do
+    if cache_directive == %CacheControl{cacheability: "private"} do
+      %CacheControl{cacheability: "public", max_age: 60}
+    else
+      cache_directive
+    end
   end
 end
