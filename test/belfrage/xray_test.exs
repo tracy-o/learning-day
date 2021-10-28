05af99d2ae21b67965e87b477244e33e47a25d4b
chrishop <christopher.hopkins@bbc.co.uk>
2021-10-28 11:51:43 +0100

    when exception -> when catches

--- test/belfrage/xray_test.exs
+++ test/belfrage/xray_test.exs
@@ -41,31 +41,31 @@ defmodule Belfrage.XrayTest do
     end
   end
 
-  describe "start_tracing/2 on success" do
+  describe "start_tracing/1 on success" do
     test "returns {:ok, trace}" do
       assert {:ok, _trace} = Xray.start_tracing("Belfrage")
     end
   end
 
-  describe "start_tracing/2 on error" do
-    test "when exception :error, returns {:error, reason}" do
+  describe "start_tracing/1 on error" do
+    test "when catches :error, returns {:error, reason}" do
       Xray.start_tracing("Belfrage")
       {:error, msg} = Xray.start_tracing("Belfrage")
       assert msg =~ "AwsExRay.start_tracing/1 errored with reason: "
     end
 
-    test "when exception :error, logs error" do
+    test "when catches :error, logs error" do
       Xray.start_tracing("Belfrage")
       log = capture_log(fn -> Xray.start_tracing("Belfrage") end)
       assert log =~ "AwsExRay.start_tracing/1 errored with reason: "
     end
 
-    test "when exception :exit, returns {:error, reason}" do
+    test "when catches :exit, returns {:error, reason}" do
       {:error, msg} = Xray.start_tracing("Belfrage", AwsExRayMockExit)
       assert msg == "AwsExRay.start_tracing/1 exited with reason: :start_tracing"
     end
 
-    test "when exception :exit, logs error" do
+    test "when catches :exit, logs error" do
       log = capture_log(fn -> Xray.start_tracing("Belfrage", AwsExRayMockExit) end)
       assert log =~ "AwsExRay.start_tracing/1 exited with reason: :start_tracing"
     end
@@ -78,20 +78,20 @@ defmodule Belfrage.XrayTest do
   end
 
   describe "finish_tracing/1 on error" do
-    test "when exception :error, returns :ok" do
+    test "when catches :error, returns :ok" do
       assert :ok == Xray.finish_tracing(@segment, AwsExRayMockError)
     end
 
-    test "when exception :error, logs error" do
+    test "when catches :error, logs error" do
       log = capture_log(fn -> Xray.finish_tracing(@segment, AwsExRayMockError) end)
       assert log =~ "AwsExRay.finish_tracing/1 errored with reason: "
     end
 
-    test "when exception :exit, returns :ok" do
+    test "when catches :exit, returns :ok" do
       assert :ok == Xray.finish_tracing(@segment, AwsExRayMockExit)
     end
 
-    test "when exception :exit, logs error" do
+    test "when catches :exit, logs error" do
       log = capture_log(fn -> Xray.finish_tracing(@segment, AwsExRayMockExit) end)
       assert log =~ "AwsExRay.finish_tracing/1 exited with reason: :finish_tracing"
     end
@@ -109,22 +109,22 @@ defmodule Belfrage.XrayTest do
   end
 
   describe "start_subsegment/1 on error" do
-    test "when exception :error, returns :ok" do
+    test "when catches :error, returns :ok" do
       {:error, msg} = Xray.start_subsegment("test_segment")
       assert msg =~ "AwsExRay.start_subsegment/1 errored with reason: "
     end
 
-    test "when exception :error, logs error" do
+    test "when catches :error, logs error" do
       log = capture_log(fn -> Xray.start_subsegment("test_segment") end)
       assert log =~ "AwsExRay.start_subsegment/1 errored with reason: "
     end
 
-    test "when exception :exit, returns :ok" do
+    test "when catches :exit, returns :ok" do
       {:error, msg} = Xray.start_subsegment("test_segment", AwsExRayMockExit)
       assert msg == "AwsExRay.start_subsegment/1 exited with reason: :start_subsegment"
     end
 
-    test "when exception :exit, logs error" do
+    test "when catches :exit, logs error" do
       log = capture_log(fn -> Xray.start_subsegment("test_segment", AwsExRayMockExit) end)
       assert log =~ "AwsExRay.start_subsegment/1 exited with reason: :start_subsegment"
     end
@@ -137,20 +137,20 @@ defmodule Belfrage.XrayTest do
   end
 
   describe "finish_subsegment/1 on error" do
-    test "when exception :error, returns :ok" do
+    test "when catches :error, returns :ok" do
       assert :ok == Xray.finish_subsegment(@segment, AwsExRayMockError)
     end
 
-    test "when exception :error, logs error" do
+    test "when catches :error, logs error" do
       log = capture_log(fn -> Xray.finish_subsegment(@segment, AwsExRayMockError) end)
       assert log =~ "AwsExRay.finish_subsegment/1 errored with reason: "
     end
 
-    test "when exception :exit, returns :ok" do
+    test "when catches :exit, returns :ok" do
       assert :ok == Xray.finish_subsegment(@segment, AwsExRayMockExit)
     end
 
-    test "when exception :exit, logs error" do
+    test "when catches :exit, logs error" do
       log = capture_log(fn -> Xray.finish_subsegment(@segment, AwsExRayMockExit) end)
       assert log =~ "AwsExRay.finish_subsegment/1 exited with reason: :finish_subsegment"
     end
