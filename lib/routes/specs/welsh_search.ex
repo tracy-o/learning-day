876ce9444d82644013a937ca90f4d235f7c0d0c1
Joseph Pack <josephapack@hotmail.co.uk>
2021-11-09 12:43:30 +0000

    Introduce :_routespec_pipeline_placeholder for WelshSearch routespec

--- lib/routes/specs/welsh_search.ex
+++ lib/routes/specs/welsh_search.ex
@@ -1,20 +1,12 @@
 defmodule Routes.Specs.WelshSearch do
-  def specs(production_env) do
+  def specs do
     %{
       owner: "D+ESearchAndNavigationDev@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/x/xo2KD",
-      pipeline: pipeline(production_env),
+      pipeline: ["ComToUKRedirect"],
       platform: Webcore,
       query_params_allowlist: ["q", "page", "scope", "filter"],
       default_language: "cy"
     }
-  end
-
-  defp pipeline("live") do
-    ["HTTPredirect", "TrailingSlashRedirector", "ComToUKRedirect", "Personalisation", "LambdaOriginAlias", "Language", "PlatformKillSwitch", "Chameleon", "CircuitBreaker"]
-  end
-
-  defp pipeline(_production_env) do
-    pipeline("live") ++ ["DevelopmentRequests"]
   end
 end
