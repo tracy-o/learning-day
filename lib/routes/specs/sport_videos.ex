3ec618bfca3126a4ca266cfe2fa6ae07e5d6778e
Joseph Pack <josephapack@hotmail.co.uk>
2021-11-09 10:56:50 +0000

    Introduce :_routespec_pipeline_placeholder for Webcore platform routespecs

--- lib/routes/specs/sport_videos.ex
+++ lib/routes/specs/sport_videos.ex
@@ -5,20 +5,12 @@ defmodule Routes.Specs.SportVideos do
       runbook: "https://confluence.dev.bbc.co.uk/display/SFV/Short+Form+Video+Run+Book",
       platform: Webcore,
       query_params_allowlist: query_params_allowlist(production_env),
-      pipeline: pipeline(production_env)
+      pipeline: ["DatalabMachineRecommendations"]
     }
   end
 
   defp query_params_allowlist("live"), do: []
   defp query_params_allowlist(_production_env), do: ["features"]
-
-  defp pipeline("live") do
-    ["HTTPredirect", "TrailingSlashRedirector", "Personalisation", "LambdaOriginAlias", "Language", "DatalabMachineRecommendations", "PlatformKillSwitch", "Chameleon", "CircuitBreaker"]
-  end
-
-  defp pipeline(_production_env) do
-    pipeline("live") ++ ["DevelopmentRequests"]
-  end
 
   def sports_disciplines_routes do
     [
