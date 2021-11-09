5f4c17a7faf49325f80847eba72f32495957cc5d
Joseph Pack <josephapack@hotmail.co.uk>
2021-11-09 11:54:58 +0000

    Introduce :_routespec_pipeline_placeholder for Search routespec

--- lib/routes/specs/search.ex
+++ lib/routes/specs/search.ex
@@ -3,17 +3,9 @@ defmodule Routes.Specs.Search do
     %{
       owner: "D+ESearchAndNavigationDev@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/x/xo2KD",
-      pipeline: pipeline(production_env),
+      pipeline: ["ComToUKRedirect"],
       platform: Webcore,
       query_params_allowlist: ["q", "page", "scope", "filter"]
     }
-  end
-
-  defp pipeline("live") do
-    ["HTTPredirect", "TrailingSlashRedirector", "ComToUKRedirect", "Personalisation", "LambdaOriginAlias", "Language", "PlatformKillSwitch", "Chameleon", "CircuitBreaker"]
-  end
-
-  defp pipeline(_production_env) do
-    pipeline("live") ++ ["DevelopmentRequests"]
   end
 end
