e9c21a5ca52bd910aac9ad1da979030986ef550a
chrishop <christopher.hopkins@bbc.co.uk>
2021-11-11 14:11:19 +0000

    adds new platform 'MorphRouter' and 2 RouteSpecs using it
    The MorphRouter platform uses the new :morph_router_endpoint as an origin
    
    This commit also creates 2 new route specs BitesizeTransition and BitesizeLegacy
    Both of these are used for the purpose of migrating bitesize from morph router to webcore.
    
    BitesizeTransition spec goes to webcore on test, and morph router on live.
    
    BitesizeLegacy spec goes to morph router.
    
    When the migration is complete these two routes will be removed and replaced with the Bitesize spec.

---
+++ lib/routes/specs/bitesize_legacy.ex
@@ -0,0 +1,15 @@
+defmodule Routes.Specs.BitesizeLegacy do
+  @moduledoc """
+  This RouteSpec is temporary. It exists because of a requirement in
+  https://jira.dev.bbc.co.uk/browse/RESFRAME-4439 to have a RouteSpec which
+  sends all of its traffic to morph router, to aid the migration of bitesize
+  from morph router to webcore.
+  """
+
+  def specs do
+    %{
+      owner: "bitesize-developers@bbc.co.uk",
+      platform: MorphRouter
+    }
+  end
+end
---
+++ lib/routes/specs/bitesize_transition.ex
@@ -0,0 +1,23 @@
+defmodule Routes.Specs.BitesizeTransition do
+  @moduledoc """
+  This RouteSpec is temporary. It exists because of a requirement in
+  https://jira.dev.bbc.co.uk/browse/RESFRAME-4439 to have a RouteSpec which goes to
+  different origins depending on environment, to aid the migration of bitesize
+  from morph router to webcore.
+  """
+
+
+  def specs("live") do
+    %{
+      owner: "bitesize-developers@bbc.co.uk",
+      platform: MorphRouter
+    }
+  end
+
+  def specs(_production_env) do
+    %{
+      owner: "bitesize-developers@bbc.co.uk",
+      platform: Webcore
+    }
+  end
+end
