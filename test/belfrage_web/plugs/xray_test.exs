b18b1a1910f4bb9d740d08f1c839edd8d5b859d3
chrishop <christopher.hopkins@bbc.co.uk>
2021-12-01 08:48:16 +0000

    reimplemented Belfrage.Plugs.Xray  with new xray approach
    also renamed Plug.XRay -> Plug.Xray for consistency

--- test/belfrage_web/plugs/xray_test.exs
+++ test/belfrage_web/plugs/xray_test.exs
@@ -26,15 +26,15 @@ defmodule BelfrageWeb.Plugs.XrayTest do
     end
 
     test "puts the segment into the conn", %{conn: conn} do
-      assert conn.private[:xray_segment]
+      assert conn.assigns[:xray_segment]
     end
 
     test "segment has annotations", %{conn: conn} do
-      assert %{request_id: _} = conn.private[:xray_segment].annotation
+      assert %{request_id: _} = conn.assigns[:xray_segment].annotation
     end
 
     test "segment has http request set", %{conn: conn} do
-      request = conn.private[:xray_segment].http.request
+      request = conn.assigns[:xray_segment].http.request
 
       assert request == %HTTPRequest{
                segment_type: :segment,
@@ -43,17 +43,13 @@ defmodule BelfrageWeb.Plugs.XrayTest do
              }
     end
 
-    test "registers a before_send callback", %{conn: conn} do
-      assert length(conn.before_send) == 1
-    end
-
     test "when response had been sent, finishes tracing", %{conn: conn} do
       conn =
         conn
         |> resp(200, "OK")
         |> send_resp()
 
-      assert conn.private[:xray_segment].end_time > 0
+      assert conn.assigns[:xray_segment].end_time > 0
     end
 
     test "when response had been sent, sends message to client", %{conn: conn} do
@@ -65,48 +61,6 @@ defmodule BelfrageWeb.Plugs.XrayTest do
       json = Jason.decode!(data)
       assert json["trace_id"]
       assert json["name"] == "Belfrage"
-    end
-  end
-
-  describe "call/2 when no tracing is occuring" do
-    setup do
-      conn =
-        conn(:get, "/some/route")
-        |> Plugs.RequestId.call([])
-        |> Plugs.Xray.call(xray_client: IgnoreXray)
-
-      %{conn: conn}
-    end
-
-    test "puts segment into the conn", %{conn: conn} do
-      assert conn.private[:xray_segment]
-    end
-
-    test "segment has no annotations", %{conn: conn} do
-      assert %{} == conn.private[:xray_segment].annotation
-    end
-
-    test "segment has no http request set", %{conn: conn} do
-      request = conn.private[:xray_segment].http.request
-
-      assert request == nil
-    end
-
-    test "when response had been sent, doesn't finish tracing", %{conn: conn} do
-      conn =
-        conn
-        |> resp(200, "OK")
-        |> send_resp()
-
-      assert conn.private[:xray_segment].end_time == 0
-    end
-
-    test "when response had been sent, doesn't send message to client", %{conn: conn} do
-      conn
-      |> resp(200, "OK")
-      |> send_resp()
-
-      refute_received {:client_mock, _data}
     end
   end
 end
