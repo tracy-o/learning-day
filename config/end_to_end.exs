c27516d8f659cb968df630c1cd496f600e6dadff
Eugene Bolshakov <eugene.bolshakov@bbc.co.uk>
2021-12-10 12:18:32 +0000

    Use the same origin URLs in all test envs
    The URLs themselves don't matter in tests, so using the same ones allows
    to unify the configuration for the environments.
    
    Also updated `AllowAllQsOverrideTest` to always set
    `production_environment` back to `"test"`, even if it fails. Before this
    change if that test failed, the value remained `"live"` and caused
    confusing failures in other tests.

--- config/end_to_end.exs
+++ config/end_to_end.exs
@@ -1,8 +1,3 @@
 use Mix.Config
 
 import_config "test.exs"
-
-config :belfrage,
-  pwa_lambda_function: "arn:aws:lambda:eu-west-1:997052946310:function:test-presentation-layer-lambda",
-  mozart_news_endpoint: "https://www.mozart-routing.test.api.bbci.co.uk",
-  fabl_endpoint: "https://fabl.test.api.bbci.co.uk"
