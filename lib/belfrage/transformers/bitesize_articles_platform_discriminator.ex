2d561d908fcb767404ea8ceedf6626d218994d4e
Hope Ngerebara <h.ngerebara@gmail.com>
2021-12-21 17:40:26 +0100

    remove unused code

--- lib/belfrage/transformers/bitesize_articles_platform_discriminator.ex
+++ lib/belfrage/transformers/bitesize_articles_platform_discriminator.ex
@@ -4,8 +4,6 @@ defmodule Belfrage.Transformers.BitesizeArticlesPlatformDiscriminator do
   """
   use Belfrage.Transformers.Transformer
 
-  app_env = Application.get_env(:belfrage, :production_environment)
-
   @webcore_test_ids [
     "zm8fhbk",
     "zjykkmn",
@@ -14,32 +12,8 @@ defmodule Belfrage.Transformers.BitesizeArticlesPlatformDiscriminator do
 
   @webcore_live_ids []
 
-  @webcore_ids (if(app_env === "live") do
-                  @webcore_live_ids
-                else
-                  @webcore_test_ids
-                end)
-
-  def call(
-        _rest,
-        struct = %Struct{request: %Struct.Request{path_params: %{"id" => id, "slug" => _slug}}}
-      )
-      when id in @webcore_ids do
-    {
-      :redirect,
-      Struct.add(struct, :response, %{
-        http_status: 302,
-        headers: %{
-          "location" => "/bitesize/articles/#{id}",
-          "x-bbc-no-scheme-rewrite" => "1",
-          "cache-control" => "public, stale-while-revalidate=10, max-age=60"
-        },
-        body: "Redirecting"
-      })
-    }
-  end
-
-  def call(_rest, struct = %Struct{request: %Struct.Request{path_params: %{"id" => id}}}) when id in @webcore_ids do
+  def call(_rest, struct = %Struct{request: %Struct.Request{path_params: %{"id" => id}}})
+      when id in @webcore_test_ids or @webcore_live_ids do
     then(
       ["Personalisation", "LambdaOriginAlias", "PlatformKillSwitch", "CircuitBreaker", "Language"],
       Struct.add(struct, :private, %{
