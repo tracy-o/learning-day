de7c2797ae6c00c03e7bb6d2afeb92e242374023
chrishop <christopher.hopkins@bbc.co.uk>
2022-01-18 12:36:56 +0000

    Refactored circuit breaker transformer now the logic is in Belfrage.CircuitBreaker
    See how in this module only includes a small amount of logic related to the Transformer, as it should be!

--- lib/belfrage/transformers/circuit_breaker.ex
+++ lib/belfrage/transformers/circuit_breaker.ex
@@ -5,40 +5,9 @@ defmodule Belfrage.Transformers.CircuitBreaker do
 
   @impl true
   def call(rest, struct) do
-    case threshold_exceeded?(error_count(struct), threshold(struct)) do
-      true ->
-        maybe_apply_circuit_breaker(rest, struct, @dial.state(:circuit_breaker))
-
-      false ->
-        then(rest, struct)
+    case Belfrage.CircuitBreaker.apply_circuit_breaker?(struct, @dial.state(:circuit_breaker)) do
+      {:active, active_struct} -> {:stop_pipeline, active_struct}
+      {:inactive, inactive_struct} -> then(rest, inactive_struct)
     end
   end
-
-  defp maybe_apply_circuit_breaker(rest, struct, dial_enabled?) do
-    case dial_enabled? do
-      true -> {:stop_pipeline, circuit_breaker_active(struct)}
-      false -> then(rest, struct)
-    end
-  end
-
-  defp circuit_breaker_active(struct = %Belfrage.Struct{}) do
-    Belfrage.Event.record(:metric, :increment, "circuit_breaker.active")
-
-    struct
-    |> Struct.add(:response, %{http_status: 500})
-    |> Struct.add(:private, %{origin: :belfrage_circuit_breaker})
-  end
-
-  defp threshold_exceeded?(error_count, threshold), do: error_count > threshold
-
-  # The origin defined in private is used to fetch :errors inside long_counter
-  # This is used to match the :errors map origin key
-  defp error_count(%Struct{private: %{origin: origin, long_counter: counts}}) do
-    parse_count(get_in(counts, [origin, :errors]))
-  end
-
-  defp parse_count(nil), do: 0
-  defp parse_count(x), do: x
-
-  defp threshold(%Struct{private: %{circuit_breaker_error_threshold: threshold}}), do: threshold
 end
