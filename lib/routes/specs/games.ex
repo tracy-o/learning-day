4a4bb01247ad36479cc80a2f1961f076878d7f1e
Nick Tipping <nick.tipping@bbc.co.uk>
2022-01-19 15:39:04 +0000

    Allow "exitGameUrl" and "v" query parameters on route

--- lib/routes/specs/games.ex
+++ lib/routes/specs/games.ex
@@ -3,7 +3,8 @@ defmodule Routes.Specs.Games do
     %{
       owner: "#games-team",
       runbook: "https://confluence.dev.bbc.co.uk/display/CE/CAGE+Games+Runbook",
-      platform: Webcore
+      platform: Webcore,
+      query_params_allowlist: ["exitGameUrl", "v"]
     }
   end
 end
