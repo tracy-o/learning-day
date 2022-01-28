a7641d8b2056bde497d266ae7591f06da3fa9619
Nanbob <hannahgrey1987@hotmail.co.uk>
2022-01-28 10:10:55 +0000

    Add language to pipeline in bitesize article route

--- lib/routes/specs/bitesize_articles.ex
+++ lib/routes/specs/bitesize_articles.ex
@@ -4,7 +4,7 @@ defmodule Routes.Specs.BitesizeArticles do
       owner: "bitesize-production@lists.forge.bbc.co.uk",
       platform: MorphRouter,
       language_from_cookie: true,
-      pipeline: ["BitesizeArticlesPlatformDiscriminator", "HTTPredirect", "LambdaOriginAlias", "TrailingSlashRedirector", "CircuitBreaker"]
+      pipeline: ["BitesizeArticlesPlatformDiscriminator", "HTTPredirect", "LambdaOriginAlias", "TrailingSlashRedirector", "CircuitBreaker", "Language"]
     }
   end
 
@@ -13,7 +13,7 @@ defmodule Routes.Specs.BitesizeArticles do
       owner: "bitesize-production@lists.forge.bbc.co.uk",
       platform: MorphRouter,
       language_from_cookie: true,
-      pipeline: ["BitesizeArticlesPlatformDiscriminator", "HTTPredirect", "LambdaOriginAlias", "TrailingSlashRedirector", "DevelopmentRequests", "CircuitBreaker"]
+      pipeline: ["BitesizeArticlesPlatformDiscriminator", "HTTPredirect", "LambdaOriginAlias", "TrailingSlashRedirector", "DevelopmentRequests", "CircuitBreaker", "Language"]
     }
   end
 end
