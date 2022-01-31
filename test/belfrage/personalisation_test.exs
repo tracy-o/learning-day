d042f0547d10f5347dcb5e157b7306e437909ef5
Joseph Pack <josephapack@hotmail.co.uk>
2022-01-31 11:43:42 +0000

    Introduce NewsArticlePersonalisation dial

--- test/belfrage/personalisation_test.exs
+++ test/belfrage/personalisation_test.exs
@@ -64,7 +64,7 @@ defmodule Belfrage.PersonalisationTest do
     end
   end
 
-  describe "enabled?/0" do
+  describe "enabled?/1" do
     defmodule BBCIDAvailable do
       def available?(), do: true
     end
@@ -85,6 +85,30 @@ defmodule Belfrage.PersonalisationTest do
 
       stub_dial(:personalisation, "on")
       assert Personalisation.enabled?(bbc_id: BBCIDAvailable)
+    end
+
+    test "returns true when both the news article personalisation and personalisation dial is on" do
+      stub_dial(:news_articles_personalisation, "on")
+      stub_dial(:personalisation, "on")
+      assert(Personalisation.enabled?(route_state_id: HomePagePersonalised))
+    end
+
+    test "returns true when both the news article personalisation and personalisation dial is on for PersonalisedContainerData routes" do
+      stub_dial(:news_articles_personalisation, "on")
+      stub_dial(:personalisation, "on")
+      assert(Personalisation.enabled?(route_state_id: PersonalisedContainerData))
+    end
+
+    test "returns false when the news article personalisation is off and personalisation dial is on for PersonalisedContainerData routes" do
+      stub_dial(:news_articles_personalisation, "off")
+      stub_dial(:personalisation, "on")
+      refute(Personalisation.enabled?(route_state_id: PersonalisedContainerData))
+    end
+
+    test "returns false when global personalisation dial is off for PersonalisedContainerData routes" do
+      stub_dial(:news_articles_personalisation, "on")
+      stub_dial(:personalisation, "off")
+      refute(Personalisation.enabled?(route_state_id: PersonalisedContainerData))
     end
   end
 
