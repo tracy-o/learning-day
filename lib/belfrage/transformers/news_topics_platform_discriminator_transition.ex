fb2e6ab6e1a860c041534160006e031834b5e057
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2022-02-16 11:55:45 +0000

    [RESFRAME-4548] Make logic in News Topics transition transformer clearer

--- lib/belfrage/transformers/news_topics_platform_discriminator_transition.ex
+++ lib/belfrage/transformers/news_topics_platform_discriminator_transition.ex
@@ -7,43 +7,40 @@ defmodule Belfrage.Transformers.NewsTopicsPlatformDiscriminatorTransition do
 
   @mozart_news_topic_ids NewsTopicIds.get()
 
-  def call(
-        _rest,
-        struct = %Struct{request: %Struct.Request{path_params: %{"id" => id, "slug" => _slug}}}
-      ) do
-    if id in @mozart_news_topic_ids do
-      then(
-        ["CircuitBreaker"],
-        Struct.add(struct, :private, %{
-          platform: MozartNews
-        })
-      )
-    else
-      {
-        :redirect,
-        Struct.add(struct, :response, %{
-          http_status: 302,
-          headers: %{
-            "location" => "/news/topics/#{id}",
-            "x-bbc-no-scheme-rewrite" => "1",
-            "cache-control" => "public, stale-while-revalidate=10, max-age=60"
-          },
-          body: "Redirecting"
-        })
-      }
+  def call(_rest, struct) do
+    cond do
+      redirect?(struct) ->
+        {
+          :redirect,
+          Struct.add(struct, :response, %{
+            http_status: 302,
+            headers: %{
+              "location" => "/news/topics/#{struct.request.path_params["id"]}",
+              "x-bbc-no-scheme-rewrite" => "1",
+              "cache-control" => "public, stale-while-revalidate=10, max-age=60"
+            },
+            body: "Redirecting"
+          })
+        }
+
+      to_mozart_news?(struct) ->
+        then(
+          ["CircuitBreaker"],
+          Struct.add(struct, :private, %{
+            platform: MozartNews
+          })
+        )
+
+      true ->
+        then([], struct)
     end
   end
 
-  def call(_rest, struct = %Struct{request: %Struct.Request{path_params: %{"id" => id}}}) do
-    if id in @mozart_news_topic_ids do
-      then(
-        ["CircuitBreaker"],
-        Struct.add(struct, :private, %{
-          platform: MozartNews
-        })
-      )
-    else
-      then([], struct)
-    end
+  defp redirect?(struct) do
+    Map.has_key?(struct.request.path_params, "slug") and struct.request.path_params["id"] not in @mozart_news_topic_ids
+  end
+
+  defp to_mozart_news?(struct) do
+    struct.request.path_params["id"] in @mozart_news_topic_ids
   end
 end
