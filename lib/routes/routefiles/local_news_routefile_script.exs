916344cf61d26a192c1a2c1df4ca704711fb52a2
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2022-02-28 12:01:17 +0000

    [RESFRAME-4543] Pass credo checks

--- lib/routes/routefiles/local_news_routefile_script.exs
+++ lib/routes/routefiles/local_news_routefile_script.exs
@@ -1,14 +1,18 @@
 IO.puts("defmodule Belfrage.Transformers.LocalNewsTopicsRedirect.LocationTopicMappings do")
 
 for n <- 1..17_000 do
-  case n do
-    1 ->
+  cond do
+    n == 1 ->
       IO.puts("@location_topic_mappings %{ \"#{n}\" => #{n},")
 
-    17_000 ->
-      IO.puts("  \"#{n}\" => #{n} }")
+    n == 17_000 ->
+      IO.puts("  \"#{n}\" => 17_000 }")
 
-    _ ->
+    n >= 10_000 ->
+      <<first_two_digits::bytes-size(2), rest::binary>> = "#{n}"
+      IO.puts("  \"#{n}\" => #{first_two_digits}_#{rest},")
+
+    true ->
       IO.puts("  \"#{n}\" => #{n},")
   end
 end
