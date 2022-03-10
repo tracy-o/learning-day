0ec2cf9496837ffcddcd1fd4324d1e3712d22236
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2022-03-10 11:28:42 +0000

    Update lib/routes/routefiles/main.ex
    Co-authored-by: RMC <59573468+rebeccamcginn@users.noreply.github.com>

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -1067,7 +1067,7 @@ defroutefile "Main" do
   handle "/zhongwen/new_topics/trad/:id", using: "WorldServiceZhongwenTopicPage", only_on: "test", examples: ["/zhongwen/simp/new_topics/c0dg90z8nqxt"] do
     return_404 if: !String.match?(id, ~r/^(c[a-zA-Z0-9]{10}t)|([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})$/)
   end
-  handle "/zhongwen/new_topics/trad:id.amp", using: "WorldServiceZhongwenTopicPage", only_on: "test", examples: ["/zhongwen/simp/new_topics/c0dg90z8nqxt.amp"] do
+  handle "/zhongwen/trad/new_topics/:id.amp", using: "WorldServiceZhongwenTopicPage", only_on: "test", examples: ["/zhongwen/trad/new_topics/c0dg90z8nqxt.amp"] do
     return_404 if: !String.match?(id, ~r/^(c[a-zA-Z0-9]{10}t)|([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})$/)
   end
   handle "/zhongwen/*_any", using: "WorldServiceZhongwen", examples: ["/zhongwen/simp", "/zhongwen/trad", "/zhongwen/trad.json", "/zhongwen/trad.amp"]
