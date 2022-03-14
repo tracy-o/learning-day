8b6c8e3b8ee3c693610e5fcb3f259891968400f3
cokoyoh <charlesokoyoh@gmail.com>
2022-03-14 17:41:34 +0300

    fix route /bitesize/levels/:level_id/year/year_id example

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -2006,7 +2006,7 @@ defroutefile "Main" do
   handle "/bitesize/subjects", using: "BitesizeTransition", examples: ["/bitesize/subjects"]
   handle "/bitesize/articles/:id", using: "BitesizeArticles", examples: ["/bitesize/articles/zjykkmn"]
   handle "/bitesize/levels/:id", using: "BitesizeWebcorePages", examples: ["/bitesize/levels/z98jmp3"]
-  handle "/bitesize/levels/:level_id/year/:year_id", using: "BitesizeWebcorePages", examples: ["/bitesize/z3g4d2p/year/zmyxxyc"]
+  handle "/bitesize/levels/:level_id/year/:year_id", using: "BitesizeWebcorePages", examples: ["/bitesize/levels/z3g4d2p/year/zmyxxyc"]
   handle "/bitesize/*_any", using: "BitesizeLegacy", examples: ["/bitesize/levels"]
 
   # Games
