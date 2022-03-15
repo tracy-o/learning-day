fa01eaf44b3c1a7ed4d8648c1df0892b012afea0
Leonie Kenyon <leonie.kenyon@bbc.co.uk>
2022-03-15 10:14:21 +0000

    Adds World Service redirect for uploader

--- lib/routes/specs/uploader_world_service.ex
+++ lib/routes/specs/uploader_world_service.ex
@@ -3,7 +3,8 @@ defmodule Routes.Specs.UploaderWorldService do
     %{
       owner: "D&EHomeParticipationTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/pages/viewpage.action?pageId=183485635",
-      platform: MorphRouter
+      platform: MorphRouter,
+      pipeline: ["HTTPredirect", "TrailingSlashRedirector", "WorldServiceRedirect"]
     }
   end
 end
