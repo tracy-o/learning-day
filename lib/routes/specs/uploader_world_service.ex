46e9be1cd23ac229016ae20c08f31768b9d9dc4a
Leonie Kenyon <leonie.kenyon@bbc.co.uk>
2022-03-15 10:23:56 +0000

    Adds correct pipeline for World Service Uploader

--- lib/routes/specs/uploader_world_service.ex
+++ lib/routes/specs/uploader_world_service.ex
@@ -1,10 +1,13 @@
 defmodule Routes.Specs.UploaderWorldService do
-  def specs do
+  def specs(production_env) do
     %{
       owner: "D&EHomeParticipationTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/pages/viewpage.action?pageId=183485635",
       platform: MorphRouter,
-      pipeline: ["HTTPredirect", "TrailingSlashRedirector", "WorldServiceRedirect"]
+      pipeline: pipeline(production_env),
     }
   end
+
+  defp pipeline("live"), do: ["HTTPredirect", "TrailingSlashRedirector", "WorldServiceRedirect", "CircuitBreaker"]
+  defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
 end
