ea451e39168bc4b4eb1b708ce5823f6b4803b7ba
chrishop <christopher.hopkins@bbc.co.uk>
2022-03-21 06:23:41 +0000

    removed ProxyOnJoan for News routespec

--- lib/routes/specs/news.ex
+++ lib/routes/specs/news.ex
@@ -3,8 +3,7 @@ defmodule Routes.Specs.News do
     %{
       owner: "DENewsFrameworksTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
-      platform: MozartNews,
-      pipeline: ["ProxyOnJoan"]
+      platform: MozartNews
     }
   end
 end
