6adcf038b1c8073318526aaa0880a02a5b906e8d
Joseph Pack <josephapack@hotmail.co.uk>
2022-03-22 15:42:57 +0000

    [RESFRAME-4581] Simplify news route checks for InfiniteLoopGuardian.

--- lib/belfrage_web/plugs/infinite_loop_guardian.ex
+++ lib/belfrage_web/plugs/infinite_loop_guardian.ex
@@ -7,16 +7,15 @@ defmodule BelfrageWeb.Plugs.InfiniteLoopGuardian do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    if loop_detected?(conn) do
+    if loop_detected?(conn.request_path, req_chain_count(conn)) do
       send_404(conn)
     else
       conn
     end
   end
 
-  defp loop_detected?(conn) do
-    count = req_chain_count(conn)
-    if news_route?(conn) and bruce_stack?(), do: count > 1, else: count > 0
+  defp loop_detected?(request_path, count) do
+    if news_route?(request_path) and bruce_stack?(), do: count > 1, else: count > 0
   end
 
   defp bruce_stack?() do
@@ -41,9 +40,9 @@ defmodule BelfrageWeb.Plugs.InfiniteLoopGuardian do
     |> Enum.count(&is_belfrage/1)
   end
 
-  defp news_route?(conn) do
+  defp news_route?(request_path) do
     # is true for /news and /news/* request paths
-    String.starts_with?(conn.request_path, "/news")
+    request_path == "/news" or String.starts_with?(request_path, "/news/")
   end
 
   defp is_belfrage("BELFRAGE"), do: true
