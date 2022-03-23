08b7a0891934b0e0b21f94d090e8c239d464a8f3
Ayman Osman <aymano.osman@gmail.com>
2022-03-23 11:45:40 +0000

    format

--- test/smoke/smoke_test.ex
+++ test/smoke/smoke_test.ex
@@ -8,7 +8,7 @@ defmodule Belfrage.SmokeTest do
 
   Routes.Routefiles.Test.routes()
   |> Enum.filter(fn {_route_matcher, %{using: route_state_id, only_on: only_env}} ->
-    not (route_state_id in @ignore_specs) and only_env != "test"
+    route_state_id not in @ignore_specs and only_env != "test"
   end)
   |> Enum.each(fn {route_matcher, matcher_spec} ->
     @matcher_spec Macro.escape(matcher_spec)
