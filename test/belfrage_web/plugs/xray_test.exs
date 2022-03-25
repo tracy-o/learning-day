3d64245a1bd010a6a5ce4c94e4cad753117fb53c
chrishop <christopher.hopkins@bbc.co.uk>
2022-03-25 08:12:03 +0000

    Introduces `Xray.parse_header/2` to `Plugs.Xray`
    Originally a new trace would be started for every request in belfrage. Now we
    are introducing the Joan stack, a belfrage stack might not be the first xray
    instrumented service that an xray trace passes through. (Joan could route to
    Mozart then Bruce). So we should only start a new trace if we don't receive the
    `x-amzn-trace-id` from an upstream service. If we don't receive that header or
    its malformed, we should start our own new trace.
    
    This is what has been implemented in `Plugs.Xray`. A check is made to see if
    the header `x-amzn-trace-id` exists, if its valid, a new segment is made using
    the data within the header. Otherwise a new segment is made using a fresh
    trace.

--- test/belfrage_web/plugs/xray_test.exs
+++ test/belfrage_web/plugs/xray_test.exs
@@ -7,7 +7,7 @@ defmodule BelfrageWeb.Plugs.XrayTest do
   alias BelfrageWeb.Plugs
   alias AwsExRay.Record.HTTPRequest
 
-  describe "call/2 when tracing" do
+  describe "call/2 when tracing and no existing 'x-amzn-trace-id' header" do
     setup do
       conn =
         conn(:get, "/some/route")
@@ -61,6 +61,72 @@ defmodule BelfrageWeb.Plugs.XrayTest do
       json = Jason.decode!(data)
       assert json["trace_id"]
       assert json["name"] == "Belfrage"
+    end
+  end
+
+  describe "call/2 when 'x-amzn-trace-id' header exists" do
+    setup do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      parent = "9d27b4c4bd4b7140"
+      sampled = "0"
+      xray_header = "Root=#{root};Parent=#{parent};Sampled=#{sampled}"
+
+      conn =
+        conn(:get, "/some/route")
+        |> Plug.Conn.put_req_header("x-amzn-trace-id", xray_header)
+        |> Plugs.RequestId.call([])
+        |> Plugs.Xray.call(xray: MockXray)
+
+      %{conn: conn, root: root, parent: parent, sampled: sampled}
+    end
+
+    test "Root value in header is the same as trace root in the segment", %{conn: conn, root: root} do
+      assert conn.assigns[:xray_segment].trace.root == root
+    end
+
+    test "the header has the same 'Parent' as the id of the current segment", %{conn: conn, parent: parent} do
+      assert conn.assigns[:xray_segment].trace.parent == parent
+    end
+
+    test "'Sampled' value in the segment is the same as the header", %{conn: conn, sampled: expected_sample} do
+      sample =
+        if conn.assigns[:xray_segment].trace.sampled do
+          "1"
+        else
+          "0"
+        end
+
+      assert sample == expected_sample
+    end
+  end
+
+  describe "call/2 when 'x-amzn-trace-id' is malformed, starts new trace" do
+    setup do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      parent = "9d27b4c4bd4b7140"
+      # this being the part which malforms the header
+      sampled = "A"
+      xray_header = "Root=#{root};Parent=#{parent};Sampled=#{sampled}"
+
+      conn =
+        conn(:get, "/some/route")
+        |> Plug.Conn.put_req_header("x-amzn-trace-id", xray_header)
+        |> Plugs.RequestId.call([])
+        |> Plugs.Xray.call(xray: MockXray)
+
+      %{conn: conn, root: root, parent: parent, sampled: sampled}
+    end
+
+    test "the root is different", %{conn: conn, root: root} do
+      assert conn.assigns[:xray_segment].trace.root != root
+    end
+
+    test "the parent is empty", %{conn: conn} do
+      assert conn.assigns[:xray_segment].trace.parent == ""
+    end
+
+    test "the sample is a true or false", %{conn: conn} do
+      assert is_boolean(conn.assigns[:xray_segment].trace.sampled)
     end
   end
 end
