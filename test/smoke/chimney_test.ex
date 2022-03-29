1fcdaf79e9861c5ce631e01ead4d3e6bece8ace9
David Birdsall <david.birdsall@bbc.co.uk>
2022-03-29 15:29:50 +0100

    bump the api version number in the sport-app-page examples

--- test/smoke/chimney_test.ex
+++ test/smoke/chimney_test.ex
@@ -56,7 +56,7 @@ defmodule BelfrageChimneySmokeTest do
       resp =
         Helper.get_route(
           endpoint,
-          "/fd/preview/sport-app-page?page=http%3A%2F%2Fwww.bbc.co.uk%2Fsport%2Fgymnastics.app&v=2&platform=ios"
+          "/fd/preview/sport-app-page?page=http%3A%2F%2Fwww.bbc.co.uk%2Fsport%2Fgymnastics.app&v=9&platform=ios"
         )
 
       assert resp.status_code == 200
@@ -70,7 +70,7 @@ defmodule BelfrageChimneySmokeTest do
       resp =
         Helper.get_route(
           endpoint,
-          "/fd/preview/sport-app-page?page=http%3A%2F%2Fwww.bbc.co.uk%2Fsport%2Fgymnastics.app&v=2&platform=ios"
+          "/fd/preview/sport-app-page?page=http%3A%2F%2Fwww.bbc.co.uk%2Fsport%2Fgymnastics.app&v=9&platform=ios"
         )
 
       assert resp.status_code == 200
@@ -84,7 +84,7 @@ defmodule BelfrageChimneySmokeTest do
       resp =
         Helper.get_route(
           endpoint,
-          "/fd/preview/sport-app-page?page=http%3A%2F%2Fwww.bbc.co.uk%2Fsport%2Fgymnastics.app&v=2&platform=ios"
+          "/fd/preview/sport-app-page?page=http%3A%2F%2Fwww.bbc.co.uk%2Fsport%2Fgymnastics.app&v=9&platform=ios"
         )
 
       assert resp.status_code == 200
