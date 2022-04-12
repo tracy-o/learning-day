004dccccc486f0befafe07af754dcee41ec33a8e
rebecca fitzsimmons <rebecca.fitzsimmons@bbc.co.uk>
2022-04-12 13:32:10 +0100

    CONNPOL-7082 - Add routes for new containers

---
+++ lib/routes/specs/container_envelope_winner_flash.ex
@@ -0,0 +1,15 @@
+defmodule Routes.Specs.ContainerEnvelopeWinnerFlash do
+  def specs(production_env) do
+    %{
+      owner: "DENewsElections@bbc.co.uk",
+      platform: Webcore,
+      pipeline: ["UserAgentValidator"],
+      runbook: "https://confluence.dev.bbc.co.uk/display/connpol/Run+book+-+UK+2022",
+      query_params_allowlist: query_params_allowlist(production_env)
+    }
+  end
+
+  defp query_params_allowlist("live"), do: ["static"]
+  defp query_params_allowlist(_production_env), do: ["static", "mode"]
+
+end
