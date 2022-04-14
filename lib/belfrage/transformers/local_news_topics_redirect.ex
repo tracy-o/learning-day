3e20ce42142b9919dd6b6343dc08ce8c93639f4f
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2022-04-14 17:00:33 +0100

    Do not redirect if location ID is not in mappings

--- lib/belfrage/transformers/local_news_topics_redirect.ex
+++ lib/belfrage/transformers/local_news_topics_redirect.ex
@@ -4,7 +4,7 @@ defmodule Belfrage.Transformers.LocalNewsTopicsRedirect do
 
   @impl true
   def call(rest, struct) do
-    if String.starts_with?(struct.request.path, "/news/localnews/") do
+    if redirect?(struct) do
       {
         :redirect,
         Struct.add(struct, :response, %{
@@ -22,19 +22,24 @@ defmodule Belfrage.Transformers.LocalNewsTopicsRedirect do
     end
   end
 
-  defp redirect(request = %Struct.Request{path_params: path_params}) do
-    topic_id =
-      path_params
-      |> Map.get("location_id_and_name")
-      |> String.split("-")
-      |> List.first()
-      |> LocationTopicMappings.get_topic_id()
+  def redirect?(struct) do
+    String.starts_with?(struct.request.path, "/news/localnews/") and topic_id(struct.request.path_params)
+  end
 
+  defp topic_id(path_params = %{}) do
+    path_params
+    |> Map.get("location_id_and_name")
+    |> String.split("-")
+    |> List.first()
+    |> LocationTopicMappings.get_topic_id()
+  end
+
+  defp redirect(request = %Struct.Request{}) do
     IO.iodata_to_binary([
       to_string(request.scheme),
       "://",
       request.host,
-      "/news/topics/#{topic_id}",
+      "/news/topics/#{topic_id(request.path_params)}",
       Belfrage.Helpers.QueryParams.encode(request.query_params)
     ])
   end
