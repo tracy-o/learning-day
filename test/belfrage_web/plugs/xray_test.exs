df3fa6851ea46fdc44067a9f65da16f37dae1ffe
chrishop <christopher.hopkins@bbc.co.uk>
2022-04-19 07:16:41 +0100

    Adds logic for whether to use existing trace or start a new one.
    The logic should go thusly.
    
    If the xray header doesn't exist start a new one
    
    If a request is from belfrage or made using local development, an attempt to
    parse the xray header should always be made.
    
    If a request isn't from belfrage or locally (from GTM or FASTLY) then we should
    only parse that header if it looks like its come from a load balancer,
    something in this PR I've been calling a 'partial' header for short hand. These
    headers can be identifies as they only have the 'Root' key or the 'Root' and
    'Self' key present. With these partial headers we should remove the 'Self' key
    if its present, and then make a sampling decision and add the 'Sampled' key.
    
    If request isn't from belfrage, and is a full (looks like its come from another
    application using xray) new trace will be started rather than parsed.
    
    The logic is like this so we can protect ourselves from people sending
    malicious headers to get us to sample every trace. While also allowed load
    balancing tracing to work, and of course for Bruce to be able to use Joan's
    traces.

--- test/belfrage_web/plugs/xray_test.exs
+++ test/belfrage_web/plugs/xray_test.exs
@@ -64,39 +64,153 @@ defmodule BelfrageWeb.Plugs.XrayTest do
     end
   end
 
-  describe "call/2 when 'x-amzn-trace-id' header exists" do
-    setup do
+  # a `x-amzn-trace-id` header look like they're from an xray instrmented
+  # application if they have at least Root,Sampled keys go to 'Tracing Header'
+  # for more detail:
+  # https://docs.aws.amazon.com/xray/latest/devguide/xray-concepts.html
+  describe "call/2 when 'x-amzn-trace-id' looks like its from an X-Ray Instrumented application" do
+    test "arbitrary key-value pairs are kept in the header" do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      parent = "9d27b4c4bd4b7140"
+      sampled = "0"
+      xray_header = "Root=#{root};Parent=#{parent};Sampled=#{sampled};ArbitraryKey=ArbitraryValue"
+
+      conn = make_request_with([["x-amzn-trace-id", xray_header]])
+
+      header_parts =
+        Belfrage.Xray.build_trace_id_header(conn.assigns[:xray_segment])
+        |> split_header()
+
+      assert [
+        ["Root", "1-623c0289-148af71fcd58836604a286a5"],
+        ["Parent", _],
+        ["Sampled", _],
+        ["ArbitraryKey", "ArbitraryValue"]
+      ] = header_parts
+    end
+
+    test "the header comes straight from a client, xray header is parsed" do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      parent = "9d27b4c4bd4b7140"
+      sampled = "0"
+      xray_header = "Root=#{root};Parent=#{parent};Sampled=#{sampled}"
+
+      conn = make_request_with([["x-amzn-trace-id", xray_header]])
+
+      assert conn.assigns[:xray_segment].trace.root == root
+    end
+
+    # 'direct client' here means that the 'req-svc-chain' doesn't exist. This
+    # means the request is either us using a direct endpoint (e.g.
+    # bruce.belfrage.test.api.bbc.co.uk/) or a local development request.
+    test "the request doesn't come from Belfrage or a direct client, new xray header is created" do
       root = "1-623c0289-148af71fcd58836604a286a5"
       parent = "9d27b4c4bd4b7140"
       sampled = "0"
       xray_header = "Root=#{root};Parent=#{parent};Sampled=#{sampled}"
 
       conn =
-        conn(:get, "/some/route")
-        |> Plug.Conn.put_req_header("x-amzn-trace-id", xray_header)
-        |> Plugs.RequestId.call([])
-        |> Plugs.Xray.call(xray: MockXray)
+        make_request_with([
+          ["x-amzn-trace-id", xray_header],
+          ["req-svc-chain", "FASTLY,GTM"]
+        ])
 
-      %{conn: conn, root: root, parent: parent, sampled: sampled}
+      assert conn.assigns[:xray_segment].trace.root != root
     end
 
-    test "Root value in header is the same as trace root in the segment", %{conn: conn, root: root} do
+    test "the request comes from belfrage, xray header is parsed" do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      parent = "9d27b4c4bd4b7140"
+      sampled = "0"
+      xray_header = "Root=#{root};Parent=#{parent};Sampled=#{sampled}"
+
+      conn =
+        make_request_with([
+          ["x-amzn-trace-id", xray_header],
+          ["req-svc-chain", "BELFRAGE"]
+        ])
+
+      assert conn.assigns[:xray_segment].trace.root == root
+    end
+  end
+
+  describe "call/2 when 'x-amzn-trace-id' is a partial trace header" do
+    test "arbitrary key-value pairs are kept in the header" do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      xray_header = "Root=#{root};ArbitraryKey=ArbitraryValue"
+
+      conn = make_request_with([["x-amzn-trace-id", xray_header]])
+
+      header_parts =
+        Belfrage.Xray.build_trace_id_header(conn.assigns[:xray_segment])
+        |> split_header()
+
+      assert [
+        ["Root", "1-623c0289-148af71fcd58836604a286a5"],
+        ["Parent", _],
+        ["Sampled", _],
+        ["ArbitraryKey", "ArbitraryValue"]
+      ] = header_parts
+
+
+    end
+
+    test "a `Sample` key is added to the header" do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      xray_header = "Root=#{root}"
+
+      conn = make_request_with([["x-amzn-trace-id", xray_header]])
+      sampled = conn.assigns[:xray_segment].trace.sampled
+
+      assert sampled == true or sampled == false
+    end
+
+    test "if a `Self` key exists its removed" do
+      self = "1-67891234-12456789abcdef012345678"
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      xray_header = "Self=#{self};Root=#{root}"
+
+      conn = make_request_with([["x-amzn-trace-id", xray_header]])
+
+      refute conn.assigns[:xray_segment].metadata[:extra_trace_data]
+    end
+
+    test "the request comes straight from a client, header is parsed" do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      xray_header = "Root=#{root}"
+
+      conn = make_request_with([["x-amzn-trace-id", xray_header]])
+
       assert conn.assigns[:xray_segment].trace.root == root
     end
 
-    test "the header has the same 'Parent' as the id of the current segment", %{conn: conn, parent: parent} do
-      assert conn.assigns[:xray_segment].trace.parent == parent
+    test "the request doesn't come from belfrage or client but is < 200 chars, header is parsed" do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      short_value = "ShortValue"
+      xray_header = "Root=#{root};Short=#{short_value}"
+
+      conn = make_request_with([["x-amzn-trace-id", xray_header], ["req-svc-chain", "FASTLY,GTM"]])
+
+      assert conn.assigns[:xray_segment].trace.root == root
     end
 
-    test "'Sampled' value in the segment is the same as the header", %{conn: conn, sampled: expected_sample} do
-      sample =
-        if conn.assigns[:xray_segment].trace.sampled do
-          "1"
-        else
-          "0"
-        end
+    test "the request doesn't come from belfrage or client but is >= 200 chars, start new trace" do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      long_value = String.duplicate("a", 200)
+      xray_header = "Root=#{root};Long=#{long_value}"
 
-      assert sample == expected_sample
+      conn = make_request_with([["x-amzn-trace-id", xray_header], ["req-svc-chain", "FASTLY,GTM"]])
+
+      assert conn.assigns[:xray_segment].trace.root != root
+    end
+
+    test "the request comes from belfrage, header is parsed" do
+      root = "1-623c0289-148af71fcd58836604a286a5"
+      xray_header = "Root=#{root};Parent=segment-id;Sample=0"
+
+      conn = make_request_with([["x-amzn-trace-id", xray_header], ["req-svc-chain", "BELFRAGE"]])
+
+      assert conn.assigns[:xray_segment].trace.root == root
     end
   end
 
@@ -108,11 +222,7 @@ defmodule BelfrageWeb.Plugs.XrayTest do
       sampled = "A"
       xray_header = "Root=#{root};Parent=#{parent};Sampled=#{sampled}"
 
-      conn =
-        conn(:get, "/some/route")
-        |> Plug.Conn.put_req_header("x-amzn-trace-id", xray_header)
-        |> Plugs.RequestId.call([])
-        |> Plugs.Xray.call(xray: MockXray)
+      conn = make_request_with([["x-amzn-trace-id", xray_header]])
 
       %{conn: conn, root: root, parent: parent, sampled: sampled}
     end
@@ -128,5 +238,19 @@ defmodule BelfrageWeb.Plugs.XrayTest do
     test "the sample is a true or false", %{conn: conn} do
       assert is_boolean(conn.assigns[:xray_segment].trace.sampled)
     end
+  end
+
+  defp split_header(header) do
+    header
+    |> String.split(";")
+    |> Enum.map(&String.split(&1, "="))
+  end
+
+  defp make_request_with(headers) do
+    conn = conn(:get, "/some/route")
+
+    Enum.reduce(headers, conn, fn [k, v], conn -> Plug.Conn.put_req_header(conn, k, v) end)
+    |> Plugs.RequestId.call([])
+    |> Plugs.Xray.call(xray: MockXray)
   end
 end
