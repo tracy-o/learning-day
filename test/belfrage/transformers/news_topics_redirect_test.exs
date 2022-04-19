3078f8b14fb46e39dac9a7354fafbe7d9b4096f5
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2022-04-19 09:56:33 +0100

    [RESFRAME-4543] Revise tests

--- test/belfrage/transformers/news_topics_redirect_test.exs
+++ test/belfrage/transformers/news_topics_redirect_test.exs
@@ -18,7 +18,7 @@ defmodule Belfrage.Transformers.LocalNewsTopicsRedirectTest do
               response: %Struct.Response{
                 http_status: 302,
                 headers: %{
-                  "location" => "https://www.bbc.com/news/topics/cxrlk4ywplxt",
+                  "location" => "/news/topics/cxrlk4ywplxt",
                   "x-bbc-no-scheme-rewrite" => "1",
                   "cache-control" => "public, stale-while-revalidate=10, max-age=60"
                 },
@@ -27,21 +27,23 @@ defmodule Belfrage.Transformers.LocalNewsTopicsRedirectTest do
             }} = LocalNewsTopicsRedirect.call([], struct)
   end
 
-  test "If the location ID is not in the mapping, no redirect will be issued" do
+  test "If the location ID is not in the mapping, a 404 will be issued" do
     struct =
       request_struct(:https, "www.bbc.com", "/news/localnews/12345-somelocation/30", %{}, %{
         "location_id_and_name" => "12345-somelocation"
       })
 
-    assert {:ok, ^struct} = LocalNewsTopicsRedirect.call([], struct)
+    assert {:stop_pipeline, %Struct{response: %Struct.Response{http_status: 404}}} =
+             LocalNewsTopicsRedirect.call([], struct)
   end
 
-  test "If the path does not start with /news/localnews/, no redirect will be issued" do
+  test "If the path does not start with /news/localnews/, a 404 will be issued" do
     struct =
       request_struct(:https, "www.bbc.com", "/some/path/6296650-somelocation/30", %{}, %{
         "location_id_and_name" => "6296650-somelocation"
       })
 
-    assert {:ok, ^struct} = LocalNewsTopicsRedirect.call([], struct)
+    assert {:stop_pipeline, %Struct{response: %Struct.Response{http_status: 404}}} =
+             LocalNewsTopicsRedirect.call([], struct)
   end
 end
