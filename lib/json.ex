a05949300e2a0c33c09dcfe6ad57486465979554
Ayman Osman <aymano.osman@gmail.com>
2022-04-20 11:49:39 +0100

    inlining does not happen across modules

--- lib/json.ex
+++ lib/json.ex
@@ -1,7 +1,5 @@
 defmodule Json do
-  @compile {:inline, encode!: 1}
   def encode!(val), do: :jiffy.encode(val)
 
-  @compile {:inline, decode!: 1}
   def decode!(val), do: :jiffy.decode(val, [:return_maps])
 end
