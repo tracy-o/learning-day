cdec99e54603185fcd34b07b29e48a3c152f8d7e
Joseph Pack <josephapack@hotmail.co.uk>
2022-04-25 17:44:04 +0100

    Create MVT Supervisor

--- lib/belfrage/mvt/file_poller.ex
+++ lib/belfrage/mvt/file_poller.ex
@@ -28,14 +28,13 @@ defmodule Belfrage.Mvt.FilePoller do
   def handle_info(:poll, interval) do
     schedule_polling(interval)
 
-    with {:ok, headers_map} <- @client.get(slots_file_location(), __MODULE__, @http_pool) do
+    with {:ok, headers_map} <- @client.get(slots_file_location(), @http_pool, name: "mvt_slots") do
       set_header_state(headers_map["projects"])
     end
 
     {:noreply, interval}
   end
 
-  def name, do: "mvt_slots"
   defp slots_file_location, do: Application.get_env(:belfrage, :mvt)["slots_file_location"]
 
   defp set_header_state(headers) do
