1381bc78802c4ea50b8b75ca5e8a92f979c5adf6
Raymonoir <raymond.ward01@bbc.co.uk>
2022-04-28 11:21:50 +0100

    create echo spec and echo route

---
+++ lib/routes/specs/echo_spec.ex
@@ -0,0 +1,11 @@
+defmodule Routes.Specs.EchoSpec do
+    def specs do
+      %{
+        owner: "DENewsFrameworksTeam@bbc.co.uk",
+        runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
+        pipeline: ["Echo"],
+        platform: Webcore
+      }
+    end
+  end
+  
