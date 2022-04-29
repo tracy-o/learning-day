844a1afba5ce4e8bb8e181b480ab23f55adc185e
chrishop <christopher.hopkins@bbc.co.uk>
2022-04-29 09:16:33 +0100

    Moved dials config to separate config
    This move the config :belfrage, dials_handlers to their own separate config
    (config/dials.exs) which is imported into the main config/config.exs. This is
    so that its easier to generate new dial handlers and place them in the config.

---
+++ config/dials.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+config :belfrage,
+  dial_handlers: %{
+    "circuit_breaker" => Belfrage.Dials.CircuitBreaker,
+    "webcore_ttl_multiplier" => Belfrage.Dials.WebcoreTtlMultiplier,
+    "non_webcore_ttl_multiplier" => Belfrage.Dials.NonWebcoreTtlMultiplier,
+    "logging_level" => Belfrage.Dials.LoggingLevel,
+    "personalisation" => Belfrage.Dials.Personalisation,
+    "news_articles_personalisation" => Belfrage.Dials.NewsArticlesPersonalisation,
+    "obit_mode" => Belfrage.Dials.ObitMode,
+    "ccp_enabled" => Belfrage.Dials.CcpEnabled,
+    "webcore_kill_switch" => Belfrage.Dials.WebcoreKillSwitch,
+    "datalab_machine_recommendations" => Belfrage.Dials.DatalabMachineRecommendations,
+    "chameleon" => Belfrage.Dials.Chameleon,
+    "cache_enabled" => Belfrage.Dials.CacheEnabled,
+    "mvt_enabled" => Belfrage.Dials.MvtEnabled,
+    "election_banner_ni_story" => Belfrage.Dials.ElectionBannerNiStory,
+    "election_banner_council_story" => Belfrage.Dials.ElectionBannerCouncilStory
+  }
