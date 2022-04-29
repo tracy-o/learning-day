2e7d76ae6210e2743e598b750a523d3a99e9d529
chrishop <christopher.hopkins@bbc.co.uk>
2022-04-29 10:20:11 +0100

    now add add new dial handlers to belfrage config

--- lib/mix/tasks/dial_gen.ex
+++ lib/mix/tasks/dial_gen.ex
@@ -16,14 +16,7 @@ defmodule Mix.Tasks.DialGen do
     {dial_name, dial_module_name} = parse_dial_name(dial_name)
     add_to_cosmos_json("jeff", "a_desc", "on", [%{"value" => "on", "description" => "this turns me on"}, %{"value" => "off", "description" => "this turns me off"}])
     add_to_dev_env("jeff", "on")
-    # add_to_config("jeff", "Jeff")
-
-
-    # after all this I think its better If I ditch the AST approach, and just
-    # write my own parser on the plain text which just searches for
-    # `dial_handlers:`
-
-    add_to_belfrage_config("jeff", Jeff)
+    add_to_dial_config("jeff", Belfrage.Dials.Jeff)
   end
 
   defp parse_dial_name(arg) do
@@ -102,90 +95,20 @@ defmodule Mix.Tasks.DialGen do
     end
   end
 
-  def add_to_belfrage_config(name, module_name) do
-    with {:ok, config_string} <- File.read("config/config.exs"),
-          {:ok, config_ast} <- Code.string_to_quoted(config_string) do
+  def add_to_dial_config(name, module_name) do
+    require EEx
 
-        source_string =
-          gen_new_config_ast(config_ast, "jeff", Jeff)
-          |> Macro.to_string()
-          |> Code.format_string!()
+    template_file = "lib/mix/tasks/dial_gen/dial_config.eex"
+    dials_config_file = "config/dials.exs"
 
-        File.write!("config/config.exs", source_string)
-    end
-  end
+    dial_handlers =
+      Application.get_env(:belfrage, :dial_handlers)
+      |> Map.put(name, module_name)
 
-  def gen_new_config_ast(config_ast, name, module_name) do
-    belfrage_opts = get_belfrage_opts(config_ast)
-    {statements, removed_index} = remove_belfrage_opts(config_ast)
-    # IO.inspect({statements, removed_index})
-    dial_handler_ast = Keyword.get(belfrage_opts, :dial_handlers)
-    dial_handler_ast = build_dial_handler("jeff", Jeff) |> add_dial_to_handlers(dial_handler_ast)
-    belfrage_opts = Keyword.put(belfrage_opts, :dial_handlers, dial_handler_ast)
-    belfrage_statement = build_belfrage_config(belfrage_opts)
-    statements = insert_statement(statements, removed_index, belfrage_statement)
+    contents =
+      EEx.eval_file(template_file, dial_handlers: inspect(dial_handlers))
+      |> Code.format_string!()
 
-    {:__block__, [], statements}
-  end
-
-  def get_belfrage_opts(config_ast) do
-    {_block, _meta, statments} = config_ast
-
-    statments
-    |> Enum.filter(fn {func, _meta, _args} -> func == :config end)
-    |> Enum.map(fn {_config_func, _meta, statments} -> statments end)
-    |> Enum.filter(fn [arg1| _rest] -> arg1 == :belfrage end)
-    |> Enum.map(fn [_belfrage_arg| opts] -> opts end)
-    |> case do
-      [[opts]] -> opts
-    end
-  end
-
-  def remove_belfrage_opts(config_ast) do
-    {_block, _meta, statments} = config_ast
-
-    {statments, index_removed} =
-      statments
-      |> Enum.with_index()
-      |> Enum.reduce({[], -1}, fn {statement, i}, {list_acc, index_removed} ->
-          case statement do
-            {:config, _meta, [:belfrage, _a_list]} -> {list_acc, i}
-            s -> {[s| list_acc], index_removed}
-          end
-        end
-      )
-
-    {Enum.reverse(statments), index_removed}
-  end
-
-  def insert_statement(statments, index, new_statment) do
-    List.insert_at(statments,index, new_statment)
-  end
-
-  def find_and_replace_belfrage_config(statment, new_statment) do
-    with {:config, _, child_statments} <- statment,
-      {:belfrage, _, _opts} <- child_statments do
-        new_statment
-    else
-      _err -> statment
-    end
-  end
-
-
-  def build_belfrage_config(opts) do
-    {:config, [], [:belfrage, opts]}
-  end
-
-  def add_handlers_to_opts(dial_handlers, opts) do
-    Keyword.put(opts, :dial_handlers, dial_handlers)
-  end
-
-  def add_dial_to_handlers(new_dial_handler_ast, dial_handlers_ast) do
-    {:%{}, _meta, handlers} = dial_handlers_ast
-    {:%{}, [], [new_dial_handler_ast| handlers]}
-  end
-
-  def build_dial_handler(name, module_name) do
-    {name, {:__aliases__, [], [Belfrage, :Dials, module_name]}}
+    File.write!(dials_config_file, contents)
   end
 end
