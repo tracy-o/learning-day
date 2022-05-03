0f89f9c1217a44bdcaea3a4b12933e9fdf197208
chrishop <christopher.hopkins@bbc.co.uk>
2022-05-03 17:30:55 +0100

    Introduces new validation for mvt headers in the `MvtMapper`
    Before the `MvtMapper` used fetch headers with the format `bbc-mvt-i` where i
    is a value from 1-20, then add them to struct.mvt.private.
    
    However now we can now poll active experiments/features from an endpoint.
    This means we can validate the mvt request headers and remove them if the
    experiement is no longer running.
    
    Information from the endpoint is stored in `Belfrage.Mvt.Slots`. We currently
    have two slots:
    - slot "1" which holds the experiments/features active on Webcore
    - slot "2" which holds the experiments/features active on Simourgh
    
    Within each of these slots is a list of maps. Each map represents an
    experiment/feature. Each experiment/feature has two attributes:
    - "header" which matches the key of an incoming mvt request header (e.g.
      `bbc-mvt-1`)
    - "key" which matches the name of the experiment (e.g. button_colour)
    
    The new functionality of the PR adds extra validation to incoming mvt request
    headers. A header is only mapped and placed in struct.mvt.private if it matches
    completely to an experiment/feature in the slot.

--- lib/belfrage/transformers/mvt_mapper.ex
+++ lib/belfrage/transformers/mvt_mapper.ex
@@ -1,12 +1,16 @@
 defmodule Belfrage.Transformers.MvtMapper do
   use Belfrage.Transformers.Transformer
+  alias Belfrage.Mvt
+
   @dial Application.get_env(:belfrage, :dial)
+
+  @platform_mapping %{Webcore => "1", MozartSimourgh => "2"}
 
   @impl true
   def call(rest, struct = %Struct{request: %Struct.Request{raw_headers: raw_headers}}) do
     struct =
       if @dial.state(:mvt_enabled) do
-        Struct.add(struct, :private, %{mvt: map_mvt_headers(raw_headers)})
+        Struct.add(struct, :private, %{mvt: map_mvt_headers(raw_headers, struct.private.platform)})
       else
         struct
       end
@@ -14,12 +18,33 @@ defmodule Belfrage.Transformers.MvtMapper do
     then_do(rest, struct)
   end
 
-  defp map_mvt_headers(headers) do
-    1..20
-    |> Enum.filter(fn i -> Map.has_key?(headers, "bbc-mvt-#{i}") end)
-    |> Enum.into(%{}, fn i ->
-      [type, name, value] = Map.get(headers, "bbc-mvt-#{i}") |> String.split(";")
-      {"mvt-#{name}", {i, "#{type};#{value}"}}
-    end)
+  def map_mvt_headers(req_headers, platform) do
+    slot = get_slot(platform)
+
+    reduction = fn i, acc ->
+      with [type, name, value] <- header_parts(req_headers, "bbc-mvt-#{i}"),
+           true <- match_slot?("bbc-mvt-#{i}", name, slot) do
+        Map.merge(acc, %{"mvt-#{name}" => {i, "#{type};#{value}"}})
+      else
+        _err -> acc
+      end
+    end
+
+    Enum.reduce(1..20, %{}, reduction)
   end
+
+  defp header_parts(req_headers, header_name) do
+    (req_headers[header_name] || "") |> String.split(";")
+  end
+
+  defp match_slot?(header_name, experiment_name, slot) do
+    Enum.any?(slot, fn e -> e["header"] == header_name and e["key"] == experiment_name end)
+  end
+
+  defp get_slot(platform) do
+    Mvt.Slots.available()
+    |> Map.get(slot_key(platform), [])
+  end
+
+  defp slot_key(platform), do: Map.get(@platform_mapping, platform)
 end
--- test/belfrage/transformers/mvt_mapper_test.exs
+++ test/belfrage/transformers/mvt_mapper_test.exs
@@ -1,63 +1,118 @@
 defmodule Belfrage.Transformers.MvtMapperTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
   use Test.Support.Helper, :mox
 
   alias Belfrage.Transformers.MvtMapper
   alias Belfrage.Struct
+  alias Belfrage.Mvt
 
-  describe "when there are mvt headers" do
-    test "the mvt map contains mvt keys and values" do
+  defp mvt_dial(context) do
+    dial_state = Map.get(context, :dial_state)
+    stub_dial(:mvt_enabled, dial_state)
+    :ok
+  end
+
+  defp set_slot(context) do
+    slot = Map.get(context, :slot)
+    Mvt.Slots.set(%{"1" => slot})
+
+    on_exit(fn -> Mvt.Slots.set(%{}) end)
+    :ok
+  end
+
+  describe "when the mvt dial is turned off" do
+    setup [:mvt_dial]
+
+    @tag dial_state: "false"
+    test "no mvt headers will ever be mapped" do
       {:ok, struct} =
-        MvtMapper.call([], %Struct{
-          request: %Belfrage.Struct.Request{
-            raw_headers: %{"bbc-mvt-1" => "experiment;button_colour;red", "bbc-mvt-5" => "feature;sidebar;false"}
-          }
-        })
+        MvtMapper.call(
+          [],
+          build_struct(
+            raw_headers: %{"bbc-mvt-1" => "experiment;button_colour;red", "bbc-mvt-3" => "feature;sidebar;false"}
+          )
+        )
 
-      assert %{"mvt-button_colour" => {1, "experiment;red"}, "mvt-sidebar" => {5, "feature;false"}} ==
-               struct.private.mvt
+      assert %{} == struct.private.mvt
     end
   end
 
-  describe "when there are no mvt headers" do
+  describe "when an mvt request header has a corresponding slot header" do
+    setup [:mvt_dial, :set_slot]
+
+    @tag dial_state: "true"
+    @tag slot: [
+           %{"header" => "bbc-mvt-1", "key" => "button_colour"},
+           %{"header" => "bbc-mvt-3", "key" => "sidebar"}
+         ]
+    test "the header is mapped and added to the struct" do
+      {:ok, struct} =
+        MvtMapper.call(
+          [],
+          build_struct(
+            raw_headers: %{"bbc-mvt-1" => "experiment;button_colour;red", "bbc-mvt-3" => "feature;sidebar;false"}
+          )
+        )
+
+      assert struct.private.mvt == %{
+               "mvt-button_colour" => {1, "experiment;red"},
+               "mvt-sidebar" => {3, "feature;false"}
+             }
+    end
+  end
+
+  describe "when a mvt request header matches a slot header but not a slot key" do
+    setup [:mvt_dial, :set_slot]
+
+    @tag dial_state: "true"
+    @tag slot: [%{"header" => "bbc-mvt-1", "key" => "you_wont_match_me"}]
+    test "the header isn't added to the struct" do
+      {:ok, struct} = MvtMapper.call([], build_struct(raw_headers: %{"bbc-mvt-1" => "experiment;button_colour;red"}))
+      assert struct.private.mvt == %{}
+    end
+  end
+
+  describe "when an mvt request header doesn't have a corresponding slot header" do
+    setup [:mvt_dial, :set_slot]
+
+    @tag dial_state: "true"
+    @tag slot: []
+    test "the header isn't added to the struct" do
+      {:ok, struct} = MvtMapper.call([], build_struct(raw_headers: %{"bbc-mvt-1" => "experiment;button_colour;red"}))
+      assert struct.private.mvt == %{}
+    end
+  end
+
+  describe "when there is a slot header but no corresponding mvt request header" do
+    setup [:mvt_dial, :set_slot]
+
+    @tag dial_state: "true"
+    @tag slot: [%{"header" => "bbc-mvt-1", "key" => "button_colour"}]
+    test "the header isn't added to the struct" do
+      {:ok, struct} = MvtMapper.call([], build_struct([]))
+      assert struct.private.mvt == %{}
+    end
+  end
+
+  describe "when there are no mvt request headers" do
     test "the mvt map is empty" do
-      {:ok, struct} =
-        MvtMapper.call([], %Struct{
-          request: %Belfrage.Struct.Request{
-            raw_headers: %{"a" => "header"}
-          }
-        })
+      {:ok, struct} = MvtMapper.call([], build_struct(raw_headers: %{"a" => "header"}))
 
       assert %{} == struct.private.mvt
     end
   end
 
-  describe "for mvt_enabled dial state" do
-    test "when dial is enabled, mvt map contains expected keys and values" do
-      stub_dial(:mvt_enabled, "true")
+  defp build_struct(opts) do
+    raw_headers = Keyword.get(opts, :raw_headers, %{})
+    platform = Keyword.get(opts, :platform, Webcore)
 
-      {:ok, struct} =
-        MvtMapper.call([], %Struct{
-          request: %Belfrage.Struct.Request{
-            raw_headers: %{"bbc-mvt-1" => "experiment;button_colour;red", "bbc-mvt-5" => "feature;sidebar;false"}
-          }
-        })
-
-      assert %{"mvt-button_colour" => {1, "experiment;red"}, "mvt-sidebar" => {5, "feature;false"}} ==
-               struct.private.mvt
-    end
-
-    test "when dial is disabled, mvt map is empty" do
-      stub_dial(:mvt_enabled, "false")
-
-      {:ok, struct} =
-        MvtMapper.call([], %Struct{
-          request: %Belfrage.Struct.Request{
-            raw_headers: %{"bbc-mvt-1" => "experiment;button_colour;red", "bbc-mvt-5" => "feature;sidebar;false"}
-          }
-        })
-
-      assert %{} == struct.private.mvt
-    end
+    %Struct{
+      request: %Belfrage.Struct.Request{
+        raw_headers: raw_headers
+      },
+      private: %Belfrage.Struct.Private{
+        platform: platform
+      }
+    }
   end
 end
