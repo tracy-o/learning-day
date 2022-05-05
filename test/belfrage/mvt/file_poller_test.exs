52c45ebc79b6c7d7c82acfeb4e2d6f465fe590a3
Joseph Pack <josephapack@hotmail.co.uk>
2022-05-05 16:14:31 +0100

    Test to ensure malformed JSON isnt set

--- test/belfrage/mvt/file_poller_test.exs
+++ test/belfrage/mvt/file_poller_test.exs
@@ -9,6 +9,8 @@ defmodule Belfrage.Mvt.FilePollerTest do
   @mvt_json_payload File.read!("test/support/fixtures/mvt_slot_headers.json")
 
   test "fetches and updates Mvt Slots from S3" do
+    Slots.set(Belfrage.Mvt.Slots, %{})
+
     assert Slots.available() == %{}
 
     expect(HTTPMock, :execute, fn _, _origin ->
@@ -21,14 +23,29 @@ defmodule Belfrage.Mvt.FilePollerTest do
     wait_for(fn -> Slots.available() == project_headers end)
   end
 
-  test "if there is an error fetching the file it will not set the file" do
+  test "if there is an error fetching the file it will not set the file content in the agent" do
+    Slots.set(Belfrage.Mvt.Slots, %{})
     assert Slots.available() == %{}
 
     expect(HTTPMock, :execute, fn _, _origin ->
-      {:ok, %HTTP.Response{status_code: 500, body: "{}"}}
+      {:ok, %HTTP.Response{status_code: 500, body: "{foo: \"bar\"}"}}
     end)
 
-    start_supervised!({FilePoller, interval: 0, name: :fail_test_mvt_file_poller})
+    start_supervised!({FilePoller, interval: 0, name: :test_mvt_file_poller})
+
+    Process.sleep(10)
+    assert Slots.available() == %{}
+  end
+
+  test "if the file is malformed it will not set the file content in the agent" do
+    Slots.set(Belfrage.Mvt.Slots, %{})
+    assert Slots.available() == %{}
+
+    expect(HTTPMock, :execute, fn _, _origin ->
+      {:ok, %HTTP.Response{status_code: 500, body: "malformed json"}}
+    end)
+
+    start_supervised!({FilePoller, interval: 0, name: :test_mvt_file_poller})
 
     Process.sleep(10)
     assert Slots.available() == %{}
