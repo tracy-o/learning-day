3dc6c267d8cf9b8faa2d811bd608675034452ed7
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2022-05-16 10:28:26 +0100

    [RESFRAME-4650] Add function documentation

--- lib/belfrage/mvt/state.ex
+++ lib/belfrage/mvt/state.ex
@@ -1,11 +1,28 @@
 defmodule Belfrage.Mvt.State do
   alias Belfrage.Struct
 
+  @doc """
+  Updates a Keyword list of 'seen' headers with new headers.
+  The keys in the keyword list are the headers, and the values
+  are datetimes. The new headers will be put into the
+  keyword list as follows:
+
+  A new header will be added to the Keyword list as a key-value pair
+  with the header as the key and a datetime as the value.
+  If the header is already in the Keyword list, the value of the
+  corresponding key-value pair will be updated to a have a later datetime.
+  """
   def put_mvt_vary_headers(seen_headers, headers) do
     now = DateTime.utc_now()
     Enum.reduce(headers, seen_headers, fn h, acc -> Keyword.put(acc, String.to_atom(h), now) end)
   end
 
+  @doc """
+  Gets the MVT vary headers from the vary header in the Struct response.
+  A list is returned.
+  If there are no MVT vary headers, or there is no vary header,
+  then an empty list is returned.
+  """
   def get_vary_headers(struct = %Struct{}) do
     struct.response.headers
     |> Map.get("vary", "")
