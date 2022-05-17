a6438e1922dcb78e401e2903001f011bc37ca115
Ayman Osman <aymano.osman@gmail.com>
2022-05-17 14:55:26 +0100

    benchmark

--- lib/belfrage_web/request_headers/mapper.ex
+++ lib/belfrage_web/request_headers/mapper.ex
@@ -85,4 +85,28 @@ defmodule BelfrageWeb.RequestHeaders.Mapper do
 
   defp empty(""), do: nil
   defp empty(val), do: val
+
+  # Remove after benchmark
+
+  def slow_map(req_headers) do
+    Enum.into(@map, %{}, fn {header_key, headers_map} ->
+      {header_key,
+       Enum.into(headers_map, %{}, fn {bbc_header_from, bbc_header_key} ->
+         {bbc_header_from, get_req_header(req_headers, bbc_header_key)}
+       end)}
+    end)
+  end
+
+  defp get_req_header(headers, _key) when headers == [{}], do: nil
+
+  defp get_req_header(headers, key) do
+    headers
+    |> Enum.find({nil, nil}, fn {k, _v} -> k == key end)
+    |> elem(1)
+    |> empty_string_to_nil
+  end
+
+  defp empty_string_to_nil(v) when v == "", do: nil
+
+  defp empty_string_to_nil(v), do: v
 end
