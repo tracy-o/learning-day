d9f77cac6f81600fdb05003e078015511a6326f7
Ayman Osman <aymano.osman@gmail.com>
2022-05-18 11:27:17 +0100

    remove unused code

--- lib/belfrage_web/request_headers/mapper.ex
+++ lib/belfrage_web/request_headers/mapper.ex
@@ -4,14 +4,8 @@ defmodule BelfrageWeb.RequestHeaders.Mapper do
     cdn: %{http: "x-bbc-edge-cdn"},
     country: %{edge: "x-bbc-edge-country", varnish: "x-country"},
     host: %{edge: "x-bbc-edge-host", forwarded: "x-forwarded-host", http: "host"},
-    is_uk: %{
-      # edge: "x-bbc-edge-isuk",
-      varnish: "x-ip_is_uk_combined"
-    },
-    is_advertise: %{
-      # edge: "x-bbc-edge-isuk",
-      varnish: "x-ip_is_advertise_combined"
-    },
+    is_uk: %{varnish: "x-ip_is_uk_combined"},
+    is_advertise: %{varnish: "x-ip_is_advertise_combined"},
     scheme: %{edge: "x-bbc-edge-scheme"},
     replayed_traffic: %{replayed_traffic: "replayed-traffic"},
     origin_simulator: %{origin_simulator: "origin-simulator"},
@@ -89,28 +83,4 @@ defmodule BelfrageWeb.RequestHeaders.Mapper do
 
   defp empty(""), do: nil
   defp empty(val), do: val
-
-  # Remove after benchmark
-
-  def slow_map(req_headers) do
-    Enum.into(@map, %{}, fn {header_key, headers_map} ->
-      {header_key,
-       Enum.into(headers_map, %{}, fn {bbc_header_from, bbc_header_key} ->
-         {bbc_header_from, get_req_header(req_headers, bbc_header_key)}
-       end)}
-    end)
-  end
-
-  defp get_req_header(headers, _key) when headers == [{}], do: nil
-
-  defp get_req_header(headers, key) do
-    headers
-    |> Enum.find({nil, nil}, fn {k, _v} -> k == key end)
-    |> elem(1)
-    |> empty_string_to_nil
-  end
-
-  defp empty_string_to_nil(v) when v == "", do: nil
-
-  defp empty_string_to_nil(v), do: v
 end
