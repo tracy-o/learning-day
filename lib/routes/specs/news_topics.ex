862806de157f95a422e9eac8f37ce466ef798f5e
Rachel O'Donnell <rachel.o'donnell@bbc.co.uk>
2022-05-20 13:40:10 +0100

    Remove transition terminology, route news topics to Webcore with exceptions, update tests

--- lib/routes/specs/news_topics.ex
+++ lib/routes/specs/news_topics.ex
@@ -1,16 +1,12 @@
 defmodule Routes.Specs.NewsTopics do
-  def specs(production_env)  do
+  def specs do
     %{
       owner: "D&EKLDevelopmentOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Topics+Runbook",
       platform: Webcore,
       query_params_allowlist: ["page"],
-      pipeline: pipeline(production_env),
+      pipeline: ["NewsTopicsPlatformDiscriminator"],
       personalisation: "test_only"
     }
   end
-
-  defp pipeline("live"), do: ["NewsTopicsPlatformDiscriminator"]
-  defp pipeline(_production_env), do: ["NewsTopicsPlatformDiscriminatorTransition"]
-
 end
