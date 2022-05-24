52f9a7793054edab782bdfdfbed831143e8ec963
Leonie Kenyon <leonie.kenyon@bbc.co.uk>
2022-05-24 09:08:31 +0100

    Removes platform discriminator for Uploader live routes, all campaigns now point to Webcore

--- lib/routes/specs/uploader.ex
+++ lib/routes/specs/uploader.ex
@@ -1,20 +1,11 @@
 defmodule Routes.Specs.Uploader do
-  def specs("live") do
-    %{
-      owner: "D&EHomeParticipationTeam@bbc.co.uk",
-      runbook: "https://confluence.dev.bbc.co.uk/pages/viewpage.action?pageId=183485635",
-      platform: MorphRouter,
-      pipeline: ["HTTPredirect", "TrailingSlashRedirector", "ComToUKRedirect", "UploaderPlatformDiscriminator", "CircuitBreaker"]
-    }
-  end
-
-  def specs(_production_env) do
+  def specs do
     %{
       owner: "D&EHomeParticipationTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/pages/viewpage.action?pageId=300173395",
       platform: Webcore,
       personalisation: "on",
-      pipeline: ["ComToUKRedirect"]
+      pipeline: ["HTTPredirect", "TrailingSlashRedirector", "ComToUKRedirect", "CircuitBreaker"]
     }
   end
 end
