9a1ef62078c1621a3da8900cd4ca60d14e977e69
Joseph Pack <josephapack@hotmail.co.uk>
2022-06-01 11:25:37 +0100

    Remove unneeded pipeline function from missed viet spec

--- lib/routes/specs/world_service_vietnamese_topic_page.ex
+++ lib/routes/specs/world_service_vietnamese_topic_page.ex
@@ -1,8 +1,7 @@
 defmodule Routes.Specs.WorldServiceVietnameseTopicPage do
-  def specs(production_env) do
+  def specs do
     %{
       platform: Simorgh,
-      pipeline: pipeline(production_env),
       query_params_allowlist: ["page"]
     }
   end
