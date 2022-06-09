da8e78aabb3ca74c6fb5c83c3e14763604149223
Iain Kydd <iain.kydd@bbc.co.uk>
2022-06-09 11:45:45 +0100

    MVT-185 add missing spec file

---
+++ test/support/mocks/routes/specs/some_mvt_route_state.ex
@@ -0,0 +1,11 @@
+defmodule Routes.Specs.SomeMvtRouteState do
+  def specs do
+    %{
+      owner: "DENewsFrameworksTeam@bbc.co.uk",
+      runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
+      platform: Webcore,
+      caching_enabled: true,
+      mvt_project_id: 1
+    }
+  end
+end
