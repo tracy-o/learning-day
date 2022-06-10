8d25e9bd7b86e0ca371233ebb9e737e7a3e50654
Raymonoir <raymond.ward01@bbc.co.uk>
2022-06-10 09:50:30 +0100

    create should_store/2 to check if distributes cache should be used

--- lib/belfrage/cache/distributed.ex
+++ lib/belfrage/cache/distributed.ex
@@ -1,6 +1,7 @@
 defmodule Belfrage.Cache.Distributed do
   alias Belfrage.{Struct, Struct.Request, Struct.Response}
   alias Belfrage.Behaviours.CacheStrategy
+  import Enum, only: [random: 1]
   @behaviour CacheStrategy
 
   @ccp_client Application.get_env(:belfrage, :ccp_client)
@@ -14,7 +15,7 @@ defmodule Belfrage.Cache.Distributed do
   end
 
   @impl CacheStrategy
-  def store(struct), do: store(struct, @dial.state(:ccp_enabled))
+  def store(struct), do: store(struct, should_store?(@dial.state(:ccp_enabled), struct.private.fallback_write_sample))
   defp store(_struct, false), do: {:ok, false}
 
   defp store(struct, true) do
@@ -22,6 +23,9 @@ defmodule Belfrage.Cache.Distributed do
 
     {:ok, true}
   end
+
+  defp should_store?(false, _fallback_write_sample), do: false
+  defp should_store?(true, fallback_write_sample), do: random(0..100) < fallback_write_sample * 100
 
   @impl CacheStrategy
   def metric_identifier, do: "distributed"
