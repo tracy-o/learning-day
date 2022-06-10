0f7e76c9c1bf21e5717a002f46c2cb20fccad865
Raymonoir <raymond.ward01@bbc.co.uk>
2022-06-10 08:57:31 +0100

    added fallback_write_sample key and feault 1 to private and routespec

--- lib/belfrage/route_spec.ex
+++ lib/belfrage/route_spec.ex
@@ -34,7 +34,8 @@ defmodule Belfrage.RouteSpec do
             circuit_breaker_error_threshold: nil,
             throughput: 100,
             mvt_project_id: 0,
-            mvt_seen: %{}
+            mvt_seen: %{},
+            fallback_write_sample: 1
 
   def specs_for(name, env \\ Application.get_env(:belfrage, :production_environment)) do
     route_attrs = get_route_attrs(name, env)
