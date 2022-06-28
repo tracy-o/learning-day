2a6dcfe5b541a07cbbb0c659872f1ada523f6e81
Jessica Leach <jessica.leach@bbc.co.uk>
2022-06-28 10:37:39 +0100

    Update run book link

--- lib/routes/specs/live.ex
+++ lib/routes/specs/live.ex
@@ -2,7 +2,7 @@ defmodule Routes.Specs.Live do
   def specs do
     %{
       owner: "#help-live",
-      runbook: "https://confluence.dev.bbc.co.uk/display/LIVEXP/BBC+Live+Run+Book",
+      runbook: "https://confluence.dev.bbc.co.uk/display/LIVEXP/BBC+Live+Experience+on+Webcore+Run+Book",
       platform: Webcore,
       query_params_allowlist: ["page", "mode"]
     }
