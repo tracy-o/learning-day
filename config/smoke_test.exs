73e4f902900ffdd979af9ed761a5ac4d1eadc8f4
Joseph Pack <josephapack@hotmail.co.uk>
2022-06-28 12:29:42 +0100

    Add Joan and Sydney to the smoke test config

--- config/smoke_test.exs
+++ config/smoke_test.exs
@@ -8,18 +8,23 @@ config :belfrage, :smoke,
     "belfrage" => %{:id => "bid", :value => "www"},
     "bruce-belfrage" => %{:id => "bid", :value => "bruce"},
     "cedric-belfrage" => %{:id => "bid", :value => "cedric"},
-    "sally-belfrage" => %{:id => "bid", :value => "sally"}
+    "joan-belfrage" => %{:id => "bid", :value => "joan"},
+    "sally-belfrage" => %{:id => "bid", :value => "sally"},
+    "sydney-belfrage" => %{:id => "bid", :value => "sydney"}
   },
   test: %{
     "belfrage" => "www.belfrage.test.api.bbc.co.uk",
     "bruce-belfrage" => "bruce.belfrage.test.api.bbc.co.uk",
-    "sally-belfrage" => "sally.belfrage.test.api.bbc.co.uk",
     "cedric-belfrage" => "cedric.belfrage.test.api.bbc.co.uk",
-    "joan-belfrage" => "joan.belfrage.test.api.bbc.co.uk"
+    "joan-belfrage" => "joan.belfrage.test.api.bbc.co.uk",
+    "sally-belfrage" => "sally.belfrage.test.api.bbc.co.uk",
+    "sydney-belfrage" => "sydney.belfrage.test.api.bbc.co.uk"
   },
   live: %{
     "belfrage" => "www.belfrage.api.bbc.co.uk",
     "bruce-belfrage" => "bruce.belfrage.api.bbc.co.uk",
+    "cedric-belfrage" => "cedric.belfrage.api.bbc.co.uk",
+    "joan-belfrage" => "joan.belfrage.api.bbc.co.uk",
     "sally-belfrage" => "sally.belfrage.api.bbc.co.uk",
-    "cedric-belfrage" => "cedric.belfrage.api.bbc.co.uk"
+    "sydney-belfrage" => "sydney.belfrage.api.bbc.co.uk"
   }
