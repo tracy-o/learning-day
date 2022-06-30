b32c21096623e83048ef6de8513168ab01ef25c0
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2022-06-30 14:24:50 +0100

    [RESFRAME-4769] Track queued requests
    Requests that are made when the number of available workers is 0
    are queued. We can track and export the number of queued requests.

--- test/belfrage/metrics/nimble_pool_test.exs
+++ test/belfrage/metrics/nimble_pool_test.exs
@@ -18,13 +18,14 @@ defmodule Belfrage.Metrics.NimblePoolTest do
       {:ok, socket: socket}
     end
 
-    test "tracks children of NimblePool.Supervisor", %{socket: socket} do
-      start_finch()
+    test "tracks available workers", %{socket: socket} do
+      start_finch(pool_size: 1)
 
       assert_metric(
         {[:nimble_pool, :status],
          %{
-           available_workers: 3
+           available_workers: 1,
+           queued_requests: 0
          },
          %{
            pool_name: "localhost"
@@ -36,22 +37,49 @@ defmodule Belfrage.Metrics.NimblePoolTest do
         socket,
         Enum.join(
           [
-            "nimble_pool.available_workers.count:3|g|#pool_name:localhost,BBCEnvironment:test"
+            "nimble_pool.available_workers.count:1|g|#pool_name:localhost,BBCEnvironment:test\nnimble_pool.queued_requests.count:0|g|#pool_name:localhost,BBCEnvironment:test"
+          ],
+          "\n"
+        )
+      )
+
+      spawn(fn -> use_worker() end)
+
+      assert_metric(
+        {[:nimble_pool, :status],
+         %{
+           available_workers: 0,
+           queued_requests: 0
+         },
+         %{
+           pool_name: "localhost"
+         }},
+        fn -> Metrics.NimblePool.track_pools(TestFinch.PoolSupervisor) end
+      )
+
+      assert_reported(
+        socket,
+        Enum.join(
+          [
+            "nimble_pool.available_workers.count:0|g|#pool_name:localhost,BBCEnvironment:test\nnimble_pool.queued_requests.count:0|g|#pool_name:localhost,BBCEnvironment:test"
           ],
           "\n"
         )
       )
     end
 
-    test "tracks checkouts", %{socket: socket} do
-      start_finch()
+    test "tracks queued requests", %{socket: socket} do
+      start_finch(pool_size: 1)
+
+      spawn(fn -> use_worker() end)
 
       spawn(fn -> use_worker() end)
 
       assert_metric(
         {[:nimble_pool, :status],
          %{
-           available_workers: 2
+           available_workers: 0,
+           queued_requests: 1
          },
          %{
            pool_name: "localhost"
@@ -63,7 +91,7 @@ defmodule Belfrage.Metrics.NimblePoolTest do
         socket,
         Enum.join(
           [
-            "nimble_pool.available_workers.count:2|g|#pool_name:localhost,BBCEnvironment:test"
+            "nimble_pool.available_workers.count:0|g|#pool_name:localhost,BBCEnvironment:test\nnimble_pool.queued_requests.count:1|g|#pool_name:localhost,BBCEnvironment:test"
           ],
           "\n"
         )
@@ -74,7 +102,8 @@ defmodule Belfrage.Metrics.NimblePoolTest do
       assert_metric(
         {[:nimble_pool, :status],
          %{
-           available_workers: 1
+           available_workers: 0,
+           queued_requests: 2
          },
          %{
            pool_name: "localhost"
@@ -86,7 +115,7 @@ defmodule Belfrage.Metrics.NimblePoolTest do
         socket,
         Enum.join(
           [
-            "nimble_pool.available_workers.count:1|g|#pool_name:localhost,BBCEnvironment:test"
+            "nimble_pool.available_workers.count:0|g|#pool_name:localhost,BBCEnvironment:test\nnimble_pool.queued_requests.count:2|g|#pool_name:localhost,BBCEnvironment:test"
           ],
           "\n"
         )
@@ -94,8 +123,10 @@ defmodule Belfrage.Metrics.NimblePoolTest do
     end
   end
 
-  defp start_finch() do
-    start_supervised!({Finch, [name: TestFinch, pools: %{"http://localhost:1234" => [size: 3]}]})
+  defp start_finch(opts) do
+    start_supervised!(
+      {Finch, [name: TestFinch, pools: %{"http://localhost:1234" => [size: Keyword.get(opts, :pool_size)]}]}
+    )
   end
 
   defp use_worker() do
