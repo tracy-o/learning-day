2addcd7a24b47816d40c9f496ad81abb12bd7107
Joseph Pack <josephapack@hotmail.co.uk>
2022-07-14 09:27:50 +0100

    Remove unneeded NewsMorph routespec

--- lib/routes/specs/news_morph.ex
+++
@@ -1,9 +0,0 @@
-defmodule Routes.Specs.NewsMorph do
-  def specs do
-    %{
-      owner: "DENewsFrameworksTeam@bbc.co.uk",
-      runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
-      platform: MorphRouter
-    }
-  end
-end
