52fe29064f1f0a66570656cac01d5475c36c2d44
chrishop <christopher.hopkins@bbc.co.uk>
2022-07-15 15:26:26 +0100

    Removed Machine Gun from clients/http.ex
    As part of that work this commit does the following:
    
    - Removes Machine Gun call from the http client
    
    - Explicitly denotes `:default` pool with 512 workers
    
    - Provides a new mapping of errors from Finch error response to Belfrage Http
      response struct, which removes the redundant error :bad_url.
    
    - Refactors http client and error tests to use Finch.
    
    - Refactored metric responses for finch
    	- now format the responses before metricing the responses. as metrics shouldn't need awareness about the http client we're using.
    	- removed :pool_full as there isn't a specific error message when this occurs, when the pool is full it will just produce :pool_timeout error.

--- test/belfrage/clients/http/error_test.exs
+++ test/belfrage/clients/http/error_test.exs
@@ -5,7 +5,7 @@ defmodule Belfrage.Clients.HTTP.ErrorTest do
 
   doctest HTTP.Error
 
-  describe "machine gun errors" do
+  describe "finch errors" do
     test "implements String.Chars protocol" do
       error = %HTTP.Error{reason: :timeout}
       expected = "%Belfrage.Clients.HTTP.Error{reason: :timeout}"
@@ -13,56 +13,86 @@ defmodule Belfrage.Clients.HTTP.ErrorTest do
       assert expected == "#{error}"
     end
 
-    test "logs known and standardised error" do
+    test "timeout" do
       log =
         capture_log(fn ->
-          %MachineGun.Error{reason: :request_timeout}
+          %Mint.TransportError{reason: :timeout}
           |> Belfrage.Clients.HTTP.Error.new()
         end)
 
-      assert log =~ ~s("third_party_reason":":request_timeout")
+      assert log =~ ~s("third_party_reason":"timeout")
       assert log =~ ~s("belfrage_http_reason":"timeout")
     end
 
-    test "logs error unknown to Belfrage" do
+    test "pool timeout" do
       log =
         capture_log(fn ->
-          %MachineGun.Error{reason: :unexpected_reason}
-          |> Belfrage.Clients.HTTP.Error.new()
+          Belfrage.Clients.HTTP.Error.new(:exit, {:timeout, {NimblePool, :checkout, [:pid_1]}})
         end)
 
-      assert log =~ ~s("third_party_reason":":unexpected_reason")
-      assert log =~ ~s("belfrage_http_reason":"nil")
+      assert log =~ ~s("third_party_reason":"exited with reason: {:timeout, {NimblePool, :checkout, [:pid_1]}}")
+      assert log =~ ~s("belfrage_http_reason":"pool_timeout")
     end
 
-    test "logs error containing charlist message" do
+    test "bad_url_scheme, scheme required" do
       log =
         capture_log(fn ->
-          %MachineGun.Error{reason: {:error_reason, 'This is a charlist message.'}}
-          |> Belfrage.Clients.HTTP.Error.new()
+          Belfrage.Clients.HTTP.Error.new(%ArgumentError{message: "scheme is required for url: www.example.com"})
         end)
 
-      assert log =~ ~s("third_party_reason":"{:error_reason, 'This is a charlist message.'}")
-      assert log =~ ~s("belfrage_http_reason":"nil")
-    end
-  end
-
-  describe "Finch Errors" do
-    test "Mint transport options error" do
-      log = capture_log(fn -> Belfrage.Clients.HTTP.Error.new(%Mint.TransportError{reason: :timeout}) end)
-
-      assert log =~ ~s("third_party_reason":"timeout")
-      assert log =~ ~s("belfrage_http_reason":"not_yet_implemented")
+      assert log =~ ~s("third_party_reason":"scheme is required for url: www.example.com")
+      assert log =~ ~s("belfrage_http_reason":"bad_url_scheme")
     end
 
-    test "Mint http error" do
+    test "bad_url_scheme, no scheme" do
+      log =
+        capture_log(fn ->
+          Belfrage.Clients.HTTP.Error.new(%ArgumentError{message: "invalid scheme"})
+        end)
+
+      assert log =~ ~s("third_party_reason":"invalid scheme")
+      assert log =~ ~s("belfrage_http_reason":"bad_url_scheme")
+    end
+
+    test "logs error not mapped to Belfrage from finch" do
       log =
         capture_log(fn ->
           Belfrage.Clients.HTTP.Error.new(%Mint.HTTPError{module: Mint.HTTP1, reason: :invalid_header})
         end)
 
       assert log =~ ~s("third_party_reason":"invalid header")
-      assert log =~ ~s("belfrage_http_reason":"not_yet_implemented")
+      assert log =~ ~s("belfrage_http_reason":"nil")
+    end
+
+    test "logs error not mapped to Belfrage from mint" do
+      log =
+        capture_log(fn ->
+          %Mint.TransportError{reason: :protocol_not_negotiated}
+          |> Belfrage.Clients.HTTP.Error.new()
+        end)
+
+      assert log =~ ~s("third_party_reason":"ALPN protocol not negotiated")
+      assert log =~ ~s("belfrage_http_reason":"nil")
+    end
+
+    test "logs error containing charlist message" do
+      log =
+        capture_log(fn ->
+          Belfrage.Clients.HTTP.Error.new('This is a charlist message.')
+        end)
+
+      assert log =~ ~s("third_party_reason":"errored with reason: 'This is a charlist message.'")
+      assert log =~ ~s("belfrage_http_reason":"nil")
+    end
+
+    test "unexpected error format" do
+      log =
+        capture_log(fn ->
+          Belfrage.Clients.HTTP.Error.new(%{error: :unexpected_error_format})
+        end)
+
+      assert log =~ ~s("third_party_reason":"errored with reason: %{error: :unexpected_error_format}")
+      assert log =~ ~s("belfrage_http_reason":"nil")
     end
   end
 end
