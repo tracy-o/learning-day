63d06b75d65798c72653f0771d1b906fe5bcf890
Ash Cleave <ashley.cleave@bbc.co.uk>
2022-07-21 18:04:02 +0100

    Add personalisation

--- lib/routes/specs/alba_home_page.ex
+++ lib/routes/specs/alba_home_page.ex
@@ -4,7 +4,8 @@ defmodule Routes.Specs.AlbaHomePage do
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Homepage%20&%20Nations%20-%20WebCore%20-%20Runbook",
       platform: Webcore,
-      default_language: "gd"
+      default_language: "gd",
+      personalisation: "on"
     }
   end
 end
