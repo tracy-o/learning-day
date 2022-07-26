2d92b89d07a6246becce5792cb9fa54b73d05eb0
Ettore Berardi <ettore.berardi@bbc.co.uk>
2022-07-26 12:48:41 +0100

    amend fallbacks write sample for home pages.

--- lib/routes/specs/programmes.ex
+++ lib/routes/specs/programmes.ex
@@ -3,7 +3,8 @@ defmodule Routes.Specs.Programmes do
     %{
       owner: "homedatacap@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/pages/viewpage.action?pageId=152098352",
-      platform: Programmes
+      platform: Programmes,
+      fallback_write_sample: 0.5
     }
   end
 end
--- lib/routes/specs/weather_home_page.ex
+++ lib/routes/specs/weather_home_page.ex
@@ -4,7 +4,8 @@ defmodule Routes.Specs.WeatherHomePage do
       owner: "DEWeather@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/pages/viewpage.action?pageId=140399154",
       platform: MozartWeather,
-      headers_allowlist: ["cookie-ckps_language"]
+      headers_allowlist: ["cookie-ckps_language"],
+      fallback_write_sample: 0.5
     }
   end
 end
