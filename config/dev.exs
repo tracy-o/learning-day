cc87acd920950c72546cd0f34c0a7f68aa288415
Sam French <Sam.French@bbc.co.uk>
2022-07-29 15:30:03 +0100

    Specify the fallback S3 bucket for running Belfrage locally

--- config/dev.exs
+++ config/dev.exs
@@ -2,6 +2,7 @@ import Config
 
 config :belfrage,
   origin_simulator: "http://test-compo-1chgcj5evn9oo-f0a64863a5d33db4.elb.eu-west-1.amazonaws.com",
+  ccp_s3_bucket: "belfrage-distributed-cache-test",
   pwa_lambda_function: "arn:aws:lambda:eu-west-1:997052946310:function:test-presentation-layer-lambda",
   webcore_lambda_role_arn: "webcore-lambda-role-arn",
   mozart_news_endpoint: "https://www.mozart-routing.test.api.bbci.co.uk",
