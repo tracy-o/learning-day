b5be54d976f44d1bef91352d2ea7fd5882965120
owentourlamain <owen.tourlamain@bbc.co.uk>
2022-08-10 15:36:59 +0100

    Remove mvt_project_id

--- lib/routes/specs/sport_videos.ex
+++ lib/routes/specs/sport_videos.ex
@@ -4,8 +4,7 @@ defmodule Routes.Specs.SportVideos do
       owner: "sfv-team@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/SFV/Short+Form+Video+Run+Book",
       platform: Webcore,
-      query_params_allowlist: query_params_allowlist(production_env),
-      mvt_project_id: 1
+      query_params_allowlist: query_params_allowlist(production_env)
     }
   end
 
