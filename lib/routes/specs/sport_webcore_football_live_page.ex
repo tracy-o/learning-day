613df0c3ac91c65cf747bdffe399714d15bd2808
Nathaniel Read <nathaniel.read@bbc.co.uk>
2022-08-24 08:19:57 +0100

    Add new route for webcore and tidy examples

--- lib/routes/specs/sport_webcore_football_live_page.ex
+++ lib/routes/specs/sport_webcore_football_live_page.ex
@@ -1,10 +1,10 @@
-defmodule Routes.Specs.SportAlphaFootballLivePage do
+defmodule Routes.Specs.SportWebcoreFootballLivePage do
     def specs do
       %{
         owner: "#help-live",
         runbook: "https://confluence.dev.bbc.co.uk/display/LIVEXP/BBC+Live+Experience+on+Webcore+Run+Book",
         platform: Webcore,
-        query_params_allowlist: ["page", "mode"]
+        query_params_allowlist: ["page"]
       }
     end
   end
