33aded2916e8652901f50a9ab74c9f09af749a60
Ettore Berardi <ettore.berardi@outlook.com>
2022-09-13 18:09:04 +0100

    Allow passing latency (in ms) and size (in kb) to the echo route.

--- lib/belfrage/transformers/echo.ex
+++ lib/belfrage/transformers/echo.ex
@@ -3,18 +3,40 @@ defmodule Belfrage.Transformers.Echo do
 
   @impl true
   def call(_rest, struct) do
+    Process.sleep(struct |> sleep_duration())
+
     {
       :stop_pipeline,
       Struct.add(struct, :response, %{
         http_status: 200,
         headers: %{},
-        body: """
-        Hey, ho!
-        PATH:           #{struct.request.path}
-        PIPELINE:       #{struct.private.pipeline}
-        ROUTE_RELEASE:  #{Application.get_env(:belfrage, :route_rel)}
-        """
+        body: body_size(struct)
       })
     }
+  end
+
+  # Sleep duration in ms
+  defp sleep_duration(struct) do
+    (struct.request.query_params["sleep"] || struct.request.raw_headers["sleep"] || "0")
+    |> String.to_integer()
+  end
+
+  # Body size duration in kb
+  defp body_size(struct) do
+    input =
+      (struct.request.query_params["size"] || struct.request.raw_headers["size"] || "0")
+      |> String.to_integer()
+
+    if input == 0 do
+      """
+      Hey, ho!
+      PATH:           #{struct.request.path}
+      PIPELINE:       #{struct.private.pipeline}
+      ROUTE_RELEASE:  #{Application.get_env(:belfrage, :route_rel)}
+      """
+    else
+      :crypto.strong_rand_bytes(input * 1024)
+      |> Base.encode64()
+    end
   end
 end
--- lib/routes/specs/echo_spec.ex
+++ lib/routes/specs/echo_spec.ex
@@ -4,8 +4,9 @@ defmodule Routes.Specs.EchoSpec do
         owner: "DENewsFrameworksTeam@bbc.co.uk",
         runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
         pipeline: ["Echo"],
-        platform: Webcore
+        platform: Webcore,
+        query_params_allowlist: ["size", "sleep"],
+        headers_allowlist: ["size", "sleep"]
       }
     end
   end
-  
