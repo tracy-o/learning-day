269c327b8f5a9b008ad24b78132d9b8058ce278e
Ettore Berardi <ettore.berardi@outlook.com>
2022-10-03 12:23:14 +0100

    Adds apps platform.

---
+++ lib/routes/platforms/apps_walter.ex
@@ -0,0 +1,15 @@
+defmodule Routes.Platforms.AppsWalter do
+  def specs(production_env) do
+    %{
+      origin: Application.get_env(:belfrage, :walter_endpoint),
+      owner: "#data-systems",
+      runbook: "https://confluence.dev.bbc.co.uk/display/TREVOR/Trevor+V3+%28News+Apps+Data+Service%29+Runbook",
+      pipeline: pipeline(production_env),
+      circuit_breaker_error_threshold: 8_000,
+      fallback_write_sample: 0.0
+    }
+  end
+
+  defp pipeline("live"), do: ["HTTPredirect", :_routespec_pipeline_placeholder, "CircuitBreaker"]
+  defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
+end
