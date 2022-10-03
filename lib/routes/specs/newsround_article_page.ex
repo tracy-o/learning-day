b2709830629e60c2bb8d3acb2b6db2352b08061c
Mark Taylor <mark.taylor02.ext@bbc.co.uk>
2022-10-03 09:17:43 +0100

    Update to the expected email address (fix for a typo)

--- lib/routes/specs/newsround_article_page.ex
+++ lib/routes/specs/newsround_article_page.ex
@@ -1,7 +1,7 @@
 defmodule Routes.Specs.NewsroundArticlePage do
   def specs do
     %{
-      owner: "childrenfutureweb@bbc.co.uk",
+      owner: "childrensfutureweb@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/NEWSCPSSTOR/News+CPS+Stories+Run+Book",
       platform: Webcore
     }
--- lib/routes/specs/newsround_video_page.ex
+++ lib/routes/specs/newsround_video_page.ex
@@ -1,7 +1,7 @@
 defmodule Routes.Specs.NewsroundVideoPage do
   def specs do
     %{
-      owner: "childrenfutureweb@bbc.co.uk",
+      owner: "childrensfutureweb@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/SFV/Short+Form+Video+Run+Book",
       platform: Webcore
     }
