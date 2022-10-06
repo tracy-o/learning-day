bc01d5953da0be29437be481accb91ea25fe409a
tracy-o <113360074+tracy-o@users.noreply.github.com>
2022-10-06 16:38:38 +0100

    Clean up route spec

--- lib/routes/specs/weather_location.ex
+++ lib/routes/specs/weather_location.ex
@@ -1,8 +1,6 @@
 defmodule Routes.Specs.WeatherLocation do
     def specs do
       %{
-        owner: "DEWeather@bbc.co.uk",
-        runbook: "https://confluence.dev.bbc.co.uk/pages/viewpage.action?pageId=140399154",
         platform: MozartWeather
       }
     end
