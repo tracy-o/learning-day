56e8a404fed6dd38c80e52fd87c27dee09b4c112
biancaciuche <bianca.ciuche@softwire.com>
2022-10-06 16:27:14 +0300

    Handled /news/topics/:id/rss.xml

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -467,6 +467,10 @@ defroutefile "Main" do
     ]
   end
 
+  handle "/news/topics/:id/rss.xml", using: "TopicRss", examples: ["/news/topics/cgmxjppkwl7t/rss.xml"] do
+    return_404 if: [!String.match?(id, ~r/^(c[a-zA-Z0-9]{10}t)|([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})$/),]
+  end
+
   redirect "/news/amp/:id", to: "/news/:id.amp", status: 301
   redirect "/news/amp/:topic/:id", to: "/news/:topic/:id.amp", status: 301
 
