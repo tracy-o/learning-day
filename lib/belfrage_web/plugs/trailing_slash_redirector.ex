016f352c625cd28338e209a8034ae5bf4e87eb61
owentourlamain <owen.tourlamain@bbc.co.uk>
2022-10-07 14:25:34 +0100

    Add x-bbc-no-scheme-rewrite header

--- lib/belfrage_web/plugs/trailing_slash_redirector.ex
+++ lib/belfrage_web/plugs/trailing_slash_redirector.ex
@@ -25,6 +25,7 @@ defmodule BelfrageWeb.Plugs.TrailingSlashRedirector do
     |> put_resp_content_type("text/plain")
     |> put_resp_header("server", "Belfrage")
     |> put_resp_header("via", "1.1 Belfrage")
+    |> put_resp_header("x-bbc-no-scheme-rewrite", "1")
     |> put_resp_header("vary", "Accept-Encoding,X-BBC-Edge-Cache,X-Country,X-IP_Is_UK_Combined,X-BBC-Edge-Scheme")
     |> put_resp_header("req-svc-chain", add_to_req_svc_chain(conn.private.bbc_headers))
     |> put_resp_header("cache-control", "public, stale-if-error=90, stale-while-revalidate=30, max-age=60")
--- test/end_to_end/trailing_slash_redirector_test.exs
+++ test/end_to_end/trailing_slash_redirector_test.exs
@@ -40,6 +40,7 @@ defmodule EndToEndTest.TrailingSlashRedirectorTest do
     assert {301, headers, ""} = sent_resp(conn)
     assert {"server", "Belfrage"} in headers
     assert {"via", "1.1 Belfrage"} in headers
+    assert {"x-bbc-no-scheme-rewrite", "1"} in headers
     assert {"vary", "Accept-Encoding,X-BBC-Edge-Cache,X-Country,X-IP_Is_UK_Combined,X-BBC-Edge-Scheme"} in headers
   end
 
