dc338b6d79bc39d6b06b7a207a473fa15d7ee382
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2022-10-13 15:28:16 +0100

    [RESFRAME-4933] Support early returns from Platform Selectors
    Also:
    
    * Return status code in error tuple from Selector
    * Only match on asset type, not section in AresData response payload

--- lib/routes/platforms/selectors/asset_type_platform_selector.ex
+++ lib/routes/platforms/selectors/asset_type_platform_selector.ex
@@ -11,49 +11,56 @@ defmodule Routes.Platforms.Selectors.AssetTypePlatformSelector do
 
   @doc """
   Makes a GET request to the FABL module: ares-data.
+  If a response with a 200 status code and a payload that
+  contains a Webcore asset type is returned, then the following
+  tuple is returned:
 
-  If a response with a:
+    {:ok, "Webcore"}
 
-    * 200 status code
-    * JSON payload that contains an 'assetType' and 'section' field
+  If a response with a 200 status code and a payload that
+  does not contain a Webcore asset type is returned, then the
+  following tuple is returned:
 
-  is present, this function will merge
-  struct.private.metadata with the data obtained from Ares:
+    {:ok, "MozartNews"}
 
-      %{asset_type: asset_type, section: section}
+  If a response with a 404 status code is returned, then the
+  following tuple is returned:
 
-  and call then_do(rest, struct) with the rest of the pipeline
-  and the updated Struct, respectively.
+    {:ok, "MozartNews"}
 
-  Otherwise then_do(rest, struct) is called with the rest of
-  the pipeline and the original Struct.
+  otherwise an error tuple is returned:
+
+    {:error, status_code}
   """
   @impl Routes.Platforms.Selector
   def call(request = %Request{}) do
-    with {:ok, %HTTP.Response{status_code: 200, body: payload}} <- AresData.fetch_metadata(request.path),
+    with {:ok, %HTTP.Response{body: payload, status_code: 200}} <- AresData.fetch_metadata(request.path),
          {:ok, asset_type} <- extract_asset_type(payload) do
-      select(asset_type)
+      select_platform(asset_type)
     else
-      e ->
-        error_msg = "#{__MODULE__} could not select platform: %{path: #{request.path}, reason: #{inspect(e)}}"
-        Logger.log(:error, error_msg)
-        raise error_msg
+      {:ok, %HTTP.Response{status_code: 404}} -> {:ok, "MozartNews"}
+    {_, reason} ->
+        Logger.log(
+          :error,
+          "#{__MODULE__} could not select platform: %{path: #{request.path}, reason: #{inspect(reason)}}"
+        )
+
+          {:error, 500}
     end
   end
 
   defp extract_asset_type(payload) do
     case Json.decode!(payload) do
-      %{"data" => %{"assetType" => asset_type, "section" => _section}} -> {:ok, asset_type}
-      %{"data" => %{"assetType" => _asset_type}} -> {:error, :no_section}
+      %{"data" => %{"assetType" => asset_type}} -> {:ok, asset_type}
       _ -> {:error, :no_asset_type}
     end
   end
 
-  defp select(asset_type) do
+  defp select_platform(asset_type) do
     if asset_type in @webcore_asset_types do
-      Webcore
+      {:ok, "Webcore"}
     else
-      MozartNews
+      {:ok, "MozartNews"}
     end
   end
 end
