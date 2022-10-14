ef6853c9823b99ae8230a69a40a0abfb3df8c686
biancaciuche <bianca.ciuche@softwire.com>
2022-10-14 16:10:03 +0300

    Updated spec files to match the recent transformers refactor

--- lib/routes/specs/world_service_vietnamese_topic_rss.ex
+++ lib/routes/specs/world_service_vietnamese_topic_rss.ex
@@ -4,7 +4,7 @@ defmodule Routes.Specs.WorldServiceVietnameseTopicRss do
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/RSS+Feeds+-+WebCore+-+Runbook",
       platform: Fabl,
-      pipeline: pipeline(env)
+      request_pipeline: pipeline(env)
     }
   end
 
