fa9da5d57ba838c0c90dca3f440a13c23388a69b
biancaciuche <bianca.ciuche@softwire.com>
2022-10-14 17:08:47 +0300

    Added spec files for R-Z ws homepage rss feeds

---
+++ lib/routes/specs/world_service_vietnamese_home_page_rss.ex
@@ -0,0 +1,9 @@
+defmodule Routes.Specs.WorldServiceVietnameseHomePageRss do
+  def specs(env) do
+    %{
+      owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
+      runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/RSS+Feeds+-+WebCore+-+Runbook",
+      platform: Karanga,
+    }
+  end
+end
