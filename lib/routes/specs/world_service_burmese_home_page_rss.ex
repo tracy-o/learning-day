5b863ff3acc3a672c0ae1830cc47f5f9866c0b7c
biancaciuche <bianca.ciuche@softwire.com>
2022-10-18 12:13:54 +0300

    Updated the transformer used for Home Rss Feeds

--- lib/routes/specs/world_service_burmese_home_page_rss.ex
+++ lib/routes/specs/world_service_burmese_home_page_rss.ex
@@ -4,7 +4,7 @@ defmodule Routes.Specs.WorldServiceBurmeseHomePageRss do
       owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/RSS+Feeds+-+WebCore+-+Runbook",
       platform: Karanga,
-      request_pipeline: ["RssFeedRedirect"]
+      request_pipeline: ["RssFeedDomainValidator"]
     }
   end
 end
