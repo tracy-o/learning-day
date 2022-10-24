7eb26417f3d811eabdaf3564f5dca1d1699b7b17
Joseph Pack <josephapack@hotmail.co.uk>
2022-10-24 09:29:44 +0100

    ClassicApp specs don't need response_pipeline as inheriting from the Platform

--- lib/routes/specs/classic_app_russian.ex
+++ lib/routes/specs/classic_app_russian.ex
@@ -5,7 +5,6 @@ defmodule Routes.Specs.ClassicAppRussian do
       runbook: "https://confluence.dev.bbc.co.uk/display/TREVOR/Trevor+V3+%28News+Apps+Data+Service%29+Runbook",
       platform: ClassicApps,
       query_params_allowlist: ["subjectId", "language", "createdBy"],
-      response_pipeline: ["CacheDirective", "ClassicAppCacheControl", "ResponseHeaderGuardian", "PreCacheCompression", "Etag"],
       etag: true
     }
   end
