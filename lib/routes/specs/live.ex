5558080a69beafd9c6e50ed4795ab5ef8fc6dff1
ishaq khan BBC <ishaq.khan@bbc.co.uk>
2022-11-09 15:34:07 +0000

    added post to query_params_allowlist

--- lib/routes/specs/live.ex
+++ lib/routes/specs/live.ex
@@ -4,7 +4,7 @@ defmodule Routes.Specs.Live do
       owner: "#help-live",
       runbook: "https://confluence.dev.bbc.co.uk/display/LIVEXP/BBC+Live+Experience+on+Webcore+Run+Book",
       platform: Webcore,
-      query_params_allowlist: ["page", "mode"]
+      query_params_allowlist: ["page", "mode", "post" ]
     }
   end
 end
