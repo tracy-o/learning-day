1a3ea63b0913c51a592650505f9eab77fdffab14
Ettore Berardi <ettore.berardi@outlook.com>
2022-11-14 17:22:52 +0000

    Move logic from Cascade to Belfrage.ex

--- lib/belfrage.ex
+++ lib/belfrage.ex
@@ -1,5 +1,5 @@
 defmodule Belfrage do
-  alias Belfrage.{Processor, Struct}
+  alias Belfrage.{Processor, Struct, WrapperError}
 
   @callback handle(Struct.t()) :: Struct.t()
 
@@ -7,9 +7,26 @@ defmodule Belfrage do
     struct
     |> Processor.pre_request_pipeline()
     |> Processor.fetch_early_response_from_cache()
-    |> Processor.request_pipeline()
-    |> Processor.perform_call()
-    |> Processor.response_pipeline()
+    |> result_or(&no_cached_response/1)
     |> Processor.post_response_pipeline()
+  end
+
+  defp result_or(struct, callback) do
+    if struct.response.http_status do
+      struct
+    else
+      callback.(struct)
+    end
+  end
+
+  defp no_cached_response(struct) do
+    struct
+    |> Processor.request_pipeline()
+    |> result_or(&perform_call/1)
+    |> Processor.response_pipeline()
+  end
+
+  defp perform_call(struct) do
+    WrapperError.wrap(&Processor.perform_call/1, struct)
   end
 end
