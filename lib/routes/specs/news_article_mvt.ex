351561c6c3ad7c0c46d36e496f779c6473c13aca
Marty-Scott Sainty <marty-scott.sainty@bbc.co.uk>
2022-11-16 10:39:42 +0000

    Remove MVT project id from config

--- lib/routes/specs/news_article_mvt.ex
+++ lib/routes/specs/news_article_mvt.ex
@@ -4,7 +4,6 @@ defmodule Routes.Specs.NewsArticleMvt do
       owner: "DEWebcoreArticlesCapabilityTeams@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/NEWSART/Optimo+Articles+Runbook",
       platform: Webcore,
-      mvt_project_id: 1,
       request_pipeline: ["ObitMode"]
     }
   end
