bb15b06b3b5d2032abb7b550335c8e90b2bd55af
Joseph Pack <josephapack@hotmail.co.uk>
2022-11-21 13:55:07 +0000

    Remove ClassicAppCacheControl and CustomRssErrorResponse from Morph Router platform

--- lib/routes/platforms/morph_router.ex
+++ lib/routes/platforms/morph_router.ex
@@ -13,7 +13,7 @@ defmodule Routes.Platforms.MorphRouter do
       owner: "D&EMorphCoreEngineering@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/morph/Morph+Router+Run+Book",
       request_pipeline: pipeline(production_env),
-      response_pipeline: ["CacheDirective", "ClassicAppCacheControl", "ResponseHeaderGuardian", "CustomRssErrorResponse", "PreCacheCompression"],
+      response_pipeline: ["CacheDirective", "ResponseHeaderGuardian", "PreCacheCompression"],
       circuit_breaker_error_threshold: 200,
     }
   end
