88b803c554043c0403b2e9b8123a54213c0e6e26
Ettore Berardi <ettore.berardi@outlook.com>
2022-12-02 16:09:13 +0000

    Tests that two times whitin the same hour would produce the same datetime.

--- test/belfrage/utils/date_time_test.exs
+++ test/belfrage/utils/date_time_test.exs
@@ -4,12 +4,23 @@ defmodule Belfrage.Utils.DateTimeTest do
   describe "beginning_of_the_hour/0" do
     test "time travels to the beginning of the hour" do
       {:ok, date_time} = DateTime.new(~D[2022-12-02], ~T[11:54:56.368815Z], "Etc/UTC")
+
       amended_date_time = Belfrage.Utils.DateTime.beginning_of_the_hour(date_time)
 
       assert amended_date_time == ~U[2022-12-02 11:00:00Z]
 
       # tests that a Datetime is actually returned, and the difference in minutes is correct
       assert (DateTime.diff(date_time, amended_date_time, :second) / 60) |> Float.floor() == 54
+    end
+
+    test "two times whitinh the same hour will produce the same result" do
+      {:ok, date_time1} = DateTime.new(~D[2022-12-02], ~T[11:45:12.165815Z], "Etc/UTC")
+      {:ok, date_time2} = DateTime.new(~D[2022-12-02], ~T[11:54:56.368815Z], "Etc/UTC")
+
+      amended_date_time1 = Belfrage.Utils.DateTime.beginning_of_the_hour(date_time1)
+      amended_date_time2 = Belfrage.Utils.DateTime.beginning_of_the_hour(date_time2)
+
+      assert amended_date_time1 == amended_date_time2
     end
   end
 end
