205c67a228620461cababac59483d4f690cb18a6
Ettore Berardi <ettore.berardi@outlook.com>
2022-12-07 18:39:18 +0000

    Flips order of plug checks for performance.

--- lib/belfrage_web/plugs/variance_reducer.ex
+++ lib/belfrage_web/plugs/variance_reducer.ex
@@ -1,7 +1,7 @@
 defmodule BelfrageWeb.Plugs.VarianceReducer do
   @moduledoc """
-  Allow lowering pressure on Origins by reducing the cache variation.
-  When the NewsAppVarianceReducer dial is enabled and the path matches, a number of query strings
+  Allow lowering pressure on origins by reducing the cache variation.
+  When the NewsAppVarianceReducer dial is enabled and the path matches, specific query strings
   get removed from the conn.
 
   This feature is currently used exclusively by News Apps but could be extended to other products.
@@ -12,7 +12,7 @@ defmodule BelfrageWeb.Plugs.VarianceReducer do
   def init(opts), do: opts
 
   def call(conn = %{request_path: "/fd/abl"}, _opts) do
-    if news_apps_variance_reducer_dial_enabled?() and reducible_query_string?(conn) do
+    if reducible_query_string?(conn) and news_apps_variance_reducer_dial_enabled?() do
       conn =
         conn
         |> Map.merge(%{
@@ -30,7 +30,7 @@ defmodule BelfrageWeb.Plugs.VarianceReducer do
             )
         })
 
-      # this does not guarantee that the order of the original query string would be mantained
+      # This does not guarantee that the order of the original query string would be mantained
       conn |> Map.merge(%{query_string: conn.query_params |> URI.encode_query()})
     else
       conn
