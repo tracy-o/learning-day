528c4da165831968b27d8cc45e938b5ce32a5bd0
Ettore Berardi <ettore.berardi@outlook.com>
2022-12-07 18:32:22 +0000

    Slightly clarifies logic.

--- lib/belfrage_web/plugs/variance_reducer.ex
+++ lib/belfrage_web/plugs/variance_reducer.ex
@@ -1,35 +1,36 @@
 defmodule BelfrageWeb.Plugs.VarianceReducer do
   @moduledoc """
   Allow lowering pressure on Origins by reducing the cache variation.
-  When the NewsAppVarianceReducer dial is enabled and the path matches a number of query strings get removed from the conn.
+  When the NewsAppVarianceReducer dial is enabled and the path matches, a number of query strings
+  get removed from the conn.
 
-  This feature is currently used exclusively by NewsApps but could be extended to other products. 
+  This feature is currently used exclusively by News Apps but could be extended to other products.
   """
 
   @dial Application.get_env(:belfrage, :dial)
 
   def init(opts), do: opts
 
-  def call(conn = %{request_path: "/fd/abl", query_params: query_params, params: params}, _opts) do
-    if news_apps_variance_reducer_dial_enabled?() and String.match?(conn.query_string, ~r(clientLoc)) do
+  def call(conn = %{request_path: "/fd/abl"}, _opts) do
+    if news_apps_variance_reducer_dial_enabled?() and reducible_query_string?(conn) do
       conn =
         conn
         |> Map.merge(%{
           query_params:
             Map.reject(
-              query_params,
+              conn.query_params,
               fn {key, _val} -> key == "clientLoc" end
             )
         })
         |> Map.merge(%{
           params:
             Map.reject(
-              params,
+              conn.params,
               fn {key, _val} -> key == "clientLoc" end
             )
         })
 
-      # this does not guarantee that the order of the original query strings will be mantained
+      # this does not guarantee that the order of the original query string would be mantained
       conn |> Map.merge(%{query_string: conn.query_params |> URI.encode_query()})
     else
       conn
@@ -42,5 +43,9 @@ defmodule BelfrageWeb.Plugs.VarianceReducer do
 
   defp news_apps_variance_reducer_dial_enabled? do
     @dial.state(:news_apps_variance_reducer) == "enabled"
+  end
+
+  defp reducible_query_string?(conn) do
+    conn.query_string |> String.contains?("clientLoc")
   end
 end
