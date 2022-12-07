d565311f5292237f2193e557b3b3c6060a2e9037
Ettore Berardi <ettore.berardi@outlook.com>
2022-12-07 18:12:11 +0000

    More test scenatios for NewsAppsVarianceReducer dial.

--- test/belfrage_web/plugs/variance_reducer_test.exs
+++ test/belfrage_web/plugs/variance_reducer_test.exs
@@ -7,9 +7,8 @@ defmodule BelfrageWeb.Plugs.VarianceReducerTest do
   alias BelfrageWeb.Plugs.VarianceReducer
 
   describe "with NewsAppsVarianceReduce Dial enabled" do
-    test "remove teh query string" do
+    test "removes the clientLoc query string on /fd/abl" do
       stub_dials(news_apps_variance_reducer: "enabled")
-
       path = "/fd/abl?clientName=Chrysalis&clientLoc=E7&type=index"
 
       conn =
@@ -21,6 +20,53 @@ defmodule BelfrageWeb.Plugs.VarianceReducerTest do
       assert "clientName=Chrysalis&type=index" == amended_conn.query_string
       assert %{"clientName" => "Chrysalis", "type" => "index"} == amended_conn.query_params
       assert %{"clientName" => "Chrysalis", "type" => "index"} == amended_conn.params
+    end
+
+    test "does NOT remove the clientLoc query string on other routes" do
+      stub_dials(news_apps_variance_reducer: "enabled")
+      path = "/fd/something/not/abl?clientName=Chrysalis&clientLoc=E7&type=index"
+
+      conn =
+        conn(:get, path)
+        |> Plug.Conn.fetch_query_params(_opts = [])
+
+      amended_conn = VarianceReducer.call(conn, [])
+
+      assert "clientName=Chrysalis&clientLoc=E7&type=index" == amended_conn.query_string
+      assert %{"clientName" => "Chrysalis", "type" => "index", "clientLoc" => "E7"} == amended_conn.query_params
+      assert %{"clientName" => "Chrysalis", "type" => "index", "clientLoc" => "E7"} == amended_conn.params
+    end
+  end
+
+  describe "with NewsAppsVarianceReduce Dial disabled" do
+    test "does NOT remove the clientLoc query string on /fd/abl" do
+      stub_dials(news_apps_variance_reducer: "disabled")
+      path = "/fd/abl?clientName=Chrysalis&clientLoc=E7&type=index"
+
+      conn =
+        conn(:get, path)
+        |> Plug.Conn.fetch_query_params(_opts = [])
+
+      amended_conn = VarianceReducer.call(conn, [])
+
+      assert "clientName=Chrysalis&clientLoc=E7&type=index" == amended_conn.query_string
+      assert %{"clientName" => "Chrysalis", "type" => "index", "clientLoc" => "E7"} == amended_conn.query_params
+      assert %{"clientName" => "Chrysalis", "type" => "index", "clientLoc" => "E7"} == amended_conn.params
+    end
+
+    test "does NOT remove the clientLoc query string on other routes" do
+      stub_dials(news_apps_variance_reducer: "disabled")
+      path = "/fd/something/not/abl?clientName=Chrysalis&clientLoc=E7&type=index"
+
+      conn =
+        conn(:get, path)
+        |> Plug.Conn.fetch_query_params(_opts = [])
+
+      amended_conn = VarianceReducer.call(conn, [])
+
+      assert "clientName=Chrysalis&clientLoc=E7&type=index" == amended_conn.query_string
+      assert %{"clientName" => "Chrysalis", "type" => "index", "clientLoc" => "E7"} == amended_conn.query_params
+      assert %{"clientName" => "Chrysalis", "type" => "index", "clientLoc" => "E7"} == amended_conn.params
     end
   end
 end
