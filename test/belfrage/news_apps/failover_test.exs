75b700d0fec22c170e8e69787c144773602743b8
Ettore Berardi <ettore.berardi@outlook.com>
2022-12-09 12:24:52 +0000

    Adds more testing.

---
+++ test/belfrage/news_apps/failover_test.exs
@@ -0,0 +1,39 @@
+defmodule Belfrage.NewsApps.FailoverTest do
+  use ExUnit.Case
+
+  alias Belfrage.Utils.Current
+  alias Belfrage.NewsApps
+
+  describe "body/0" do
+    test "returns an hardcoded gzipped JSON response to be used by NewsApps on failover scenarios" do
+      payload =
+        NewsApps.Failover.body()
+        |> :zlib.gunzip()
+        |> Json.decode!()
+
+      assert payload["data"]["metadata"]["name"] == "Home"
+    end
+  end
+
+  describe "update/0" do
+    test "refresh the hardcoded epoch times" do
+      body_t1 =
+        NewsApps.Failover.body()
+        |> :zlib.gunzip()
+        |> Json.decode!()
+
+      Current.Mock.freeze(~D[2022-12-02], ~T[11:14:52.368815Z])
+      NewsApps.Failover.update()
+
+      body_t2 =
+        NewsApps.Failover.body()
+        |> :zlib.gunzip()
+        |> Json.decode!()
+
+      assert body_t2["data"]["metadata"]["lastUpdated"] == 1_669_978_800_000
+      assert body_t1["data"]["metadata"]["lastUpdated"] != body_t2["data"]["metadata"]["lastUpdated"]
+
+      on_exit(&Current.Mock.unfreeze/0)
+    end
+  end
+end
