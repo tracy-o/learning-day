0539cf1fd9224bd331495017572dc5984e218b23
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2022-12-12 16:07:51 +0000

    Add AppPersonalisation Transformer to MyTopics{Page,Follows} RouteSpecs

--- lib/routes/specs/my_topics_page.ex
+++ lib/routes/specs/my_topics_page.ex
@@ -1,5 +1,5 @@
 defmodule Routes.Specs.MyTopicsPage do
-  def specs do
+  def specs(env) do
     %{
       owner: "DandESportApp@bbc.co.uk",
       runbook: "https://confluence.dev.bbc.co.uk/display/sportws4/My+Topics+Run+Book",
@@ -7,8 +7,13 @@ defmodule Routes.Specs.MyTopicsPage do
       personalisation: "on",
       fallback_write_sample: 0,
       caching_enabled: false,
+      request_pipeline: request_pipeline(env),
       response_pipeline: ["CacheDirective", "ClassicAppCacheControl", "ResponseHeaderGuardian", "PreCacheCompression", "Etag"],
       etag: true
     }
   end
+
+  defp request_pipeline("live"), do: []
+
+  defp request_pipeline(_env), do: ["AppPersonalisation"]
 end
