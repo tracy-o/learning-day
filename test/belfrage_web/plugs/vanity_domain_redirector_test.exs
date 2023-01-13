6b6059cc8178563b4fb4d514160a3858d1da723a
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-01-13 11:47:02 +0000

    Tests more accurately match the Plug's purpose - all tests use VanityDomainRedirector.call/2 - changed name of request function - removed tests checking scheme as that's not the point of the plug - update test descriptions

--- test/belfrage_web/plugs/vanity_domain_redirector_test.exs
+++ test/belfrage_web/plugs/vanity_domain_redirector_test.exs
@@ -3,26 +3,15 @@ defmodule BelfrageWeb.Plugs.VanityDomainRedirectTest do
   use Plug.Test
   alias BelfrageWeb.Plugs.VanityDomainRedirector
 
-  defp incoming_request(path) do
+  defp make_request(path) do
     conn(:get, path)
     |> Plug.Conn.put_private(:bbc_headers, %{req_svc_chain: "GTM,BELFRAGE"})
     |> resp(200, "not being redirected")
+    |> VanityDomainRedirector.call([])
   end
 
-  test "redirect when vanity url and scheme is http" do
-    conn =
-      incoming_request("http://www.bbcafaanoromoo.com")
-      |> VanityDomainRedirector.call([])
-
-    assert conn.status == 302
-    assert conn.resp_body == ""
-    assert get_resp_header(conn, "location") == ["https://www.bbc.com/afaanoromoo"]
-  end
-
-  test "redirect when vanity url and scheme is https" do
-    conn =
-      incoming_request("https://www.bbcafaanoromoo.com")
-      |> VanityDomainRedirector.call([])
+  test "redirect when vanity url" do
+    conn = make_request("http://www.bbcafaanoromoo.com")
 
     assert conn.status == 302
     assert conn.resp_body == ""
@@ -30,26 +19,15 @@ defmodule BelfrageWeb.Plugs.VanityDomainRedirectTest do
   end
 
   test "no redirect when not vanity url" do
-    conn = incoming_request("http://www.afaanoromoobbc.com")
+    conn = make_request("http://www.afaanoromoobbc.com")
 
     assert conn.status == 200
+    assert conn.resp_body == "not being redirected"
     assert get_resp_header(conn, "location") == []
   end
 
-  test "redirect when vanity url with path and scheme is http" do
-    conn =
-      incoming_request("http://www.bbcafaanoromoo.com/articles/c841r1v9deko")
-      |> VanityDomainRedirector.call([])
-
-    assert conn.status == 302
-    assert conn.resp_body == ""
-    assert get_resp_header(conn, "location") == ["https://www.bbc.com/afaanoromoo/articles/c841r1v9deko"]
-  end
-
-  test "redirect when vanity url with path and scheme is https" do
-    conn =
-      incoming_request("https://www.bbcafaanoromoo.com/articles/c841r1v9deko")
-      |> VanityDomainRedirector.call([])
+  test "redirect when vanity url with path" do
+    conn = make_request("http://www.bbcafaanoromoo.com/articles/c841r1v9deko")
 
     assert conn.status == 302
     assert conn.resp_body == ""
