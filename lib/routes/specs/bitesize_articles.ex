d84b9cff427d86c3049c77b35fb03892a91b1bb9
Fayokemi <fayoaright@gmail.com>
2023-01-20 10:41:01 +0000

    string personalisation

--- lib/routes/specs/bitesize_articles.ex
+++ lib/routes/specs/bitesize_articles.ex
@@ -5,7 +5,7 @@ defmodule Routes.Specs.BitesizeArticles do
       platform: MorphRouter,
       language_from_cookie: true,
       personalisation: "on"
-      request_pipeline: ["ComToUKRedirect", Personalisation, "BitesizeArticlesPlatformDiscriminator", "LambdaOriginAlias", "Language"],
+      request_pipeline: ["ComToUKRedirect", "Personalisation", "BitesizeArticlesPlatformDiscriminator", "LambdaOriginAlias", "Language"],
       query_params_allowlist: ["course", "topicJourney"],
     }
   end
