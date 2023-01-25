f81037b54951a5f2d5a98b84568def0b5c9b8cf0
Nathaniel Read <nathaniel.read@bbc.co.uk>
2023-01-25 14:20:30 +0000

    Add live route validation and .app path

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -2520,6 +2520,15 @@ defroutefile "Main" do
   ## Live WebCore
   handle "/live/:asset_id", using: "Live", only_on: "test", examples: ["/live/c1v596ken6vt", "/live/c1v596ken6vt&page=6"] do
     return_404 if: [
+      !String.match?(asset_id, ~r/^c[abcdefghjklmnpqrstuvwxyz0-9]{10,}t$/), # TIPO IDs
+      !String.match?(conn.query_params["page"] || "1", ~r/\A([1-4][0-9]|50|[1-9])\z/)
+    ]
+  end
+
+  ## Live WebCore - .app route
+  handle "/live/:asset_id", using: "Live", only_on: "test", examples: ["/live/cvpx5wr4nv8t.app", "/live/cvpx5wr4nv8t.app&page=6"] do
+    return_404 if: [
+      !String.match?(asset_id, ~r/^c[abcdefghjklmnpqrstuvwxyz0-9]{10,}t$/), # TIPO IDs
       !String.match?(conn.query_params["page"] || "1", ~r/\A([1-4][0-9]|50|[1-9])\z/)
     ]
   end
