08cf99dcf2d8cdb8ad5c26e72f8ea33a2627621f
kingdomlevel <nllmrrs@aol.com>
2023-01-26 12:14:26 +0000

    add 8 POS to test and live webcore discriminators

--- lib/belfrage/request_transformers/bitesize_subjects_platform_discriminator.ex
+++ lib/belfrage/request_transformers/bitesize_subjects_platform_discriminator.ex
@@ -6,12 +6,28 @@ defmodule Belfrage.RequestTransformers.BitesizeSubjectsPlatformDiscriminator do
 
   @webcore_test_ids [
     "zxtnvcw",
-    "zmpfb9q"
+    "zmpfb9q",
+    "zphybqt",
+    "zhtf3j6",
+    "z7mtsbk",
+    "zvsc96f",
+    "z8yrwmn",
+    "zjcdxnb",
+    "znwqtfr",
+    "zmj2n39"
   ]
 
   @webcore_live_ids [
     "zxtnvcw",
-    "zmpfb9q"
+    "zmpfb9q",
+    "zphybqt",
+    "zhtf3j6",
+    "z7mtsbk",
+    "zvsc96f",
+    "z8yrwmn",
+    "zjcdxnb",
+    "znwqtfr",
+    "zmj2n39"
   ]
 
   @impl Transformer
