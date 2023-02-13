c1587f9a54189efda4448c88e70cd41a0c18d5ef
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2023-02-13 15:25:25 +0000

    Fix Belfrage.Logger.FormatterTest unit tests

--- test/belfrage/logger/formatter.exs
+++ test/belfrage/logger/formatter.exs
@@ -2,27 +2,58 @@ defmodule Belfrage.Logger.FormatterTest do
   alias Belfrage.Logger.Formatter
   use ExUnit.Case, async: true
 
-  test "core returns formatted log when metadata does not have :cloudwatch" do
-    message = "my log"
+  describe "format/4" do
+    test "returns formatted log" do
+      message = "my log"
 
-    assert message <> "\n" == Formatter.core(:error, message, "", [])
+      assert [log, "\n"] = Formatter.format(:error, message, "", [])
+      assert log =~ ~s("message":"my log","level":"error")
+    end
+
+    test "handles metadata with an :envelope" do
+      message = "my log"
+
+      assert [log, "\n"] =
+               Formatter.format(:error, message, {{2023, 2, 13}, {00, 00, 00, 000}},
+                 erl_level: :error,
+                 application: :belfrage,
+                 domain: [:elixir],
+                 envelope: %Belfrage.Envelope{
+                   request: %Belfrage.Envelope.Request{path: "/content/cps/learning_english/"}
+                 }
+               )
+
+      assert log =~ ~s("message":"my log","level":"error")
+    end
   end
 
-  test "core returns empty string when metadata has :cloudwatch" do
-    message = "my log"
+  describe "cloudwatch/4" do
+    test "returns empty string when metadata does not have :cloudwatch" do
+      message = "my log"
 
-    assert "" == Formatter.core(:error, message, "", cloudwatch: true)
+      assert "" == Formatter.cloudwatch(:error, message, "", [])
+    end
+
+    test "returns formatted log when metadata has :cloudwatch" do
+      message = "my log"
+
+      assert [log, "\n"] = Formatter.cloudwatch(:error, message, "", cloudwatch: true)
+      assert log =~ ~s("message":"my log","level":"error")
+    end
   end
 
-  test "cloudwatch returns empty string when metadata does not have :cloudwatch" do
-    message = "my log"
+  describe "app/4" do
+    test "returns formatted log when metadata does not have :cloudwatch" do
+      message = "my log"
 
-    assert "" == Formatter.cloudwatch(:error, message, "", [])
-  end
+      assert [log, "\n"] = Formatter.app(:error, message, "", [])
+      assert log =~ ~s("message":"my log","level":"error")
+    end
 
-  test "cloudwatch returns formatted log when metadata has :cloudwatch" do
-    message = "my log"
+    test "returns empty string when metadata has :cloudwatch" do
+      message = "my log"
 
-    assert message <> "\n" == Formatter.cloudwatch(:error, message, "", cloudwatch: true)
+      assert "" == Formatter.app(:error, message, "", cloudwatch: true)
+    end
   end
 end
