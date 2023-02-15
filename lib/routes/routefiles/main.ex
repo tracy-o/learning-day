878263660ef643399a0f24c44428b8d05f67304c
BBCMarR <martin.reilly@bbc.co.uk>
2023-02-15 12:05:59 +0000

    BBCHOME-1372 remove ask america redirect

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -500,7 +500,6 @@ defroutefile "Main" do
   redirect "/news/business-12686570", to: "/news/topics/c89l21e4068t", status: 301
   redirect "/news/uk-northern-ireland-38323577", to: "/news/topics/c77jz3mdmgzt", status: 301
   redirect "/news/world-43160365", to: "/news/topics/czpqp1q456vt", status: 301
-  redirect "/news/world-us-canada-44928743", to: "/news/topics/c163jpkepkqt", status: 301
   redirect "/news/business-40863265", to: "/news/topics/cxn4z4nmzelt", status: 301
   redirect "/news/technology-22774341", to: "/news/topics/c008ql15vdxt", status: 301
   redirect "/news/uk-55220521", to: "/news/topics/c8nq32jwn4rt", status: 301
