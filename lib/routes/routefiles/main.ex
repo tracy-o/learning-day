2ff7e0b5daaff6916f216aab81ec4d54cc3e2b2e
Adam Beswick <adam.beswick@bbc.co.uk>
2023-02-24 11:43:48 +0000

    add missing commas

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -2777,7 +2777,7 @@ defroutefile "Main" do
   handle "/bitesize/levels/:id", using: "BitesizeLevels", platform: "MorphRouter", examples: ["/bitesize/levels/zr48q6f"]
   handle "/bitesize/levels/:id/year/:year_id", using: "BitesizeLevels", platform: "MorphRouter", examples: ["/bitesize/levels/z3g4d2p/year/zjpqqp3"] do
     return_404 if: [
-      !(String.match?(id, ~r/^(z3g4d2p)$/) and String.match?(year_id, ~r/^(zjpqqp3|z7s22sg)$/))
+      !(String.match?(id, ~r/^(z3g4d2p)$/) and String.match?(year_id, ~r/^(zjpqqp3|z7s22sg)$/)),
       !(String.match?(id, ~r/^(zbr9wmn)$/) and String.match?(year_id, ~r/^(zmyxxyc|z63tt39|zhgppg8|zncsscw)$/))
     ]
   end
@@ -2785,7 +2785,7 @@ defroutefile "Main" do
   handle "/bitesize/preview/levels/:id", using: "Bitesize", platform: "Webcore", only_on: "test", examples: ["/bitesize/preview/levels/zgckjxs"]
   handle "/bitesize/preview/levels/:id/year/:year_id", using: "Bitesize", platform: "Webcore", only_on: "test", examples: ["/bitesize/preview/levels/zbr9wmn/year/zmyxxyc"] do
     return_404 if: [
-      !(String.match?(id, ~r/^(z3g4d2p)$/) and String.match?(year_id, ~r/^(zjpqqp3|z7s22sg)$/))
+      !(String.match?(id, ~r/^(z3g4d2p)$/) and String.match?(year_id, ~r/^(zjpqqp3|z7s22sg)$/)),
       !(String.match?(id, ~r/^(zbr9wmn)$/) and String.match?(year_id, ~r/^(zmyxxyc|z63tt39|zhgppg8|zncsscw)$/))
     ]
   end
