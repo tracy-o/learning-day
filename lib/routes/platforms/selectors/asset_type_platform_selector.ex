41d5876a2bbf411217e05f4464b9585c398115cf
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-02-28 12:11:15 +0000

    Update AssetTypePlatformSelector - call/1 takes an envelope instead of a request and returns an envelope

--- lib/routes/platforms/selectors/asset_type_platform_selector.ex
+++ lib/routes/platforms/selectors/asset_type_platform_selector.ex
@@ -1,5 +1,5 @@
 defmodule Routes.Platforms.Selectors.AssetTypePlatformSelector do
-  alias Belfrage.Envelope.Request
+  alias Belfrage.Envelope
   alias Belfrage.Clients.HTTP
   alias Routes.Platforms.Selectors.Fetchers.AresData
 
@@ -15,47 +15,47 @@ defmodule Routes.Platforms.Selectors.AssetTypePlatformSelector do
   contains a Webcore asset type is returned, then the following
   tuple is returned:
 
-    {:ok, "Webcore"}
+    {:ok, %Envelope{private: %Private{platform: "Webcore"}}}
 
   If a response with a 200 status code and a payload that
   does not contain a Webcore asset type is returned, then the
   following tuple is returned:
 
-    {:ok, "MozartNews"}
+    {:ok, %Envelope{private: %Private{platform: "MozartNews"}}}
 
   If a response with a 404 status code is returned, then the
   following tuple is returned:
 
-    {:ok, "MozartNews"}
+    {:ok, %Envelope{private: %Private{platform: "MozartNews"}}}
 
   otherwise an error tuple is returned:
 
     {:error, status_code}
   """
   @impl Routes.Platforms.Selector
-  def call(request = %Request{}) do
-    case AresData.fetch_metadata(request.path) do
+  def call(envelope = %Envelope{}) do
+    case AresData.fetch_metadata(envelope.request.path) do
       {:ok, asset_type} ->
-        select_platform(asset_type)
+        select_platform(envelope, asset_type)
 
       {:error, %HTTP.Response{status_code: 404}} ->
-        {:ok, "MozartNews"}
+        {:ok, Envelope.add(envelope, :private, %{platform: "MozartNews"})}
 
       {_, reason} ->
         Logger.log(
           :error,
-          "#{__MODULE__} could not select platform: %{path: #{request.path}, reason: #{inspect(reason)}}"
+          "#{__MODULE__} could not select platform: %{path: #{envelope.request.path}, reason: #{inspect(reason)}}"
         )
 
         {:error, 500}
     end
   end
 
-  defp select_platform(asset_type) do
+  defp select_platform(envelope, asset_type) do
     if asset_type in @webcore_asset_types do
-      {:ok, "Webcore"}
+      {:ok, Envelope.add(envelope, :private, %{platform: "Webcore"})}
     else
-      {:ok, "MozartNews"}
+      {:ok, Envelope.add(envelope, :private, %{platform: "MozartNews"})}
     end
   end
 end
