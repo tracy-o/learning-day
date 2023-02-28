d39cb7e9bc950b970d5543a2bdcf64c0d1832463
BBCMarR <martin.reilly@bbc.co.uk>
2023-02-28 14:32:21 +0000

    remove unused route handler

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -532,7 +532,6 @@ defroutefile "Main" do
   handle "/news/uk-england-47486169", using: "NewsUk", platform: "MozartNews", examples: ["/news/uk-england-47486169"]
   handle "/news/science-environment-56837908", using: "NewsScienceAndTechnology", platform: "MozartNews", examples: ["/news/science-environment-56837908"]
   handle "/news/world-48623037", using: "NewsWorld", platform: "MozartNews", examples: ["/news/world-48623037"]
-  handle "/news/world-middle-east-48433977", using: "NewsWorld", platform: "MozartNews", examples: ["/news/world-middle-east-48433977"]
   handle "/news/world-us-canada-15949569", using: "NewsWorld", platform: "MozartNews", examples: ["/news/world-us-canada-15949569"]
 
   # News archive assets
