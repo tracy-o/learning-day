ca37457bf6db982c073ca35a9a04839ebba42e4b
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-02-28 13:23:53 +0000

    Update unit tests - expect an Envelope with path params and platform in test assertions

--- test/routes/platforms/selectors/bitesize_topics_platform_selector_test.exs
+++ test/routes/platforms/selectors/bitesize_topics_platform_selector_test.exs
@@ -1,113 +1,202 @@
 defmodule Routes.Platforms.Selectors.BitesizeTopicsPlatformSelectorTest do
   use ExUnit.Case
   alias Routes.Platforms.Selectors.BitesizeTopicsPlatformSelector
-  alias Belfrage.Envelope.Request
+  alias Belfrage.{Envelope, Envelope.Request, Envelope.Private}
 
   import Test.Support.Helper, only: [set_environment: 1]
 
   describe "requests with id and year_id path parameter keys" do
     test "if the id is a test webcore id, the year_id is valid and the production environment is test Webcore is returned" do
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "z82hsbk", "year_id" => "zncsscw"}
-             }) == {:ok, "Webcore"}
+      path_params = %{"id" => "z82hsbk", "year_id" => "zncsscw"}
+
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "Webcore"}
+                }}
     end
 
     test "if the id is a live webcore id, the year_id is valid and the production environment is live Webcore is returned" do
       set_environment("live")
+      path_params = %{"id" => "zhtcvk7", "year_id" => "zncsscw"}
 
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "zhtcvk7", "year_id" => "zncsscw"}
-             }) == {:ok, "Webcore"}
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "Webcore"}
+                }}
     end
 
     test "if the id is a not a test webcore id, the year_id is valid and the production environment is test MorphRouter is returned" do
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "some_id", "year_id" => "zncsscw"}
-             }) == {:ok, "MorphRouter"}
+      path_params = %{"id" => "some_id", "year_id" => "zncsscw"}
+
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "MorphRouter"}
+                }}
     end
 
     test "if the id is a test webcore id, the year_id is not valid and the production environment is test MorphRouter is returned" do
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "z82hsbk", "year_id" => "some_year_id"}
-             }) == {:ok, "MorphRouter"}
+      path_params = %{"id" => "z82hsbk", "year_id" => "some_year_id"}
+
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "MorphRouter"}
+                }}
     end
 
     test "if the id is a test webcore id, the year_id is valid and the production environment is live MorphRouter is returned" do
       set_environment("live")
+      path_params = %{"id" => "z82hsbk", "year_id" => "zncsscw"}
 
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "z82hsbk", "year_id" => "zncsscw"}
-             }) == {:ok, "MorphRouter"}
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "MorphRouter"}
+                }}
     end
 
     test "if the id is not a live webcore id, the year_id is valid and the production environment is live MorphRouter is returned" do
       set_environment("live")
+      path_params = %{"id" => "some_id", "year_id" => "zncsscw"}
 
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "some_id", "year_id" => "zncsscw"}
-             }) == {:ok, "MorphRouter"}
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "MorphRouter"}
+                }}
     end
 
     test "if the id is a live webcore id, the year_id is invalid and the production environment is live MorphRouter is returned" do
       set_environment("live")
+      path_params = %{"id" => "zhtcvk7", "year_id" => "some_year_id"}
 
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "zhtcvk7", "year_id" => "some_year_id"}
-             }) == {:ok, "MorphRouter"}
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "MorphRouter"}
+                }}
     end
 
     test "if the id is a live webcore id, the year_id is valid and the production environment is test Webcore is returned as all the live ids are a subset of the test ids" do
       set_environment("test")
+      path_params = %{"id" => "zhtcvk7", "year_id" => "zncsscw"}
 
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "zhtcvk7", "year_id" => "zncsscw"}
-             }) == {:ok, "Webcore"}
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "Webcore"}
+                }}
     end
   end
 
   describe "requests with an id paramater key" do
     test "if the id is a test webcore id and the production environment is test Webcore is returned" do
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "z82hsbk"}
-             }) == {:ok, "Webcore"}
+      path_params = %{"id" => "z82hsbk"}
+
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "Webcore"}
+                }}
     end
 
     test "if the id is a live webcore id and the production environment is live Webcore is returned" do
       set_environment("live")
+      path_params = %{"id" => "zhtcvk7"}
 
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "zhtcvk7"}
-             }) == {:ok, "Webcore"}
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "Webcore"}
+                }}
     end
 
     test "if the id is a not a test webcore id and the production environment is test MorphRouter is returned" do
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "some_id"}
-             }) == {:ok, "MorphRouter"}
+      path_params = %{"id" => "some_id"}
+
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "MorphRouter"}
+                }}
     end
 
     test "if the id is a test webcore id and the production environment is live MorphRouter is returned" do
       set_environment("live")
+      path_params = %{"id" => "z82hsbk"}
 
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "z82hsbk"}
-             }) == {:ok, "MorphRouter"}
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "MorphRouter"}
+                }}
     end
 
     test "if the id is not a live webcore id and the production environment is live MorphRouter is returned" do
       set_environment("live")
+      path_params = %{"id" => "some_id"}
 
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "some_id"}
-             }) == {:ok, "MorphRouter"}
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "MorphRouter"}
+                }}
     end
 
     test "if the id is a live webcore id and the production environment is test MorphRouter is returned as all the live ids are a subset of the test ids" do
       set_environment("test")
+      path_params = %{"id" => "zhtcvk7"}
 
-      assert BitesizeTopicsPlatformSelector.call(%Request{
-               path_params: %{"id" => "zhtcvk7"}
-             }) == {:ok, "Webcore"}
+      assert BitesizeTopicsPlatformSelector.call(%Envelope{
+               request: %Request{path_params: path_params}
+             }) ==
+               {:ok,
+                %Envelope{
+                  request: %Request{path_params: path_params},
+                  private: %Private{platform: "Webcore"}
+                }}
     end
   end
 end
