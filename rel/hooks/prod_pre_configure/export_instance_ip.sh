c2dbb5c77b7c851fdf81f2f5b081d276388f61a6
Sam French <Sam.French@bbc.co.uk>
2023-03-07 09:51:18 +0000

    Use instance metadata endpoint with token (#1881)

--- rel/hooks/prod_pre_configure/export_instance_ip.sh
+++ rel/hooks/prod_pre_configure/export_instance_ip.sh
@@ -2,4 +2,5 @@
 set -e
 
 export REPLACE_OS_VARS=true
-export INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+export INSTANCE_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
