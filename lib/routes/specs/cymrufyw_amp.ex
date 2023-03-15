2ed8680ef3139af1cd615b6ddcb2b717cd4ccf4e
Marty Sainty <sainty11cardiff@hotmail.co.uk>
2023-03-15 15:05:31 +0000

    Revert "Add `.amp` routes for Cymrufyw & Naidheachdan optimo articles (#1900)"
    This reverts commit a4b29c4487341084e4066ffecf4d8ad6b3e77ffb.

--- lib/routes/specs/cymrufyw_amp.ex
+++
@@ -1,9 +0,0 @@
-defmodule Routes.Specs.CymrufywAmp do
-  def specs do
-    %{
-      owner: "#support-simorgh",
-      runbook: "https://confluence.dev.bbc.co.uk/display/NEWSART/Simorgh+Run+Book",
-      platform: "MozartSimorgh"
-    }
-  end
-end
