2b1d054b2444d2b80f1aa2bd636a1c27e443f37b
thymusvulgaris <87661013+thymusvulgaris@users.noreply.github.com>
2023-03-24 13:41:35 +0000

    Move Formatter to support directory

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,7 +4,7 @@
 # defp elixirc_paths(:smoke_test) do ["lib", "test/support", "test/smoke"] end
 
 Code.require_file("smoke/expectations.ex", __DIR__)
-Code.require_file("formatter/joe_formatter.ex", __DIR__)
+Code.require_file("support/joe_formatter.ex", __DIR__)
 Code.require_file("smoke/belfrage_smoke_test_case.ex", __DIR__)
 
 {:ok, _} = Application.ensure_all_started(:credo)
