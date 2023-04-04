add61ffdcdc5497f7d767b2188345e43dd32d498
Artur Novak <artur.novak.ext@bbc.co.uk>
2023-04-04 09:31:40 +0100

    Do not adapt envelope private when redirecting requests

--- lib/belfrage_web/envelope_adapter/private.ex
+++ lib/belfrage_web/envelope_adapter/private.ex
@@ -1,16 +1,7 @@
 defmodule BelfrageWeb.EnvelopeAdapter.Private do
   alias Belfrage.Envelope
 
-  def adapt(envelope = %Envelope{}, private, route_state_id) when is_tuple(route_state_id) do
-    Envelope.add(envelope, :private, %{
-      route_state_id: route_state_id,
-      overrides: private.overrides,
-      production_environment: private.production_environment,
-      preview_mode: private.preview_mode
-    })
-  end
-
-  def adapt(envelope = %Envelope{}, private, spec) when is_binary(spec) do
+  def adapt(envelope = %Envelope{}, private, spec) do
     Envelope.add(envelope, :private, %{
       spec: spec,
       overrides: private.overrides,
