8abce346769e46cf66e0ee66e5c2ecb546307b10
Gui Heurich <guliherme.heurich@protonmail.com>
2023-04-20 16:24:19 +0100

    Add new integration tests

--- test/end_to_end/electoral_commission_test.exs
+++ test/end_to_end/electoral_commission_test.exs
@@ -98,5 +98,77 @@ defmodule EndToEnd.ElectoralCommissionTest do
       assert ["Accept-Encoding"] = get_resp_header(conn, "vary")
       assert ["text/html; charset=utf-8"] = get_resp_header(conn, "content-type")
     end
+
+    test "returns 401 on invalid token" do
+      ec_endpoint = Application.get_env(:belfrage, :electoral_commission_endpoint)
+
+      # assuming an invalid token
+      url = "#{ec_endpoint}/api/v1/address/25050756/?token=f110d812b94d8cdf517765ff8657b89e8b08ebd6"
+
+      expect(HTTPMock, :execute, 1, fn %HTTP.Request{url: ^url}, _pool ->
+        {:ok,
+         %HTTP.Response{
+           headers: %{
+             "cache-control" => "private",
+             "vary" => "something",
+             "content-type" => "application/json"
+           },
+           status_code: 401,
+           body: "{}"
+         }}
+      end)
+
+      conn =
+        conn(
+          :get,
+          "https://web-cdn.test.api.bbci.co.uk/election2023address/25050756"
+        )
+        |> put_req_header("x-bbc-edge-cdn", "1")
+        |> Router.call([])
+
+      assert conn.status == 401
+
+      assert ["public, stale-if-error=90, stale-while-revalidate=30, max-age=5"] =
+               get_resp_header(conn, "cache-control")
+
+      assert ["Accept-Encoding"] = get_resp_header(conn, "vary")
+      assert ["application/json"] = get_resp_header(conn, "content-type")
+    end
+
+    test "returns 400 on invalid postcode" do
+      ec_endpoint = Application.get_env(:belfrage, :electoral_commission_endpoint)
+
+      # assuming an invalid postcode
+      url = "#{ec_endpoint}/api/v1/postcode/IP224DN/?token=f110d812b94d8cdf517765ff8657b89e8b08ebd6"
+
+      expect(HTTPMock, :execute, 1, fn %HTTP.Request{url: ^url}, _pool ->
+        {:ok,
+         %HTTP.Response{
+           headers: %{
+             "cache-control" => "private",
+             "vary" => "something",
+             "content-type" => "application/json"
+           },
+           status_code: 400,
+           body: ~s({"error":"Could not geocode from any source"})
+         }}
+      end)
+
+      conn =
+        conn(
+          :get,
+          "https://web-cdn.test.api.bbci.co.uk/election2023postcode/IP224DN"
+        )
+        |> put_req_header("x-bbc-edge-cdn", "1")
+        |> Router.call([])
+
+      assert conn.status == 400
+
+      assert ["public, stale-if-error=90, stale-while-revalidate=30, max-age=5"] =
+               get_resp_header(conn, "cache-control")
+
+      assert ["Accept-Encoding"] = get_resp_header(conn, "vary")
+      assert ["application/json"] = get_resp_header(conn, "content-type")
+    end
   end
 end
