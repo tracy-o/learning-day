7811a71f4363cb7f11f06cf83b7217a3846985b7
ettomatic <ettore@fastmail.com>
2023-05-17 17:38:31 +0100

    set specific RouteSpec for /news/election/2023/northern-ireland/councils/:gss_id

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -174,6 +174,12 @@ defroutefile "Main" do
     ]
   end
 
+  handle "/news/election/2023/northern-ireland/councils/:gss_id", using: "NewsNiElectionResults", examples: ["/news/election/2023/northern-ireland/councils/N09000001"] do
+    return_404 if: [
+      !String.match?(gss_id, ~r/^[A-Z][0-9]{8}$/)
+    ]
+  end
+
   handle "/news/election/2023/:polity/councils/:gss_id", using: "NewsElectionResults", examples: ["/news/election/2023/england/councils/E08000019"] do
     return_404 if: [
       !String.match?(polity, ~r/^(england|northern-ireland)$/),
