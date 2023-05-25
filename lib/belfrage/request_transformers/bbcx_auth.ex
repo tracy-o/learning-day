b5457c4d1853072f0eaf93ee155673b32c439293
Ettore Berardi <ettore.berardi@outlook.com>
2023-05-25 12:34:48 +0100

    Add BBCX authorisation transformer.
    This will be remove once basic auth get disabled on the origin.

---
+++ lib/belfrage/request_transformers/bbcx_auth.ex
@@ -0,0 +1,19 @@
+defmodule Belfrage.RequestTransformers.BBCX_auth do
+  use Belfrage.Behaviours.Transformer
+
+  @impl Transformer
+  def call(envelope = %Envelope{request: %Envelope.Request{raw_headers: raw_headers}}) do
+    credentials = "bbcx:cihWhx2WAaQrMSUaw1N9B0tq"
+
+     envelope =
+      Envelope.add(envelope, :request, %{
+        raw_headers:
+          Map.merge(raw_headers, %{
+            "authorization" => "Basic " <> Base.encode64(credentials),
+
+          })
+      })
+
+    {:ok, envelope}
+  end
+end
