74af5c92bfbe465e881894f9ad6b4ecad25681ad
Ettore Berardi <ettore.berardi@outlook.com>
2023-05-25 12:34:28 +0100

    Create new BBCX platform.

---
+++ lib/routes/platforms/bbcx.ex
@@ -0,0 +1,19 @@
+defmodule Routes.Platforms.BBCX do
+  def specs(production_env) do
+    %{
+      origin: Application.get_env(:belfrage, :bbcx_endpoint),
+      owner: "GnlDevOps@bbc.com",
+      runbook: "TBD",
+      request_pipeline: pipeline(production_env),
+      response_pipeline: ["CacheDirective", "ResponseHeaderGuardian", "PreCacheCompression"],
+      query_params_allowlist: query_params_allowlist(production_env),
+      circuit_breaker_error_threshold: 200
+    }
+  end
+
+  defp pipeline("live"), do: ["BBCX_auth", :_routespec_pipeline_placeholder, "CircuitBreaker"]
+  defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
+
+  defp query_params_allowlist("live"), do: []
+  defp query_params_allowlist(_production_env), do: "*"
+end
