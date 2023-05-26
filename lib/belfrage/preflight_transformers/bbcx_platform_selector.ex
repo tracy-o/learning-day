62864902537e177508d515f5148b69813d5eac45
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-05-26 14:50:08 +0100

    Update platform selector transformer logic

--- lib/belfrage/preflight_transformers/bbcx_platform_selector.ex
+++ lib/belfrage/preflight_transformers/bbcx_platform_selector.ex
@@ -2,19 +2,21 @@ defmodule Belfrage.PreflightTransformers.BBCXPlatformSelector do
   use Belfrage.Behaviours.Transformer
   alias Belfrage.Envelope
 
+  @allowed_countries ["us", "ca"]
+
   @impl Transformer
   def call(envelope = %Envelope{request: request}) do
     platform =
-      cond do
-        !Map.has_key?(request.raw_headers, "cookie_ckns_bbccom_beta") -> "Webcore"
-        true -> select_platform(envelope.request)
+      if !Map.has_key?(request.raw_headers, "cookie_ckns_bbccom_beta") do
+        "Webcore"
+      else
+        select_platform(request)
       end
 
     {:ok, Envelope.add(envelope, :private, %{platform: platform})}
   end
 
-  defp select_platform(%{raw_headers: %{"cookie_ckns_bbccom_beta" => nil}}), do: "Webcore"
-  defp select_platform(%{host: "www.bbc.com", country: "us"}), do: "BBCX"
-  defp select_platform(%{host: "www.bbc.com", country: "ca"}), do: "BBCX"
+  defp select_platform(%{raw_headers: %{"cookie_ckns_bbccom_beta" => ""}}), do: "Webcore"
+  defp select_platform(%{host: "www.bbc.com", country: country}) when country in @allowed_countries, do: "BBCX"
   defp select_platform(_), do: "Webcore"
 end
