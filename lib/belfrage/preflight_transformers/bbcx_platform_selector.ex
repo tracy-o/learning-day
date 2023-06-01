56eb21eaef5d23a07ce009fcb2ffa87d2fa36f1b
Ettore Berardi <ettore@fastmail.com>
2023-06-01 14:34:48 +0100

    refactor preflight transformer logic.

--- lib/belfrage/preflight_transformers/bbcx_platform_selector.ex
+++ lib/belfrage/preflight_transformers/bbcx_platform_selector.ex
@@ -5,18 +5,18 @@ defmodule Belfrage.PreflightTransformers.BBCXPlatformSelector do
   @allowed_countries ["us", "ca"]
 
   @impl Transformer
-  def call(envelope = %Envelope{request: request, private: %Envelope.Private{production_environment: prod_env}}) do
-    platform = if prod_env == "test" &&
+  def call(envelope = %Envelope{}) do
+    {:ok, Envelope.add(envelope, :private, %{bbcx_enabled: true, platform: select_platform(envelope)})}
+  end
+
+  defp select_platform(%Envelope{request: request, private: %Envelope.Private{production_environment: prod_env}}) do
+    if prod_env == "test" &&
          String.ends_with?(request.host, "bbc.com") &&
          Map.get(request.raw_headers, "cookie-ckns_bbccom_beta") == "1" &&
          request.country in @allowed_countries do
       "BBCX"
-         else
-          "Webcore"
+    else
+      "Webcore"
     end
-
-    {:ok, Envelope.add(envelope, :private, %{bbcx_enabled: true, platform: platform})}
   end
-
- # defp select_platform()
 end
