695a0ff6b01d278c7a744ee45e903ac57965da01
Gui Heurich <guliherme.heurich@protonmail.com>
2023-06-14 17:07:44 +0100

    Check if envelope qualifies for BBCX content
    Co-authored-by: Harvey Peachey <harvey.peachey@bbc.co.uk>

---
+++ lib/belfrage/brands.ex
@@ -0,0 +1,15 @@
+defmodule Belfrage.Brands do
+  alias Belfrage.Envelope
+
+  @dial Application.compile_env(:belfrage, :dial)
+
+  @allowed_countries ["us", "ca"]
+
+  def is_bbcx?(%Envelope{request: request, private: %Envelope.Private{production_environment: prod_env}}) do
+    prod_env == "test" &&
+    @dial.state(:bbcx_enabled) == true &&
+    String.ends_with?(request.host, "bbc.com") &&
+    Map.get(request.raw_headers, "cookie-ckns_bbccom_beta") == "1" &&
+    request.country in @allowed_countries
+  end
+end
