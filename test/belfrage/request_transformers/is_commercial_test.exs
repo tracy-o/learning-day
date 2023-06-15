591a947ddfefffe424f586b8099b2091ac670b1c
Harvey Peachey <harveypeachey@hotmail.co.uk>
2023-06-15 11:08:05 +0100

    adds is_commercial transformer and unit tests
    Co-authored-by: Gui Heurich <guliherme.heurich@protonmail.com>

--- test/belfrage/request_transformers/is_commercial_test.exs
+++ test/belfrage/request_transformers/is_commercial_test.exs
@@ -1,28 +1,44 @@
 defmodule Belfrage.RequestTransformers.IsCommercialTest do
   use ExUnit.Case, async: true
 
-  import Test.Support.Helper, only: [set_environment: 1]
-  import Test.Support.StubHelper, only: [stub_dial: 2]
+  use Test.Support.Helper, :mox
 
   alias Belfrage.RequestTransformers.IsCommercial
-  alias Belfrage.Utils.Current
+  alias Belfrage.{Envelope, Envelope.Request}
 
   describe "call/1" do
     test "adds a request header if request qualifies for bbcx" do
-      stub_dial(:bbcx_enabled, "enabled")
-      set_environment("test")
+      stub_dial(:bbcx_enabled, "true")
 
       bbcx_envelope = %Envelope{
         request: %Envelope.Request{
-          cookies: %{"cookie-ckns_bbccom_beta" => "1"},
+          raw_headers: %{"cookie-ckns_bbccom_beta" => "1"},
           host: "bbc.com",
           country: "ca"
-        }
+        },
+        private: %Envelope.Private{production_environment: "test"}
       }
 
       {:ok, envelope} = IsCommercial.call(bbcx_envelope)
 
-      assert envelope.request.raw_headers = %{"is_commercial" => "true"}
+      assert envelope.request.raw_headers == %{"is_commercial" => "true"}
+    end
+
+    test "does not add is_commercial header" do
+      stub_dial(:bbcx_enabled, "true")
+
+      bbcx_envelope = %Envelope{
+        request: %Envelope.Request{
+          cookies: %{"cookie-ckns_bbccom_beta" => "1"},
+          host: "bbc.co.uk",
+          country: "ca"
+        },
+        private: %Envelope.Private{production_environment: "test"}
+      }
+
+      {:ok, envelope} = IsCommercial.call(bbcx_envelope)
+
+      refute envelope.request.raw_headers == %{"is_commercial" => "true"}
     end
   end
 end
