136299258e693234e65aa5687a3df134ceab3021
Artur Novak <artur.novak.ext@bbc.co.uk>
2023-06-15 14:37:58 +0100

    Update example migrator script to migrate test examples only

--- lib/mix/tasks/example_migrator.ex
+++ lib/mix/tasks/example_migrator.ex
@@ -2,8 +2,10 @@ defmodule Mix.Tasks.ExampleMigrator do
   @moduledoc "Adds examples from routefiles to their respective routespecs"
   use Mix.Task
 
+  # This script migrates single-platform specs only to make the script simple
   @multi_platform_specs [
     "BitesizeArticles",
+    "BitesizeGuides",
     "BitesizeTopics",
     "BitesizeTransition",
     "NewsArticleMvt",
@@ -13,7 +15,9 @@ defmodule Mix.Tasks.ExampleMigrator do
 
   def run([]) do
     @routes
-    |> Enum.filter(fn {_path, route_map} -> route_map.using not in @multi_platform_specs end)
+    |> Enum.filter(fn {_path, route_map} ->
+      route_map.using not in @multi_platform_specs and route_map.only_on != "test"
+    end)
     |> Enum.map(fn {_path, route_map} -> route_map end)
     |> Enum.group_by(&Map.get(&1, :using))
     |> Enum.each(fn {spec_name, route_maps} -> write_tests(spec_name, route_maps) end)
@@ -27,33 +31,19 @@ defmodule Mix.Tasks.ExampleMigrator do
   defp write_tests(spec_name, route_maps) do
     spec_path = spec_src(spec_name)
 
-    examples = filter_examples_env(route_maps)
-    test_examples = filter_examples_env(route_maps, "test")
+    examples =
+      route_maps
+      |> Enum.map(&format_examples/1)
+      |> List.flatten()
 
     {:ok, original} = File.read(spec_path)
     {:ok, io_device} = File.open(spec_path, [:write])
 
-    updated_spec = update_spec(String.split(original, "\n      }\n    }"), examples, test_examples)
+    [head, tail] = String.split(original, "\n      }\n    }")
+    updated_spec = head <> ",\n        examples: #{inspect(examples, limit: :infinity)}\n      }\n    }" <> tail
 
     IO.write(io_device, updated_spec)
     File.close(io_device)
-  end
-
-  defp update_spec([head, tail], examples, []) do
-    "#{head}," <> "\n        examples: #{inspect(examples, limit: :infinity)}\n      }\n    }" <> tail
-  end
-
-  defp update_spec([head, tail], examples, test_examples) do
-    head = String.replace(head, "specification do", "specification(production_env) do")
-
-    tail =
-      String.replace(
-        tail,
-        "\nend\n",
-        "\n\n  defp examples(\"live\"), do: #{inspect(examples, limit: :infinity)}\n  defp examples(_production_env), do: #{inspect(test_examples, limit: :infinity)}\nend\n"
-      )
-
-    "#{head}," <> "\n        examples: examples(production_env)\n      }\n    }" <> tail
   end
 
   defp format_examples(%{examples: examples}) do
@@ -66,11 +56,4 @@ defmodule Mix.Tasks.ExampleMigrator do
 
   defp format_example(example) when is_binary(example), do: example
   defp format_example({example, status}), do: %{path: example, expected_status: status}
-
-  defp filter_examples_env(examples, env \\ nil) do
-    examples
-    |> Enum.filter(fn e -> e.only_on == env end)
-    |> Enum.map(&format_examples/1)
-    |> List.flatten()
-  end
 end
