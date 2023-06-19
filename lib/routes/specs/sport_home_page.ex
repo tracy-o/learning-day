bb208f5fbb6a7942bf30e8675041f5b13c330c73
nei1d0r <neil.davis02@bbc.co.uk>
2023-06-19 09:45:44 +0100

    Bbchome 1496 allow personalisation for sport home (#2064)
    * feat: added handlers for stories, have_your_say and the_reporters routes and a redirect for the_reporters url
    
    * chore: add redirect back in after resolving changes
    
    * feat: enables personalisation for the sport homepage
    
    ---------
    
    Co-authored-by: ${nei1d0r} <${neil.davis02@bbc.co.uk}>

--- lib/routes/specs/sport_home_page.ex
+++ lib/routes/specs/sport_home_page.ex
@@ -5,6 +5,7 @@ defmodule Routes.Specs.SportHomePage do
         owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
         runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Homepage%20&%20Nations%20-%20WebCore%20-%20Runbook",
         platform: "Webcore",
+        personalisation: "on",
         fallback_write_sample: 0.5,
         examples: ["/sport"]
       }
