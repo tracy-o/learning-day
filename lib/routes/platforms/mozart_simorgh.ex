6a7923062698df27ce0e9639919e26ae0968a3bc
Gui Heurich <guliherme.heurich@protonmail.com>
2023-06-21 11:19:53 +0100

    Do not include Mozart-Simorgh and Mozart-Weather platforms in these changes

--- lib/routes/platforms/mozart_simorgh.ex
+++ lib/routes/platforms/mozart_simorgh.ex
@@ -6,7 +6,6 @@ defmodule Routes.Platforms.MozartSimorgh do
       runbook: "https://confluence.dev.bbc.co.uk/display/MOZART/Mozart+Run+Book",
       request_pipeline: pipeline(production_env),
       response_pipeline: ["CacheDirective", "ResponseHeaderGuardian", "PreCacheCompression"],
-      headers_allowlist: ["cookie-ckns_bbccom_beta"],
       query_params_allowlist: query_params_allowlist(production_env),
       circuit_breaker_error_threshold: 200,
       signature_keys: %{add: [:is_advertise], skip: [:country]}
@@ -16,6 +15,6 @@ defmodule Routes.Platforms.MozartSimorgh do
   defp query_params_allowlist("live"), do: []
   defp query_params_allowlist(_production_env), do: ["component_env", "morph_env", "renderer_env"]
 
-  defp pipeline("live"), do: ["IsCommercial", "CircuitBreaker"]
+  defp pipeline("live"), do: ["CircuitBreaker"]
   defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
 end
--- lib/routes/platforms/mozart_weather.ex
+++ lib/routes/platforms/mozart_weather.ex
@@ -7,7 +7,7 @@ defmodule Routes.Platforms.MozartWeather do
       request_pipeline: pipeline(production_env),
       response_pipeline: ["CacheDirective", :_routespec_pipeline_placeholder, "ResponseHeaderGuardian", "PreCacheCompression"],
       query_params_allowlist: query_params_allowlist(production_env),
-      headers_allowlist: ["cookie-ckps_language", "cookie-ckns_bbccom_beta"],
+      headers_allowlist: ["cookie-ckps_language"],
       circuit_breaker_error_threshold: 200
     }
   end
@@ -15,6 +15,6 @@ defmodule Routes.Platforms.MozartWeather do
   defp query_params_allowlist("live"), do: ["alternativeJsLoading", "amp", "batch", "before", "category_site", "component_id", "components", "config_path", "embeddingPageTitle", "embeddingPageUri", "lang", "options", "page", "presenter", "ptrt", "q", "redirect_location", "s", "search", "show-service-calls", "start", "ticker", "anchor"]
   defp query_params_allowlist(_production_env), do: "*"
 
-  defp pipeline("live"), do: [:_routespec_pipeline_placeholder, "IsCommercial", "CircuitBreaker"]
+  defp pipeline("live"), do: [:_routespec_pipeline_placeholder, "CircuitBreaker"]
   defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
 end
