f8dc40d45bb8eb6b35f17f32c5199d3083a7bd55
Harvey Peachey <harveypeachey@hotmail.co.uk>
2023-06-28 13:57:46 +0100

    integrates brands into bbcx preflight transformers

--- lib/belfrage/preflight_transformers/bbcx_platform_selector_common.ex
+++ lib/belfrage/preflight_transformers/bbcx_platform_selector_common.ex
@@ -1,34 +1,19 @@
 defmodule Belfrage.PreflightTransformers.BBCXPlatformSelectorCommon do
-  alias Belfrage.Envelope
-
-  @dial Application.compile_env(:belfrage, :dial)
-
-  @allowed_countries ["us", "ca"]
+  alias Belfrage.{Envelope, Brands}
 
   def add_platform_to_envelope(envelope = %Envelope{}, route_platform) do
     {:ok,
      Envelope.add(envelope, :private, %{
-       bbcx_enabled: bbcx_enabled?(envelope),
+       bbcx_enabled: Brands.bbcx_enabled?(envelope),
        platform: select_platform(envelope, route_platform)
      })}
   end
 
-  defp bbcx_enabled?(%Envelope{private: %Envelope.Private{production_environment: prod_env}}) do
-    prod_env == "test"
-  end
-
   defp select_platform(
-         %Envelope{
-           request: request,
-           private: %Envelope.Private{production_environment: prod_env}
-         },
+         envelope,
          route_platform
        ) do
-    if prod_env == "test" &&
-         @dial.state(:bbcx_enabled) == true &&
-         String.ends_with?(request.host, "bbc.com") &&
-         Map.get(request.raw_headers, "cookie-ckns_bbccom_beta") == "1" &&
-         request.country in @allowed_countries do
+    if Brands.is_bbcx?(envelope) do
       "BBCX"
     else
       route_platform
