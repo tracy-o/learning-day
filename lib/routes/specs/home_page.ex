ad7c132d999d05a4c222a890b5db80f1fc6556cc
DavidThorpe71 <davidthorpe878@gmail.com>
2023-07-03 12:46:31 +0100

    Updates the homepage route spec to include a preflight pipeline and alternate DotCom Homepage platform spec

--- lib/routes/specs/home_page.ex
+++ lib/routes/specs/home_page.ex
@@ -1,14 +1,20 @@
 defmodule Routes.Specs.HomePage do
   def specification do
     %{
-      specs: %{
-        owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
-        runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Homepage%20&%20Nations%20-%20WebCore%20-%20Runbook",
-        platform: "Webcore",
-        personalisation: "on",
-        fallback_write_sample: 0.5,
-        examples: ["/"]
-      }
+      preflight_pipeline: ["DotComHomepagePlatformSelector"],
+      specs: [
+        %{
+          owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
+          runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/Homepage%20&%20Nations%20-%20WebCore%20-%20Runbook",
+          platform: "Webcore",
+          personalisation: "on",
+          fallback_write_sample: 0.5,
+          examples: ["/"]
+        },
+        %{
+          platform: "DotComHomepage"
+        }
+      ]
     }
   end
 end
