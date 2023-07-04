6970899e444daf4c19076d0126351b73ff6fdeaf
DavidThorpe71 <davidthorpe878@gmail.com>
2023-07-04 13:35:46 +0100

    Update the tests to remove some duplication - in BBCXFuturePlatformSelectorTest we test the basic functionality, the more complete test coverage for the BBCXPlatformSelectorCommon is in BBCXPlatformSelectorCommonTest

--- test/belfrage/preflight_transformers/bbcx_future_platform_selector_test.exs
+++ test/belfrage/preflight_transformers/bbcx_future_platform_selector_test.exs
@@ -5,7 +5,21 @@ defmodule Belfrage.PreflightTransformers.BBCXFuturePlatformSelectorTest do
   alias Belfrage.PreflightTransformers.BBCXFuturePlatformSelector
   alias Belfrage.{Envelope, Envelope.Request}
 
-  test "when the cookie is present, and the host is bbc.com but the country is not us or ca return DotComFuture" do
+  test "when the envelope data does fulfill the bbcx selection requirements, return BBCX" do
+    {:ok, response} =
+      BBCXFuturePlatformSelector.call(%Envelope{
+        request: %Request{
+          host: "www.bbc.com",
+          country: "us",
+          raw_headers: %{"cookie-ckns_bbccom_beta" => "1"}
+        },
+        private: %Envelope.Private{production_environment: "test"}
+      })
+
+    assert response.private.platform == "BBCX"
+  end
+
+  test "when the envelope data does NOT fulfill the bbcx selection requirements, return DotComFuture" do
     {:ok, response} =
       BBCXFuturePlatformSelector.call(%Envelope{
         request: %Request{
@@ -17,126 +31,5 @@ defmodule Belfrage.PreflightTransformers.BBCXFuturePlatformSelectorTest do
       })
 
     assert response.private.platform == "DotComFuture"
-    assert response.private.bbcx_enabled == true
-  end
-
-  test "when the cookie is not present, and the host is bbc.com but the country is not us or ca return DotComFuture" do
-    {:ok, response} =
-      BBCXFuturePlatformSelector.call(%Envelope{
-        request: %Request{
-          host: "www.bbc.com",
-          country: "gb"
-        },
-        private: %Envelope.Private{production_environment: "test"}
-      })
-
-    assert response.private.platform == "DotComFuture"
-    assert response.private.bbcx_enabled == true
-  end
-
-  test "when the cookie is not present, and the host is bbc.com and the country is us or ca return DotComFuture" do
-    {:ok, response} =
-      BBCXFuturePlatformSelector.call(%Envelope{
-        request: %Request{
-          host: "www.bbc.com",
-          country: "us"
-        },
-        private: %Envelope.Private{production_environment: "test"}
-      })
-
-    assert response.private.platform == "DotComFuture"
-    assert response.private.bbcx_enabled == true
-  end
-
-  test "when the cookie is present, and the host is bbc.com and the country is us or ca return BBCX" do
-    {:ok, response} =
-      BBCXFuturePlatformSelector.call(%Envelope{
-        request: %Request{
-          raw_headers: %{"cookie-ckns_bbccom_beta" => "1"},
-          host: "www.bbc.com",
-          country: "ca"
-        },
-        private: %Envelope.Private{production_environment: "test"}
-      })
-
-    assert response.private.platform == "BBCX"
-    assert response.private.bbcx_enabled == true
-  end
-
-  test "when the cookie is present, and the host is bbc.com and the country is us or ca, but the Cosmos environment is live return WebCore" do
-    {:ok, response} =
-      BBCXFuturePlatformSelector.call(%Envelope{
-        request: %Request{
-          raw_headers: %{"cookie-ckns_bbccom_beta" => "1"},
-          host: "www.bbc.com",
-          country: "ca"
-        },
-        private: %Envelope.Private{production_environment: "live"}
-      })
-
-    assert response.private.platform == "DotComFuture"
-    assert response.private.bbcx_enabled == false
-  end
-
-  test "when the Cosmos environment is live always return webcore and set bbcx_enbled to false" do
-    {:ok, response} =
-      BBCXFuturePlatformSelector.call(%Envelope{
-        request: %Request{
-          raw_headers: %{"cookie-ckns_bbccom_beta" => "1"},
-          host: "www.bbc.co.uk",
-          country: "gb"
-        },
-        private: %Envelope.Private{production_environment: "live"}
-      })
-
-    assert response.private.platform == "DotComFuture"
-    assert response.private.bbcx_enabled == false
-  end
-
-  test "when the cookie is present, and the host is bbc.com and the country is us or ca, the Cosmos environment is test but the Dial is disabled return DotComFuture" do
-    stub_dials(bbcx_enabled: "false")
-
-    {:ok, response} =
-      BBCXFuturePlatformSelector.call(%Envelope{
-        request: %Request{
-          raw_headers: %{"cookie-ckns_bbccom_beta" => "1"},
-          host: "www.bbc.com",
-          country: "ca"
-        },
-        private: %Envelope.Private{production_environment: "test"}
-      })
-
-    assert response.private.platform == "DotComFuture"
-    assert response.private.bbcx_enabled == true
-  end
-
-  test "when the cookie is present, and the host is not bbc.com and the country is us or ca return DotComFuture" do
-    {:ok, response} =
-      BBCXFuturePlatformSelector.call(%Envelope{
-        request: %Request{
-          raw_headers: %{"cookie-ckns_bbccom_beta" => "value"},
-          host: "www.bbc.co.uk",
-          country: "ca"
-        },
-        private: %Envelope.Private{production_environment: "test"}
-      })
-
-    assert response.private.platform == "DotComFuture"
-    assert response.private.bbcx_enabled == true
-  end
-
-  test "when the cookie is set to '0', and the host is bbc.com and the country is us or ca return DotComFuture" do
-    {:ok, response} =
-      BBCXFuturePlatformSelector.call(%Envelope{
-        request: %Request{
-          raw_headers: %{"cookie-ckns_bbccom_beta" => "0"},
-          host: "www.bbc.com",
-          country: "ca"
-        },
-        private: %Envelope.Private{production_environment: "test"}
-      })
-
-    assert response.private.platform == "DotComFuture"
-    assert response.private.bbcx_enabled == true
   end
 end
