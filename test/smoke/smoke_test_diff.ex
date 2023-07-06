38ae6541f187da4a6b812240495ff750690fe6af
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-07-06 13:48:00 +0100

    Refactoring for accurate naming - get_live_endpoint/1 retrieves the endpoint url

--- test/smoke/smoke_test_diff.ex
+++ test/smoke/smoke_test_diff.ex
@@ -1,57 +1,58 @@
 defmodule Belfrage.SmokeTestDiff do
   alias Test.Support.Helper
 
-  @compare_bid System.get_env("WITH_DIFF")
+  @comparison_bid System.get_env("WITH_DIFF")
 
   def build(response = %Finch.Response{headers: headers}, path, spec) do
-    if "www" == Map.new(headers)["bid"] and @compare_bid do
-      now_compare_with_live(response, path, spec)
+    if "www" == Map.new(headers)["bid"] and @comparison_bid do
+      now_compare_with_stack(response, path, spec)
     else
       :ok
     end
   end
 
-  defp now_compare_with_live(_resp = %Finch.Response{headers: canary_headers}, path, spec) do
-    live_endpoint = Application.get_env(:belfrage, :smoke)[:live][get_compare_endpoint()]
-    {:ok, %Finch.Response{headers: live_headers}} = Helper.get_route(live_endpoint, path, [], spec)
-    compare_headers(path, Map.new(canary_headers), Map.new(live_headers))
+  defp now_compare_with_stack(_resp = %Finch.Response{headers: www_headers}, path, spec) do
+    comparison_endpoint = get_live_endpoint(@comparison_bid)
+    {:ok, %Finch.Response{headers: comparison_headers}} = Helper.get_route(comparison_endpoint, path, [], spec)
+
+    compare_headers(path, Map.new(www_headers), Map.new(comparison_headers))
   end
 
-  defp compare_headers(path, canary_headers, live_headers) do
-    www_endpoint = "https://" <> Application.get_env(:belfrage, :smoke)[:live]["belfrage"] <> path
-    named_endpoint = "https://" <> Application.get_env(:belfrage, :smoke)[:live][get_compare_endpoint()] <> path
+  defp compare_headers(path, www_headers, comparison_headers) do
+    www_endpoint = "https://" <> get_live_endpoint("www") <> path
+    comparison_endpoint = "https://" <> get_live_endpoint(@comparison_bid) <> path
 
     headers_mismatches =
-      check_identical_headers(canary_headers, live_headers) ++ check_similar_headers(canary_headers, live_headers)
+      check_identical_headers(www_headers, comparison_headers) ++ check_similar_headers(www_headers, comparison_headers)
 
     case headers_mismatches do
       [] ->
         :ok
 
       reason ->
-        to_log("\nPath: " <> path <> "\nRoutes: " <> www_endpoint <> ", " <> named_endpoint)
+        to_log("\nPath: " <> path <> "\nRoutes: " <> www_endpoint <> ", " <> comparison_endpoint)
         for r <- reason, do: to_log(r)
         {:error, reason}
     end
   end
 
-  defp check_identical_headers(canary_headers, live_headers) do
+  defp check_identical_headers(www_headers, comparison_headers) do
     ["cache-control", "content-type", "req-svc-chain", "vary"]
     |> Enum.reduce([], fn h, acc ->
-      if canary_headers[h] != live_headers[h] do
-        ["\"#{h}\" mismatch: www: #{canary_headers[h]} --- #{@compare_bid}: #{live_headers[h]}" | acc]
+      if www_headers[h] != comparison_headers[h] do
+        ["\"#{h}\" mismatch: www: #{www_headers[h]} --- #{@comparison_bid}: #{comparison_headers[h]}" | acc]
       else
         acc
       end
     end)
   end
 
-  defp check_similar_headers(canary_headers, live_headers) do
+  defp check_similar_headers(www_headers, comparison_headers) do
     acceptance = 2.0
 
     perc_diff = fn h ->
-      ch = String.to_integer(canary_headers[h])
-      lh = String.to_integer(live_headers[h])
+      ch = String.to_integer(www_headers[h])
+      lh = String.to_integer(comparison_headers[h])
 
       dec = if ch + lh == 0, do: 0.0, else: abs(ch - lh) / ((ch + lh) / 2)
       Float.round(dec * 100, 2)
@@ -60,7 +61,7 @@ defmodule Belfrage.SmokeTestDiff do
     ["content-length"]
     |> Enum.reduce([], fn h, acc ->
       if perc_diff.(h) > acceptance do
-        ["\"#{h}\" exceeding variance: www: #{canary_headers[h]} --- #{@compare_bid}: #{live_headers[h]}" | acc]
+        ["\"#{h}\" exceeding variance: www: #{www_headers[h]} --- #{@comparison_bid}: #{comparison_headers[h]}" | acc]
       else
         acc
       end
@@ -73,9 +74,11 @@ defmodule Belfrage.SmokeTestDiff do
     File.close(io_device)
   end
 
-  defp get_compare_endpoint() do
-    Application.get_env(:belfrage, :smoke)[:endpoint_to_stack_id_mapping]
-    |> Enum.find(fn {_endpoint, id} -> id[:value] == @compare_bid end)
-    |> elem(0)
+  defp get_live_endpoint(bid) do
+    {endpoint_name, _} =
+      Application.get_env(:belfrage, :smoke)[:endpoint_to_stack_id_mapping]
+      |> Enum.find(fn {_e, id} -> id[:value] == bid end)
+
+    Application.get_env(:belfrage, :smoke)[:live][endpoint_name]
   end
 end
