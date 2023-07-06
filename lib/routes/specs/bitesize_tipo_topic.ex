d0a1bd4f5a61e3a3e4e021b029992a2e7a7ad877
Mark Taylor <104359546+MarkTaylorBBC@users.noreply.github.com>
2023-07-06 15:48:42 +0100

    Update to a valid example (#2108)

--- lib/routes/specs/bitesize_tipo_topic.ex
+++ lib/routes/specs/bitesize_tipo_topic.ex
@@ -8,7 +8,7 @@ defmodule Routes.Specs.BitesizeTipoTopic do
         runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Topics+Runbook",
         query_params_allowlist: ["page"],
         request_pipeline: ["ComToUKRedirect"],
-        examples: ["/bitesize/parents", "/bitesize/groups/cz4wkv77g55t"]
+        examples: ["/bitesize/parents", "/bitesize/groups/cw9v6l8d0q6t"]
       }
     }
   end
