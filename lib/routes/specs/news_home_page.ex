03b82ae5acf7884e544292758f34287faf21d6cb
DavidThorpe71 <davidthorpe878@gmail.com>
2023-07-07 10:57:44 +0100

    Adds BBCXMozartNewsPlatformSelector preflight transformer to the news homepage route spec

--- lib/routes/specs/news_home_page.ex
+++ lib/routes/specs/news_home_page.ex
@@ -1,12 +1,19 @@
 defmodule Routes.Specs.NewsHomePage do
   def specification do
     %{
-      specs: %{
-        owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
-        platform: "MozartNews",
-        fallback_write_sample: 0.5,
-        examples: ["/news"]
-      }
+      preflight_pipeline: ["BBCXMozartNewsPlatformSelector"],
+      specs: [
+        %{
+          owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
+          platform: "MozartNews",
+          fallback_write_sample: 0.5,
+          examples: ["/news"]
+        },
+        %{
+          platform: "BBCX",
+          fallback_write_sample: 0.5,
+        }
+      ]
     }
   end
 end
