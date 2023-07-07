0d41610835657157ed66c56158acc4a40830155d
DavidThorpe71 <davidthorpe878@gmail.com>
2023-07-07 10:30:08 +0100

    Reorders tests a little

--- test/belfrage/request_transformers/bbcx_redirect_test.exs
+++ test/belfrage/request_transformers/bbcx_redirect_test.exs
@@ -5,24 +5,6 @@ defmodule Belfrage.RequestTransformers.BBCXRedirectTest do
   alias Belfrage.RequestTransformers.BBCXRedirect
   alias Belfrage.{Envelope, Envelope.Request}
 
-  test "when the envelope data does not fulfill bbcx selection requirements then we redirect to the correct path from the redirect map" do
-    {:stop, envelope} =
-      BBCXRedirect.call(%Envelope{
-        request: %Request{
-          path: "/news/war-in-ukraine",
-          host: "www.bbc.com",
-          country: "gb",
-          raw_headers: %{"cookie-ckns_bbccom_beta" => "0"}
-        },
-        private: %Envelope.Private{production_environment: "test"}
-      })
-
-    assert envelope.response.http_status == 302
-    assert envelope.response.headers["x-bbc-no-scheme-rewrite"] == "1"
-    assert envelope.response.headers["cache-control"] == "public, max-age=60"
-    assert envelope.response.headers["location"] == "/news/world-60525350"
-  end
-
   test "when the envelope data fulfills the bbcx selection requirements then we continue without redirection" do
     {:ok, envelope} =
       BBCXRedirect.call(%Envelope{
@@ -38,6 +20,24 @@ defmodule Belfrage.RequestTransformers.BBCXRedirectTest do
     assert envelope.request.path == "/news/war-in-ukraine"
     assert envelope.request.host == "www.bbc.com"
     assert envelope.response.http_status == nil
+  end
+
+  test "when the envelope data does not fulfill bbcx selection requirements then we redirect to the correct path from the redirect map" do
+    {:stop, envelope} =
+      BBCXRedirect.call(%Envelope{
+        request: %Request{
+          path: "/news/war-in-ukraine",
+          host: "www.bbc.com",
+          country: "us",
+          raw_headers: %{"cookie-ckns_bbccom_beta" => "0"}
+        },
+        private: %Envelope.Private{production_environment: "test"}
+      })
+
+    assert envelope.response.http_status == 302
+    assert envelope.response.headers["x-bbc-no-scheme-rewrite"] == "1"
+    assert envelope.response.headers["cache-control"] == "public, max-age=60"
+    assert envelope.response.headers["location"] == "/news/world-60525350"
   end
 
   test "when the envelope data does not fulfill bbcx selection requirements but there is no entry in the redirect map for the path then we redirect to the default path" do
