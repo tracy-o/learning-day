75ba16ddbc1e0e4ea1c94f8a709f74c292d86d8a
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-07-09 20:53:30 +0100

    map_headers/1 alters some headers and returns map - now_compare_with_stack/4 doesn't need whole response - Refactor, rename and format

--- test/smoke/smoke_test_diff.ex
+++ test/smoke/smoke_test_diff.ex
@@ -3,28 +3,28 @@ defmodule Belfrage.SmokeTestDiff do
 
   @comparison_bid System.get_env("WITH_DIFF")
 
-  def build(response = %Finch.Response{headers: headers}, matcher_spec, "live") do
-    if "www" == Map.new(headers)["bid"] and @comparison_bid do
-      now_compare_with_stack(response, matcher_spec.path, matcher_spec.headers, matcher_spec.spec)
+  def build(_resp = %Finch.Response{headers: resp_headers}, matcher_spec, "live") do
+    if "www" == Map.new(resp_headers)["bid"] and @comparison_bid do
+      now_compare_with_stack(resp_headers, matcher_spec.path, matcher_spec.headers, matcher_spec.spec)
     else
       :ok
     end
   end
 
-  def build(_resp, _matcher_spec, _env), do: :ok
+  def build(_response, _matcher_spec, _env), do: :ok
 
-  defp now_compare_with_stack(_resp = %Finch.Response{headers: www_headers}, path, req_headers, spec) do
+  defp now_compare_with_stack(www_headers, path, req_headers, spec) do
     comparison_endpoint = get_live_endpoint(@comparison_bid)
 
     {:ok, %Finch.Response{headers: comparison_headers}} =
       Helper.get_route(comparison_endpoint, path, Map.to_list(req_headers), spec)
 
-    compare_headers(path, Map.new(www_headers), Map.new(comparison_headers))
+    compare_headers(path, map_headers(www_headers), map_headers(comparison_headers))
   end
 
   defp compare_headers(path, www_headers, comparison_headers) do
-    www_endpoint = "https://" <> get_live_endpoint("www") <> path
-    comparison_endpoint = "https://" <> get_live_endpoint(@comparison_bid) <> path
+    www_url = "https://" <> get_live_endpoint("www") <> path
+    comparison_url = "https://" <> get_live_endpoint(@comparison_bid) <> path
 
     headers_mismatches =
       check_identical_headers(www_headers, comparison_headers) ++ check_similar_headers(www_headers, comparison_headers)
@@ -34,8 +34,10 @@ defmodule Belfrage.SmokeTestDiff do
         :ok
 
       reason ->
-        reasons = Enum.join(reason, "\n")
-        to_log("Path: " <> path <> "\n" <> www_endpoint <> " || " <> comparison_endpoint <> "\n" <> reasons <> "\n")
+        to_log(
+          "Path: " <> path <> "\n" <> www_url <> " || " <> comparison_url <> "\n" <> Enum.join(reason, "\n") <> "\n"
+        )
+
         {:error, reason}
     end
   end
@@ -55,10 +57,10 @@ defmodule Belfrage.SmokeTestDiff do
     acceptance = 2.0
 
     perc_diff = fn h ->
-      ch = String.to_integer(www_headers[h])
-      lh = String.to_integer(comparison_headers[h])
+      wh = String.to_integer(www_headers[h])
+      ch = String.to_integer(comparison_headers[h])
 
-      dec = if ch + lh == 0, do: 0.0, else: abs(ch - lh) / ((ch + lh) / 2)
+      dec = if wh + ch == 0, do: 0.0, else: abs(wh - ch) / ((wh + ch) / 2)
       Float.round(dec * 100, 2)
     end
 
@@ -84,5 +86,15 @@ defmodule Belfrage.SmokeTestDiff do
       |> Enum.find(fn {_e, id} -> id[:value] == bid end)
 
     Application.get_env(:belfrage, :smoke)[:live][endpoint_name]
+  end
+
+  defp map_headers(headers) do
+    headers = Map.new(headers)
+
+    if headers["req-svc-chain"] do
+      Map.put(headers, "req-svc-chain", String.replace(headers["req-svc-chain"], "FASTLY,GTM,", ""))
+    else
+      headers
+    end
   end
 end
