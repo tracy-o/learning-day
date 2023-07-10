35443cfc8bdb744c85c49152cd29d8788274d48b
DavidThorpe71 <davidthorpe878@gmail.com>
2023-07-10 15:39:04 +0100

    Updates the NewsTopics route spec to use the BBCXWebcorePlatformSelector in the preflight piepline

--- lib/routes/specs/news_topics.ex
+++ lib/routes/specs/news_topics.ex
@@ -1,15 +1,21 @@
 defmodule Routes.Specs.NewsTopics do
   def specification(production_env) do
     %{
-      specs: %{
-        owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
-        runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Topics+Runbook",
-        platform: "Webcore",
-        query_params_allowlist: query_params_allowlist(production_env),
-        request_pipeline: ["NewsTopicsPlatformDiscriminator"],
-        personalisation: "test_only",
-        examples: ["/news/topics/cljev4jz3pjt", %{expected_status: 301, path: "/news/topics/66535a45-8563-4598-be75-851e8e3cb9a9"}, "/news/topics/c207p54mljpt/aberdeenshire-council", %{expected_status: 301, path: "/news/topics/66535a45-8563-4598-be75-851e8e3cb9a9/aberdeenshire-council"}]
-      }
+      preflight_pipeline: ["BBCXWebcorePlatformSelector"],
+      specs: [
+        %{
+          owner: "DEHomepageTopicsOnCallTeam@bbc.co.uk",
+          runbook: "https://confluence.dev.bbc.co.uk/display/DPTOPICS/Topics+Runbook",
+          platform: "Webcore",
+          query_params_allowlist: query_params_allowlist(production_env),
+          request_pipeline: ["NewsTopicsPlatformDiscriminator"],
+          personalisation: "test_only",
+          examples: ["/news/topics/cljev4jz3pjt", %{expected_status: 301, path: "/news/topics/66535a45-8563-4598-be75-851e8e3cb9a9"}, "/news/topics/c207p54mljpt/aberdeenshire-council", %{expected_status: 301, path: "/news/topics/66535a45-8563-4598-be75-851e8e3cb9a9/aberdeenshire-council"}]
+        },
+        %{
+          platform: "BBCX"
+        }
+      ]
     }
   end
 
