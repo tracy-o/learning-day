74ac9dbe04b189305a9123f09e32b94c7ccbc4f4
Artur Novak <artur.novak.ext@bbc.co.uk>
2023-07-10 10:18:16 +0100

    Allow 'cookie-ckns_bbccom_beta' for bbcx headers on test only

--- lib/routes/platforms/bbcx.ex
+++ lib/routes/platforms/bbcx.ex
@@ -6,10 +6,13 @@ defmodule Routes.Platforms.BBCX do
       runbook: "https://confluence.dev.bbc.co.uk/display/BBCX/BBCX+Product+and+Technology+Run+book",
       request_pipeline: pipeline(production_env),
       response_pipeline: ["CacheDirective", "ResponseHeaderGuardian", "PreCacheCompression"],
-      headers_allowlist: ["cookie-ckns_bbccom_beta"],
+      headers_allowlist: headers_allowlist(production_env),
       circuit_breaker_error_threshold: 200
     }
   end
+
+  defp headers_allowlist("live"), do: []
+  defp headers_allowlist(_production_env), do: ["cookie-ckns_bbccom_beta"]
 
   defp pipeline("live"), do: [:_routespec_pipeline_placeholder, "CircuitBreaker"]
   defp pipeline(_production_env), do: pipeline("live") ++ ["DevelopmentRequests"]
