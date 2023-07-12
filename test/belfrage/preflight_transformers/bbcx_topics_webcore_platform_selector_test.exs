64ac638f8288b163a0116a66a31856debfb3241a
DavidThorpe71 <davidthorpe878@gmail.com>
2023-07-12 09:08:01 +0100

    Further test criteria

--- test/belfrage/preflight_transformers/bbcx_topics_webcore_platform_selector_test.exs
+++ test/belfrage/preflight_transformers/bbcx_topics_webcore_platform_selector_test.exs
@@ -72,4 +72,36 @@ defmodule Belfrage.PreflightTransformers.BBCXTopicsWebcorePlatformSelectorTest d
     assert response.private.platform == "Webcore"
     assert response.private.bbcx_enabled == true
   end
+
+  test "when the request does not meet the criteria for BBCX and id is a GUID and there is no slug param then the Webcore platform is selected" do
+    {:ok, response} =
+      BBCXTopicsWebcorePlatformSelector.call(%Envelope{
+        request: %Request{
+          raw_headers: %{"cookie-ckns_bbccom_beta" => "0"},
+          host: "www.bbc.com",
+          country: "us",
+          path_params: %{"id" => "66535a45-8563-4598-be75-851e8e3cb9a9"}
+        },
+        private: %Envelope.Private{production_environment: "test"}
+      })
+
+    assert response.private.platform == "Webcore"
+    assert response.private.bbcx_enabled == false
+  end
+
+  test "when the request does not meet the criteria for BBCX and id is a Tipo ID and there is a slug param then the Webcore platform is selected" do
+    {:ok, response} =
+      BBCXTopicsWebcorePlatformSelector.call(%Envelope{
+        request: %Request{
+          raw_headers: %{"cookie-ckns_bbccom_beta" => "0"},
+          host: "www.bbc.com",
+          country: "us",
+          path_params: %{"id" => "66535a45-8563-4598-be75-851e8e3cb9a9", "slug" => "my-slug"}
+        },
+        private: %Envelope.Private{production_environment: "test"}
+      })
+
+    assert response.private.platform == "Webcore"
+    assert response.private.bbcx_enabled == false
+  end
 end
