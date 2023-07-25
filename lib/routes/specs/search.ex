b4fb1b48e7d265929a76e3a7f4bc30a3e910fdcf
ettomatic <ettore@fastmail.com>
2023-07-25 11:58:37 +0100

    Add /search route to BBCX.

--- lib/routes/specs/search.ex
+++ lib/routes/specs/search.ex
@@ -1,19 +1,27 @@
 defmodule Routes.Specs.Search do
   def specification(production_env) do
     %{
-      specs: %{
-        owner: "D+ESearchAndNavigationDev@bbc.co.uk",
-        runbook: "https://confluence.dev.bbc.co.uk/x/xo2KD",
-        request_pipeline: ["ComToUKRedirect"],
-        platform: "Webcore",
-        query_params_allowlist: query_params_allowlist(production_env),
-        caching_enabled: false,
-        examples: ["/bitesize/search", "/cbbc/search", "/cbeebies/search", "/search"]
-      }
+      preflight_pipeline: ["BBCXWebcorePlatformSelector"],
+      specs: [
+        %{
+          owner: "D+ESearchAndNavigationDev@bbc.co.uk",
+          runbook: "https://confluence.dev.bbc.co.uk/x/xo2KD",
+          request_pipeline: ["ComToUKRedirect"],
+          platform: "Webcore",
+          query_params_allowlist: query_params_allowlist(production_env),
+          caching_enabled: false,
+          examples: ["/bitesize/search", "/cbbc/search", "/cbeebies/search", "/search"]
+        },
+        %{
+          platform: "BBCX",
+          query_params_allowlist: query_params_allowlist(production_env),
+          caching_enabled: false,
+          examples: ["/search"]
+        }
+      ]
     }
   end
 
   defp query_params_allowlist("live"), do: ["q", "page", "d"]
   defp query_params_allowlist(_production_env), do: query_params_allowlist("live") ++ ["contentenv"]
-
 end
