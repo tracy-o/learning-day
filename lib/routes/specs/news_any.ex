3fbbfcddaff44005347960bde885568169b0e860
Sam French <Sam.French@bbc.co.uk>
2023-07-25 16:34:51 +0100

    Send NewsAny to Mozart (#2178)

--- lib/routes/specs/news_any.ex
+++ lib/routes/specs/news_any.ex
@@ -1,23 +1,12 @@
 defmodule Routes.Specs.NewsAny do
   def specification do
     %{
-      preflight_pipeline: ["AssetTypePlatformSelector"],
-      specs: [
-        %{
-          owner: "DENewsFrameworksTeam@bbc.co.uk",
-          runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
-          platform: "MozartNews",
-          examples: []
-        },
-        %{
-          owner: "DEWebcoreArticlesCapabilityTeams@bbc.co.uk",
-          runbook: "https://confluence.dev.bbc.co.uk/display/NEWSCPSSTOR/News+CPS+Stories+Run+Book",
-          platform: "Webcore",
-          request_pipeline: [
-            "ObitMode"
-          ]
-        }
-      ]
+      specs: %{
+        owner: "DENewsFrameworksTeam@bbc.co.uk",
+        runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
+        platform: "MozartNews",
+        examples: []
+      }
     }
   end
 end
