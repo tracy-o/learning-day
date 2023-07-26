a1a71862462237ff7928d6f080ab3ea3fcfb3cab
BBCMarR <55087279+BBCMarR@users.noreply.github.com>
2023-07-26 13:40:37 +0100

    BBCHOME-NT support sport index rss (#2184)
    * BBCHOME-NT enable golf
    
    * BBCHOME-NT example
    
    * BBCHOME-NT all of them
    
    * BBCHOME-NT tidy up
    
    * BBCHOME-NT possible fix
    
    * BBCHOME-NT another possible fix

--- lib/routes/specs/sport_rss.ex
+++ lib/routes/specs/sport_rss.ex
@@ -6,7 +6,7 @@ defmodule Routes.Specs.SportRss do
         runbook: "https://confluence.dev.bbc.co.uk/display/BBCHOME/RSS+Feeds+-+WebCore+-+Runbook",
         platform: "Karanga",
         request_pipeline: ["RssFeedRedirect"],
-        examples: ["/sport/football/european-championship/2016/rss.xml", "/sport/football/premier-league/rss.xml"]
+        examples: ["/sport/football/european-championship/2016/rss.xml"]
       }
     }
   end
