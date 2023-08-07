ec87a11a9572589aff2ff8c32e529ca7eb0a7fad
Artur Novak <artur.novak.ext@bbc.co.uk>
2023-08-07 18:48:47 +0100

    Report cache_memory and average_cache_entry_size metrics

--- test/belfrage/metrics/cachex_test.exs
+++ test/belfrage/metrics/cachex_test.exs
@@ -3,91 +3,127 @@ defmodule Belfrage.Metrics.CachexTest do
 
   import Belfrage.Test.MetricsHelper
 
-  test "expose expected metrics from cachex" do
-    cache = String.to_atom("cache#{System.unique_integer()}")
-    {:ok, _pid} = Cachex.start_link(cache, stats: true)
-    {socket, port} = given_udp_port_opened()
+  describe "expose" do
+    setup do
+      cache = String.to_atom("cache#{System.unique_integer()}")
+      {:ok, _pid} = Cachex.start_link(cache, stats: true)
+      {socket, port} = given_udp_port_opened()
 
-    start_reporter(
-      metrics: Belfrage.Metrics.Statsd.cachex_metrics(),
-      formatter: :datadog,
-      global_tags: [BBCEnvironment: "live"],
-      port: port
-    )
-
-    Cachex.get(cache, "key1")
-
-    Belfrage.Metrics.Cachex.track(cache)
-
-    assert_reported(
-      socket,
-      Enum.join(
-        [
-          "cachex.evictions:0|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.expirations:0|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.hits:0|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.misses:1|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.updates:0|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.writes:0|g|#BBCEnvironment:live,cache_name:#{cache}"
-        ],
-        "\n"
+      start_reporter(
+        metrics: Belfrage.Metrics.Statsd.cachex_metrics(),
+        formatter: :datadog,
+        global_tags: [BBCEnvironment: "live"],
+        port: port
       )
-    )
 
-    assert_reported(
-      socket,
-      "cachex.size:0|g|#BBCEnvironment:live,cache_name:#{cache}"
-    )
+      %{cache: cache, socket: socket}
+    end
 
-    Cachex.put(cache, "key1", "value1", ttl: 50)
-    Cachex.get(cache, "key1")
+    test "stats measurement metrics from cachex", %{cache: cache, socket: socket} do
+      Cachex.get(cache, "key1")
 
-    Belfrage.Metrics.Cachex.track(cache)
+      Belfrage.Metrics.Cachex.track(cache)
 
-    assert_reported(
-      socket,
-      Enum.join(
-        [
-          "cachex.evictions:0|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.expirations:0|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.hits:1|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.misses:1|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.updates:0|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.writes:1|g|#BBCEnvironment:live,cache_name:#{cache}"
-        ],
-        "\n"
+      assert_reported(
+        socket,
+        Enum.join(
+          [
+            "cachex.evictions:0|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.expirations:0|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.hits:0|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.misses:1|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.updates:0|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.writes:0|g|#BBCEnvironment:live,cache_name:#{cache}"
+          ],
+          "\n"
+        )
       )
-    )
 
-    assert_reported(
-      socket,
-      "cachex.size:1|g|#BBCEnvironment:live,cache_name:#{cache}"
-    )
-
-    # wait for entry to expire and trigger a cleanup
-    Process.sleep(100)
-    Cachex.purge(cache)
-
-    Belfrage.Metrics.Cachex.track(cache)
-
-    assert_reported(
-      socket,
-      Enum.join(
-        [
-          "cachex.evictions:1|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.expirations:1|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.hits:1|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.misses:1|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.updates:0|g|#BBCEnvironment:live,cache_name:#{cache}",
-          "cachex.writes:1|g|#BBCEnvironment:live,cache_name:#{cache}"
-        ],
-        "\n"
+      assert_reported(
+        socket,
+        "cachex.size:0|g|#BBCEnvironment:live,cache_name:#{cache}"
       )
-    )
 
-    assert_reported(
-      socket,
-      "cachex.size:0|g|#BBCEnvironment:live,cache_name:#{cache}"
-    )
+      Cachex.put(cache, "key1", "value1", ttl: 50)
+      Cachex.get(cache, "key1")
+
+      Belfrage.Metrics.Cachex.track(cache)
+
+      assert_reported(
+        socket,
+        Enum.join(
+          [
+            "cachex.evictions:0|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.expirations:0|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.hits:1|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.misses:1|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.updates:0|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.writes:1|g|#BBCEnvironment:live,cache_name:#{cache}"
+          ],
+          "\n"
+        )
+      )
+
+      assert_reported(
+        socket,
+        "cachex.size:1|g|#BBCEnvironment:live,cache_name:#{cache}"
+      )
+
+      # wait for entry to expire and trigger a cleanup
+      Process.sleep(100)
+      Cachex.purge(cache)
+
+      Belfrage.Metrics.Cachex.track(cache)
+
+      assert_reported(
+        socket,
+        Enum.join(
+          [
+            "cachex.evictions:1|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.expirations:1|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.hits:1|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.misses:1|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.updates:0|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.writes:1|g|#BBCEnvironment:live,cache_name:#{cache}"
+          ],
+          "\n"
+        )
+      )
+
+      assert_reported(
+        socket,
+        "cachex.size:0|g|#BBCEnvironment:live,cache_name:#{cache}"
+      )
+    end
+
+    test "cache memory metrics from cachex", %{cache: cache, socket: socket} do
+      Cachex.put(cache, "key1", "value1", ttl: 50)
+      Belfrage.Metrics.Cachex.track_cache_memory(cache)
+
+      assert_reported(
+        socket,
+        Enum.join(
+          [
+            "cachex.cache_memory:6|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.average_cache_entry_size:6|g|#BBCEnvironment:live,cache_name:#{cache}"
+          ],
+          "\n"
+        )
+      )
+
+      Cachex.put(cache, "key2", "other-value2", ttl: 50)
+      Belfrage.Metrics.Cachex.track_cache_memory(cache)
+
+      assert_reported(
+        socket,
+        Enum.join(
+          [
+            "cachex.cache_memory:18|g|#BBCEnvironment:live,cache_name:#{cache}",
+            "cachex.average_cache_entry_size:9|g|#BBCEnvironment:live,cache_name:#{cache}"
+          ],
+          "\n"
+        )
+      )
+    end
   end
 end
