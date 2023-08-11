370f2d6cedb5e879d8ed473ee5717f8e46a36927
louisearchibald <louise.archibald@bbc.co.uk>
2023-08-11 15:08:07 +0100

    adds homepage redirect for kyrgyz service

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -958,6 +958,7 @@ defroutefile "Main" do
   redirect "/hausa/index.html", to: "/hausa", status: 301
   redirect "/hindi/index.html", to: "/hindi", status: 301
   redirect "/indonesia/index.html", to: "/indonesia", status: 301
+  redirect "/kyrgyz/index.html", to: "/kyrgyz", status: 301
 
   # World Service - Indian Sports Woman of The Year
   redirect "/gujarati/iswoty", to: "/gujarati/resources/idt-c01e87cf-898c-4ec6-86ea-5ef77f9e58a0", status: 302
