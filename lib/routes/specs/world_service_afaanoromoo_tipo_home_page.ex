e9f97ec30fe451dcdfbfc5e3bf7bfabf436730ef
louisearchibald <louise.archibald@bbc.co.uk>
2023-08-16 14:35:44 +0100

    updates routespecs for afaanoromoo service

--- lib/routes/specs/world_service_afaanoromoo_tipo_home_page.ex
+++ lib/routes/specs/world_service_afaanoromoo_tipo_home_page.ex
@@ -4,6 +4,7 @@ defmodule Routes.Specs.WorldServiceAfaanoromooTipoHomePage do
       specs: %{
         platform: "Simorgh",
         request_pipeline: ["WorldServiceRedirect"]
+        examples: ["/afaanoromoo", "/afaanoromoo.amp"]
       }
     }
   end
