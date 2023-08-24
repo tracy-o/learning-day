891365cdcb01864bd90be392c70d19e29a54f81f
louisearchibald <louise.archibald@bbc.co.uk>
2023-08-24 15:11:44 +0100

    adds mapping entry to corresponding topic id for amharic

--- lib/belfrage/request_transformers/world_service_topic_rss_feeds_mapper.ex
+++ lib/belfrage/request_transformers/world_service_topic_rss_feeds_mapper.ex
@@ -7,6 +7,7 @@ defmodule Belfrage.RequestTransformers.WorldServiceTopicRssFeedsMapper do
 
   @world_service_rss_feed_mapping %{
     "/afaanoromoo/rss.xml" => "c44dyn08mejt",
+    "/amharic/rss.xml" => "cg58k91re1gt",
     "/azeri/rss.xml" => "c1295dq496yt",
     "/burmese/rss.xml" => "cn6rql5k0z5t",
     "/gujarati/rss.xml" => "cx0edn859g0t",
