a58e30e01b81630e12e29fc1125b0f7a099dcbdd
Oliver Spence <115625185+olispe-fdg@users.noreply.github.com>
2023-09-01 15:02:25 +0100

    SWDSOFT-392 - Allow search query param on WebCore football table pages (#2278)
    * Allow search query param on WebCore football table pages
    
    * Add contentenv to non-live allowlists
    
    * Fix not actually defining allowlist on routespec

--- lib/routes/specs/sport_football_standings_table_page.ex
+++ lib/routes/specs/sport_football_standings_table_page.ex
@@ -5,6 +5,7 @@ defmodule Routes.Specs.SportFootballStandingsTablePage do
         owner: "#help-sport",
         runbook: "https://confluence.dev.bbc.co.uk/display/SLS/Sport+Football+Standings+Webcore+Container+Run+Book",
         platform: "Webcore",
+        query_params_allowlist: ["q"],
         examples: ["/sport/football/league-two/table"]
       }
     }
