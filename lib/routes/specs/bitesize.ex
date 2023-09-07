e0fe9602bc5b6c31d96857547bd734e712b297da
Nicola <52782713+nicola-d-bbc@users.noreply.github.com>
2023-09-07 17:19:48 +0100

    remove personalisation from bitesize routes spec (#2281)

--- lib/routes/specs/bitesize.ex
+++ lib/routes/specs/bitesize.ex
@@ -5,7 +5,6 @@ defmodule Routes.Specs.Bitesize do
         owner: "bitesize-production@lists.forge.bbc.co.uk",
         platform: "Webcore",
         request_pipeline: ["ComToUKRedirect"],
-        personalisation: "test_only",
         language_from_cookie: true,
         query_params_allowlist: ["course"],
         examples: ["/bitesize/subjects"]
