1d95083bc8fcc687f6224d9af3123cc0402c35a7
DavidThorpe71 <davidthorpe878@gmail.com>
2023-09-20 16:17:17 +0100

    Adds news/technology and news/business specs

--- lib/routes/specs/news_index.ex
+++ lib/routes/specs/news_index.ex
@@ -1,24 +1,12 @@
 defmodule Routes.Specs.NewsIndex do
   def specification do
     %{
-      preflight_pipeline: ["BBCXMozartNewsPlatformSelector"],
-      specs: [
-        %{
-          owner: "DENewsFrameworksTeam@bbc.co.uk",
-          runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
-          platform: "MozartNews",
-          examples: ["/news/world_radio_and_tv", "/news/wales", "/news/technology", "/news/scotland", "/news/science_and_environment", "/news/politics", "/news/paradisepapers", "/news/northern_ireland", "/news/newsbeat", "/news/health", "/news/front-page-service-worker.js", "/news/front_page", "/news/explainers", "/news/entertainment_and_arts", "/news/england", "/news/education", "/news/disability", "/news/coronavirus", "/news/business", "/news/access-to-news"]
-        },
-        %{
-          platform: "BBCX",
-          request_pipeline: ["DomesticToBBCXRedirect"],
-          examples: [
-            %{expected_status: 302, path: "/news/business"},
-            %{expected_status: 302, path: "/news/technology"},
-            %{expected_status: 302, path: "/news/entertainment_and_arts"},
-          ]
-        }
-      ]
+      specs: %{
+        owner: "DENewsFrameworksTeam@bbc.co.uk",
+        runbook: "https://confluence.dev.bbc.co.uk/display/BELFRAGE/Belfrage+Run+Book",
+        platform: "MozartNews",
+        examples: ["/news/world_radio_and_tv", "/news/wales", "/news/scotland", "/news/science_and_environment", "/news/politics", "/news/paradisepapers", "/news/northern_ireland", "/news/newsbeat", "/news/health", "/news/front-page-service-worker.js", "/news/front_page", "/news/explainers", "/news/england", "/news/education", "/news/disability", "/news/coronavirus", "/news/access-to-news"]
+      }
     }
   end
 end
