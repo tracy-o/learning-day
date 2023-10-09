c55b61fc1036b68c7295b448f47e0f71e1ce7437
Gui Heurich <guliherme.heurich@protonmail.com>
2023-10-09 14:30:37 +0100

    Put test headers on any environment except live

--- lib/belfrage/preflight_services/ares_data.ex
+++ lib/belfrage/preflight_services/ares_data.ex
@@ -19,8 +19,8 @@ defmodule Belfrage.PreflightServices.AresData do
     }
   end
 
-  defp maybe_put_test_header("test", query_params), do: put_test_header(query_params)
-  defp maybe_put_test_header(_, _query_params), do: %{}
+  defp maybe_put_test_header("live", _query_params), do: %{}
+  defp maybe_put_test_header(_, query_params), do: put_test_header(query_params)
 
   defp put_test_header(%{"mode" => "testData"}), do: %{"ctx-service-env" => "test"}
   defp put_test_header(%{"mode" => "previewFABLWithTestData"}), do: %{"ctx-service-env" => "test"}
