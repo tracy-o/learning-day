ed79ce506e89673ad01d90853c49f16c9a11c527
Artur Novak <artur.novak.ext@bbc.co.uk>
2023-10-11 16:42:18 +0100

    Remove not used modules

--- lib/belfrage/dial.ex
+++
@@ -1,5 +0,0 @@
-defmodule Belfrage.Dial do
-  @callback transform(any()) :: any()
-  @callback on_change(any()) :: :ok
-  @optional_callbacks on_change: 1
-end
--- lib/belfrage/dials/news_apps_hardcoded_response.ex
+++
@@ -1,9 +0,0 @@
-defmodule Belfrage.Dials.NewsAppsHardcodedResponse do
-  @behaviour Belfrage.Dial
-
-  @impl Belfrage.Dial
-  def transform("disabled"), do: "disabled"
-
-  @impl Belfrage.Dial
-  def transform("enabled"), do: "enabled"
-end
