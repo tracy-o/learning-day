11d7101d0b8324f3e767f925e5aa4abbe9be04e1
Ettore Berardi <ettore@fastmail.com>
2023-10-12 17:35:01 +0100

    Amend classic apps specs to use preflight.

--- lib/routes/specs/classic_app_service.ex
+++ lib/routes/specs/classic_app_service.ex
@@ -1,14 +1,12 @@
 defmodule Routes.Specs.ClassicAppService do
   def specification do
     %{
-      specs: %{
-        owner: "#data-systems",
-        runbook: "https://confluence.dev.bbc.co.uk/display/TREVOR/Trevor+V3+%28News+Apps+Data+Service%29+Runbook",
-        platform: "ClassicApps",
-        query_params_allowlist: ["subjectId", "language", "createdBy"],
-        etag: true,
-        examples: []
-      }
+      preflight_pipeline: ["ClassicAppsPlatformSelector"],
+      specs: [
+        %{platform: "AppsTrevor"},
+        %{platform: "AppsWalter"},
+        %{platform: "AppsPhilippa"}
+      ]
     }
   end
 end
--- lib/routes/specs/classic_app_sport.ex
+++ lib/routes/specs/classic_app_sport.ex
@@ -1,15 +1,12 @@
 defmodule Routes.Specs.ClassicAppSport do
   def specification do
     %{
-      specs: %{
-        owner: "#data-systems",
-        runbook: "https://confluence.dev.bbc.co.uk/display/TREVOR/Trevor+V3+%28News+Apps+Data+Service%29+Runbook",
-        platform: "ClassicApps",
-        query_params_allowlist: ["subjectId", "language", "createdBy"],
-        response_pipeline: ["CacheDirective", "ClassicAppCacheControl", "ResponseHeaderGuardian", "PreCacheCompression", "Etag"],
-        etag: true,
-        examples: []
-      }
+      preflight_pipeline: ["ClassicAppsPlatformSelector"],
+      specs: [
+        %{platform: "AppsTrevor"},
+        %{platform: "AppsWalter"},
+        %{platform: "AppsPhilippa"}
+      ]
     }
   end
 end
