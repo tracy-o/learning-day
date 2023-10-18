95e164cd30ce8b8638f521f1bd609bc83faf3dd0
markcantcode <mark.cahill@bbc.co.uk>
2023-10-18 14:49:42 +0100

    CFW-653
    Adding in redirects from https://jira.dev.bbc.co.uk/browse/CFWEB-653

--- lib/routes/routefiles/main.ex
+++ lib/routes/routefiles/main.ex
@@ -3626,6 +3626,7 @@ defroutefile "Main" do
   redirect "/bitesize/articles/z999r2p", to: "/bitesize/groups/cd5exmm663et", status: 301                             # Study Support - Exams and revision
   redirect "/bitesize/articles/zrjqwnb", to: "/bitesize/groups/cd5exmm663et", status: 301                             # Study Support - Exams and revision
   redirect "/bitesize/articles/ztxnp4j", to: "/bitesize/groups/cd5exmm663et", status: 301                             # Study Support - Exams and revision
+  redirect "/bitesize/collections/exams-and-revision/1", to: "bitesize/groups/c89rm0640e9t", status: 301              # Study Support - Exams and revision (Welsh)
   redirect "/bitesize/articles/z3x6m39", to: "/bitesize/groups/c306433371zt", status: 301                             # Study Support - Wellbeing
   redirect "/bitesize/articles/zc72qfr", to: "/bitesize/groups/c306433371zt", status: 301                             # Study Support - Wellbeing
   redirect "/bitesize/articles/z9cd8hv", to: "/bitesize/groups/c306433371zt", status: 301                             # Study Support - Wellbeing
@@ -3640,6 +3641,8 @@ defroutefile "Main" do
   redirect "/bitesize/articles/zn8jwnb", to: "/bitesize/groups/c306433371zt", status: 301                             # Study Support - Wellbeing
   redirect "/bitesize/articles/zhc2jhv", to: "/bitesize/groups/c306433371zt", status: 301                             # Study Support - Wellbeing
   redirect "/bitesize/articles/z2rccmn", to: "/bitesize/groups/c306433371zt", status: 301                             # Study Support - Wellbeing
+  redirect "/bitesize/collections/life-and-wellbeing/1", to: "bitesize/groups/c306433371zt", status: 301              # Study Support - Wellbeing
+  redirect "/bitesize/tags/z7qg6v4/mental-health/1", to: "bitesize/groups/c306433371zt", status: 301                  # Study Support - Wellbeing
 
   redirect "/bitesize/articles/zv6yhbk", to: "/usingthebbc/privacy/privacy-notice-for-bbc-quizzes-and-polls", status: 301      # privacy-notice-for-bbc-quizzes-and-polls
 
