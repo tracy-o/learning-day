07b9c91d95f462a303a29751d41628c56b977ec0
Gui Heurich <guliherme.heurich@protonmail.com>
2023-10-18 12:51:50 +0100

    Undo changes to req-svc-chain in response

--- lib/belfrage_web/response/headers/req-svc-chain.ex
+++ lib/belfrage_web/response/headers/req-svc-chain.ex
@@ -4,11 +4,17 @@ defmodule BelfrageWeb.Response.Headers.ReqSvcChain do
   @behaviour BelfrageWeb.Response.Headers.Behaviour
 
   @impl true
-  def add_header(conn = %Plug.Conn{private: %{bbc_headers: bbc_headers}}, _envelope) do
+  def add_header(conn, envelope) do
     put_resp_header(
       conn,
       "req-svc-chain",
-      Map.get(bbc_headers, :req_svc_chain, "BELFRAGE")
+      header(Map.get(envelope.response.headers, "req-svc-chain"), envelope.request.req_svc_chain)
     )
   end
+
+  defp header(_existing = nil, _req_svc_chain = nil), do: "BELFRAGE"
+
+  defp header(_existing = nil, req_svc_chain), do: req_svc_chain
+
+  defp header(existing, _), do: existing
 end
--- lib/belfrage_web/response/internal.ex
+++ lib/belfrage_web/response/internal.ex
@@ -17,7 +17,7 @@ defmodule BelfrageWeb.Response.Internal do
                  ]
              )
 
-  def new(envelope = %Envelope{}, conn = %Conn{private: %{bbc_headers: bbc_headers}}) do
+  def new(envelope = %Envelope{}, conn = %Conn{private: private}) do
     Metrics.latency_span(:generate_internal_response, fn ->
       {content_type, body} = body(envelope.response, conn)
 
@@ -25,7 +25,7 @@ defmodule BelfrageWeb.Response.Internal do
         envelope.response
         | headers: %{
             "content-type" => content_type,
-            "req-svc-chain" => get_svc_chain(bbc_headers)
+            "req-svc-chain" => get_svc_chain(private)
           },
           body: body,
           cache_directive: cache_control(envelope)
@@ -33,23 +33,8 @@ defmodule BelfrageWeb.Response.Internal do
     end)
   end
 
-  def new(envelope = %Envelope{}, conn = %Conn{}) do
-    Metrics.latency_span(:generate_internal_response, fn ->
-      {content_type, body} = body(envelope.response, conn)
-
-      %Response{
-        envelope.response
-        | headers: %{
-            "content-type" => content_type,
-            "req-svc-chain" => "BELFRAGE"
-          },
-          body: body,
-          cache_directive: cache_control(envelope)
-      }
-    end)
-  end
-
-  defp get_svc_chain(bbc_headers), do: Map.get(bbc_headers, :req_svc_chain, "BELFRAGE")
+  defp get_svc_chain(%{bbc_headers: bbc_headers}), do: Map.get(bbc_headers, :req_svc_chain, "BELFRAGE")
+  defp get_svc_chain(_), do: "BELFRAGE"
 
   defp body(%Response{http_status: status}, conn = %Conn{}) do
     accepted_content_type = Conn.get_req_header(conn, "accept") |> List.first()
