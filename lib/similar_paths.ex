43b5073cbbfcb2a141ce55664fc5b8d4b356bb9d
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-10-20 12:30:49 +0100

    Update similar paths module

--- lib/similar_paths.ex
+++ lib/similar_paths.ex
@@ -1,19 +1,26 @@
 defmodule SimilarPaths do
-  defp did_you_mean(routes, %Conn{request_path: path}) do
-    similar =
-      routes
-      |> Enum.filter(fn {matcher, args} ->
-        args[:only_on] != "test" and
-        !String.contains?(matcher, [":", "*"]) and
-        !String.equivalent?(path, matcher) and
-        Levenshtein.distance(String.downcase(path), matcher) <= 3
-      end)
-      |> IO.inspect(label: "did you mean")
-      |> Enum.map(fn {matcher, _args} ->
-        "<a href=" <> matcher <> ">" <> matcher <> "</a>"
-      end)
+  def map_similar_routes(routes, %Conn{request_path: path}) do
+    routes
+    |> filter_routes()
+    |> IO.inspect(label: "did you mean")
+    |> Enum.map(fn {matcher, _args} ->
+      "<a href=" <> matcher <> ">" <> matcher <> "</a>"
+    end)
+    |> generate_resp_page()
+  end
 
-    unless Enum.empty?(similar) do
+  defp filter_routes(routes) do
+    routes
+    |> Enum.filter(fn {matcher, args} ->
+      args[:only_on] != "test" and
+      !String.contains?(matcher, [":", "*"]) and
+      !String.equivalent?(path, matcher) and
+      Levenshtein.distance(String.downcase(path), matcher) <= 3
+    end)
+  end
+
+  defp generate_resp_page(similar_routes) do
+    unless Enum.empty?(similar_routes) do
       """
       <h1>404 Page Not Found</h1>\n<!-- Belfrage -->
       <p>Did you mean...
