de0d20f3647518ddd55ccb2c6d40e95b27b80774
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-10-26 12:14:55 +0100

    Add Belfrage search docs

---
+++ lib/mix/tasks/search_docs.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.SearchDocs do
+  use Mix.Task
+
+  def run([]) do
+    get_confluence("smoke+test")
+  end
+
+  def get_confluence(my_query) do
+    endpoint = "confluence.dev.bbc.co.uk"
+    req_path = "/dosearchsite.action?cql=siteSearch+~+\"#{my_query}\"+and+space+%3D+\"BELFRAGE\"&queryString=#{my_query}"
+
+    request_route(endpoint, req_path)
+  end
+
+  defp request_route(endpoint, path) do
+    case HTTPoison.get("https://#{endpoint}#{path}") do
+      {:ok, %{status_code: 200, body: body}} ->
+        Poison.decode!(body)
+
+      {:ok, %{status_code: 404}} ->
+        "returned 404"
+
+      {:error, %{reason: reason}} ->
+        "could not get route"
+    end
+  end
+end
