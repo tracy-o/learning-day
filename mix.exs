71f7c95c81daa77985f8d9c15bff9150b8b26dd3
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-11-28 13:30:20 +0000

    Update to Elixir 1.15.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LearningDay.MixProject do
     [
       app: :learning_day,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
