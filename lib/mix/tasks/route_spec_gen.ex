e164a6bf910922806765aaed1b0ccf615284e1e9
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-12-16 04:09:13 +0000

    Add arg parsing - use OptionParser - add aliases and strict rules for OptionParser - run/1 no longer handles arg parsing - fill_template/1 generates content and returns {name, content} or content - generate_file/2 creates the file in routspec dir

--- lib/mix/tasks/route_spec_gen.ex
+++ lib/mix/tasks/route_spec_gen.ex
@@ -36,29 +36,51 @@ defmodule Mix.Tasks.RouteSpecGen do
   }
   """
 
+  @aliases [n: :name, m: :mock]
+  @strict_args [{:name, :string}, {:platforms, :string}, {:personalised, :boolean}, {:mock, :boolean}]
+
   use Mix.Task
 
-  def run(["--route_spec", route_spec, "--platform", platform]) do
-    build_template(route_spec: route_spec, platform: platform)
-    |> to_string()
-    |> IO.puts()
+  def run(args) do
+    {valid_args, _, _} = OptionParser.parse(args, aliases: @aliases, strict: @strict_args)
+    {name, content} = fill_template(valid_args)
+    generate_file(name, content)
   end
 
-  def run(["--platform", platform]) do
-    build_template(platform: platform)
-    |> to_string()
-    |> IO.puts()
+  defp fill_template(platforms: platforms) do
+    {:ok, template} =
+      @platform_template
+      |> String.replace("\n", "")
+      |> Solid.parse()
+
+    platforms
+    |> String.split(",", trim: true)
+    |> Enum.map_join(",\n", fn platform ->
+      Solid.render!(template, %{"platform" => %{"name" => platform}})
+    end)
   end
 
-  def run(_), do: nil
+  defp fill_template(opts) do
+    t = if opts[:personalised], do: @personalised_route_spec_template, else: @route_spec_template
+    {:ok, template} = Solid.parse(t)
 
-  defp build_template(route_spec: route_spec, platform: platform) do
-    {:ok, route_spec_template} = Solid.parse(@route_spec_template)
-    Solid.render!(route_spec_template, %{"route_spec" => %{"name" => route_spec, "specs" => build_template(platform: platform)}})
+    content =
+      template
+      |> Solid.render!(%{
+        "route_spec" => %{"name" => opts[:name], "specs" => fill_template(platforms: opts[:platforms])}
+      })
+      |> to_string()
+
+    {opts[:name], content}
   end
 
-  defp build_template([platform: platform]) do
-    {:ok, template} = Solid.parse(@platform_template)
-    Solid.render!(template, %{"platform" => %{"name" => platform}})
+  defp generate_file(name, content) do
+    path_to_file = "lib/routes/specs/" <> Macro.underscore(name) <> ".ex"
+
+    if File.exists?(path_to_file) do
+      raise "File already exists! Please try another name."
+    else
+      File.write!(path_to_file, Code.format_string!(content))
+    end
   end
 end
